4566495b4dae5f3d6a5de503a6abb6505871237b
edotau <eric.au@duke.edu>
2018-07-27 11:48:24 -0400

    used go fmt on the following files

--- fasta/info.go
+++ fasta/info.go
@@ -1,34 +1,34 @@
 package fasta
 
 import (
-	"github.com/vertgenlab/gonomics/dna"
 	"github.com/vertgenlab/gonomics/bed"
+	"github.com/vertgenlab/gonomics/dna"
 )
 
-func UngappedRegions(fa *Fasta) ([]bed.Bed) {
+func UngappedRegions(fa *Fasta) []bed.Bed {
 	var answer []bed.Bed
 	var inRegion bool = false
 	var startIndex = 0
- 	for index, _ := range fa.Seq {
- 		if dna.DefineBase(fa.Seq[index]) && inRegion == false {
- 			inRegion = true
- 			startIndex = index
- 		} else if !(dna.DefineBase(fa.Seq[index])) && inRegion == true {
-				answer = append(answer, bed.Bed{Name: fa.Name, Start:startIndex, End:index})
-				inRegion = false
-			}	
-		 
+	for index, _ := range fa.Seq {
+		if dna.DefineBase(fa.Seq[index]) && inRegion == false {
+			inRegion = true
+			startIndex = index
+		} else if !(dna.DefineBase(fa.Seq[index])) && inRegion == true {
+			answer = append(answer, bed.Bed{Name: fa.Name, Start: startIndex, End: index})
+			inRegion = false
+		}
+
 	}
 	if inRegion == true {
-		answer = append(answer, bed.Bed{Name: fa.Name, Start:startIndex, End:len(fa.Seq)})
+		answer = append(answer, bed.Bed{Name: fa.Name, Start: startIndex, End: len(fa.Seq)})
 	}
 	return answer
 }
 
-func UngappedRegionsAll(records []Fasta) ([]bed.Bed){
+func UngappedRegionsAll(records []Fasta) []bed.Bed {
 	var b []bed.Bed
-        for idx, _ := range records {
-               b = append(b, UngappedRegions(&records[idx])...)
+	for idx, _ := range records {
+		b = append(b, UngappedRegions(&records[idx])...)
 	}
 	return b
 }
