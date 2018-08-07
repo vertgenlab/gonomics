b3194659c3a20d79de578b08168d85ce0a9264fd
edotau <eric.au@duke.edu>
2018-08-07 14:30:31 -0400

    slightly modified fasta.go

--- fasta/fasta.go
+++ fasta/fasta.go
@@ -78,48 +78,6 @@ func ReadNew(filename string) ([]*Fasta, error) {
 	}
 	return answer, scanner.Err()
 }
-/*
-func SplitRead(filename stringe, n int) ([]*Fasta, error) {
-	var line string
-	var currSeq []dna.Base
-	var answer []*Fasta
-	var seqIdx int64 = -1
-
-	file, err := os.Open(filenam)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line = scanner.Text()
-		switch {
-		case strings.HasPrefix(line, "#"):
-			// comment line in fasta file
-		case strings.HasPrefix(line, ">"):
-			//Append the number of Fasta slices, given we group the bases by 'n'
-			for i :=0; i < len(dna.StringToBases(line))/n; i++ {
-				tmp := Fasta{Name: line[1:len(line)]}
-				answer = append(answer, &tmp)
-				seqIdx++
-			}
-		default:
-			currSeq, err = dna.StringToBases(line)
-			if err != nil {
-				log.Fatal(err)
-			}
-			for i:=0; i < len(currSeq)/n; i+=n {
-				//Loop through and append the groups one by 1.
-				answer[seqIdx].Seq = append(answer[seqIdx].Seq, currSeq[i:i+n]...)
-				
-			}
-			
-		}
-	}
-	return answer, scanner.Err()
-}
-*/
 
 func WriteToFileHandle(file *os.File, records []Fasta, lineLength int) error {
 	var err error
