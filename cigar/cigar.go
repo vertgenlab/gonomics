ec394a018b0c9d28fb0df8bfbd156339d63efa7e
rimangan <riley.mangan@duke.edu>
2019-06-26 17:44:29 -0400

    Checking in the cigar package

---
+++ cigar/cigar.go
@@ -0,0 +1,163 @@
+package cigar
+
+import(
+	"log"
+	"github.com/vertgenlab/gonomics/common"
+	"fmt"
+)
+
+type Cigar struct {
+	RunLength int64
+	Op        rune
+}
+
+func ToString(c []*Cigar) string {
+	var output string = ""
+
+	for _, v := range c {
+		output = output + fmt.Sprintf("%v%c", v.RunLength, v.Op)
+	}
+	return output
+}
+
+func FromString(input string) []*Cigar {
+	var output []*Cigar
+	var currentNumber string
+
+	for _, v := range input {
+		if RuneIsDigit(v) {
+			currentNumber = currentNumber + fmt.Sprintf("%c", v)	
+		} else if RuneIsValidCharacter(v) {
+			currentCigar := Cigar{RunLength: common.StringToInt64(currentNumber), Op: v}
+			output = append(output, &currentCigar)
+			currentNumber = ""
+		} else {
+			log.Fatalf("Invalid character: %c", v)
+		}
+	}
+/*
+	if RuneIsDigit(v) { //will not work, v is not seen outside loop
+		log.Fatalf("Cigar ended with digit")
+	} */
+	return output
+}
+
+func ReferenceLength(c []*Cigar) int64 {
+	var ans int64
+	for _, v := range c {
+		if ConsumesReference(v.Op) {
+			ans = ans + v.RunLength
+		}
+	}
+	return ans
+}
+
+func QueryLength(c []*Cigar) int64 {
+	var ans int64
+	for _, v := range c {
+		if ConsumesQuery(v.Op) {
+			ans = ans + v.RunLength
+		}
+	}
+	return ans
+}
+
+func RuneIsDigit(r rune) bool {
+	switch r {
+	case '0':
+		return true
+	case '1':
+		return true
+	case '2':
+		return true
+	case '3':
+		return true
+	case '4':
+		return true
+	case '5':
+		return true	
+	case '6':
+		return true
+	case '7':
+		return true
+	case '8':
+		return true
+	case '9':
+		return true		
+	}
+	return false
+}
+
+func RuneIsValidCharacter(r rune) bool {
+	switch r {
+	case 'M':
+		return true
+	case 'I':
+		return true
+	case 'D':
+		return true
+	case 'N':
+		return true
+	case 'S':
+		return true
+	case 'H':
+		return true
+	case 'P':
+		return true
+	case '=':
+		return true
+	case 'X':
+		return true	
+	}
+	return false
+}
+
+func ConsumesReference(r rune) bool {
+	switch r {
+	case 'M':
+		return true
+	case 'I':
+		return false
+	case 'D':
+		return true
+	case 'N':
+		return true
+	case 'S':
+		return false
+	case 'H':
+		return false
+	case 'P':
+		return false
+	case '=':
+		return true
+	case 'X':
+		return true	
+	}
+	log.Fatalf("Invalid rune: %c", r)
+	return false
+}
+
+func ConsumesQuery(r rune) bool {
+	switch r {
+	case 'M':
+		return true
+	case 'I':
+		return true
+	case 'D':
+		return false
+	case 'N':
+		return false
+	case 'S':
+		return true
+	case 'H':
+		return false
+	case 'P':
+		return false
+	case '=':
+		return true
+	case 'X':
+		return true	
+	}
+	log.Fatalf("Invalid rune: %c", r)
+	return false
+}
---
+++ cigar/cigar_test.go
@@ -0,0 +1,43 @@
+package cigar 
+
+import (
+	"testing"
+	"strings"
+)
+
+func TestFromString(t *testing.T) {
+	var cigarsString = "35M2I16D"
+	var c1 Cigar = Cigar{RunLength: 35, Op: 'M'}
+	var c2 Cigar = Cigar{RunLength: 2, Op: 'I'}
+	var c3 Cigar = Cigar{RunLength: 16, Op: 'D'}
+	var cigars []*Cigar = []*Cigar{&c1, &c2, &c3}
+
+	cigarCheck := FromString(cigarsString)
+
+	for i := 0; i < len(cigarCheck); i++ {
+		if !isEqual(*cigars[i], *cigarCheck[i]) {
+			t.Errorf("Error with FromString")
+		}
+	}
+}
+
+func TestToString(t *testing.T) {
+	var cigarsString = "35M2I16D"
+	var c1 Cigar = Cigar{RunLength: 35, Op: 'M'}
+	var c2 Cigar = Cigar{RunLength: 2, Op: 'I'}
+	var c3 Cigar = Cigar{RunLength: 16, Op: 'D'}
+	var cigars []*Cigar = []*Cigar{&c1, &c2, &c3}
+
+	cigarCheck := ToString(cigars)
+
+	if strings.Compare(cigarCheck, cigarsString) != 0 {
+		t.Errorf("Error with ToString")
+	}
+}
+
+func isEqual(a Cigar, b Cigar) bool {
+	if !(a.RunLength == b.RunLength && a.Op == b.Op) {
+		return false
+	}
+	return true
+}
