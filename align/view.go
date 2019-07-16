a164ebeddc6acfac241d3448b2ffaa8210f40575
edotau <eric.au@duke.edu>
2019-07-16 11:14:48 -0400

    small changes

--- align/view.go
+++ align/view.go
@@ -63,56 +63,35 @@ func LocalView(alpha []dna.Base, beta []dna.Base, operations []Cigar, maxI int64
 	for k := 0; k < len(operations); k++ {
 		alignLen += operations[k].RunLength
 	}
-	var answer []Cigar
 	var startCig int64
 	var endCig int64
 	endCig = int64(len(alpha)) - int64(maxI)
 	startCig = int64(len(alpha)) - alignLen - endCig
-	fmt.Println("start", startCig, "end", endCig)
-	answer = append(answer, Cigar{RunLength: startCig, Op: ColD})
-	for x := 0; x < len(operations); x++ {
-		answer = append(answer, operations[x])
-	}
-	answer = append(answer, Cigar{RunLength: endCig, Op: ColD})
-	if int(alignLen) != len(alpha) {
-		for _, y := range answer {
-			for count = 0; count < y.RunLength; count++ {
-				switch y.Op {
-				case ColM:
-					seqOne.WriteRune(dna.BaseToRune(alpha[i]))
-					seqTwo.WriteRune(dna.BaseToRune(beta[j]))
-					i, j = i+1, j+1
-				case ColI:
-					seqOne.WriteRune('-')
-					seqTwo.WriteRune(dna.BaseToRune(beta[j]))
-					j++
-				case ColD:
-					seqOne.WriteRune(dna.BaseToRune(alpha[i]))
-					seqTwo.WriteRune('-')
-					i++
-				}
-			}
-		}
-	} else {
-		for _, operation := range operations {
-			for count = 0; count < operation.RunLength; count++ {
-				switch operation.Op {
-				case ColM:
-					seqOne.WriteRune(dna.BaseToRune(alpha[i]))
-					seqTwo.WriteRune(dna.BaseToRune(beta[j]))
-					i, j = i+1, j+1
-				case ColI:
-					seqOne.WriteRune('-')
-					seqTwo.WriteRune(dna.BaseToRune(beta[j]))
-					j++
-				case ColD:
-					seqOne.WriteRune(dna.BaseToRune(alpha[i]))
-					seqTwo.WriteRune('-')
-					i++
-				}
-			}
-		}
+	if startCig != 0 {
 
+		operations = append([]Cigar{Cigar{RunLength: startCig, Op: ColD}}, operations...)
+	}
+	if endCig != 0 {
+		operations = append(operations, Cigar{RunLength: endCig, Op: ColD})
+	}
+	fmt.Println("start", startCig, "end", endCig)
+	for _, operation := range operations {
+		for count = 0; count < operation.RunLength; count++ {
+			switch operation.Op {
+			case ColM:
+				seqOne.WriteRune(dna.BaseToRune(alpha[i]))
+				seqTwo.WriteRune(dna.BaseToRune(beta[j]))
+				i, j = i+1, j+1
+			case ColI:
+				seqOne.WriteRune('-')
+				seqTwo.WriteRune(dna.BaseToRune(beta[j]))
+				j++
+			case ColD:
+				seqOne.WriteRune(dna.BaseToRune(alpha[i]))
+				seqTwo.WriteRune('-')
+				i++
+			}
+		}
 	}
 	return seqOne.String() + "\n" + seqTwo.String() + "\n"
 }
