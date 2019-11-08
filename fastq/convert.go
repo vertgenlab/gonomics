d2917119d038cbb464bb2cfac1dbf5728e38e80a
Craig Lowe <craiglowe@users.noreply.github.com>
2019-11-08 17:06:01 -0500

    updated fastq to make memory copying easier to track

--- fastq/convert.go
+++ fastq/convert.go
@@ -2,38 +2,21 @@ package fastq
 
 import (
 	"github.com/vertgenlab/gonomics/dna"
-	//"github.com/vertgenlab/gonomics/qDna"
-	//"log"
 )
 
-func ReverseComplementFastq(record *Fastq) *Fastq {
-	var answer *Fastq = record
-	dna.ReverseComplement(answer.Seq)
-	reverseQualRecord(answer.Qual)
-	return answer
-
+func ReverseComplement(record *Fastq) {
+	dna.ReverseComplement(record.Seq)
+	reverseQualRecord(record.Qual)
 }
 
-func ReverseComplementAll(records []*Fastq) []*Fastq {
-	var answer []*Fastq
+func ReverseComplementAll(records []*Fastq) {
 	for idx, _ := range records {
-		answer = append(answer, ReverseComplementFastq(records[idx]))
+		ReverseComplement(records[idx])
 	}
-	return answer
-}
-
-func swapQual(alpha rune, beta rune) (rune, rune) {
-	return beta, alpha
 }
 
 func reverseQualRecord(qualScore []rune) {
 	for i, j := 0, len(qualScore)-1; i <= j; i, j = i+1, j-1 {
-		qualScore[i], qualScore[j] = swapQual(qualScore[i], qualScore[j])
+		qualScore[i], qualScore[j] = qualScore[j], qualScore[i]
 	}
 }
-
-/*
-func FromFastq(fq *Fastq) []*qDna.QBase {
-	answer := FromDna(fq.Seq, ErrorRate(fq.Qual))
-	return answer
-}*/
--- fastq/fastq_test.go
+++ fastq/fastq_test.go
@@ -2,6 +2,8 @@ package fastq
 
 import (
 	"fmt"
+	"github.com/vertgenlab/gonomics/common"
+	"reflect"
 	"testing"
 )
 
@@ -9,27 +11,15 @@ var readWriteTests = []struct {
 	filename string // input
 }{
 	{"testdata/test.fastq"},
-	//{"testdata/CL12-3_16w_test.fastq"},
 }
 
 func TestRead(t *testing.T) {
 	for _, test := range readWriteTests {
-		_ = Read(test.filename)
 		fastq := Read(test.filename)
-
-		//testing a few functions implemented
-		fmt.Println("Value: ", string(fastq[0].Qual[0]), "ASCII: ", fastq[0].Qual[0])
-		fmt.Println(ErrorRate(fastq[0].Qual))
-		//fmt.Println(fastq[0].Seq[0], string(fastq[0].Qual[0]))
-		//fmt.Println(FromBase(fastq[0].Seq, PhredToPError(fastq[0].Qual)))
-		PrintFastq(fastq)
-		rev := ReverseComplementAll(fastq)
-		PrintFastq(rev)
-		//qq := FromDna(fastq[0].Seq, ErrorRate(fastq[0].Qual))
-		//for i :=0; i < len(qq);i++ {
-		//	fmt.Println(qq[i])
-		//}
-
+		Write(test.filename+".tmp", fastq)
+		fastqTwo := Read(test.filename + ".tmp")
+		if !reflect.DeepEqual(fastq, fastqTwo) {
+			common.ExitIfError(fmt.Errorf("Error: Read,Write,Read was not equal to the Read\n"))
+		}
 	}
-
 }
