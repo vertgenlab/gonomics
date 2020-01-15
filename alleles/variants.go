77076be5c2973fb3c16cff3fd886fd7eac0a4d59
das106 <das106@duke.edu>
2020-01-15 14:24:32 -0500

    retrieve alleles by struct of chr and pos instead of map of maps. Merged insertions and deletions into one slice

--- alleles/variants.go
+++ alleles/variants.go
@@ -14,19 +14,18 @@ import (
 )
 // TODO: look into limiting goroutines
 type BatchAlleleCount struct {
-	Sample string
-	Ref    dna.Base
-	Counts int32
-	BaseA  int32
-	BaseC  int32
-	BaseG  int32
-	BaseT  int32
-	Ins    []Indel
-	Del    []Indel
+	Sample 	string
+	Ref    	dna.Base
+	Counts 	int32
+	BaseA  	int32
+	BaseC  	int32
+	BaseG  	int32
+	BaseT  	int32
+	Indel  	[]Indel
 }
 
 // Map structure: map[Chromosome]map[Position][Sample]*BatchAlleleCount
-type BatchSampleMap map[string]map[int64][]*BatchAlleleCount
+type BatchSampleMap map[Location][]*BatchAlleleCount
 
 // Input a directory filled with AlleleCount (.ac) files. Merges the files into a nested map (chr->pos->[sample]data)
 func CreateBatchSampleMap(inDirectory string) BatchSampleMap {
@@ -41,83 +40,59 @@ func CreateBatchSampleMap(inDirectory string) BatchSampleMap {
 	var fileCount int = 1
 	var i, j int
 
-	SampleMap := make(map[string]map[int64][]*BatchAlleleCount)
+	SampleMap := make(BatchSampleMap)
 
 	for _, file := range files {
 
 		SampleName = file.Name()
 		SamplePath := fmt.Sprintf("%s%s", inDirectory, SampleName)
-		fmt.Printf("#\n# File %d\n", fileCount)
+		log.Printf("#\n# File %d\n", fileCount)
 		AlleleCounts := ReadVcfToAlleleCounts(SamplePath)
 		fileCount++
 
 		// Loop through input map and deposit info into output map
-		for chrName, chr := range AlleleCounts {
-			for pos, alleles := range chr {
+		for loc, alleles := range AlleleCounts {
 
-				//if the chromosome has already been added to the matrix, move along
-				_, ok := SampleMap[chrName]
-
-				//if the chromosome is NOT in the matrix, initialize
-				if !ok {
-					SampleMap[chrName] = make(map[int64][]*BatchAlleleCount)
-				}
-
-				// Check if position is in the map, if not then add the background struct
-				_, okk := SampleMap[chrName][pos]
-				if !okk {
-					current = &BatchAlleleCount{"Background", alleles.Ref, 0, 0, 0, 0, 0, make([]Indel, 0), make([]Indel, 0)}
-					SampleMap[chrName][pos] = append(SampleMap[chrName][pos], current)
-				}
-
-				//add in and increment background as entry zero in batch allele count slice
-				SampleMap[chrName][pos][0].BaseA = SampleMap[chrName][pos][0].BaseA + alleles.BaseA
-				SampleMap[chrName][pos][0].BaseC = SampleMap[chrName][pos][0].BaseC + alleles.BaseC
-				SampleMap[chrName][pos][0].BaseG = SampleMap[chrName][pos][0].BaseG + alleles.BaseG
-				SampleMap[chrName][pos][0].BaseT = SampleMap[chrName][pos][0].BaseT + alleles.BaseT
-				SampleMap[chrName][pos][0].Counts = SampleMap[chrName][pos][0].Counts + alleles.Counts
-
-				// Loop through each insertion and see if it is already in the background struct, if not then append it
-				for i = 0; i < len(alleles.Ins); i++ {
-					var Match bool = false
-					for j = 0; j < len(SampleMap[chrName][pos][0].Ins); j++ {
-						if dna.CompareSeqsIgnoreCase(alleles.Ins[i].Alt, SampleMap[chrName][pos][0].Ins[j].Alt) == 0 {
-							SampleMap[chrName][pos][0].Ins[j].Count = SampleMap[chrName][pos][0].Ins[j].Count + alleles.Ins[i].Count
-							Match = true
-						}
-					}
-					if Match == false {
-						SampleMap[chrName][pos][0].Ins = append(SampleMap[chrName][pos][0].Ins, alleles.Ins[i])
-					}
-				}
-
-				// Loop through each deletion and see if it is already in the background struct, if not then append it
-				for i = 0; i < len(alleles.Del); i++ {
-					var Match bool = false
-					for j = 0; j < len(SampleMap[chrName][pos][0].Del); j++ {
-						if dna.CompareSeqsIgnoreCase(alleles.Del[i].Ref, SampleMap[chrName][pos][0].Del[j].Ref) == 0 {
-							SampleMap[chrName][pos][0].Del[j].Count = SampleMap[chrName][pos][0].Del[j].Count + alleles.Del[i].Count
-							Match = true
-						}
-					}
-					if Match == false {
-						SampleMap[chrName][pos][0].Del = append(SampleMap[chrName][pos][0].Del, alleles.Del[i])
-					}
-				}
-
-				current = &BatchAlleleCount{
-					Sample: SampleName,
-					Ref:    alleles.Ref,
-					Counts: alleles.Counts,
-					BaseA:  alleles.BaseA,
-					BaseC:  alleles.BaseC,
-					BaseG:  alleles.BaseG,
-					BaseT:  alleles.BaseT,
-					Ins:    alleles.Ins,
-					Del:    alleles.Del}
-
-				SampleMap[chrName][pos] = append(SampleMap[chrName][pos], current)
+			// Check if position is in the map, if not then add the background struct
+			_, ok := SampleMap[Location{loc.Chr, loc.Pos}]
+			if !ok {
+				current = &BatchAlleleCount{"Background", alleles.Ref, 0, 0, 0, 0, 0, make([]Indel, 0)}
+				SampleMap[Location{loc.Chr, loc.Pos}] = append(SampleMap[Location{loc.Chr, loc.Pos}], current)
 			}
+
+			//add in and increment background as entry zero in batch allele count slice
+			SampleMap[Location{loc.Chr, loc.Pos}][0].BaseA = SampleMap[Location{loc.Chr, loc.Pos}][0].BaseA + alleles.BaseA
+			SampleMap[Location{loc.Chr, loc.Pos}][0].BaseC = SampleMap[Location{loc.Chr, loc.Pos}][0].BaseC + alleles.BaseC
+			SampleMap[Location{loc.Chr, loc.Pos}][0].BaseG = SampleMap[Location{loc.Chr, loc.Pos}][0].BaseG + alleles.BaseG
+			SampleMap[Location{loc.Chr, loc.Pos}][0].BaseT = SampleMap[Location{loc.Chr, loc.Pos}][0].BaseT + alleles.BaseT
+			SampleMap[Location{loc.Chr, loc.Pos}][0].Counts =SampleMap[Location{loc.Chr, loc.Pos}][0].Counts + alleles.Counts
+
+			// Loop through each indel and see if it is already in the background struct, if not then append it
+			for i = 0; i < len(alleles.Indel); i++ {
+				var Match bool = false
+				for j = 0; j < len(SampleMap[Location{loc.Chr, loc.Pos}][0].Indel); j++ {
+					if dna.CompareSeqsIgnoreCase(alleles.Indel[i].Alt, SampleMap[Location{loc.Chr, loc.Pos}][0].Indel[j].Alt) == 0 &&
+						dna.CompareSeqsIgnoreCase(alleles.Indel[i].Ref, SampleMap[Location{loc.Chr, loc.Pos}][0].Indel[j].Alt) == 0 {
+						SampleMap[Location{loc.Chr, loc.Pos}][0].Indel[j].Count = SampleMap[Location{loc.Chr, loc.Pos}][0].Indel[j].Count + alleles.Indel[i].Count
+						Match = true
+					}
+				}
+				if Match == false {
+					SampleMap[Location{loc.Chr, loc.Pos}][0].Indel = append(SampleMap[Location{loc.Chr, loc.Pos}][0].Indel, alleles.Indel[i])
+				}
+			}
+
+			current = &BatchAlleleCount{
+				Sample: SampleName,
+				Ref:    alleles.Ref,
+				Counts: alleles.Counts,
+				BaseA:  alleles.BaseA,
+				BaseC:  alleles.BaseC,
+				BaseG:  alleles.BaseG,
+				BaseT:  alleles.BaseT,
+				Indel:  alleles.Indel}
+
+			SampleMap[Location{loc.Chr, loc.Pos}] = append(SampleMap[Location{loc.Chr, loc.Pos}], current)
 		}
 	}
 
@@ -130,7 +105,7 @@ func ScoreVariants(input BatchSampleMap, sigThreshold float64, afThreshold float
 	fmt.Printf("#\n# Calling Variants\n")
 	var VariantScores []*vcf.Vcf
 	var progressMeter int
-	var cA, cC, cG, cT, cIns, cDel, dA, dC, dG, dT, dIns, dDel int32
+	var cA, cC, cG, cT, cIndel, dA, dC, dG, dT, dIndel int32
 	var a, b []int32
 
 	// Initialize a channel to send completed vcf structs through
@@ -138,125 +113,111 @@ func ScoreVariants(input BatchSampleMap, sigThreshold float64, afThreshold float
 	var wg sync.WaitGroup
 
 	// Loop through BatchSampleMap
-	for chrName, chr := range input {
-		for pos, alleles := range chr {
+	for loc, alleles := range input {
 
-			if progressMeter%1000 == 0 {
-				log.Printf("# Processed %d Positions\n", progressMeter)
-			}
-			progressMeter++
+		if progressMeter%1000 == 0 {
+			log.Printf("# Processed %d Positions\n", progressMeter)
+		}
+		progressMeter++
 
-			// Must be at least 2 samples in the slice to generate a batch p value.
-			// A single sample would be len = 2 because the background values are always element 0
-			if len(alleles) <= 2 {
-				continue
-			}
+		// Must be at least 2 samples in the slice to generate a batch p value.
+		// A single sample would be len = 2 because the background values are always element 0
+		if len(alleles) <= 2 {
+			continue
+		}
 
-			// Begin gathering parameters for Fishers Exact Test done in the numbers package
-			// test is for the matrix:
-			// [a b]
-			// [c d]
-			// a = Samples Ref Allele Count
-			// b = Background Ref Allele Count - Samples Ref Allele Count
-			// c = Samples Alt Allele Count
-			// d = Background Alt Allele Count - Samples Alt Allele Count
+		// Begin gathering parameters for Fishers Exact Test done in the numbers package
+		// test is for the matrix:
+		// [a b]
+		// [c d]
+		// a = Samples Ref Allele Count
+		// b = Background Ref Allele Count - Samples Ref Allele Count
+		// c = Samples Alt Allele Count
+		// d = Background Alt Allele Count - Samples Alt Allele Count
 
-			// Determine Reference Base
-			// If ref base is unknown then skip
-			var i, j, k, l int
-			a = make([]int32, len(alleles)-1)
-			b = make([]int32, len(alleles)-1)
-			switch alleles[0].Ref {
-			// Loop through samples and gather inputs for a and b
-			// For loop starts at index 1 because index zero is the background values
-			case dna.A:
-				for i = 1; i < len(alleles); i++ {
-					a[i-1] = alleles[i].BaseA
-					b[i-1] = alleles[0].BaseA - alleles[i].BaseA
-				}
-			case dna.C:
-				for i = 1; i < len(alleles); i++ {
-					a[i-1] = alleles[i].BaseC
-					b[i-1] = alleles[0].BaseC - alleles[i].BaseC
-				}
-			case dna.G:
-				for i = 1; i < len(alleles); i++ {
-					a[i-1] = alleles[i].BaseG
-					b[i-1] = alleles[0].BaseG - alleles[i].BaseG
-				}
-			case dna.T:
-				for i = 1; i < len(alleles); i++ {
-					a[i-1] = alleles[i].BaseT
-					b[i-1] = alleles[0].BaseT - alleles[i].BaseT
-				}
-			default:
-				continue
-			}
-
-			// Loop through samples and generate scores
-			// For loop starts at index 1 because index zero is the background values
+		// Determine Reference Base
+		// If ref base is unknown then skip
+		var i, j, l int
+		a = make([]int32, len(alleles)-1)
+		b = make([]int32, len(alleles)-1)
+		switch alleles[0].Ref {
+		// Loop through samples and gather inputs for a and b
+		// For loop starts at index 1 because index zero is the background values
+		case dna.A:
 			for i = 1; i < len(alleles); i++ {
+				a[i-1] = alleles[i].BaseA
+				b[i-1] = alleles[0].BaseA - alleles[i].BaseA
+			}
+		case dna.C:
+			for i = 1; i < len(alleles); i++ {
+				a[i-1] = alleles[i].BaseC
+				b[i-1] = alleles[0].BaseC - alleles[i].BaseC
+			}
+		case dna.G:
+			for i = 1; i < len(alleles); i++ {
+				a[i-1] = alleles[i].BaseG
+				b[i-1] = alleles[0].BaseG - alleles[i].BaseG
+			}
+		case dna.T:
+			for i = 1; i < len(alleles); i++ {
+				a[i-1] = alleles[i].BaseT
+				b[i-1] = alleles[0].BaseT - alleles[i].BaseT
+			}
+		default:
+			continue
+		}
 
-				// Retrieve Values for c
-				cA = alleles[i].BaseA
-				cC = alleles[i].BaseC
-				cG = alleles[i].BaseG
-				cT = alleles[i].BaseT
+		// Loop through samples and generate scores
+		// For loop starts at index 1 because index zero is the background values
+		for i = 1; i < len(alleles); i++ {
 
-				// Retrieve Values for d
-				dA = alleles[0].BaseA - alleles[i].BaseA
-				dC = alleles[0].BaseC - alleles[i].BaseC
-				dG = alleles[0].BaseG - alleles[i].BaseG
-				dT = alleles[0].BaseT - alleles[i].BaseT
+			// Retrieve Values for c
+			cA = alleles[i].BaseA
+			cC = alleles[i].BaseC
+			cG = alleles[i].BaseG
+			cT = alleles[i].BaseT
 
-				// Generate Scores
-				if alleles[i].Ref != dna.A {
-					wg.Add(1)
-					go score(&wg, vcfChannel, a[i-1], b[i-1], cA, dA, afThreshold, alleles[i], "A", 0, chrName, pos, sigThreshold)
-				}
-				if alleles[i].Ref != dna.C {
-					wg.Add(1)
-					go score(&wg, vcfChannel, a[i-1], b[i-1], cC, dC, afThreshold, alleles[i], "C", 0, chrName, pos, sigThreshold)
-				}
-				if alleles[i].Ref != dna.G {
-					wg.Add(1)
-					go score(&wg, vcfChannel, a[i-1], b[i-1], cG, dG, afThreshold, alleles[i], "G", 0, chrName, pos, sigThreshold)
-				}
-				if alleles[i].Ref != dna.T {
-					wg.Add(1)
-					go score(&wg, vcfChannel, a[i-1], b[i-1], cT, dT, afThreshold, alleles[i], "T", 0, chrName, pos, sigThreshold)
-				}
+			// Retrieve Values for d
+			dA = alleles[0].BaseA - alleles[i].BaseA
+			dC = alleles[0].BaseC - alleles[i].BaseC
+			dG = alleles[0].BaseG - alleles[i].BaseG
+			dT = alleles[0].BaseT - alleles[i].BaseT
 
-				// Calculate p for each Ins
-				for j = 0; j < len(alleles[i].Ins); j++ {
-					cIns = alleles[i].Ins[j].Count
-					// Find Ins in the background Ins slice
-					for l = 0; l < len(alleles[0].Ins); l++ {
-						if dna.CompareSeqsIgnoreCase(alleles[i].Ins[j].Alt, alleles[0].Ins[l].Alt) == 0 {
-							dIns = alleles[0].Ins[l].Count - alleles[i].Ins[j].Count
-							break
-						}
+			// Generate Scores
+			if alleles[i].Ref != dna.A {
+				wg.Add(1)
+				go score(&wg, vcfChannel, a[i-1], b[i-1], cA, dA, afThreshold, alleles[i], "A", 0, loc.Chr, loc.Pos, sigThreshold)
+			}
+			if alleles[i].Ref != dna.C {
+				wg.Add(1)
+				go score(&wg, vcfChannel, a[i-1], b[i-1], cC, dC, afThreshold, alleles[i], "C", 0, loc.Chr, loc.Pos, sigThreshold)
+			}
+			if alleles[i].Ref != dna.G {
+				wg.Add(1)
+				go score(&wg, vcfChannel, a[i-1], b[i-1], cG, dG, afThreshold, alleles[i], "G", 0, loc.Chr, loc.Pos, sigThreshold)
+			}
+			if alleles[i].Ref != dna.T {
+				wg.Add(1)
+				go score(&wg, vcfChannel, a[i-1], b[i-1], cT, dT, afThreshold, alleles[i], "T", 0, loc.Chr, loc.Pos, sigThreshold)
+			}
+
+			// Calculate p for each Indel
+			for j = 0; j < len(alleles[i].Indel); j++ {
+				cIndel = alleles[i].Indel[j].Count
+				// Find Indel in the background Indel slice
+				for l = 0; l < len(alleles[0].Indel); l++ {
+					if dna.CompareSeqsIgnoreCase(alleles[i].Indel[j].Alt, alleles[0].Indel[l].Alt) == 0 &&
+						dna.CompareSeqsIgnoreCase(alleles[i].Indel[j].Ref, alleles[0].Indel[l].Ref) == 0 {
+						dIndel = alleles[0].Indel[l].Count - alleles[i].Indel[j].Count
+						break
 					}
-					wg.Add(1)
-					go score(&wg, vcfChannel, a[i-1], b[i-1], cIns, dIns, afThreshold, alleles[i], "Ins", j, chrName, pos, sigThreshold)
 				}
-
-				// Calculate p for each Del
-				for k = 0; k < len(alleles[i].Del); k++ {
-					cDel = alleles[i].Del[k].Count
-					// Find Del in the background Del slice
-					for l = 0; l < len(alleles[0].Del); l++ {
-						if dna.CompareSeqsIgnoreCase(alleles[i].Del[k].Ref, alleles[0].Del[l].Ref) == 0 {
-							dDel = alleles[0].Del[l].Count - alleles[i].Del[k].Count
-							break
-						}
-					}
-					wg.Add(1)
-					go score(&wg, vcfChannel, a[i-1], b[i-1], cDel, dDel, afThreshold, alleles[i], "Del", k, chrName, pos, sigThreshold)
-				}
+				wg.Add(1)
+				go score(&wg, vcfChannel, a[i-1], b[i-1], cIndel, dIndel, afThreshold, alleles[i], "Indel", j, loc.Chr, loc.Pos, sigThreshold)
 			}
 		}
 	}
+
 
 	fmt.Println("# Waiting for Goroutines to finish")
 
@@ -318,17 +279,11 @@ func score(wg *sync.WaitGroup, vcfChannel chan *vcf.Vcf, a int32, b int32, c int
 			answer.Ref = dna.BaseToString(inStruct.Ref)
 			answer.Alt = altbase
 
-		case "Ins":
+		case "Indel":
 			answer.Pos = pos
-			answer.Info = "INS"
-			answer.Ref = dna.BasesToString(inStruct.Ins[indelslicepos].Ref)
-			answer.Alt = dna.BasesToString(inStruct.Ins[indelslicepos].Alt)
-
-		case "Del":
-			answer.Pos = pos
-			answer.Info = "DEL"
-			answer.Ref = dna.BasesToString(inStruct.Del[indelslicepos].Ref)
-			answer.Alt = dna.BasesToString(inStruct.Del[indelslicepos].Alt)
+			answer.Info = "."
+			answer.Ref = dna.BasesToString(inStruct.Indel[indelslicepos].Ref)
+			answer.Alt = dna.BasesToString(inStruct.Indel[indelslicepos].Alt)
 		}
 		vcfChannel <- answer
 	}
