c2af0ebbe0c6d3eb927ef58e729161898449b410
rimangan <riley.mangan@duke.edu>
2020-01-22 23:19:36 -0500

    bedFilter update and principle component analysis program.

--- cmd/bedFilter/bedFilter.go
+++ cmd/bedFilter/bedFilter.go
@@ -4,10 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"github.com/vertgenlab/gonomics/bed"
-	"log"
-	"strings"
-	"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/common"
+	"github.com/vertgenlab/gonomics/fileio"
+	"log"
+	"math"
+	"strings"
 )
 
 func bedFilter(infile string, outfile string, minScore int64, maxScore int64, minLength int64, maxLength int64, minStart int64, maxStart int64, minEnd int64, maxEnd int64, chrom string) {
@@ -16,6 +17,7 @@ func bedFilter(infile string, outfile string, minScore int64, maxScore int64, mi
 	var startNum, endNum, length int64
 	var doneReading bool = false
 	var pass bool = false
+	var numFields int
 	var current *bed.Bed
 	file := fileio.EasyOpen(infile)
 	defer file.Close()
@@ -26,81 +28,73 @@ func bedFilter(infile string, outfile string, minScore int64, maxScore int64, mi
 		startNum = common.StringToInt64(words[1])
 		endNum = common.StringToInt64(words[2])
 		length = endNum - startNum
-		if len(words) > 4{
+		if len(words) > 4 {
 			if common.StringToInt64(words[4]) < minScore {
 				pass = false
-		} 
-			if maxScore != 0 {
-				if common.StringToInt64(words[4]) > maxScore {
-					pass = false
-				}
-			} 
-
-		}
-		
-		if length < minLength {
-			pass = false
-		} 
-		if maxLength != 0 {
-			if length > maxLength {
+			}
+			if common.StringToInt64(words[4]) > maxScore {
 				pass = false
 			}
-		} 
+		}
+		if length < minLength {
+			pass = false
+		}
+		if length > maxLength {
+			pass = false
+		}
 		if startNum < minStart {
 			pass = false
 		}
-		if maxStart != 0 {
-			if startNum > maxStart {
-				pass = false
-			}
-		} 
+		if startNum > maxStart {
+			pass = false
+		}
 		if endNum < minEnd {
 			pass = false
 		}
-		if maxStart != 0 {
-			if endNum > maxEnd {
-				pass = false
-			}
-		} 
+		if endNum > maxEnd {
+			pass = false
+		}
 		if chrom != "" {
 			if words[0] != chrom {
 				pass = false
 			}
 		}
-
 		if pass {
 			if len(words) == 3 {
 				current = &bed.Bed{Chrom: words[0], ChromStart: startNum, ChromEnd: endNum}
+				numFields = common.Max(3, numFields)
 			} else if len(words) == 4 {
-				current = &bed.Bed{Chrom: words[0], ChromStart: startNum, ChromEnd: endNum, Name: words[1]}
+				current = &bed.Bed{Chrom: words[0], ChromStart: startNum, ChromEnd: endNum, Name: words[3]}
+				numFields = common.Max(4, numFields)
 			} else {
-				current = &bed.Bed{Chrom: words[0], ChromStart: startNum, ChromEnd: endNum, Name: words[1], Score: common.StringToInt64(words[4])}
+				current = &bed.Bed{Chrom: words[0], ChromStart: startNum, ChromEnd: endNum, Name: words[3], Score: common.StringToInt64(words[4])}
+				numFields = common.Max(5, numFields)
 			}
 			outlist = append(outlist, current)
 		}
 	}
-	bed.Write(outfile, outlist, 5)
+	bed.Write(outfile, outlist, numFields)
 }
 
 func usage() {
 	fmt.Print(
 		"bedFilter\n" +
-		"Usage:\n" +
-		"bedFilter input.bed output.bed\n" +
-		"options:\n")
+			"Usage:\n" +
+			"bedFilter input.bed output.bed\n" +
+			"options:\n")
 	flag.PrintDefaults()
 }
 
 func main() {
 	var expectedNumArgs int = 2
-	var minScore *int64 = flag.Int64("minScore", 0, "Specifies the minimum score in the fourth field.")
-	var maxScore *int64 = flag.Int64("maxScore", 0, "Specifies the maximum score in the fourth field.")
-	var minLength *int64 = flag.Int64("minLength", 0, "Specifies the minimum length of the region.")
-	var maxLength *int64 = flag.Int64("maxLength", 0, "Specifies the maximum length of the region.")
-	var minStart *int64 = flag.Int64("minStart", 0, "Specifies the minimum starting position of the region.")
-	var maxStart *int64 = flag.Int64("maxStart", 0, "Specifies the maximum starting position of the region.")
-	var minEnd *int64 = flag.Int64("minEnd", 0, "Specifies the minimum ending position of the region.")
-	var maxEnd *int64 = flag.Int64("maxEnd", 0, "Specifies the maximum ending position of the region.")
+	var minScore *int64 = flag.Int64("minScore", math.MinInt64, "Specifies the minimum score in the fourth field.")
+	var maxScore *int64 = flag.Int64("maxScore", math.MaxInt64, "Specifies the maximum score in the fourth field.")
+	var minLength *int64 = flag.Int64("minLength", math.MinInt64, "Specifies the minimum length of the region.")
+	var maxLength *int64 = flag.Int64("maxLength", math.MaxInt64, "Specifies the maximum length of the region.")
+	var minStart *int64 = flag.Int64("minStart", math.MinInt64, "Specifies the minimum starting position of the region.")
+	var maxStart *int64 = flag.Int64("maxStart", math.MaxInt64, "Specifies the maximum starting position of the region.")
+	var minEnd *int64 = flag.Int64("minEnd", math.MinInt64, "Specifies the minimum ending position of the region.")
+	var maxEnd *int64 = flag.Int64("maxEnd", math.MaxInt64, "Specifies the maximum ending position of the region.")
 	var chrom *string = flag.String("chrom", "", "Specifies the chromosome name.")
 
 	flag.Usage = usage
---
+++ cmd/prcompFormat/prcompFormat.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/vertgenlab/gonomics/dna"
+	"github.com/vertgenlab/gonomics/fasta"
+	"github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/common"
+	"log"
+)
+
+/*
+	The first loop checks each position in the specified range, with the inner loop checking each 
+	species to determine if any species contain a gap or an N at that position or if all of the 
+	species have the same base at that position. If there are no gaps or Ns and at least one species
+	has an alternative base position, that column of bases is added to the output fasta.
+	
+	Next, the output fasta, once constructed, is converted into a binary matrix for input for PCA.
+*/
+
+func prcompFormat(infile string, outfile string) {
+	records := fasta.Read(infile)
+	var subFa  = make([]fasta.Fasta, len(records))
+	var allMatch, allValid bool
+	var currentBase dna.Base
+	//fmt.Printf("Len Seq: %d\n", len(records[0].Seq))
+	//fmt.Printf("Len Rec: %d\n", len(records))
+
+	//add species name to subFa
+	for i := 0; i < len(records); i++ {
+		subFa[i].Name = records[i].Name
+	}
+	
+	for i := 0; i < len(records[0].Seq); i++ {
+		//check for gaps, identity, or Ns
+		currentBase = records[0].Seq[i]
+		allMatch = true
+		allValid = true
+		if !(currentBase == dna.N || currentBase == dna.Gap) {
+			for j := 1; j < len(records); j++ {
+				//fmt.Printf("i = %d, j = %d\n", i, j)
+				if records[j].Seq[i] != currentBase && records[j].Seq[i] != dna.Gap && records[j].Seq[i] != dna.N {
+					fmt.Printf("i = %d, j = %d\n", i, j)
+					allMatch = false
+				}
+				if records[j].Seq[i] == dna.Gap || records[j].Seq[i] == dna.N {
+					allValid = false
+				}
+			}
+			//now we add to output fasta
+			if !allMatch && allValid {
+				for k := 0; k < len(records); k++ {
+					subFa[k].Seq = append(subFa[k].Seq, records[k].Seq[i])
+				}
+			}
+		}
+	}
+
+	//expand bases to binary values while writing to the outfile
+	file := fileio.EasyCreate(outfile)
+	defer file.Close()
+	var outline string
+
+	//print header
+	var headerstring string = "Sample"
+	for n := 0; n < (4*len(subFa[0].Seq)); n++ {
+		headerstring = headerstring + fmt.Sprintf("\tVar_%d", n)
+	}
+
+	_, err := fmt.Fprintf(file, "%s\n", headerstring)
+	common.ExitIfError(err)
+
+	for i := 0; i < len(subFa); i++ {
+		outline = subFa[i].Name
+		fmt.Println(len(subFa[i].Seq))
+		for j := 0; j < len(subFa[0].Seq); j++ {
+			if subFa[i].Seq[j] == dna.A {
+				outline = outline + "\t1\t0\t0\t0"
+			} else if subFa[i].Seq[j] == dna.C {
+				outline = outline + "\t0\t1\t0\t0"
+			} else if subFa[i].Seq[j] == dna.G {
+				outline = outline + "\t0\t0\t1\t0"
+			} else if subFa[i].Seq[j] == dna.T {
+				outline = outline + "\t0\t0\t0\t1"
+			} else {
+				fmt.Printf("Base: %d\n", subFa[i].Seq[j])
+				common.ExitIfError(fmt.Errorf("Critical Failure!!\n"))
+			}
+		}
+		outline = outline + "\n"
+	_, err := fmt.Fprintf(file, "%s", outline)
+	common.ExitIfError(err)
+	}
+}
+
+func usage() {
+	fmt.Print(
+		"prcompFormat - Generates a binary input matrix for PCA in R.\n" +
+		"Usage:\n" +
+		"prcompFormat infile.fa outfile.tsv\n" +
+		"options:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	var expectedNumArgs int = 2
+	flag.Usage = usage
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	flag.Parse()
+
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Error: expecting %d arguments, but got %d\n",
+			expectedNumArgs, len(flag.Args()))
+	}
+
+	infile := flag.Arg(0)
+	outfile := flag.Arg(1)
+
+	prcompFormat(infile, outfile)
+}
