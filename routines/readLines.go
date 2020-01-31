40e520d6d977b2c6c4f614d56163d14e639aca71
das106 <das106@duke.edu>
2020-01-30 22:15:31 -0500

    added readin functionality for generic files

--- routines/readLines.go
+++ routines/readLines.go
@@ -3,46 +3,59 @@ package routines
 import (
 	"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/sam"
+	"github.com/vertgenlab/gonomics/vcf"
+	"io"
 	"log"
 	"sync"
 )
 
-type SamOperation func(*sam.SamAln, interface{}) interface{}
+func NextSamLine(reader *fileio.EasyReader) (interface{}, bool) {
+	_, err := reader.Peek(1)
+	if err != io.EOF {sam.ReadHeader(reader)}
+	return sam.NextAlignment(reader)
+}
 
-func goReadSam(samFilename string, sendLine chan *sam.SamAln) {
-	samFile := fileio.EasyOpen(samFilename)
-	defer samFile.Close()
+func NextVcfLine(reader *fileio.EasyReader) (interface{}, bool) {
+	return vcf.NextVcf(reader)
+}
+
+func NextString(reader *fileio.EasyReader) (interface{}, bool) {
+	return fileio.EasyNextLine(reader)
+}
+
+type Operation func(interface{}, interface{}) interface{}
+
+type NextLine func(reader *fileio.EasyReader) (interface{}, bool)
+
+func goReadFile(filename string, sendLine chan interface{}, fnNext NextLine) {
+	file := fileio.EasyOpen(filename)
+	defer file.Close()
 	var done = false
-	var aln *sam.SamAln
-
-
-
-	sam.ReadHeader(samFile)
+	var line interface{}
 
 	progressMeter := 0
-	for aln, done = sam.NextAlignment(samFile); done != true; aln, done = sam.NextAlignment(samFile) {
-
+	for line, done = fnNext(file); done != true; line, done = fnNext(file) {
 		progressMeter++
 		if progressMeter % 10000 == 0 {
 			log.Println("Processed", progressMeter, "Lines")
 		}
-		sendLine <- aln
+		sendLine <- line
 	}
 	close(sendLine)
 }
 
-func ReadByLine(samFilename string, function SamOperation, data1 interface{}, threads int) chan interface{}{
+func ReadByLine(filename string, dataStructure interface{}, fnWork Operation, fnNext NextLine, threads int) chan interface{} {
 	var wg sync.WaitGroup
-	records := make(chan *sam.SamAln)
+	records := make(chan interface{})
 	output := make(chan interface{})
 
-	go goReadSam(samFilename, records)
+	go goReadFile(filename, records, fnNext)
 
 	for k := 0; k < threads; k++ {
 		wg.Add(1)
 		go func() {
 			for data := range records {
-				a := function(data, data1)
+				a := fnWork(data, dataStructure)
 				output <- a
 			}
 			wg.Done()
