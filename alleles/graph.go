2214500101ac79c160aa1424b6fd690b2c853a8a
das106 <das106@duke.edu>
2020-02-12 14:26:53 -0500

    added function to count alleles in graph

--- alleles/graph.go
+++ alleles/graph.go
@@ -2,6 +2,7 @@ package alleles
 
 import (
 	"fmt"
+	"github.com/vertgenlab/gonomics/cigar"
 	"github.com/vertgenlab/gonomics/dna"
 	"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/numbers"
@@ -9,6 +10,7 @@ import (
 	"github.com/vertgenlab/gonomics/simpleGraph"
 	"github.com/vertgenlab/gonomics/vcf"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"strconv"
 	"strings"
@@ -198,7 +200,6 @@ func MakeTestReads(graph *simpleGraph.SimpleGraph, numReads int, readLen int, nu
 	var i, j int
 	var possibleSNPids = make([]int, 0)
 	var SNPids = make([]int, 0)
-	fmt.Println(possibleSNPids)
 	// Choose random path through graph
 	basePath := RandPath(graph)
 
@@ -301,30 +302,34 @@ func MakeTestReads(graph *simpleGraph.SimpleGraph, numReads int, readLen int, nu
 		for j = 0; j < len(readPath); j++ {
 			writePath = append(writePath, readPath[j].Id)
 		}
-
 		current = &sam.SamAln{}
 		current.RName = readPath[0].Name
 		current.Seq = readSeq
-		current.Pos = int64(readStartPos)
+		current.Pos = int64(readStartPos) + 1
 		current.Extra = simpleGraph.PathToString(writePath, graph)
-
+		current.Cigar = []*cigar.Cigar{&cigar.Cigar{int64(readLen), 'M'}} // TODO: trouble on this line
 		answer = append(answer, current)
-
 	}
 	return answer
 }
 
 
-// Practical Functions
-type NodeP struct {
-	Node 	*simpleGraph.Node
-	pVal	float64
+
+// Functions to find vars that exist in graph
+type SampleData struct {
+	Sample	string
+	Counts 	[]uint32
 }
 
-func addPathToCount(data []uint32, path []uint32) {
-	for i := 0; i < len(path); i++ {
-		data[path[i]]++
-	}
+type BatchData struct {
+	Samples []string
+	Counts 	[][]uint32
+}
+
+type NodeP struct {
+	Sample 	string
+	Node 	*simpleGraph.Node
+	pVal	float64
 }
 
 func StringToPath(input string) []uint32 {
@@ -335,6 +340,12 @@ func StringToPath(input string) []uint32 {
 		answer = append(answer, uint32(node))
 	}
 	return answer
+}
+
+func addPathToCount(data []uint32, path []uint32) {
+	for i := 0; i < len(path); i++ {
+		data[path[i]]++
+	}
 }
 
 func CountPaths(graph *simpleGraph.SimpleGraph, samFilename string) []uint32 {
@@ -352,19 +363,49 @@ func CountPaths(graph *simpleGraph.SimpleGraph, samFilename string) []uint32 {
 	return answer
 }
 
-func FindMajorLocalNode(start *simpleGraph.Node, pathCounts [][]uint32) (*simpleGraph.Node, *simpleGraph.Edge) {
+func CountPathsInDir(graph *simpleGraph.SimpleGraph, inDirectory string) *BatchData {
+	var wg sync.WaitGroup
+	receivePathCount := make(chan *SampleData)
+
+	if !strings.HasSuffix(inDirectory, "/") {
+		inDirectory = inDirectory + "/"
+	}
+
+	files, _ := ioutil.ReadDir(inDirectory)
+
+	for _, file := range files {
+		filePath := fmt.Sprintf("%s%s", inDirectory, file.Name())
+		// Count Paths for Each Sample in Directory
+		wg.Add(1)
+		go func(filePath string, fileName string) {
+			pathCount := CountPaths(graph, filePath)
+			tmp := &SampleData{fileName, pathCount}
+			receivePathCount <- tmp
+			wg.Done()
+		}(filePath, file.Name())
+	}
+
+	go func() {
+		wg.Wait()
+		close(receivePathCount)
+	}()
+
+	// Get path counts for each sample
+	pathCounts := make([][]uint32, 0)
+	samples := make([]string, 0)
+	for count := range receivePathCount {
+		samples = append(samples, count.Sample)
+		pathCounts = append(pathCounts, count.Counts)
+	}
+
+	answer := &BatchData{samples, pathCounts}
+	return answer
+}
+
+func findMajorLocalNode(nodesToCheck []*simpleGraph.Node, pathCounts [][]uint32) *simpleGraph.Node {
+	var answer *simpleGraph.Node
+
 	var i, j, sumCount, maxCount int
-	var maxNode *simpleGraph.Node
-	var maxEdge *simpleGraph.Edge
-
-	var nodesToCheck = make([]*simpleGraph.Node, 0)
-
-	for i = 0; i < len(start.Prev); i++ {
-		nodesToCheck = append(nodesToCheck, start.Prev[i].Dest)
-	}
-	for i = 0; i < len(start.Next); i++ {
-		nodesToCheck = append(nodesToCheck, start.Next[i].Dest)
-	}
 
 	maxCount = 0
 	for i = 0; i < len(nodesToCheck); i++ {
@@ -373,40 +414,76 @@ func FindMajorLocalNode(start *simpleGraph.Node, pathCounts [][]uint32) (*simple
 			sumCount += int(pathCounts[j][nodesToCheck[i].Id])
 		}
 		if sumCount > maxCount {
-			maxNode = nodesToCheck[i]
-		}
-	}
-
-	edges := append(start.Prev, start.Next...)
-	for i = 0; i < len(edges); i++ {
-		if edges[i].Dest.Id == start.Id {
-			maxEdge = edges[i]
-			break
+			maxCount = sumCount
+			answer = nodesToCheck[i]
 		}
 	}
 
 	if maxCount == 0 {
+		return nil
+	} else {
+		return answer
+	}
+}
+
+func FindMajorLocalNode(start *simpleGraph.Node, pathCounts [][]uint32) (*simpleGraph.Node, *simpleGraph.Edge) {
+	var i int
+	var maxNode, maxPrevNode *simpleGraph.Node
+	var maxEdge *simpleGraph.Edge
+
+	var prevNodes, nextNodes []*simpleGraph.Node
+
+	for i = 0; i < len(start.Prev); i++ {
+		prevNodes = append(prevNodes, start.Prev[i].Dest)
+	}
+
+	for i = 0; i < len(start.Next); i++ {
+		nextNodes = append(nextNodes, start.Next[i].Dest)
+	}
+
+	maxNode = findMajorLocalNode(append(prevNodes, nextNodes...), pathCounts)
+	maxPrevNode = findMajorLocalNode(prevNodes, pathCounts)
+
+
+	for i = 0; i < len(start.Prev); i++ {
+		if start.Prev[i].Dest == maxPrevNode {
+			maxEdge = start.Prev[i]
+			break
+		}
+	}
+
+	if maxNode == nil {
 		return nil, nil
 	} else {
 		return maxNode, maxEdge
 	}
 }
 
-func calcRareNode(wg *sync.WaitGroup, sendResult chan *NodeP, start *simpleGraph.Node, pathCounts [][]uint32, maxPopFreq float64, minReadFreq float64, minPval float64) {
+func calcRareNode(wg *sync.WaitGroup, sendResult chan *NodeP, start *simpleGraph.Node, sampleData *BatchData, maxPopFreq float64, minReadFreq float64, minPval float64) {
 	defer wg.Done()
+	pathCounts := sampleData.Counts
 
-	majorLocalNode, majorLocalEdge := FindMajorLocalNode(start, pathCounts)
+	majorLocalNode, majorPrevEdge := FindMajorLocalNode(start, pathCounts)
 
 	// If there were no reads in any adjacent nodes then exit
 	if majorLocalNode == nil {
 		return
 	}
 
-	// If the prob of entry from the majorLocalNode is > maxPopFreq then exit
-	if majorLocalEdge.Prob > maxPopFreq {
-		return
+	// If the prob of entry from the majorLocalNode is > maxPopFreq then exit.
+	// No probabilty for first node in graph
+
+
+	var prob float64
+	if len(start.Prev) > 0 {
+		prob = majorPrevEdge.Prob
+	} else {
+		prob = 1
 	}
 
+	if prob > maxPopFreq {
+		return
+	}
 	// Begin testing on each sample
 	// test is for the matrix:
 	// [a b]
@@ -418,15 +495,15 @@ func calcRareNode(wg *sync.WaitGroup, sendResult chan *NodeP, start *simpleGraph
 	for i := 0; i < len(pathCounts); i++ {
 		a := int(pathCounts[i][majorLocalNode.Id])
 		c := int(pathCounts[i][start.Id])
-
 		var b, d int
 		for j := 0; j < len(pathCounts); j++ {
-			b += int(pathCounts[j][majorLocalNode.Id]) - a
-			d += int(pathCounts[j][start.Id]) - c
+			b += int(pathCounts[j][majorLocalNode.Id])
+			d += int(pathCounts[j][start.Id])
 		}
+		b = b - a
+		d = d - c
 
 		p := numbers.FisherExact(a, b, c, d, true)
-
 		switch {
 			// If alternate allele is zero then there is no variant and should exit
 			case c == 0:
@@ -438,22 +515,23 @@ func calcRareNode(wg *sync.WaitGroup, sendResult chan *NodeP, start *simpleGraph
 		}
 
 		if p < minPval {
-			answer := &NodeP{start, p}
+			answer := &NodeP{sampleData.Samples[i], start, p}
 			sendResult <- answer
 		}
 	}
 }
 
-func FindRareNodes(graph *simpleGraph.SimpleGraph, pathCounts [][]uint32, maxPopFreq float64, minReadFreq float64, minPval float64) []*NodeP {
+func FindRareNodes(graph *simpleGraph.SimpleGraph, sampleData *BatchData, maxPopFreq float64, minReadFreq float64, minPval float64) []*NodeP {
 	var answer = make([]*NodeP, 0)
 	result := make(chan *NodeP)
 	var wg sync.WaitGroup
 	var i, j int
 
+	pathCounts := sampleData.Counts
+
 	// Note: pathCounts is a [sample][nodes]
 	// For each node ---->
 	for i = 0; i < len(pathCounts[0]); i++ {
-
 		var areThereReads = false
 		// For each sample ---->
 		for j = 0; j < len(pathCounts); j++ {
@@ -467,10 +545,8 @@ func FindRareNodes(graph *simpleGraph.SimpleGraph, pathCounts [][]uint32, maxPop
 		if areThereReads == false {
 			continue
 		}
-
-
 		wg.Add(1)
-		go calcRareNode(&wg, result, graph.Nodes[i], pathCounts, maxPopFreq, minReadFreq, minPval)
+		go calcRareNode(&wg, result, graph.Nodes[i], sampleData, maxPopFreq, minReadFreq, minPval)
 	}
 
 	go func() {
@@ -491,9 +567,9 @@ func nodeToVcf(node *NodeP) *vcf.Vcf {
 		Id: 	"",
 		Ref: 	"",
 		Alt: 	dna.BasesToString(node.Node.Seq),
-		Qual: 	node.pVal,
+		Qual: 	0,
 		Filter: "",
-		Info: 	"",
+		Info: 	fmt.Sprintf("Sample:%s, p:%e", node.Sample, node.pVal),
 		Format: "",
 		Sample: []string{}}
 
@@ -507,8 +583,9 @@ func NodesToVcf(nodes []*NodeP) []*vcf.Vcf {
 
 	for i := 0; i < len(nodes); i++ {
 		wg.Add(1)
+		currNode := nodes[i]
 		go func() {
-			receiveVcf <- nodeToVcf(nodes[i])
+			receiveVcf <- nodeToVcf(currNode)
 			wg.Done()
 		}()
 	}
@@ -525,42 +602,293 @@ func NodesToVcf(nodes []*NodeP) []*vcf.Vcf {
 	return answer
 }
 
-func FindVarsInGraph(graph *simpleGraph.SimpleGraph, inDirectory string, maxPopFreq float64, minReadFreq float64, minPval float64) []*vcf.Vcf {
+func FindVarsInsideGraph(graph *simpleGraph.SimpleGraph, sampleData *BatchData, maxPopFreq float64, minReadFreq float64, minPval float64) []*vcf.Vcf {
 	var answer []*vcf.Vcf
-	var wg sync.WaitGroup
-	receivePathCount := make(chan []uint32)
-
-	if !strings.HasSuffix(inDirectory, "/") {
-		inDirectory = inDirectory + "/"
-	}
-
-	files, _ := ioutil.ReadDir(inDirectory)
-
-	for _, file := range files {
-		// Count Paths for Each Sample in Directory
-		wg.Add(1)
-		go func() {
-			pathCount := CountPaths(graph, file.Name())
-			receivePathCount <- pathCount
-			wg.Done()
-		}()
-	}
-
-	go func() {
-		wg.Wait()
-		close(receivePathCount)
-	}()
-
-	// Get path counts for each sample
-	pathCounts := make([][]uint32, 0)
-	for count := range receivePathCount {
-		pathCounts = append(pathCounts, count)
-	}
 
 	// Find rare nodes in graph
-	rareNodes := FindRareNodes(graph, pathCounts, maxPopFreq, minReadFreq, minPval)
+	rareNodes := FindRareNodes(graph, sampleData, maxPopFreq, minReadFreq, minPval)
 
 	answer = NodesToVcf(rareNodes)
+
+	return answer
+}
+
+
+// Functions to find vars that DO NOT exist in graph
+type GraphSampleMap map[GraphLocation]*AlleleCount
+
+type BatchGraphSampleMap map[GraphLocation][]*BatchAlleleCount
+
+type GraphLocation struct {
+	Node 	*simpleGraph.Node
+	Pos 	int64
+}
+
+func GraphCountAlleles(graph *simpleGraph.SimpleGraph, samFilename string, minMapQ int64) GraphSampleMap {
+	AlleleMap := make(GraphSampleMap)
+
+	var i, k int32
+	var j int
+	samFile := fileio.EasyOpen(samFilename)
+	defer samFile.Close()
+	var done = false
+	var RefIndex, SeqIndex int64
+	var currentSeq []dna.Base
+	var aln *sam.SamAln
+	var currentIndel Indel
+	var indelSeq []dna.Base
+	var OrigRefIndex int64
+	var Match bool
+
+	sam.ReadHeader(samFile)
+
+	var progressMeter int32
+	for aln, done = sam.NextAlignment(samFile); done != true; aln, done = sam.NextAlignment(samFile) {
+
+		readPath := StringToPath(aln.Extra)
+
+		if progressMeter%10000 == 0 {
+			log.Printf("#Read %d Alignments\n", progressMeter)
+		}
+		progressMeter++
+
+		// If read is unmapped then go to the next alignment
+		if aln.Cigar[0].Op == '*' {
+			continue
+		}
+
+		// If mapping quality is less than the threshold then go to next alignment
+		if aln.MapQ < minMapQ {
+			continue
+		}
+
+		SeqIndex = 0
+		RefIndex = aln.Pos - 1
+
+		currNode := 0
+		ref := graph.Nodes[readPath[currNode]]
+
+		for i = 0; i < int32(len(aln.Cigar)); i++ {
+			currentSeq = aln.Seq
+
+			//Handle deletion relative to ref
+			//Each position deleted is annotated with counts + 1
+			if aln.Cigar[i].Op == 'D' {
+				OrigRefIndex = RefIndex
+				OrigNode := ref
+				indelSeq = make([]dna.Base, 1)
+
+				// First base in indel is the base prior to the indel sequence per VCF standard format
+				indelSeq[0] = OrigNode.Seq[OrigRefIndex-1]
+
+				for k = 0; k < int32(aln.Cigar[i].RunLength); k++ {
+
+					// If the position has already been added to the map, move along
+					_, ok := AlleleMap[GraphLocation{ref, RefIndex}]
+
+					// If the position is NOT in the map, initialize
+					if !ok {
+						AlleleMap[GraphLocation{ref, RefIndex}] = &AlleleCount{
+							Ref: ref.Seq[RefIndex], Counts: 0, BaseA: make([]int32, 3), BaseC: make([]int32, 3), BaseG: make([]int32, 3), BaseT: make([]int32, 3), Indel: make([]Indel, 0)}
+					}
+
+					// Keep track of deleted sequence
+					indelSeq = append(indelSeq, ref.Seq[RefIndex])
+
+					AlleleMap[GraphLocation{ref, RefIndex}].Counts++
+
+					if RefIndex + 1 == int64(len(ref.Seq)) {
+						RefIndex = 0
+						currNode++
+						if currNode + 1 <= len(readPath) {
+							ref = graph.Nodes[readPath[currNode]]
+						} else {break}
+					} else {
+						RefIndex++
+					}
+
+				}
+
+				Match = false
+				for j = 0; j < len(AlleleMap[GraphLocation{OrigNode, OrigRefIndex}].Indel); j++ {
+					// If the deletion has already been seen before, increment the existing entry
+					// For a deletion the indelSeq should match the Ref
+					if dna.CompareSeqsIgnoreCase(indelSeq, AlleleMap[GraphLocation{OrigNode, OrigRefIndex}].Indel[j].Ref) == 0 &&
+						dna.CompareSeqsIgnoreCase(indelSeq[:1], AlleleMap[GraphLocation{OrigNode, OrigRefIndex}].Indel[j].Alt) == 0 {
+						AlleleMap[GraphLocation{OrigNode, OrigRefIndex}].Indel[j].Count[0]++
+						if sam.IsForwardRead(aln) == true {
+							AlleleMap[GraphLocation{OrigNode, OrigRefIndex}].Indel[j].Count[1]++
+						} else if sam.IsReverseRead(aln) == true {
+							AlleleMap[GraphLocation{OrigNode, OrigRefIndex}].Indel[j].Count[2]++
+						}
+
+						Match = true
+						break
+					}
+				}
+
+				// If the deletion has not been seen before, then append it to the Del slice
+				// For Alt indelSeq[:1] is used to give me a slice of just the first base in the slice which we defined earlier
+				if Match == false {
+
+					currentIndel = Indel{indelSeq, indelSeq[:1], make([]int32, 3)}
+					currentIndel.Count[0]++
+					if sam.IsForwardRead(aln) == true {
+						currentIndel.Count[1]++
+					} else if sam.IsReverseRead(aln) == false {
+						currentIndel.Count[2]++
+					}
+					AlleleMap[GraphLocation{OrigNode, OrigRefIndex}].Indel = append(AlleleMap[GraphLocation{OrigNode, OrigRefIndex}].Indel, currentIndel)
+				}
+
+				//Handle insertion relative to ref
+				//The base after the inserted sequence is annotated with an Ins read
+			} else if aln.Cigar[i].Op == 'I' {
+
+				// If the position has already been added to the map, move along
+				_, ok := AlleleMap[GraphLocation{ref, RefIndex}]
+
+				// If the position is NOT in the map, initialize
+				if !ok {
+					AlleleMap[GraphLocation{ref, RefIndex}] = &AlleleCount{
+						Ref: ref.Seq[RefIndex], Counts: 0, BaseA: make([]int32, 3), BaseC: make([]int32, 3), BaseG: make([]int32, 3), BaseT: make([]int32, 3), Indel: make([]Indel, 0)}
+				}
+
+				// Loop through read sequence and keep track of the inserted bases
+				indelSeq = make([]dna.Base, 1)
+
+				// First base in indel is the base prior to the indel sequence per VCF standard format
+				indelSeq[0] = ref.Seq[RefIndex-1]
+
+				// Keep track of inserted sequence by moving along the read
+				for k = 0; k < int32(aln.Cigar[i].RunLength); k++ {
+					indelSeq = append(indelSeq, currentSeq[SeqIndex])
+					SeqIndex++
+				}
+
+				Match = false
+				for j = 0; j < len(AlleleMap[GraphLocation{ref, RefIndex}].Indel); j++ {
+					// If the inserted sequence matches a previously inserted sequence, then increment the count
+					// For an insertion, the indelSeq should match the Alt
+					if dna.CompareSeqsIgnoreCase(indelSeq, AlleleMap[GraphLocation{ref, RefIndex}].Indel[j].Alt) == 0 &&
+						dna.CompareSeqsIgnoreCase(indelSeq[:1], AlleleMap[GraphLocation{ref, RefIndex}].Indel[j].Ref) == 0 {
+						AlleleMap[GraphLocation{ref, RefIndex}].Indel[j].Count[0]++
+						if sam.IsForwardRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].Indel[j].Count[1]++
+						} else if sam.IsReverseRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].Indel[j].Count[2]++
+						}
+						Match = true
+						break
+					}
+				}
+
+				if Match == false {
+					currentIndel = Indel{indelSeq[:1], indelSeq, make([]int32, 3)}
+					currentIndel.Count[0]++
+					if sam.IsForwardRead(aln) == true {
+						currentIndel.Count[1]++
+					} else if sam.IsReverseRead(aln) == true {
+						currentIndel.Count[2]++
+					}
+					AlleleMap[GraphLocation{ref, RefIndex}].Indel = append(AlleleMap[GraphLocation{ref, RefIndex}].Indel, currentIndel)
+				}
+
+				// Note: Insertions do not contribute to the total counts as the insertion is associated with the previous reference base
+
+				//Handle matching pos relative to ref
+			} else if cigar.CigarConsumesReference(*aln.Cigar[i]) {
+
+				for k = 0; k < int32(aln.Cigar[i].RunLength); k++ {
+
+					//if the position has already been added to the matrix, move along
+					_, ok := AlleleMap[GraphLocation{ref, RefIndex}]
+
+					//if the position is NOT in the matrix, add it
+					if !ok {
+						AlleleMap[GraphLocation{ref, RefIndex}] = &AlleleCount{
+							Ref: ref.Seq[RefIndex], Counts: 0, BaseA: make([]int32, 3), BaseC: make([]int32, 3), BaseG: make([]int32, 3), BaseT: make([]int32, 3), Indel: make([]Indel, 0)}
+					}
+
+					switch currentSeq[SeqIndex] {
+					case dna.A:
+						if sam.IsForwardRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].BaseA[1]++
+						} else if sam.IsReverseRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].BaseA[2]++
+						}
+						AlleleMap[GraphLocation{ref, RefIndex}].BaseA[0]++
+						AlleleMap[GraphLocation{ref, RefIndex}].Counts++
+					case dna.T:
+						if sam.IsForwardRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].BaseT[1]++
+						} else if sam.IsReverseRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].BaseT[2]++
+						}
+						AlleleMap[GraphLocation{ref, RefIndex}].BaseT[0]++
+						AlleleMap[GraphLocation{ref, RefIndex}].Counts++
+					case dna.G:
+						if sam.IsForwardRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].BaseG[1]++
+						} else if sam.IsReverseRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].BaseG[2]++
+						}
+						AlleleMap[GraphLocation{ref, RefIndex}].BaseG[0]++
+						AlleleMap[GraphLocation{ref, RefIndex}].Counts++
+					case dna.C:
+						if sam.IsForwardRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].BaseC[1]++
+						} else if sam.IsReverseRead(aln) == true {
+							AlleleMap[GraphLocation{ref, RefIndex}].BaseC[2]++
+						}
+						AlleleMap[GraphLocation{ref, RefIndex}].BaseC[0]++
+						AlleleMap[GraphLocation{ref, RefIndex}].Counts++
+					}
+					SeqIndex++
+					if RefIndex + 1 == int64(len(ref.Seq)) {
+						RefIndex = 0
+						currNode++
+						if currNode + 1 <= len(readPath) {
+							ref = graph.Nodes[readPath[currNode]]
+						} else {break}
+					} else {
+						RefIndex++
+					}
+				}
+			} else if aln.Cigar[i].Op != 'H' {
+				SeqIndex = SeqIndex + aln.Cigar[i].RunLength
+			}
+		}
+	}
+
+
+	return AlleleMap
+}
+
+func GraphCountAllelesInDir() []GraphSampleMap {
+	answer := make([]GraphSampleMap, 0)
+
+	return answer
+}
+
+func CreateBatchGraphSampleMap() BatchGraphSampleMap {
+	answer := make(BatchGraphSampleMap)
+
+	return answer
+}
+
+func GraphScoreVariants() {
+
+}
+
+func GraphScoresToVcf() []*vcf.Vcf {
+	answer := make([]*vcf.Vcf, 0)
+
+	return answer
+}
+
+func FindVarsOutsideGraph(graph *simpleGraph.SimpleGraph, simpleData *BatchData) []*vcf.Vcf {
+	var answer []*vcf.Vcf
 
 	return answer
 }
