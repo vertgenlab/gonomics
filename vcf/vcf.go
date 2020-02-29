ef3f4c84d0472e83afd7634fdb9092a923e7f1bc
das106 <das106@duke.edu>
2020-02-28 19:51:25 -0500

    patch to vcf read to handle long lines

--- vcf/vcf.go
+++ vcf/vcf.go
@@ -1,10 +1,12 @@
 package vcf
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/vertgenlab/gonomics/common"
 	"github.com/vertgenlab/gonomics/fasta"
 	"github.com/vertgenlab/gonomics/fileio"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -67,27 +69,41 @@ func NextVcf(reader *fileio.EasyReader) (*Vcf, bool) {
 }
 
 func Read(filename string) []*Vcf {
-	file := fileio.EasyOpen(filename)
-	defer file.Close()
-
-	var line string
-	var done bool
 	var answer []*Vcf
-	ReadHeader(file)
-	for line, done = fileio.EasyNextLine(file); !done; line, done = fileio.EasyNextLine(file) {
+	//var curr *Vcf
+	var line string
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	if err != nil {
+		return nil
+	}
+	ReadHeader(reader)
+	var err2 error
+
+	for line, _ = reader.ReadString('\n'); err2 != io.EOF; line, err2 = reader.ReadString('\n') {
+		line = strings.TrimSuffix(line, "\n")
 		answer = append(answer, processVcfLine(line))
 	}
 	return answer
 }
 
 func ReadVcf(filename string) *VCF {
-	file := fileio.EasyOpen(filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil
+	}
+	reader := bufio.NewReader(file)
 	defer file.Close()
 
-	header := ReadHeader(file)
+	header := ReadHeader(reader)
 	vcfRecords := Read(filename)
 	return &VCF{Header: header, Vcf: vcfRecords}
 }
+
 
 func processHeader(header *VcfHeader, line string) {
 	var err error
@@ -99,14 +115,14 @@ func processHeader(header *VcfHeader, line string) {
 	}
 }
 
-func ReadHeader(er *fileio.EasyReader) *VcfHeader {
+func ReadHeader(er *bufio.Reader) *VcfHeader {
 	var line string
 	var err error
 	var nextBytes []byte
 	var header VcfHeader
-
 	for nextBytes, err = er.Peek(1); nextBytes[0] == '#' && err == nil; nextBytes, err = er.Peek(1) {
-		line, _ = fileio.EasyNextLine(er)
+		line, _ = er.ReadString('\n')
+		line = strings.TrimSuffix(line, "\n")
 		processHeader(&header, line)
 	}
 	return &header
