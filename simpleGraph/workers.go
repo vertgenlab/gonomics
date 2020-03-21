bae9edfb0bf607b5ee9f26a3b62c19c77260dc53
Craig Lowe <craiglowe@users.noreply.github.com>
2020-03-20 20:34:15 -0400

    fixed copypaste into workers.go

--- simpleGraph/workers.go
+++ simpleGraph/workers.go
@@ -1,8 +1,14 @@
 package simpleGraph
 
+import (
+	"github.com/vertgenlab/gonomics/cigar"
+	"github.com/vertgenlab/gonomics/fastq"
+	"github.com/vertgenlab/gonomics/sam"
+	"sync"
+)
+
 // this is just for speed testing to see how much of the speed slowdown is due to alignment time
-func gswNothingWorker(gg *SimpleGraph, seedHash map[uint64][]uint64, seedLen int, stepSize int, incomingFastqs <-chan *fastq.FastqBig, outgoingSams chan<- *sam.
-	SamAln, wg *sync.WaitGroup) {
+func gswNothingWorker(gg *SimpleGraph, seedHash map[uint64][]uint64, seedLen int, stepSize int, incomingFastqs <-chan *fastq.FastqBig, outgoingSams chan<- *sam.SamAln, wg *sync.WaitGroup) {
 	var curr *sam.SamAln
 	for read := range incomingFastqs {
 		curr = &sam.SamAln{QName: read.Name, Flag: 4, RName: "*", Pos: 0, MapQ: 255, Cigar: []*cigar.Cigar{&cigar.Cigar{Op: '*'}}, RNext: "*", PNext: 0, TLen: 0, Seq: read.Seq, Qual: string(read.Qual), Extra: "BZ:i:0"}
@@ -11,7 +17,7 @@ func gswNothingWorker(gg *SimpleGraph, seedHash map[uint64][]uint64, seedLen int
 	wg.Done()
 }
 
-func gswWorkerMemPool(gg *SimpleGraph, seedHash map[uint64][]uint64, seedLen int, stepSize int, incomingFastqs <-chan *fastq.FastqBig, outgoingSams chan<- *sam.SamAln, wg *sync.WaitGroup) {
+/*func gswWorkerMemPool(gg *SimpleGraph, seedHash map[uint64][]uint64, seedLen int, stepSize int, incomingFastqs <-chan *fastq.FastqBig, outgoingSams chan<- *sam.SamAln, wg *sync.WaitGroup) {
 	m, trace := swMatrixSetup(10000)
 	memChunk := make([]SeedDev, 100000)
 	for i := 0; i < len(memChunk)-1; i++ {
@@ -22,4 +28,4 @@ func gswWorkerMemPool(gg *SimpleGraph, seedHash map[uint64][]uint64, seedLen int
 		outgoingSams <- GraphSmithWatermanMemPool(gg, read, seedHash, seedLen, stepSize, m, trace, &memStart)
 	}
 	wg.Done()
-}
+}*/
