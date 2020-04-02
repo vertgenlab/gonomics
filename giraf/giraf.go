d2800397b8c9d802cc31d56fa59d8a9ae459b60d
Craig Lowe <craiglowe@users.noreply.github.com>
2020-04-01 21:35:43 -0400

    proposal for giraf format

---
+++ giraf/giraf.go
@@ -0,0 +1,34 @@
+package giraf
+
+import (
+	"github.com/vertgenlab/gonomics/cigar"
+)
+
+type Giraf struct {
+	QName     string
+	QStart    int
+	QEnd      int
+	PosStrand bool
+	Path      *Path
+	Aln       *cigar.Cigar
+	AlnScore  int
+	MapQ      uint8
+	Notes     []Note // Similar to sam, this is should be a list of notes.
+	// Each note should be of the form TAG:TYPE:VALUE
+	// TAG is two characters
+	// TYPE is a single character
+	// VALUE will be stored as a string and can then be de-coded based on type
+	// An example would be "BZ:4000
+}
+
+type Path struct {
+	TStart int      // The path starts on the TStart base (0-based, closed) of Nodes[0]
+	Nodes  []uint32 // The node Id/Index of all the nodes in the path
+	TEnd   int      // The path ends on the TEnd base (0-based, open) of Nodes[len(Nodes)-1]
+}
+
+type Note struct {
+	Tag   string
+	Type  rune
+	Value string
+}
