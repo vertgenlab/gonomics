788575814b4f79c161d5a56d575a16edd15299fd
das106 <das106@duke.edu>
2020-04-23 18:43:14 -0400

    sync function complete

--- alleles/sync.go
+++ alleles/sync.go
@@ -4,117 +4,64 @@ import (
 	"fmt"
 	"github.com/vertgenlab/gonomics/fasta"
 	"github.com/vertgenlab/gonomics/simpleGraph"
-	"sync"
 )
 
+// Syncs allele streams output from SamToAlleles and returns a []*Allele for each position
+//TODO: still needs to tested with graph input
 func SyncAlleleStreams(reference interface{}, alleleStreams ...<-chan *Allele) <-chan []*Allele {
 	fmt.Println("\nSTART SYNC ALLELES")
 	answer := make(chan []*Allele)
 
-	var currLoc *Location = &Location{"dummy", 0}
-
-	var receiveAlleles = make(chan *Allele)
-
-	var wg sync.WaitGroup
-	locks := make([]*bool, 0)
-	locker := sync.NewCond(&sync.Mutex{})
-
-	for _, singleStream := range alleleStreams {
-		var lock bool = false
-		go sendAlleles(singleStream, receiveAlleles, &lock, locker, &wg, currLoc)
-		locks = append(locks, &lock)
-	}
-
-	go syncAlleles(reference, receiveAlleles, answer, locks, locker, &wg, currLoc)
+	go syncAlleles(reference, alleleStreams, answer)
 
 	return answer
 }
 
-func sendAlleles(inStream <-chan *Allele, outStream chan<- *Allele, lock *bool, locker *sync.Cond, wg *sync.WaitGroup, currLoc *Location) {
-	for allele := range inStream {
-		// Loop forever until allele is sent on outStream
-		for {
-			if *lock == true  {
-				locker.L.Lock()
-				fmt.Println("waiting")
-				locker.Wait()
-			}
-			if allele.Location.Pos == currLoc.Pos && allele.Location.Chr == currLoc.Chr {
-				outStream <- allele
-				fmt.Println("sent", allele.Location)
-				break
-			} else {
-				// If allele is not ready to send then activate its lock and sleep
-				fmt.Println("locked on", allele.Location, currLoc)
-				*lock = true
-				//wg.Wait()
-			}
-		}
-	}
-}
-
-func syncAlleles(reference interface{}, outStream <-chan *Allele, answer chan<- []*Allele, locks []*bool, locker *sync.Cond, wg *sync.WaitGroup, currLoc *Location) {
-
-	var curr []*Allele
-	var k int
+func syncAlleles(reference interface{}, alleleStreams []<-chan *Allele, answer chan<- []*Allele) {
+	var i int
+	var ok bool
 	var refIdx int = 0
+	var currLoc *Allele = &Allele{Location: &Location{"dummy", 0}}
+	var currAlleles []*Allele = make([]*Allele, len(alleleStreams))
+	var currAnswer []*Allele
 
-	go func(){
-		for allele := range outStream {
-			curr = append(curr, allele)
-		}
-	}()
-
-	//locker.L.Lock()
 	for nextPos(reference, currLoc, &refIdx) {
-		fmt.Println("current pos", currLoc)
-		locker.Broadcast()
-		//locker.L.Unlock()
-		wg.Add(1)
-		for {
-			var readyToProgress bool = true
-			// Check to see if all workers are ready for next currLocation
-			for k = 0; k < len(locks); k++ {
-				if *locks[k] == false {
-					readyToProgress = false
-					break
-				}
+		for i = 0; i < len(alleleStreams); i++ {
+			if currAlleles[i] == nil {
+				currAlleles[i], ok = <-alleleStreams[i]
+				if !ok {continue}
 			}
-			// If all workers are ready, then reset the locks, send the allele, and progress
-			if readyToProgress == true {
-				fmt.Println("resetting locks")
-				for k = 0; k < len(locks); k++ {
-					*locks[k] = false
-				}
-
-				if curr != nil {
-					answer <- curr
-				}
-				curr = nil
-				wg.Done()
-				break
-			} else {locker.Broadcast()}
+			if *currAlleles[i].Location == *currLoc.Location {
+				currAnswer = append(currAnswer, currAlleles[i])
+				currAlleles[i] = nil
+			}
 		}
-		//locker.L.Lock()
+
+		if currAnswer != nil {
+			answer <- currAnswer
+		}
+		currAnswer = nil
 	}
+
+	close(answer)
 }
 
-func nextPos (reference interface{}, currLoc *Location, refIdx *int) bool {
+func nextPos (reference interface{}, currLoc *Allele, refIdx *int) bool {
 	switch r := reference.(type) {
 	case []*fasta.Fasta:
 
-		if currLoc.Chr == "dummy" {
-			currLoc.Chr = r[0].Name
-			currLoc.Pos = 0
+		if currLoc.Location.Chr == "dummy" {
+			currLoc.Location.Chr = r[0].Name
+			currLoc.Location.Pos = 0
 			return true
 		} else {
-			if int(currLoc.Pos) < len(r[*refIdx].Seq) { // Increment Position
-				currLoc.Pos++
+			if int(currLoc.Location.Pos) < len(r[*refIdx].Seq) { // Increment Position
+				currLoc.Location.Pos++
 				return true
-			} else if *refIdx < len(r){ // Increment Chromosome
+			} else if *refIdx < (len(r) - 1) { // Increment Chromosome
 				*refIdx++
-				currLoc.Chr = r[*refIdx].Name
-				currLoc.Pos = 0
+				currLoc.Location.Chr = r[*refIdx].Name
+				currLoc.Location.Pos = 0
 				return true
 			} else { // Report end of reference
 				return false
@@ -123,18 +70,18 @@ func nextPos (reference interface{}, currLoc *Location, refIdx *int) bool {
 
 	case *simpleGraph.SimpleGraph:
 
-		if currLoc.Chr == "dummy" {
-			currLoc.Chr = r.Nodes[0].Name
-			currLoc.Pos = 0
+		if currLoc.Location.Chr == "dummy" {
+			currLoc.Location.Chr = r.Nodes[0].Name
+			currLoc.Location.Pos = 0
 			return true
 		} else {
-			if int(currLoc.Pos) < len(r.Nodes[*refIdx].Seq) { // Increment Position
-				currLoc.Pos++
+			if int(currLoc.Location.Pos) < len(r.Nodes[*refIdx].Seq) { // Increment Position
+				currLoc.Location.Pos++
 				return true
 			} else if *refIdx < len(r.Nodes) { // Increment Node
 				*refIdx++
-				currLoc.Chr = r.Nodes[*refIdx].Name
-				currLoc.Pos = 0
+				currLoc.Location.Chr = r.Nodes[*refIdx].Name
+				currLoc.Location.Pos = 0
 				return true
 			} else { // Report end of reference
 				return false
--- alleles/sync_test.go
+++ alleles/sync_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestSyncAlleleStreams(t *testing.T) {
 	ref := fasta.Read("testdata/human_chrM.fasta")
-	//one := SamToAlleles("testdata/human_chrM.sam", ref, 0)
+	one := SamToAlleles("testdata/human_chrM.sam", ref, 0)
 	two := SamToAlleles("testdata/human_chrM.sam", ref, 0)
 
-	answer := SyncAlleleStreams(ref, two)
+	answer := SyncAlleleStreams(ref, one, two)
 
 	for i := range answer {
-		fmt.Println("finished", i[0].Location)
+		fmt.Println("finished", i[0].Location, len(i))
 	}
 }
