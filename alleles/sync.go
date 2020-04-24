3faab2a6d10664e4369bce18ffa4f94358ec7efb
das106 <das106@duke.edu>
2020-04-24 14:34:03 -0400

    added 2d slice for allele strorage and added comments

--- alleles/sync.go
+++ alleles/sync.go
@@ -1,39 +1,61 @@
 package alleles
 
 import (
-	"fmt"
 	"github.com/vertgenlab/gonomics/fasta"
 	"github.com/vertgenlab/gonomics/simpleGraph"
 )
 
-// Syncs allele streams output from SamToAlleles and returns a []*Allele for each position
+// Syncs allele streams output from SamToAlleles and returns a []*Allele with the Allele struct for each sample at position n
 //TODO: still needs to tested with graph input
-func SyncAlleleStreams(reference interface{}, alleleStreams ...<-chan *Allele) <-chan []*Allele {
-	fmt.Println("\nSTART SYNC ALLELES")
+func SyncAlleleStreams(reference interface{}, memBufferSize int, alleleStreams ...<-chan *Allele) <-chan []*Allele {
 	answer := make(chan []*Allele)
-
-	go syncAlleles(reference, alleleStreams, answer)
-
+	go syncAlleles(reference, memBufferSize, alleleStreams, answer)
 	return answer
 }
 
-func syncAlleles(reference interface{}, alleleStreams []<-chan *Allele, answer chan<- []*Allele) {
+func syncAlleles(reference interface{}, memBufferSize int, alleleStreams []<-chan *Allele, answer chan<- []*Allele) {
 	var i int
-	var ok bool
 	var refIdx int = 0
+	// To get alleles for each position we increment throught the reference (whether linear or graph) and request alleles for each position
+	// currLoc stores the current location we are asking for and begins with a dummy value to start for the nextPos function
 	var currLoc *Allele = &Allele{Location: &Location{"dummy", 0}}
-	var currAlleles []*Allele = make([]*Allele, len(alleleStreams))
+	// The currAlleles data structure is arranged as [SampleStream][Allele (length = memBufferSize)]
+	// currAlleles transiently stores data read from each alleleStream and allows for a variable buffer size depending on the desired memory allocation
+	var currAlleles [][]*Allele = make([][]*Allele, len(alleleStreams))
 	var currAnswer []*Allele
 
+	// Make the second dimension of currAlleles for each alleleStream
+	for i = 0; i < len(alleleStreams); i++ {
+		currAlleles[i] = make([]*Allele, 0)
+	}
+
+	// Loop through each position in the reference
 	for nextPos(reference, currLoc, &refIdx) {
+		// For each alleleStream
 		for i = 0; i < len(alleleStreams); i++ {
-			if currAlleles[i] == nil {
-				currAlleles[i], ok = <-alleleStreams[i]
-				if !ok {continue}
+			// If there is space in the memory buffer, read an allele from the stream and store it
+			if len(currAlleles[i]) < memBufferSize {
+				recdAllele, ok := <-alleleStreams[i]
+				if !ok { // If reached EOF and the alleleStream has closed
+					// AND all of the alleles from that stream have been sent
+					if len(currAlleles[i]) == 0 {
+						// Remove the closed alleleStream from the list and from currAlleles
+						alleleStreams = append(alleleStreams[:i], alleleStreams[i+1:]...)
+						currAlleles = append(currAlleles[:i], currAlleles[i+1:]...)
+						// Decrement i since the current alleleStream corresponding to i has been deleted
+						i--
+					}
+					continue
+				} else {
+					currAlleles[i] = append(currAlleles[i], recdAllele)
+				}
 			}
-			if *currAlleles[i].Location == *currLoc.Location {
-				currAnswer = append(currAnswer, currAlleles[i])
-				currAlleles[i] = nil
+
+			// If the location we are asking for is the same as the first allele stored in currAlleles[i]
+			if *currAlleles[i][0].Location == *currLoc.Location {
+				// Append value to answer and delete allele from currAlleles
+				currAnswer = append(currAnswer, currAlleles[i][0])
+				currAlleles[i] = currAlleles[i][1:]
 			}
 		}
 
@@ -46,6 +68,7 @@ func syncAlleles(reference interface{}, alleleStreams []<-chan *Allele, answer c
 	close(answer)
 }
 
+// Inputs a []*fasta.Fasta or a *simpleGraph.SimpleGraph and increments currLoc
 func nextPos (reference interface{}, currLoc *Allele, refIdx *int) bool {
 	switch r := reference.(type) {
 	case []*fasta.Fasta:
--- alleles/sync_test.go
+++ alleles/sync_test.go
@@ -9,9 +9,9 @@ import (
 func TestSyncAlleleStreams(t *testing.T) {
 	ref := fasta.Read("testdata/human_chrM.fasta")
 	one := SamToAlleles("testdata/human_chrM.sam", ref, 0)
-	two := SamToAlleles("testdata/human_chrM.sam", ref, 0)
+	two := SamToAlleles("testdata/chrM_big.sam", ref, 0)
 
-	answer := SyncAlleleStreams(ref, one, two)
+	answer := SyncAlleleStreams(ref, 10,  one, two)
 
 	for i := range answer {
 		fmt.Println("finished", i[0].Location, len(i))
