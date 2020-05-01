392f423b8259516ecca55d7b7d46bf51a01b8ff7
das106 <das106@duke.edu>
2020-05-01 17:01:16 -0400

    added testfile

---
+++ alleles/newVariation_test.go
@@ -0,0 +1,20 @@
+package alleles
+
+import (
+	"github.com/vertgenlab/gonomics/fasta"
+	"github.com/vertgenlab/gonomics/vcf"
+	"testing"
+)
+
+
+// TODO: build test for graph genome
+func TestFindNewVariation(t *testing.T) {
+	ref := fasta.Read("testdata/human_chrM.fasta")
+	experimental := "testdata/human_chrM.sam"
+	normal := "testdata/chrM_head.sam"
+	answer := FindNewVariation(ref, experimental, normal, 0, 1, 20, 100)
+
+	for data := range answer {
+		vcf.PrintSingleLine(data)
+	}
+}
