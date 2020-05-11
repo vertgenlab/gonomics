e8d76c039af9c8131ce841f519fba7df8ae74aae
Christi Fauci <christiana.fauci@duke.edu>
2020-05-11 11:08:42 -0400

    adding import line

--- dna/examine.go
+++ dna/examine.go
@@ -3,6 +3,7 @@ package dna
 import (
 	"fmt"
 	"github.com/vertgenlab/gonomics/common"
+	"log"
 )
 
 func Count(seq []Base) (ACount int, CCount int, GCount int, TCount int, NCount int, aCount int, cCount int, gCount int, tCount int, nCount int, gapCount int) {
