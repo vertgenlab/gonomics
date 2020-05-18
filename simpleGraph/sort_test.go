ceea31d9bc8ffb6adf5f96847b9e5710784f7421
das106 <das106@duke.edu>
2020-05-17 21:57:02 -0400

    suggested change

--- simpleGraph/sort_test.go
+++ simpleGraph/sort_test.go
@@ -11,9 +11,9 @@ func TestGetSortOrder(t *testing.T) {
 	nodesToSort := append(graph.Nodes)
 	order := breadthFirstSearch(nodesToSort)
 
-	correctOrder := []uint32{0,1,2,3,4}
+	correctOrder := []uint32{0, 1, 2, 3, 4}
 
-	for i := 0 ; i < len(order) ; i++ {
+	for i := 0; i < len(order); i++ {
 		if order[i] == correctOrder[i] {
 			continue
 		}
