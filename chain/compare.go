eaf40aba953c17bd9308107bc2f611950d1c1c87
edotau <eric.au@duke.edu>
2020-05-20 09:27:22 -0700

    another attempt to separate ggtools

---
+++ chain/compare.go
@@ -0,0 +1,104 @@
+package chain
+
+import (
+	"sort"
+	"strings"
+)
+
+//Uses bool to compare target or query coordinates as one function
+//true is for target, false is for query
+func compareStartCoord(a *Chain, b *Chain, whichGenome bool) int {
+	if whichGenome {
+		sameChr := strings.Compare(a.TName, b.TName)
+		if sameChr != 0 {
+			return sameChr
+		}
+		if a.TStart < b.TStart {
+			return -1
+		}
+		if a.TStart > b.TStart {
+			return 1
+		}
+	} else {
+		sameChr := strings.Compare(a.QName, b.QName)
+		if sameChr != 0 {
+			return sameChr
+		}
+		if a.QStart < b.QStart {
+			return -1
+		}
+		if a.QStart > b.QStart {
+			return 1
+		}
+	}
+	return 0
+}
+
+//true/false bool to either sort by target or query
+//true=target, false=query
+func SortByCoordinates(align []*Chain, whichGenome bool) {
+	sort.Slice(align, func(i, j int) bool { return compareStartCoord(align[i], align[j], whichGenome) == -1 })
+}
+
+func compareScores(a *Chain, b *Chain) int {
+	if a.Score < b.Score {
+		return -1
+	}
+	if a.Score > b.Score {
+		return -1
+	}
+	return 0
+}
+
+func CompareName(alpha string, beta string) int {
+	return strings.Compare(alpha, beta)
+}
+
+func Equal(a []*Chain, b []*Chain) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if !sameRecord(a[i], b[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func sameRecord(a *Chain, b *Chain) bool {
+	if strings.Compare(a.TName, b.TName) != 0 {
+		return false
+	}
+	if a.TSize != b.TSize {
+		return false
+	}
+	if a.TStrand != b.TStrand {
+		return false
+	}
+	if a.TStart != b.TStart {
+		return false
+	}
+	if a.TEnd != b.TEnd {
+		return false
+	}
+	if strings.Compare(a.QName, b.QName) != 0 {
+		return false
+	}
+	if a.QSize != b.QSize {
+		return false
+	}
+	if a.QStrand != b.QStrand {
+		return false
+	}
+	if a.QStart != b.QStart {
+		return false
+	}
+	if a.QEnd != b.QEnd {
+		return false
+	}
+	if a.Id != b.Id {
+		return false
+	}
+	return true
+}
---
+++ chain/toolsToChain.go
@@ -0,0 +1,32 @@
+package chain
+
+import (
+	"github.com/vertgenlab/gonomics/bed"
+	"github.com/vertgenlab/gonomics/common"
+	"strings"
+)
+//TODO: Will move to the overlap interface once we have that set up, essentially all the functions 
+//if target bool is true, we select the target/refernce regions, if target bool is false we use the query
+func OverlapChainBed(alpha *Chain, beta *bed.Bed, target bool) bool {
+	if target {
+		if (common.MaxInt64(int64(alpha.TStart), beta.ChromStart) < common.MinInt64(int64(alpha.TEnd), beta.ChromEnd)) && strings.Compare(alpha.TName, beta.Chrom) == 0 {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		if (common.MaxInt64(int64(alpha.QStart), beta.ChromStart) < common.MinInt64(int64(alpha.QEnd), beta.ChromEnd)) && strings.Compare(alpha.QName, beta.Chrom) == 0 {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+func ChainToBed(ch *Chain, target bool) *bed.Bed {
+	if target {
+		return &bed.Bed{Chrom: ch.TName, ChromStart: int64(ch.TStart), ChromEnd: int64(ch.TEnd), Name: ch.QName, Score: int64(ch.Score)}
+	} else {
+		return &bed.Bed{Chrom: ch.QName, ChromStart: int64(ch.QStart), ChromEnd: int64(ch.QEnd), Name: ch.TName, Score: int64(ch.Score)}
+	}
+}
