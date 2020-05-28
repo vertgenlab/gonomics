a2e13c289913456d4c352a59fa3fe96ffaf80eba
das106 <das106@duke.edu>
2020-05-28 17:05:59 -0400

    more robust test

--- sort/mergeSort_test.go
+++ sort/mergeSort_test.go
@@ -1,7 +1,18 @@
 package sort
 
-import "testing"
+import (
+	"github.com/vertgenlab/gonomics/vcf"
+	"testing"
+)
 
+//TODO: add test function for each data type
 func TestExternalMergeSort(t *testing.T) {
 	ExternalMergeSort("testdata/test.vcf", 100, "tmp", "sorted.vcf")
+	sorted := vcf.Read("sorted.vcf")
+	test := vcf.ByGenomicCoordinates(sorted)
+	for i := 1; i < len(sorted); i++ {
+		if !test.Less(i-1, i) {
+			t.Errorf("ERROR: Problem with external merge sort of vcf files: \n %v \n is not less than \n %v", sorted[i-1], sorted[i])
+		}
+	}
 }
