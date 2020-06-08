aa486067e34d8dc44cb23e456367d59da771df86
edotau <eric.au@duke.edu>
2020-06-08 16:59:45 -0700

    fixed bug introduced after comments from code review

--- axt/axtToSam.go
+++ axt/axtToSam.go
@@ -21,7 +21,7 @@ func AxtToSam(axtFmt *Axt) *sam.SamAln {
 		Cigar: PairSeqToCigar(axtFmt.RSeq, axtFmt.QSeq),
 		RNext: "*",
 		PNext: 0,
-		TLen:  axtFmt.REnd - axtFmt.REnd, //Could leave at zero or make TLen be the length of alignment, start and end (not sure if i can get target length from an axt)
+		TLen:  axtFmt.REnd - axtFmt.RStart, //Could leave at zero or make TLen be the length of alignment, start and end (not sure if i can get target length from an axt)
 		Seq:   dna.RemoveBase(axtFmt.QSeq, dna.Gap),
 		Qual:  "*",
 		Extra: fmt.Sprintf("AS:i:%d\tXS:i:%d\tXE:i:%d", axtFmt.Score, axtFmt.QStart, axtFmt.QEnd),
@@ -37,12 +37,14 @@ func PairSeqToCigar(a []dna.Base, b []dna.Base) []*cigar.Cigar {
 	curr := &cigar.Cigar{}
 	var i int64
 	for i = 0; i < int64(len(a)); i++ {
+		//Removing the extra case statements introduced a bug in real data
+		//TODO: add another test to catch this
 		switch true {
-		case a[i] == b[i] && a[i] != dna.Gap || b[i] != dna.Gap:
+		case a[i] == b[i] && (a[i] != dna.Gap || b[i] != dna.Gap):
 			curr = equalMatchCigar(a, b, i)
 			i += curr.RunLength - 1
 			align = append(align, curr)
-		case a[i] != b[i] && a[i] != dna.Gap && b[i] != dna.Gap:
+		case a[i] != b[i] && (a[i] != dna.Gap || b[i] != dna.Gap):
 			curr = diffMatchCigar(a, b, i)
 			i += curr.RunLength - 1
 			align = append(align, curr)
@@ -108,6 +110,7 @@ func insertCigar(a []dna.Base, b []dna.Base, index int64) *cigar.Cigar {
 	}
 	return insertion
 }
+
 func deletionCigar(a []dna.Base, b []dna.Base, index int64) *cigar.Cigar {
 	deletion := &cigar.Cigar{Op: 'D', RunLength: 1}
 	var i int64
@@ -122,18 +125,11 @@ func deletionCigar(a []dna.Base, b []dna.Base, index int64) *cigar.Cigar {
 	return deletion
 }
 
-func generateFakeQual(length int) []rune {
-	var answer []rune = make([]rune, length)
-	for i := 0; i < length; i++ {
-		answer[i] = 'J'
-	}
-	return answer
-}
-
 func setStrandFlag(strand bool) int64 {
 	if strand {
 		return 0
 	} else {
 		return 16
 	}
+
 }
