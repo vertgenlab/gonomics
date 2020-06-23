410db9b4f770566028dd10dcae5e96e8ece5a8a3
rimangan <riley.mangan@duke.edu>
2020-06-23 12:11:46 -0400

    Bool arrays for gtf

---
+++ gtf/arrays.go
@@ -0,0 +1,90 @@
+package gtf
+
+import (
+	"github.com/vertgenlab/gonomics/chromInfo"
+	"github.com/vertgenlab/gonomics/vcf"
+	)
+
+func VariantArrayOverlap(v *vcf.Vcf, a map[string][]bool) bool {
+	return a[v.Chr][v.Pos - 1]
+}
+
+//returns a map of chromosome names to bool arrays, true if that position lies in an exon
+func ExonBoolArray(g map[string]*Gene, c map[string]*chromInfo.ChromInfo) map[string][]bool {
+	var answer map[string][]bool
+	for k, _ := range c {
+		answer[k] = make([]bool, c[k].Size)
+	}
+
+	for k, _ := range g {
+		for i := 0; i < len(g[k].Transcripts); i++ {
+			for j := 0; j < len(g[k].Transcripts[i].Exons); j++ {
+				//End is not minus 1 because the 1 basing is cancelled out by the closed right interval of gtf.
+				for m := g[k].Transcripts[i].Exons[j].Start - 1; m < g[k].Transcripts[i].Exons[j].End; m++ {
+					answer[k][m] = true
+				}
+			}
+		}
+	}
+	return answer
+}
+
+func CdsBoolArray(g map[string]*Gene, c map[string]*chromInfo.ChromInfo) map[string][]bool {
+	var answer map[string][]bool
+	for k, _ := range c {
+		answer[k] = make([]bool, c[k].Size)
+	}
+
+	for k, _ := range g {
+		for i := 0; i < len(g[k].Transcripts); i++ {
+			for j := 0; j < len(g[k].Transcripts[i].Exons); j++ {
+				if g[k].Transcripts[i].Exons[j].Cds != nil {
+					for m := g[k].Transcripts[i].Exons[j].Cds.Start - 1; m < g[k].Transcripts[i].Exons[j].Cds.End; m++ {
+					answer[k][m] = true
+					}
+				}
+			}
+		}
+	}
+	return answer
+}
+
+func FiveUtrBoolArray(g map[string]*Gene, c map[string]*chromInfo.ChromInfo) map[string][]bool {
+	var answer map[string][]bool
+	for k, _ := range c {
+		answer[k] = make([]bool, c[k].Size)
+	}
+
+	for k, _ := range g {
+		for i := 0; i < len(g[k].Transcripts); i++ {
+			for j := 0; j < len(g[k].Transcripts[i].Exons); j++ {
+				if g[k].Transcripts[i].Exons[j].FiveUtr != nil {
+					for m := g[k].Transcripts[i].Exons[j].FiveUtr.Start - 1; m < g[k].Transcripts[i].Exons[j].FiveUtr.End; m++ {
+					answer[k][m] = true
+					}
+				}
+			}
+		}
+	}
+	return answer
+}
+
+func ThreeUtrBoolArray(g map[string]*Gene, c map[string]*chromInfo.ChromInfo) map[string][]bool {
+	var answer map[string][]bool
+	for k, _ := range c {
+		answer[k] = make([]bool, c[k].Size)
+	}
+
+	for k, _ := range g {
+		for i := 0; i < len(g[k].Transcripts); i++ {
+			for j := 0; j < len(g[k].Transcripts[i].Exons); j++ {
+				if g[k].Transcripts[i].Exons[j].ThreeUtr != nil {
+					for m := g[k].Transcripts[i].Exons[j].ThreeUtr.Start - 1; m < g[k].Transcripts[i].Exons[j].ThreeUtr.End; m++ {
+					answer[k][m] = true
+					}
+				}
+			}
+		}
+	}
+	return answer
+}
---
+++ gtf/filter.go
@@ -0,0 +1,54 @@
+package gtf
+
+import (
+	"github.com/vertgenlab/gonomics/vcf"
+	"github.com/vertgenlab/gonomics/chromInfo"
+	"github.com/vertgenlab/gonomics/fasta"
+	"github.com/vertgenlab/gonomics/dna"
+
+)
+
+func FilterVariantGtf(v *vcf.Vcf, g map[string]*Gene, c map[string]*chromInfo.ChromInfo, exon bool, code bool, five bool, three bool) bool {
+	if exon {
+		if !FilterVariantExon(v, g, c) {
+			return false
+		}
+	}
+	if code {
+		if !FilterVariantCds(v, g, c) {
+			return false
+		}
+	}
+	if three {
+		if !FilterVariantThreeUtr(v, g, c) {
+			return false
+		}
+	}
+	if five {
+		if !FilterVariantFiveUtr(v, g, c) {
+			return false
+		}
+	}
+	return true
+}
+
+func FilterVariantExon(v *vcf.Vcf, g map[string]*Gene, c map[string]*chromInfo.ChromInfo) bool {
+	a := ExonBoolArray(g, c)
+	return VariantArrayOverlap(v, a)
+}
+
+func FilterVariantCds(v *vcf.Vcf, g map[string]*Gene, e bool, c map[string]*chromInfo.ChromInfo) bool {
+	a := CdsBoolArray(g, c)
+	return VariantArrayOverlap(v, a)
+}
+
+func FilterVariantThreeUtr(v *vcf.Vcf, g map[string]*Gene, e bool, c map[string]*chromInfo.ChromInfo) bool {
+	a := ThreeUtrBoolArray(g, c)
+	return VariantArrayOverlap(v, a)
+}
+
+func FilterVariantFiveUtr(v *vcf.Vcf, g map[string]*Gene, e bool, c map[string]*chromInfo.ChromInfo) bool {
+	a := FiveUtrBoolArray(g, c)
+	return VariantArrayOverlap(v, a)
+}
+
