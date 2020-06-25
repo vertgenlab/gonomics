c99b43df13236a49172cd51e8a0ecc4400128c6f
edotau <eric.au@duke.edu>
2020-06-25 13:56:44 -0700

    axt to fasta works for trivial

--- cmd/axTools/axTools.go
+++ cmd/axTools/axTools.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"github.com/vertgenlab/gonomics/axt"
 	"github.com/vertgenlab/gonomics/dna"
-	//"github.com/vertgenlab/gonomics/fasta"
+	"github.com/vertgenlab/gonomics/fasta"
 	"github.com/vertgenlab/gonomics/fileio"
 	"log"
 )
@@ -21,8 +21,9 @@ func usage() {
 
 func main() {
 	var expectedNumArgs int = 2
-	var targetGaps *bool = flag.Bool("tGap", false, "Return `.axt` alignments when query contains Ns, but target does not")
-	var queryGaps *bool = flag.Bool("qGap", false, "Return '`axt` alignments when target contains Ns, but query does not")
+
+	var targetGaps *bool = flag.Bool("tGap", false, "Return '`axt` alignments when target contains Ns, but query does not")
+	var queryGaps *bool = flag.Bool("qGap", false, "Return `.axt` alignments when query contains Ns, but target does not")
 	//var concensus *bool = flag.Bool("concensus", false, "Output `.fasta` consensus sequence based on the axt alignment")
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime)
@@ -59,7 +60,7 @@ func searchAxt(input string, output string, target *bool, query *bool) {
 	}
 }
 
-func axtQueryGap(record *axt.Axt) bool {
+func axtTargetGap(record *axt.Axt) bool {
 	log.SetFlags(0)
 	if dna.CountBase(record.RSeq, dna.N) != 0 && dna.CountBase(record.QSeq, dna.N) == 0 {
 		//log.Printf("%s", axt.ToString(record, 0))
@@ -69,7 +70,7 @@ func axtQueryGap(record *axt.Axt) bool {
 	}
 }
 
-func axtTargetGap(record *axt.Axt) bool {
+func axtQueryGap(record *axt.Axt) bool {
 	log.SetFlags(0)
 	if dna.CountBase(record.QSeq, dna.N) != 0 && dna.CountBase(record.RSeq, dna.N) == 0 {
 		//log.Printf("%s", axt.ToString(record, 0))
@@ -80,7 +81,7 @@ func axtTargetGap(record *axt.Axt) bool {
 }
 
 //TODO: Coming soon: axt alignment to Fasta
-/*
+
 func axtToFa(input string, output string) {
 	file := fileio.EasyOpen(input)
 	reader := make(chan *axt.Axt)
@@ -88,20 +89,31 @@ func axtToFa(input string, output string) {
 	go axt.ReadToChan(file, reader)
 
 	for each := range reader {
-		fasta.WriteFasta(writer, axtSeq(each), 50)
+		fasta.WriteFasta(writer, axtSeq(each, nil), 50)
 	}
 }
+
 //if target sequence contains Ns, uses query non N bases to fill Ns
-func axtSeq(axtRecord *axt.Axt) *fasta.Fasta {
-	var concensus *fasta.Fasta = &fasta.Fasta{}
-	concensus.Name = fmt.Sprintf("%s_%d_%d", axtRecord.RName, axtRecord.RStart, axtRecord.REnd)
+func axtSeq(axtRecord *axt.Axt, fa *fasta.Fasta) *fasta.Fasta {
+	concensus := &fasta.Fasta{
+		Name: fmt.Sprintf("%s", axtRecord.RName),
+		Seq:  make([]dna.Base, 0, len(fa.Seq)),
+	}
 
-	concensus.Seq = axtRecord.QSeq
+	concensus.Seq = append(concensus.Seq, fa.Seq[:axtRecord.RStart-1]...)
 
-	for i := 0; i < len(concensus.Seq); i++ {
-		if concensus.Seq[i] == dna.Gap && axtRecord.QSeq[i] != dna.Gap {
-			concensus.Seq[i] = axtRecord.QSeq[i]
+	for i := 0; i < len(axtRecord.RSeq); i++ {
+		if axtRecord.RSeq[i] == dna.N && axtRecord.QSeq[i] != dna.N {
+			concensus.Seq = append(concensus.Seq, axtRecord.QSeq[i])
+		} else {
+			concensus.Seq = append(concensus.Seq, axtRecord.RSeq[i])
 		}
 	}
+
+	concensus.Seq = append(concensus.Seq, fa.Seq[axtRecord.REnd:]...)
+
+	if len(concensus.Seq) != len(fa.Seq) {
+		log.Fatalf("Error: Sequence length is not the same...\n")
+	}
 	return concensus
-}*/
+}
---
+++ cmd/axTools/axTools_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/axt"
+	"github.com/vertgenlab/gonomics/fasta"
+	"log"
+	"testing"
+
+	"github.com/vertgenlab/gonomics/dna"
+)
+
+func TestAxtToFasta(t *testing.T) {
+	log.SetFlags(0)
+	var testAxt = &axt.Axt{
+		RName:      "chr19",
+		RStart:     11,
+		REnd:       40,
+		QName:      "chr11",
+		QStart:     2,
+		QEnd:       31,
+		QStrandPos: false,
+		Score:      3500,
+		RSeq:       dna.StringToBases("TCTGNNNNTAAACCACCNNNNATGACAAGC"),
+		QSeq:       dna.StringToBases("TCAGCTCATAAATCACCTCCCATGACAAGC"),
+	}
+	var testFa = &fasta.Fasta{
+		Name: "chr19",
+		Seq:  dna.StringToBases("AAAAAAAAAATCTGNNNNTAAACCACCNNNNATGACAAGC"),
+	}
+
+	seq := axtSeq(testAxt, testFa)
+	log.Printf("%s %s\n", seq.Name, dna.BasesToString(seq.Seq))
+}
