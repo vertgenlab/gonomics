cc652253257aaa89fd4674d2f8504fd28f1fc168
edotau <eric.au@duke.edu>
2020-06-27 18:24:17 -0700

    Combined start and end trim coordinates

--- cmd/faTools/faTools.go
+++ cmd/faTools/faTools.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/vertgenlab/gonomics/common"
 	"github.com/vertgenlab/gonomics/fasta"
 	"github.com/vertgenlab/gonomics/fileio"
 	"log"
@@ -22,9 +23,8 @@ func usage() {
 func main() {
 	var expectedNumArgs int = 2
 	var sortMethod *string = flag.String("sort", "", "sort fasta sequences either `byname or bylen`")
-	var faTrim *bool = flag.Bool("trim", false, "return a fragment trimmed by providing `start end` coordinates")
-	var start *int = flag.Int("start", 0, "starting base postion for fasta trim zero based, left closed")
-	var end *int = flag.Int("end", 0, "ending base postion for fasta trim right open")
+	var faTrim *string = flag.String("trim", "", "return a fragment trimmed by providing `start:end` coordinates, zero based, left closed, right open")
+	//var window *string = flag.String("window", "", "start and end base postion for fasta trim zero based, left closed, right open `start:end`")
 	var rename *string = flag.String("rename", "", "provide a prefix to edit the names fasta records")
 
 	flag.Usage = usage
@@ -36,10 +36,10 @@ func main() {
 	}
 	inputFile, outputFile := flag.Arg(0), flag.Arg(1)
 	switch true {
-	case *faTrim:
+	case *faTrim != "":
 		//TODO: figure out what is better, create new tags for start and end or just have them as extra arguments
 		//decided to go with args because it will thow an error if user does not input an int whereas I might have to catch if it is passed in as a string
-		faSubsetFrag(inputFile, outputFile, *start, *end)
+		faSubsetFrag(inputFile, outputFile, *faTrim)
 	case *sortMethod != "":
 		faSortRecords(inputFile, outputFile, *sortMethod)
 	case *rename != "":
@@ -65,13 +65,14 @@ func faSortRecords(inFile string, outFile string, method string) {
 	fasta.WriteToFileHandle(file.File, fa, 50)
 }
 
-func faSubsetFrag(inputFile string, outputFile string, start int, end int) {
+func faSubsetFrag(inputFile string, outputFile string, window string) {
 	faSeq := fasta.Read(inputFile)
 	writer := fileio.EasyCreate(outputFile)
+	pos := strings.Split(window, ":")
 	if len(faSeq) != 1 {
 		log.Fatalf("Error: Must provide a single fasta sequence for fragment trimming")
 	}
-	fasta.WriteToFileHandle(writer, []*fasta.Fasta{TrimFasta(faSeq[0], start, end)}, 50)
+	fasta.WriteToFileHandle(writer, []*fasta.Fasta{TrimFasta(faSeq[0], common.StringToInt(pos[0]), common.StringToInt(pos[1]))}, 50)
 }
 
 func setNewPrefix(inputFile string, outputFile string, prefix string) {
