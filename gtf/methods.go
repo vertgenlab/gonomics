ce6fe74781ee8c8c640c5f91b3eec27028bf804e
das106 <das106@duke.edu>
2020-07-06 15:39:28 -0400

    progress

--- gtf/methods.go
+++ gtf/methods.go
@@ -1,9 +1,23 @@
 package gtf
 
-func (t *Transcript) GetChrom() string {
-	return t.Chr
+import (
+	"github.com/vertgenlab/gonomics/common"
+	"github.com/vertgenlab/gonomics/fileio"
+)
+
+func (g *Gene) GetChrom() string {
+	return g.Transcripts[0].Chr
 }
 
-func (t *Transcript) GetChromStart() int {
-	t.Start - 1
+func (g *Gene) GetChromStart() int {
+	return g.Transcripts[0].Start - 1
+}
+
+func (g *Gene) GetChromEnd() int {
+	return g.Transcripts[0].End
+}
+
+func (g *Gene) WriteToFileHandle(file *fileio.EasyWriter) {
+	err := WriteToFileHandle(file, g)
+	common.ExitIfError(err)
 }
--- gtf/variant.go
+++ gtf/variant.go
@@ -1,28 +1,134 @@
 package gtf
 
 import (
+	"errors"
+	"fmt"
 	"github.com/vertgenlab/gonomics/dna"
+	"github.com/vertgenlab/gonomics/interval"
 	"github.com/vertgenlab/gonomics/vcf"
+	"math"
 )
 
 type Variant struct {
 	vcf.Vcf
-	RefId   string // e.g. NC_000023.10, LRG_199, NG_012232.1, NM_004006.2, LRG-199t1, NR_002196.1, NP_003997.1, etc.
-	Gene    string
-	CDNAPos int
-	AAPos   int
-	AARef   []dna.AminoAcid
-	AAAlt   []dna.AminoAcid
+	RefId      string // e.g. NC_000023.10, LRG_199, NG_012232.1, NM_004006.2, LRG-199t1, NR_002196.1, NP_003997.1, etc.
+	Gene       string
+	NearestCDS *CDS
+	CDNAPos    int // 1-base
+	AAPos      int // 1-base
+	AARef      []dna.AminoAcid
+	AAAlt      []dna.AminoAcid
 }
 
-func VcfToVariant(v *vcf.Vcf, genes map[string]*Gene) *Variant {
-	answer := new(Variant)
+func GenesToIntervalTree(genes map[string]*Gene) map[string]*interval.IntervalNode {
 	MoveAllCanonicalToZero(genes)
+	intervals := make([]interval.Interval, len(genes))
+	var i int = 0
+	for k, g := range genes {
+		intervals[i] = g
+		delete(genes, k)
+		i++
+	}
+	return interval.BuildTree(intervals)
+}
 
+func VcfToVariant(v *vcf.Vcf, tree map[string]*interval.IntervalNode, seq map[string][]dna.Base) (*Variant, error) {
+	var answer *Variant
+	var err error
+	overlappingGenes := interval.Query(tree, v, "any")
+
+	if len(overlappingGenes) > 1 {
+		err = errors.New(fmt.Sprintf("Variant overlaps with multiple genes. Mutation will be based on first gene."))
+	}
+
+	var annotatingGene *Gene
+	if len(overlappingGenes) > 0 {
+		annotatingGene = overlappingGenes[0].(*Gene)
+	}
+
+	answer = vcfToVariant(v, annotatingGene, seq)
+
+	return answer, err
+}
+
+func vcfToVariant(v *vcf.Vcf, gene *Gene, seq map[string][]dna.Base) *Variant {
+	answer := new(Variant)
+	answer.Vcf = *v
+	answer.RefId = gene.Transcripts[0].TranscriptID
+	answer.Gene = gene.GeneID
+	vcfCdsIntersect(v, gene, answer)
+	if int(v.Pos) >= answer.NearestCDS.Start && int(v.Pos) <= answer.NearestCDS.End {
+		findAAChange(answer, seq)
+	}
 	return answer
 }
 
-func VariantToHGVS(variant *Variant) string {
-
+func vcfCdsIntersect(v *vcf.Vcf, gene *Gene, answer *Variant) {
+	var cdsPos int
+	var exon *Exon
+	for i := 0; i < len(gene.Transcripts[0].Exons); i++ {
+		exon = gene.Transcripts[0].Exons[i]
+		if exon.Cds != nil && int(v.Pos) > exon.Cds.End { // variant is further in gene
+			cdsPos += exon.Cds.End - exon.Cds.Start + 1
+		} else if exon.Cds != nil && int(v.Pos) <= exon.Cds.End { // variant is before end of this exon
+			if int(v.Pos) < exon.Cds.Start { // Variant is NOT in CDS
+				if exon.Cds.Start-int(v.Pos) < int(v.Pos)-gene.Transcripts[0].Exons[i-1].Cds.Start {
+					answer.NearestCDS = exon.Cds
+				} else {
+					answer.NearestCDS = gene.Transcripts[0].Exons[i-1].Cds
+				}
+				break
+			}
+			cdsPos += int(v.Pos) - exon.Cds.Start + 1
+			answer.CDNAPos = cdsPos
+			answer.NearestCDS = exon.Cds
+		}
+	}
 }
 
+func findAAChange(variant *Variant, seq map[string][]dna.Base) {
+	var refBases = make([]dna.Base, 0)
+	var altBases = make([]dna.Base, 0)
+	var seqPos int = int(variant.Pos) - 1
+	var currCDS *CDS = variant.NearestCDS
+	seqPos -= determineFrame(variant)
+	for ;seqPos < 0; seqPos++ {
+		if seqPos < currCDS.Start -1 {
+			seqPos = currCDS.Prev.End - 1
+			currCDS = currCDS.Prev
+		} else if seqPos > currCDS.End - 1 {
+			seqPos = currCDS.Next.Start - 1
+			currCDS = currCDS.Next
+		}
+		refBases = append(refBases, seq[variant.Chr][seqPos])
+		altBases = append(altBases, seq[variant.Chr][seqPos])
+	}
+	refBases = append(refBases, dna.StringToBases(variant.Ref)...)
+	altBases = append(altBases, dna.StringToBases(variant.Alt)...)
+	seqPos += len(dna.StringToBases(variant.Ref))
+	for offset := 1; len(refBases)%3 != 0; offset++ {
+		if seqPos + offset > currCDS.End - 1 {
+			seqPos = currCDS.Next.Start - 1
+			currCDS = currCDS.Next
+		}
+		refBases = append(refBases, seq[variant.Chr][seqPos + offset])
+	}
+	for offset := 1; len(altBases)%3 != 0; offset++ {
+		if seqPos + offset > currCDS.End - 1 {
+			seqPos = currCDS.Next.Start - 1
+			currCDS = currCDS.Next
+		}
+		altBases = append(altBases, seq[variant.Chr][seqPos + offset])
+	}
+	variant.AARef = dna.TranslateSeq(refBases)
+	variant.AAAlt = dna.TranslateSeq(altBases)
+	variant.AAPos = int(math.Round((float64(variant.CDNAPos)/3) + 0.4)) // Add 0.4 so pos will always round up
+}
+
+func determineFrame(v *Variant) int {
+	return (int(v.Pos) - v.NearestCDS.Start)%3 + v.NearestCDS.Frame
+}
+
+func VariantToHGVS(variant *Variant) string {
+	return ""
+}
