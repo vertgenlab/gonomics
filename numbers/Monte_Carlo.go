1741f317bda4b9aabf7a608601b331fd532371a1
rimangan <riley.mangan@duke.edu>
2020-07-15 14:30:22 -0400

    Moved declarations in rejection sampling code

--- numbers/Monte_Carlo.go
+++ numbers/Monte_Carlo.go
@@ -14,10 +14,11 @@ func SampleInverseNormal(mu float64, sigma float64) float64 {
 //pass in support of function
 //truncate for infinite functions
 func RejectionSample(xleft float64, xright float64, f func(float64) float64, maxIteration int) float64 {
+	var x, y, yRand float64
 	for i := 0; i < maxIteration; i++ {
-		x := rand.Float64() //rand float64 in range xleft to xright
-		y := f(x)
-		yRand := rand.Float64() //accept reject random variable
+		x = rand.Float64() //rand float64 in range xleft to xright
+		y = f(x)
+		yRand = rand.Float64() //accept reject random variable
 		if yRand < y {
 			return y
 		}
