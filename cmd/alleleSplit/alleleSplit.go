d0beb6964a183048570d145868424f1d17697395
edotau <eric.au@duke.edu>
2020-07-15 02:12:18 -0700

    updated and commented out told allele split function, did not delete because there might be some useful code

--- cmd/alleleSplit/alleleSplit.go
+++ cmd/alleleSplit/alleleSplit.go
@@ -8,20 +8,15 @@ import (
 	"github.com/vertgenlab/gonomics/sam"
 	"github.com/vertgenlab/gonomics/vcf"
 	"log"
+	//"sync"
 )
 
 func usage() {
 	fmt.Print(
-		"alleleSplit - a tool that separates a heterozygous sam alignment into different alleles\n" +
-			"1) Takes sam file of aligned reads\n2)Vcf file containing SNPs\n3)Name of output files\n\n" +
-			"usage:\n" +
-			"./alleleSplit [options] input.sam input.vcf output.sam\n" +
-			"\t--ref ref.output\n" +
-			"\t\tname output ref allele sam file\n" +
-			"\t--alt alt.output\n" +
-			"\t\tname output alt allele sam file\n" +
-			"\t--undetermined undetermined.sam\n" +
-			"\t\toutputs third file containing reads discarded from analysis\n")
+		"alleleSplit - a tool that separates a heterozygous sam alignment into different alignments by alleles\n" +
+			"Usage:\n" +
+			"./alleleSplit [options] input.sam input.vcf output.sam\n\n")
+	flag.PrintDefaults()
 }
 
 type Alleles struct {
@@ -54,7 +49,7 @@ func AlleleMap(v []*vcf.Vcf) map[Location]Alleles {
 	return aMap
 }
 
-func AlleleExpression(samFilename string, v []*vcf.Vcf, fileName string, ref string, alt string, unFlag bool) {
+func BasicAlleleExpression(samFilename string, v []*vcf.Vcf, fileName string, ref string, alt string) {
 	var aln *sam.SamAln = nil
 	var done bool = false
 	//var err error
@@ -70,12 +65,12 @@ func AlleleExpression(samFilename string, v []*vcf.Vcf, fileName string, ref str
 	defer altFile.Close()
 	sam.WriteHeaderToFileHandle(refFile, header)
 	sam.WriteHeaderToFileHandle(altFile, header)
-	var un *fileio.EasyWriter = nil
-	if unFlag {
-		un = fileio.EasyCreate(fileName + "_undetermined.sam")
-		defer un.Close()
-		sam.WriteHeaderToFileHandle(un, header)
-	}
+	//var un *fileio.EasyWriter = nil
+	//if unFlag {
+	//	un = fileio.EasyCreate(fileName + "_undetermined.sam")
+	//	defer un.Close()
+	//	sam.WriteHeaderToFileHandle(un, header)
+	//}
 	var aMap = AlleleMap(v)
 
 	var j, refCount, altCount int
@@ -115,8 +110,8 @@ func AlleleExpression(samFilename string, v []*vcf.Vcf, fileName string, ref str
 			sam.WriteAlnToFileHandle(refFile, aln)
 		} else if altCount > refCount {
 			sam.WriteAlnToFileHandle(altFile, aln)
-		} else if un != nil {
-			sam.WriteAlnToFileHandle(un, aln)
+			//} else if un != nil {
+			//sam.WriteAlnToFileHandle(un, aln)
 		} else {
 			//Skip read
 		}
@@ -124,17 +119,163 @@ func AlleleExpression(samFilename string, v []*vcf.Vcf, fileName string, ref str
 }
 
 func main() {
-	var expectedNumArgs int = 3
+	var expectedNumArgs int = 2
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
-	var refPrefix *string = flag.String("ref", "ref", "name of ref allele")
-	var altPrefix *string = flag.String("alt", "alt", "name of alt allele")
-	var undetermined *bool = flag.Bool("undetermined", false, "undetermined.sam")
+	//var refPrefix *string = flag.String("ref", "ref", "name of ref allele")
+	//var altPrefix *string = flag.String("alt", "alt", "name of alt allele")
+	//var undetermined *bool = flag.Bool("undetermined", false, "undetermined.sam")
+	var parental arrayFlags
+	flag.Var(&parental, "parent", "Define of two parential that appears homozygous in the genotype Vcf. Must match Vcf Header exactly and include second `name -parent two -f1 name`")
+	var f1Genome *string = flag.String("f1", "", "F1 hybrid sample that appears heterozygous in genotype Vcf. Must match Vcf Header exactly and include `name -parent one -parent two`")
+	//var prefix *string = flag.String("prefix", "", "set prefix for output sam files``")
+
+	var sampleName *bool = flag.Bool("samples", false, "Get names of samples that appear in Vcf header. (Default: /dev/stdout)")
+
 	flag.Parse()
 	if len(flag.Args()) != expectedNumArgs {
 		flag.Usage()
 		log.Fatalf("\n\n./alleleSplit [options] $sam $vcf $name\n\n")
 	}
-	AlleleExpression(flag.Arg(0), vcf.Read(flag.Arg(1)), flag.Arg(2), *refPrefix, *altPrefix, *undetermined)
+	if *sampleName {
+		file := fileio.EasyOpen(flag.Arg(0))
+		defer file.Close()
+		header := vcf.ReadHeader(file)
+		fmt.Printf("%s", vcf.PrintSampleNames(header))
+	}
+	SnpSearch(flag.Arg(0), flag.Arg(1), *f1Genome, parental[0], parental[1], *f1Genome)
+	//BasicAlleleExpression(flag.Arg(0), vcf.Read(flag.Arg(1)), flag.Arg(2), *refPrefix, *altPrefix)
 }
+
+//Define flag value as an array. Used to define parent genomes.
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "my string representation"
+}
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+/*
+func GoRoutinesSnpSearch(samfile string, genotypeVcf string, parentOne string, parentTwo, prefix string, f1 string, threads int) {
+	var wg sync.WaitGroup
+	gvcf := make(chan *vcf.Vcf)
+	genotypeReader := fileio.EasyOpen(genotypeVcf)
+	defer genotypeReader.Close()
+
+	vcfHeader := vcf.ReadHeader(genotypeReader)
+
+	sampleHash := vcf.HeaderToMaps(vcfHeader)
+
+	go vcf.ReadToChan(genotypeReader, gvcf)
+
+	//children := strings.Split(f1, ",")
+	//parents := []string{parentOne, parentTwo}
+	//hets, homs := vcf.MapNameToIndex(sampleHash.Index, children), vcf.MapNameToIndex(sampleHash.Index, parents)
+
+	snpDb := make(map[uint64]*vcf.GVcf)
+	var parentalOne, parentalTwo, fOne int16 = sampleHash.IndexAllele[parentOne], sampleHash.IndexAllele[parentTwo], sampleHash.IndexAllele[f1]
+	for genotype := range gvcf {
+		if vcf.ASFilter(genotype, parentalOne, parentalTwo, fOne) {
+			snpDb = vcf.GenotypeToMap(genotype, sampleHash.FaIndex)
+		}
+	}
+	samFile := fileio.EasyOpen(samfile)
+	defer samFile.Close()
+	header := sam.ReadHeader(samFile)
+
+	samReader := make(chan *sam.SamAln)
+	go sam.ReadToChan(samFile, samReader)
+	wg.Wait()
+	var wgReader, wgWriter sync.WaitGroup
+	childOne := make(chan *sam.SamAln)
+	childTwo := make(chan *sam.SamAln)
+
+	parents := []string{parentOne, parentTwo}
+	for i := 0; i < threads; i++ {
+		wgReader.Add(1)
+		go snpAnalysis(snpDb, sampleHash, parents, samReader, childOne, childTwo, &wgReader)
+	}
+	wgWriter.Add(2)
+	go sam.SamChanToFile(childOne, fmt.Sprintf("%s.%s.SNPs.sam", prefix, parentOne), header, &wgWriter)
+	go sam.SamChanToFile(childTwo, fmt.Sprintf("%s.%s.SNPs.sam", prefix, parentTwo), header, &wgWriter)
+	wgReader.Wait()
+	close(childOne)
+	close(childTwo)
+	wgWriter.Wait()
+}
+
+func snpAnalysis(snpDb map[uint64]*vcf.GVcf, sampleHash *vcf.SampleIdMap, parents []string, samReader <-chan *sam.SamAln, childOne chan<- *sam.SamAln, childTwo chan<- *sam.SamAln, wg *sync.WaitGroup) {
+	//for read, done := sam.NextAlignment(samFile); done != true; read, done = sam.NextAlignment(samFile) {
+	for read := range samReader {
+		parentAllele1, parentAllele2 := 0, 0
+		var target int64 = read.Pos - 1
+		var query int64 = 0
+		var code uint64
+		var ok bool
+		var gV *vcf.GVcf
+		for i := 0; i < len(read.Cigar); i++ {
+			switch read.Cigar[i].Op {
+			case 'S':
+				query += read.Cigar[i].RunLength
+			case 'I':
+				//code = vcf.ChromPosToUInt64(int(sampleHash.FaIndex[read.RName]), int(target))
+				//_, ok = snpDb[code]
+				//if ok {
+				//	gV = snpDb[code]
+				//	if dna.CompareSeqsIgnoreCase(read.Seq[query:query+read.Cigar[i].RunLength], gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[0]]].AlleleOne]) == 0 && dna.CompareSeqsIgnoreCase(read.Seq[query:query+read.Cigar[i].RunLength], gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[0]]].AlleleTwo]) == 0 {
+				//		parentAllele1++
+				//	}
+				//	if dna.CompareSeqsIgnoreCase(read.Seq[query:query+read.Cigar[i].RunLength], gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[1]]].AlleleOne]) == 0 && dna.CompareSeqsIgnoreCase(read.Seq[query:query+read.Cigar[i].RunLength], gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[1]]].AlleleTwo]) == 0 {
+				//		parentAllele2++
+				//	}
+				//}
+				query += read.Cigar[i].RunLength
+			case 'D':
+				code = vcf.ChromPosToUInt64(int(sampleHash.FaIndex[read.RName]), int(target))
+				_, ok = snpDb[code]
+				if ok {
+					gV = snpDb[code]
+					if dna.CountBase(gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[0]]].AlleleOne], dna.Gap) == int(read.Cigar[i].RunLength) && dna.CountBase(gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[0]]].AlleleTwo], dna.Gap) == int(read.Cigar[i].RunLength) {
+						parentAllele1++
+					}
+					if dna.CountBase(gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[1]]].AlleleOne], dna.Gap) == int(read.Cigar[i].RunLength) && dna.CountBase(gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[1]]].AlleleTwo], dna.Gap) == int(read.Cigar[i].RunLength) {
+						parentAllele1++
+					}
+				}
+				target += read.Cigar[i].RunLength
+			case 'M':
+				var j int64
+				for j = 0; j < read.Cigar[i].RunLength; j++ {
+					code = vcf.ChromPosToUInt64(int(sampleHash.FaIndex[read.RName]), int(target+j))
+					_, ok = snpDb[code]
+					if ok {
+						gV = snpDb[code]
+						if dna.CompareSeqsIgnoreCase([]dna.Base{read.Seq[query+j]}, gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[1]]].AlleleOne]) == 0 && dna.CompareSeqsIgnoreCase([]dna.Base{read.Seq[query+j]}, snpDb[code].Seq[gV.Genotypes[sampleHash.IndexAllele[parents[0]]].AlleleTwo]) == 0 {
+							parentAllele1++
+						}
+						if dna.CompareSeqsIgnoreCase([]dna.Base{read.Seq[query+j]}, gV.Seq[gV.Genotypes[sampleHash.IndexAllele[parents[1]]].AlleleOne]) == 0 && dna.CompareSeqsIgnoreCase([]dna.Base{read.Seq[query+j]}, snpDb[code].Seq[gV.Genotypes[sampleHash.IndexAllele[parents[1]]].AlleleTwo]) == 0 {
+							parentAllele2++
+						}
+					}
+
+				}
+				target += read.Cigar[i].RunLength
+				query += read.Cigar[i].RunLength
+			}
+		}
+		if parentAllele1 > parentAllele2 {
+			childOne <- read
+			//sam.WriteAlnToFileHandle(childOne, read)
+		} else if parentAllele2 > parentAllele1 {
+			//sam.WriteAlnToFileHandle(childTwo, read)
+			childTwo <- read
+		} else {
+			//Skip read
+		}
+	}
+	wg.Done()
+}*/
---
+++ cmd/alleleSplit/toSam.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vertgenlab/gonomics/dna"
+	"github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/sam"
+	"github.com/vertgenlab/gonomics/vcf"
+	"log"
+)
+
+func SnpSearch(samfile string, genotypeVcf string, fOne string, parentOne string, parentTwo, prefix string) {
+	reader := vcf.ReadGVcf(genotypeVcf)
+	dict := vcf.HeaderToMaps(reader.Header)
+	snpDb := make(map[uint64]*vcf.GVcf)
+	for genotype := range reader.Vcfs {
+		if vcf.ASFilter(genotype, dict.GIndex[parentOne], dict.GIndex[parentTwo], dict.GIndex[fOne]) {
+			vcf.GenotypeToMap(genotype, dict.Fa, snpDb)
+		}
+	}
+	samFile := fileio.EasyOpen(samfile)
+	defer samFile.Close()
+	header := sam.ReadHeader(samFile)
+
+	childOne := fileio.EasyCreate(fmt.Sprintf("%s.%s.SNPs.sam", prefix, parentOne))
+	defer childOne.Close()
+	childTwo := fileio.EasyCreate(fmt.Sprintf("%s.%s.SNPs.sam", prefix, parentTwo))
+	defer childTwo.Close()
+
+	sam.WriteHeaderToFileHandle(childOne, header)
+	sam.WriteHeaderToFileHandle(childTwo, header)
+
+	var i, parentAllele1, parentAllele2 int
+	var target, query, j int64
+	var ok bool
+	var code uint64
+	//wg.Wait()
+
+	var gV *vcf.GVcf
+	for read, done := sam.NextAlignment(samFile); done != true; read, done = sam.NextAlignment(samFile) {
+		parentAllele1, parentAllele2 = 0, 0
+		target = read.Pos - 1
+		query = 0
+		for i = 0; i < len(read.Cigar); i++ {
+			switch read.Cigar[i].Op {
+			case 'S':
+				query += read.Cigar[i].RunLength
+			case 'I':
+				//TODO: Figure out how to take insertions into account
+				//code = ChromPosToUInt64(int(dict.Fa[read.RName]), int(target))
+				//_, ok = snpDb[code]
+				//if ok {
+				//	gV = snpDb[code]
+				//	if dna.CompareSeqsIgnoreCase(read.Seq[query:query+read.Cigar[i].RunLength], gV.Alleles[gV.Genotypes[dict.GIndex[parentOne]].AlleleOne]) == 0 && dna.CompareSeqsIgnoreCase(read.Seq[query:query+read.Cigar[i].RunLength], gV.Alleles[gV.Genotypes[dict.GIndex[parentOne]].AlleleTwo]) == 0 {
+				//		parentAllele1++
+				//	}
+				//	if dna.CompareSeqsIgnoreCase(read.Seq[query:query+read.Cigar[i].RunLength], gV.Alleles[gV.Genotypes[dict.GIndex[parentTwo]].AlleleOne]) == 0 && dna.CompareSeqsIgnoreCase(read.Seq[query:query+read.Cigar[i].RunLength], gV.Alleles[gV.Genotypes[dict.GIndex[parentTwo]].AlleleTwo]) == 0 {
+				//		parentAllele2++
+				//	}
+				//}
+				query += read.Cigar[i].RunLength
+			case 'D':
+				code = vcf.ChromPosToUInt64(int(dict.Fa[read.RName]), int(target))
+				_, ok = snpDb[code]
+				if ok {
+					gV = snpDb[code]
+					if dna.CountBase(gV.Seq[gV.Genotypes[dict.GIndex[parentOne]].AlleleOne], dna.Gap) == int(read.Cigar[i].RunLength) && dna.CountBase(gV.Seq[gV.Genotypes[dict.GIndex[parentOne]].AlleleTwo], dna.Gap) == int(read.Cigar[i].RunLength) {
+						parentAllele1++
+					}
+					if dna.CountBase(gV.Seq[gV.Genotypes[dict.GIndex[parentTwo]].AlleleOne], dna.Gap) == int(read.Cigar[i].RunLength) && dna.CountBase(gV.Seq[gV.Genotypes[dict.GIndex[parentTwo]].AlleleTwo], dna.Gap) == int(read.Cigar[i].RunLength) {
+						parentAllele1++
+					}
+				}
+				target += read.Cigar[i].RunLength
+			case 'M':
+				for j = 0; j < read.Cigar[i].RunLength; j++ {
+					code = vcf.ChromPosToUInt64(int(dict.Fa[read.RName]), int(target+j))
+					_, ok = snpDb[code]
+					if ok {
+						gV = snpDb[code]
+						if dna.CompareSeqsIgnoreCase([]dna.Base{read.Seq[query+j]}, gV.Seq[gV.Genotypes[dict.GIndex[parentOne]].AlleleOne]) == 0 && dna.CompareSeqsIgnoreCase([]dna.Base{read.Seq[query+j]}, snpDb[code].Seq[gV.Genotypes[dict.GIndex[parentOne]].AlleleTwo]) == 0 {
+							parentAllele1++
+						}
+						if dna.CompareSeqsIgnoreCase([]dna.Base{read.Seq[query+j]}, gV.Seq[gV.Genotypes[dict.GIndex[parentTwo]].AlleleOne]) == 0 && dna.CompareSeqsIgnoreCase([]dna.Base{read.Seq[query+j]}, snpDb[code].Seq[gV.Genotypes[dict.GIndex[parentTwo]].AlleleTwo]) == 0 {
+							parentAllele2++
+						}
+					}
+
+				}
+				target += read.Cigar[i].RunLength
+				query += read.Cigar[i].RunLength
+			}
+		}
+		if parentAllele1 > parentAllele2 {
+			sam.WriteAlnToFileHandle(childOne, read)
+		} else if parentAllele2 > parentAllele1 {
+			sam.WriteAlnToFileHandle(childTwo, read)
+		} else {
+			//Skip read
+			log.Printf("Warning: read %s skipped\n", read.QName)
+		}
+	}
+	reader.File.Close()
+}
