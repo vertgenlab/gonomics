3f316fe1623e5b3a8cdbdcdfbd3ac4a8da1f2831
edotau <eric.au@duke.edu>
2020-07-15 00:11:38 -0700

    added todo to testing function

--- vcf/gVcf_test.go
+++ vcf/gVcf_test.go
@@ -1,15 +1,10 @@
 package vcf
 
 import (
-	//"github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/fileio"
 	"testing"
 )
 
-func TestSamToAlleles(t *testing.T) {
-	SnpSearch("testdata/CL12_wgsTest.sam", "testdata/multiSampleTest.vcf", "CL12_wgs_merged", "LITC", "MATA", "testdata/CL12_FINAL_test")
-}
-
-/*
 func TestASFilter(t *testing.T) {
 	file := fileio.EasyOpen("testdata/multiSampleTest.vcf")
 	defer file.Close()
@@ -20,18 +15,24 @@ func TestASFilter(t *testing.T) {
 	go ReadToChan(file, reader)
 
 	var passFilter []*Vcf
-	var parentalOne, parentalTwo, fOne int16 = sampleHash.IndexAllele["LITC"], sampleHash.IndexAllele["MATA"], sampleHash.IndexAllele["CL12_wgs_merged"]
+	var parentalOne, parentalTwo, fOne int16 = sampleHash.GIndex["LITC"], sampleHash.GIndex["MATA"], sampleHash.GIndex["CL12_wgs_merged"]
 	for each := range reader {
 		if ASFilter(each, parentalOne, parentalTwo, fOne) {
 			//PrintReOrder(each, []int16{parentalOne, parentalTwo, fOne})
 			passFilter = append(passFilter, each)
 		}
 	}
-	var currGt []Sample
+	var currGt []GenomeSample
 	for i := 0; i < len(passFilter); i++ {
 		currGt = GetAlleleGenotype(passFilter[i])
-		if isHeterozygous(currGt[parentalOne]) || isHeterozygous(currGt[parentalTwo]) || isHomozygous(currGt[fOne]) {
+		if IsHeterozygous(currGt[parentalOne]) || IsHeterozygous(currGt[parentalTwo]) || IsHomozygous(currGt[fOne]) {
 			t.Errorf("Error: Parental genomes should be Homozygous and F1 should be Heterozygous...\n")
 		}
 	}
+}
+
+//TODO: Finish Sam split testing code
+/*
+func TestSamToAlleles(t *testing.T) {
+	SnpSearch("testdata/CL12_wgsTest.sam", "testdata/multiSampleTest.vcf", "CL12_wgs_merged", "LITC", "MATA", "testdata/CL12_FINAL_test")
 }*/
