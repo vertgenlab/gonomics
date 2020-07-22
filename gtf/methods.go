0468861c2881ff8c9c8356ec5ea27429287b0154
das106 <das106@duke.edu>
2020-07-22 11:43:56 -0400

    added methods

---
+++ gtf/methods.go
@@ -0,0 +1,21 @@
+package gtf
+
+import (
+	"github.com/vertgenlab/gonomics/fileio"
+)
+
+func (g *Gene) GetChrom() string {
+	return g.Transcripts[0].Chr
+}
+
+func (g *Gene) GetChromStart() int {
+	return g.Transcripts[0].Start - 1
+}
+
+func (g *Gene) GetChromEnd() int {
+	return g.Transcripts[0].End
+}
+
+func (g *Gene) WriteToFileHandle(file *fileio.EasyWriter) {
+	WriteToFileHandle(file, g)
+}
