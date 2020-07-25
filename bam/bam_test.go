b00683ac4ea311cd42def6b65da6b68823c8cd3b
edotau <eric.au@duke.edu>
2020-07-25 14:05:45 -0700

    added 2 bam files to test binary reader

--- bam/bam_test.go
+++ bam/bam_test.go
@@ -5,19 +5,30 @@ import (
 	"testing"
 )
 
+//readBamTests are the files pairs used to test our binary reader functions
+var readBamTests = []struct {
+	bam string
+	sam string
+}{
+	{"testdata/small.bam", "testdata/small.sam"},
+	{"testdata/tenXbarcodeTest.bam", "testdata/tenXbarcodeTest.sam"},
+}
+
 //TestBamToSamReader will convert a bam file into a sam record and perform a comparison with the same sam file that was created using samtools view.
 func TestBamToSamReader(t *testing.T) {
-	bamFile := Read("testdata/tenXbarcodeTest.bam")
-	samFile, err := sam.Read("testdata/tenXbarcodeTest.sam")
-	if err != nil {
-		t.Errorf("Error: There was a problem reading in the sam file...\n")
-	}
-	if len(bamFile) != len(samFile.Aln) {
-		t.Errorf("Error: File lines are not equal...\n")
-	}
-	for i := 0; i < len(bamFile); i++ {
-		if !sam.IsEqualDebug(bamFile[i], samFile.Aln[i]) {
-			t.Fatalf("Error: Did not create the same sam file as samtools view...\n")
+	for _, test := range readBamTests {
+		bamFile := Read(test.bam)
+		samFile, err := sam.Read(test.sam)
+		if err != nil {
+			t.Errorf("Error: There was a problem reading in the sam file...\n")
+		}
+		if len(bamFile) != len(samFile.Aln) {
+			t.Errorf("Error: File lines are not equal...\n")
+		}
+		for i := 0; i < len(bamFile); i++ {
+			if !sam.IsEqualDebug(bamFile[i], samFile.Aln[i]) {
+				t.Fatalf("Error: Did not create the same sam file as samtools view...\n")
+			}
 		}
 	}
 }
@@ -28,7 +39,9 @@ func BenchmarkSamReader(b *testing.B) {
 	var err error
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
-		samFile, err = sam.Read("testdata/tenXbarcodeTest.sam")
+		for _, test := range readBamTests {
+			samFile, err = sam.Read(test.sam)
+		}
 	}
 	if err != nil {
 		b.Errorf("Error: There was a problem reading in the sam file after %d lines...\n", len(samFile.Aln))
@@ -40,7 +53,10 @@ func BenchmarkBamReader(b *testing.B) {
 	var bamFile []*sam.SamAln
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
-		bamFile = Read("testdata/tenXbarcodeTest.bam")
+		for _, test := range readBamTests {
+			bamFile = Read(test.bam)
+		}
+
 	}
 	if bamFile == nil {
 		b.Errorf("Error: There was a problem reading in the sam file after %d lines...\n", len(bamFile))
