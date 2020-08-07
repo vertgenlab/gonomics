0710b01b104b78a80a3a96b08364476530e69340
rimangan <riley.mangan@duke.edu>
2020-08-07 16:19:31 -0400

    Segregating sites for fasta func

--- fasta/modify.go
+++ fasta/modify.go
@@ -163,6 +163,31 @@ func DistColumn(records []*Fasta) []*Fasta {
 	return subFa
 }
 
+//This function takes in a multiFa alignment block and returns only the columns that contain segregating sites.
+func SegregatingSites(aln []*Fasta) []*Fasta {
+	var answer = make([]*Fasta, len(aln))
+	for i := 0; i < len(aln); i++ {
+		answer[i] = &Fasta{Name: aln[i].Name, Seq: make([]dna.Base, 0)}
+	}
+	var current dna.Base
+	var isSegregating bool
+	for i := 0; i < len(aln[0].Seq); i++ {
+		current = aln[0].Seq[i]
+		isSegregating = false
+		for j := 1; j < len(aln); j++ {
+			if aln[j].Seq[i] != current {
+				isSegregating = true
+			}
+		}
+		if isSegregating {
+			for k := 0; k < len(aln); k++ {
+				answer[k].Seq = append(answer[k].Seq, aln[k].Seq[i])
+			}
+		}
+	}
+	return answer
+}
+
 func ChangePrefix(records []*Fasta, prefix string) {
 	for idx := 0; idx < len(records); idx++ {
 		records[idx].Name = fmt.Sprintf("%s_%d", prefix, idx)
