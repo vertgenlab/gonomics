bb22aded7e5cf640c37a455a4e5419ccae2e64f1
Daniel Snellings <37418559+das106@users.noreply.github.com>
2020-08-10 15:30:25 -0400

    Update sam.go

--- sam/sam.go
+++ sam/sam.go
@@ -39,22 +39,6 @@ type SamAln struct {
 	Extra string
 }
 
-//func ReadToChan(reader *fileio.EasyReader, output chan<- *SamAln) {
-//	var curr *SamAln
-//	var done bool
-//	for curr, done = NextAlignment(reader); done != true; curr, done = NextAlignment(reader) {
-//		output <- curr
-//	}
-//	close(output)
-//}
-//
-//func GoReadToChan(filename string) chan *SamAln {
-//	answer := make(chan *SamAln)
-//	file := fileio.EasyOpen(filename)
-//	go ReadToChan(file, answer)
-//	return answer
-//}
-
 func ReadToChan(file *fileio.EasyReader, data chan<- *SamAln, wg *sync.WaitGroup) {
 	for curr, done := NextAlignment(file); !done; curr, done = NextAlignment(file) {
 		data <- curr
