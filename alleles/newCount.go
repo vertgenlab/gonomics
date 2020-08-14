34ce4c400f49127bbffcb8299e445d7d45349c2d
das106 <das106@duke.edu>
2020-08-14 17:01:23 -0400

    giraf count coded

--- alleles/newCount.go
+++ alleles/newCount.go
@@ -13,7 +13,7 @@ import (
 
 // sendPassedPositions sends positions that have been passed in the file
 // TODO: Version for graph. Maybe readin graph and use simpleGraph.GetSortOrder to determine ordering
-func sendPassedPositions(answer chan<- *Allele, aln *sam.SamAln, samFilename string, runningCount []*Location, currAlleles map[Location]*AlleleCount) []*Location {
+func sendPassedPositionsSam(answer chan<- *Allele, aln *sam.SamAln, samFilename string, runningCount []*Location, currAlleles map[Location]*AlleleCount) []*Location {
 	for i := 0; i < len(runningCount); i++ {
 
 		if runningCount[i].Chr != aln.RName {
@@ -47,7 +47,7 @@ func sendPassedPositions(answer chan<- *Allele, aln *sam.SamAln, samFilename str
 	return runningCount
 }
 
-func countLinearRead(aln *sam.SamAln, currAlleles map[Location]*AlleleCount, runningCount []*Location, ref map[string][]dna.Base, minMapQ int64, progress int) (map[Location]*AlleleCount, []*Location) {
+func countSamRead(aln *sam.SamAln, currAlleles map[Location]*AlleleCount, runningCount []*Location, ref map[string][]dna.Base, minMapQ int64, progress int) (map[Location]*AlleleCount, []*Location) {
 
 	if aln.Cigar[0].Op == '*' {
 		return currAlleles, runningCount
@@ -238,11 +238,11 @@ func countLinearRead(aln *sam.SamAln, currAlleles map[Location]*AlleleCount, run
 	return currAlleles, runningCount
 }
 
-func GoCountAlleles(samFilename string, reference []*fasta.Fasta, minMapQ int64) <-chan *Allele {
+func GoCountSamAlleles(samFilename string, reference []*fasta.Fasta, minMapQ int64) <-chan *Allele {
 	answer := make(chan *Allele)
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go NewCountAlleles(answer, samFilename, reference, minMapQ, &wg)
+	go CountSamAlleles(answer, samFilename, reference, minMapQ, &wg)
 
 	go func() {
 		wg.Wait()
@@ -252,7 +252,7 @@ func GoCountAlleles(samFilename string, reference []*fasta.Fasta, minMapQ int64)
 	return answer
 }
 
-func NewCountAlleles(answer chan<- *Allele, samFilename string, reference []*fasta.Fasta, minMapQ int64, wg *sync.WaitGroup) {
+func CountSamAlleles(answer chan<- *Allele, samFilename string, reference []*fasta.Fasta, minMapQ int64, wg *sync.WaitGroup) {
 	samChan, _ := sam.GoReadToChan(samFilename)
 	var currAlleles = make(map[Location]*AlleleCount)
 	var runningCount = make([]*Location, 0)
@@ -262,13 +262,104 @@ func NewCountAlleles(answer chan<- *Allele, samFilename string, reference []*fas
 	ref := fasta.FastaMap(reference)
 
 	for read := range samChan {
-		runningCount = sendPassedPositions(answer, read, samFilename, runningCount, currAlleles)
-		currAlleles, runningCount = countLinearRead(read, currAlleles, runningCount, ref, minMapQ, progress)
+		runningCount = sendPassedPositionsSam(answer, read, samFilename, runningCount, currAlleles)
+		currAlleles, runningCount = countSamRead(read, currAlleles, runningCount, ref, minMapQ, progress)
 	}
 	wg.Done()
 }
 
-func countGraphRead(aln *giraf.Giraf, currAlleles map[GraphLocation]*AlleleCount, runningCount []*GraphLocation, ref simpleGraph.SimpleGraph, minMapQ uint8, progress int) (map[GraphLocation]*AlleleCount, []*GraphLocation) {
+func GoCountGirafAlleles(girafFilename string, reference *simpleGraph.SimpleGraph, minMapQ uint8) <-chan *Allele {
+	answer := make(chan *Allele)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	//TODO: simpleGraph.AllToUpper(reference)
+
+	go CountGirafAlleles(answer, girafFilename, reference, minMapQ, &wg)
+
+	go func() {
+		wg.Wait()
+		close(answer)
+	}()
+
+	return answer
+}
+
+func CountGirafAlleles(answer chan<- *Allele, girafFilename string, reference *simpleGraph.SimpleGraph, minMapQ uint8, wg *sync.WaitGroup) {
+	girafChan := giraf.GoReadToChan(girafFilename)
+	var currAlleles = make(map[GraphLocation]*AlleleCount)
+	var runningCount = make([]*GraphLocation, 0)
+	var progress int // TODO: Make option to print progress
+
+	for read := range girafChan {
+		runningCount = sendPassedPositionsGiraf(runningCount)
+		currAlleles, runningCount = countGraphRead(read, currAlleles, runningCount, reference, minMapQ, progress)
+	}
+	wg.Done()
+}
+
+func sendPassedPositionsGiraf(runningCount []*GraphLocation) []*GraphLocation {
+
+	return runningCount
+}
+
+func countBase(base dna.Base, indel *Indel, aln *giraf.Giraf, currLoc *GraphLocation, currAlleles map[GraphLocation]*AlleleCount) map[GraphLocation]*AlleleCount {
+	currAlleles[*currLoc].Counts++
+
+	if indel == nil { // If counting an SNV
+		switch base {
+		case dna.A:
+			if giraf.IsReverseRead(aln) {
+				currAlleles[*currLoc].BaseAR++
+			} else { // If reads are unpaired then all counts will be recorded as fwd
+				currAlleles[*currLoc].BaseAF++
+			}
+		case dna.C:
+			if giraf.IsReverseRead(aln) {
+				currAlleles[*currLoc].BaseCR++
+			} else {
+				currAlleles[*currLoc].BaseCF++
+			}
+		case dna.G:
+			if giraf.IsReverseRead(aln) {
+				currAlleles[*currLoc].BaseGR++
+			} else {
+				currAlleles[*currLoc].BaseGF++
+			}
+		case dna.T:
+			if giraf.IsReverseRead(aln) {
+				currAlleles[*currLoc].BaseTR++
+			} else {
+				currAlleles[*currLoc].BaseTF++
+			}
+		}
+	} else { // If counting an indel
+		for j := 0; j < len(currAlleles[*currLoc].Indel); j++ {
+			// If the indel has already been seen before, increment the existing entry
+			if dna.CompareSeqsIgnoreCase(indel.Ref, currAlleles[*currLoc].Indel[j].Ref) == 0 &&
+				dna.CompareSeqsIgnoreCase(indel.Alt, currAlleles[*currLoc].Indel[j].Alt) == 0 {
+				if giraf.IsForwardRead(aln) == true {
+					currAlleles[*currLoc].Indel[j].CountF++
+				} else if giraf.IsReverseRead(aln) == true {
+					currAlleles[*currLoc].Indel[j].CountR++
+				}
+				return currAlleles
+			}
+		}
+
+		// If the indel has not been seen before, then append it to the Indel slice
+		if giraf.IsForwardRead(aln) == true {
+			indel.CountF++
+		} else if giraf.IsReverseRead(aln) == false {
+			indel.CountR++
+		}
+		currAlleles[*currLoc].Indel = append(currAlleles[*currLoc].Indel, *indel)
+	}
+
+	return currAlleles
+}
+
+func countGraphRead(aln *giraf.Giraf, currAlleles map[GraphLocation]*AlleleCount, runningCount []*GraphLocation, ref *simpleGraph.SimpleGraph, minMapQ uint8, progress int) (map[GraphLocation]*AlleleCount, []*GraphLocation) {
 	if aln.Aln[0].Op == '*' {
 		return currAlleles, runningCount
 	}
@@ -280,16 +371,18 @@ func countGraphRead(aln *giraf.Giraf, currAlleles map[GraphLocation]*AlleleCount
 	var i, refPos int64
 	var readNodeIdx int = 0
 	var currNode *simpleGraph.Node = ref.Nodes[aln.Path.Nodes[readNodeIdx]]
+	var currLoc, originalLoc GraphLocation
+	var currIndel Indel
 
 	for _, cig := range aln.Aln {
 		switch cig.Op {
 		case '=': // Match
-			for i = 0; i < cig.RunLength; i++ { // For each matching base
-				currLoc := GraphLocation{Node: currNode, Pos: refPos}                        // define location on reference
-				currAlleles, runningCount = addPosToMap(&currLoc, currAlleles, runningCount) // add the pos to map if necessary
-				// count the base //TODO
-				refPos++                               // move to the next base in the ref
-				if int(refPos) > len(currNode.Seq)-1 { // check if next base is on another node
+			for i = 0; i < cig.RunLength; i++ { // For each consecutive matching base
+				currLoc = GraphLocation{Node: currNode, Pos: refPos}                               // define location on reference
+				currAlleles, runningCount = addPosToMap(&currLoc, currAlleles, runningCount)       // add the pos to map if necessary
+				currAlleles = countBase(currAlleles[currLoc].Ref, nil, aln, &currLoc, currAlleles) // count the base
+				refPos++                                                                           // move to the next base in the ref
+				if int(refPos) > len(currNode.Seq)-1 {                                             // check if next base is on another node, if so move to next node
 					readNodeIdx++
 					currNode = ref.Nodes[aln.Path.Nodes[readNodeIdx]]
 					refPos = 0
@@ -297,10 +390,63 @@ func countGraphRead(aln *giraf.Giraf, currAlleles map[GraphLocation]*AlleleCount
 			}
 
 		case 'X': // Mismatch
+			for i = 0; i < cig.RunLength; i++ { // For each consecutive mismatched base
+				currLoc = GraphLocation{Node: currNode, Pos: refPos}                         // define location on reference
+				currAlleles, runningCount = addPosToMap(&currLoc, currAlleles, runningCount) // add the pos to map if necessary
+				currAlleles = countBase(cig.Sequence[i], nil, aln, &currLoc, currAlleles)    // count the base
+				refPos++                                                                     // move to the next base in the ref
+				if int(refPos) > len(currNode.Seq)-1 {                                       // check if next base is on another node, if so move to next node
+					readNodeIdx++
+					currNode = ref.Nodes[aln.Path.Nodes[readNodeIdx]]
+					refPos = 0
+				}
+			}
 
 		case 'I': // Insertion
+			// First base in indel is the base prior to the indel sequence per VCF standard format
+			currLoc = GraphLocation{Node: currNode, Pos: refPos - 1}                     // define location on reference
+			currAlleles, runningCount = addPosToMap(&currLoc, currAlleles, runningCount) // add the pos to map if necessary
+
+			currIndel = Indel{ // initialize indel entry
+				Ref:    make([]dna.Base, 1),
+				Alt:    make([]dna.Base, 1),
+				CountF: 0,
+				CountR: 0,
+			}
+
+			currIndel.Ref[0] = currLoc.Node.Seq[currLoc.Pos] // add the base prior the the deleted sequence per vcf standard
+			currIndel.Alt[0] = currLoc.Node.Seq[currLoc.Pos]
+
+			currIndel.Alt = append(currIndel.Alt, cig.Sequence...) // add inserted bases to the Alt field
+
+			currAlleles = countBase(0, &currIndel, aln, &currLoc, currAlleles) // count indel to the base prior to insertion. Base argument will be ignored in this call
 
 		case 'D': // Deletion
+			// First base in indel is the base prior to the indel sequence per VCF standard format
+			currLoc = GraphLocation{Node: currNode, Pos: refPos - 1}                     // define location on reference
+			originalLoc = currLoc                                                        // save the start location for use later (in case we jump nodes adding deleted sequence)
+			currAlleles, runningCount = addPosToMap(&currLoc, currAlleles, runningCount) // add the pos to map if necessary
+
+			currIndel = Indel{ // initialize indel entry
+				Ref:    make([]dna.Base, 1),
+				Alt:    make([]dna.Base, 1),
+				CountF: 0,
+				CountR: 0,
+			}
+			currIndel.Ref[0] = currLoc.Node.Seq[currLoc.Pos] // add the base prior the the deleted sequence per vcf standard
+			currIndel.Alt[0] = currLoc.Node.Seq[currLoc.Pos]
+
+			for i = 0; i < cig.RunLength; i++ { // add deleted sequence to currIndel.Ref
+				//TODO: Think carefully about whether a read count should be added for each deleted base or if only one count should be recorded at the base prior to deletion
+				if int(refPos) > len(currNode.Seq)-1 { // check if next base is on another node, if so move to next node
+					readNodeIdx++
+					currNode = ref.Nodes[aln.Path.Nodes[readNodeIdx]]
+					refPos = 0
+				}
+				currIndel.Ref = append(currIndel.Ref, currLoc.Node.Seq[refPos]) // add base to currIndel
+				refPos++                                                        // move to next base
+			}
+			currAlleles = countBase(0, &currIndel, aln, &originalLoc, currAlleles) // count indel to the base prior to deletion (originalLoc). Base argument will be ignored in this call
 
 		case 'M': // Likely that cigar is using sam format and not fancy format. Throw error to let user know they may need to reformat.
 			log.Fatalf("ERROR: Unrecognized character 'M' in cigar. Reformat to fancy cigars to use this functionality.")
