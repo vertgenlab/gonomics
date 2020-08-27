08567238a8ffd27b82a2053dd2265061ba63fe6e
edotau <eric.au@duke.edu>
2020-08-27 00:24:32 -0400

    GoDocs for simpleReader

--- fileio/simpleio.go
+++ fileio/simpleio.go
@@ -1,24 +1,31 @@
 package fileio
 
-import(
-	"strings"
-	"os"
+import (
 	"bufio"
-	"io"
-	"log"
 	"compress/gzip"
 	"github.com/vertgenlab/gonomics/common"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
+// SimpleReader implements the io.Reader interface by providing
+// the Read(b []byte) method. The struct contains an embedded *bufio.Reader
+// and a pointer to os.File for closeure when reading is complete.
 type SimpleReader struct {
 	*bufio.Reader
 	File *os.File
 }
 
+// Read reads data into p. It returns the number of bytes read into p.
 func (reader *SimpleReader) Read(b []byte) (n int, err error) {
 	return reader.Read(b)
 }
 
+// NewSimpleReader will process a given file and performs error handling if an error occurs.
+// SimpleReader will prcoess gzipped files accordinging by performing a check on the suffix
+// of the provided file.
 func NewSimpleReader(filename string) *SimpleReader {
 	var answer SimpleReader = SimpleReader{
 		File: MustOpen(filename),
@@ -34,20 +41,24 @@ func NewSimpleReader(filename string) *SimpleReader {
 	return &answer
 }
 
+// ReadLine will return a slice of bytes for each line and provided this function is called within a loop,
+// will return bool to continue reading. Please be aware that the function will call close on the file once
+// the reader encounters EOF.
 func ReadLine(reader *SimpleReader) ([]byte, bool) {
-	if curr, err := reader.ReadBytes('\n'); err == nil {
+	curr, err := reader.ReadBytes('\n')
+	if err == nil {
 		if curr[len(curr)-1] == '\n' {
 			return curr[:len(curr)-1], false
 		} else {
 			log.Fatalf("Error: end of line did not end with a white space character...\n")
 		}
-	} else {
-		CatchErrThrowEOF(err)
-		reader.Close()
 	}
+	CatchErrThrowEOF(err)
+	reader.Close()
 	return nil, true
 }
 
+// CatchErrThrowEOF will silently handles and throws the EOF error and will log and exit any other errors.
 func CatchErrThrowEOF(err error) {
 	if err == io.EOF {
 		return
@@ -56,6 +67,9 @@ func CatchErrThrowEOF(err error) {
 	}
 }
 
+// Close closes the File, rendering it unusable for I/O. On files that support SetDeadline,
+// any pending I/O operations will be canceled and return immediately with an error.
+// Close will return an error if it has already been called.
 func (reader *SimpleReader) Close() {
 	if reader != nil {
 		err := reader.File.Close()
