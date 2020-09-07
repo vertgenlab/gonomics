1b10bf2861eea6b566123c23b62ed8315f55a707
Christi Fauci <christiana.fauci@duke.edu>
2020-09-07 16:40:42 -0400

    increased simulation sequence length

--- reconstruct/reconstruct_test.go
+++ reconstruct/reconstruct_test.go
@@ -9,10 +9,10 @@ import (
 
 var GCcontent = 0.42
 var input = []struct {
-	newick_filename string // first input
-	length          int    // second input
+	newickFilename string // first input
+	length         int    // second input
 }{
-	{"newTestFiles/newickLongBranches.txt", 150},
+	{"newTestFiles/newickLongBranches.txt", 1005},
 	//{"newTestFiles/newickShortBranches.txt", 333},
 	//{"newTestFiles/newickShortBranches.txt", 66}
 }
@@ -21,7 +21,7 @@ func Test_reconstruct(t *testing.T) {
 	for _, test := range input {
 
 		//example of how to run simulation: 1) read in tree (no fastas) 2) simulate random gene 3) simulate evolution 4) remove ancestors for reconstruction 4) assign fastas
-		tre, er := expandedTree.ReadNewick(test.newick_filename)
+		tre, er := expandedTree.ReadNewick(test.newickFilename)
 		if er != nil {
 		}
 		fasta.Write("RandGeneOutput.fasta", simulate.RandGene("test", test.length, GCcontent)) //galGal6 GC
@@ -30,7 +30,7 @@ func Test_reconstruct(t *testing.T) {
 		expandedTree.AssignFastas(tre, "simOut.fasta")
 
 		//example of how to run reconstruction: 1) reconstruct 2) check accuracy
-		tr := expandedTree.ReadTree(test.newick_filename, "simOut.fasta")
+		tr := expandedTree.ReadTree(test.newickFilename, "simOut.fasta")
 		for i := 0; i < len(tr.Fasta.Seq); i++ {
 			LoopNodes(tr, i)
 		}
