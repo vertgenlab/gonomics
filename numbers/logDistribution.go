763661926e9da517bc51fa603dc5a7505cde880b
rimangan <riley.mangan@duke.edu>
2020-09-15 13:41:17 -0400

    Updated binomial calculation to slice instead of map, per code review comments

--- numbers/logDistribution.go
+++ numbers/logDistribution.go
@@ -1,9 +1,8 @@
 package numbers
 
-/*DEBUG:
 import(
-	"fmt"
-)*/
+	//DEBUG: "fmt"
+)
 
 //BinomialDistLog returns log(BinomialDist), where log is the natural logarithm.
 //This is ideal for very small probabilities to avoid underflow.
@@ -19,14 +18,15 @@ func BinomialDistLog(n int, k int, p float64) float64 {
 //This function is similar to BinomialDistLog but passes in a map[int][]float64, where the int key
 //refers to n and the []float64 map values are the corresponding binomial coefficients for index k.
 //Useful to not recalculate the binomial coefficient each time when binomial densities must be constantly evaluated in logSpace, like in MCMC.
-func BinomialDistLogMap(n int, k int, p float64, binomMap *map[int][]float64) float64 {
-	if _, ok := (*binomMap)[n]; !ok {
-		(*binomMap)[n] = AddBinomMapEntry(n)
+func BinomialDistLogSlice(n int, k int, p float64, binomMap [][]float64) float64 {
+	//DEBUG: fmt.Printf("n: %v. k: %v. len(binomMap): %v.", n, k, len(binomMap))
+	if binomMap[n] == nil {
+		binomMap[n] = AddBinomMapEntry(n)
 	}
 	s := LogPowInt(p, k)
 	f := LogPowInt(1.0-p, n-k)
 	expression := MultiplyLog(s, f)
-	return MultiplyLog(expression, (*binomMap)[n][k])
+	return MultiplyLog(expression, binomMap[n][k])
 }
 
 //AddBinomMapEntry adds an entry to a binomMap containing a slice of binomial coefficients in logSpace for a particular n value.
