5370dabb1d94cc1e525a8c021d3d85f344b490a3
Christi Fauci <christiana.fauci@duke.edu>
2020-09-16 13:35:10 -0400

    Removed old Accuracy func

--- reconstruct/reconstruct.go
+++ reconstruct/reconstruct.go
@@ -6,37 +6,6 @@ import (
 	"log"
 )
 
-//test accuracy of the reconstruction compared to the simulation
-//func Accuracy(simFilename string, recFilename string) float64 {
-//	tot := 0.0                     //why is this here when it could just be num?
-//	sim := fasta.Read(simFilename) //produced by simulate.printSeqForNodes, returned by simulate.Simulate
-//	rec := fasta.Read(recFilename)
-//	des := "descendents_" + simFilename //refers to file created by simulate.RemoveAncestors which has only leaf nodes labelled
-//	simLeaves := fasta.Read(des)
-//	for i := 0; i < len(sim); i++ {
-//		for j := 0; j < len(simLeaves); j++ {
-//			if sim[i].Name == simLeaves[j].Name { //if current fasta is a leaf fasta
-//				sim = append(sim[:i], sim[i+1:]...)
-//				//sim is fastas of every node but the leaf nodes, so we are adding leaf fastas to the original simulation file
-//			}
-//		}
-//	}
-//	for i := 0; i < len(sim); i++ {
-//		num := 0.0
-//
-//		for k := 0; k < len(sim[0].Seq); k++ {
-//			if sim[i].Seq[k] != rec[i].Seq[k] {
-//				num = num + 1
-//			}
-//		}
-//		tot = tot + num
-//	}
-//	accuracy := tot / (float64(len(sim)) * float64(len(sim[0].Seq))) * 100.0
-//	acc := 100 - accuracy
-//	fmt.Print("accuracy over all nodes= ", acc, "%", "\n")
-//	return acc
-//}
-
 //calculate probability of switching from one base to another
 func Prob(a int, b int, t float64) float64 {
 	var p float64
