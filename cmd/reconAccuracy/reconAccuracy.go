88e950edaec6623a7ae10b5c45fbfa86f6187e1b
Christi Fauci <christiana.fauci@duke.edu>
2020-09-16 13:30:53 -0400

    updated usage

--- cmd/reconAccuracy/reconAccuracy.go
+++ cmd/reconAccuracy/reconAccuracy.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/vertgenlab/gonomics/fasta"
+	"log"
 )
 
 func ReconAccuracy(simFilename string, reconFilename string) float64 {
+	//TODO: print to file?
 	tot := 0.0
 	sim := fasta.Read(simFilename)
 	recon := fasta.Read(reconFilename)
@@ -23,4 +26,32 @@ func ReconAccuracy(simFilename string, reconFilename string) float64 {
 	acc := 100 - accuracy
 	fmt.Print("accuracy over all nodes= ", acc, "%", "\n")
 	return acc //probably a better way to do it than to print a number to the screen?
+}
+
+func usage() {
+	fmt.Print(
+		"ReconAccuracy takes in a fasta file of simulated evolution along a tree, and the reconstructed fastas of the same tree and returns the percentage accuracy of the sequences.\n" +
+			"reconAccuracy <simulationOut.fasta> <reconstructionOut.fasta> \n" +
+			//TODO: update if printing to another file
+			"options:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	var expectedNumArgs = 2
+
+	flag.Usage = usage
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	flag.Parse()
+
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Error: expecting %d arguments, but got %d\n",
+			expectedNumArgs, len(flag.Args()))
+	}
+
+	simulationOut := flag.Arg(0)
+	reconstructionOut := flag.Arg(1)
+
+	ReconAccuracy(simulationOut, reconstructionOut)
 }
