68eb341f88d61a57b9e87d77de756499d09342a1
DanielSnellings <das106@duke.edu>
2020-09-25 15:31:49 -0400

    rm prints

--- cmd/callVariants/callVariants.go
+++ cmd/callVariants/callVariants.go
@@ -44,7 +44,6 @@ func callVariants(linearRef string, graphRef string, expSamples string, normSamp
 
 	//vcf.WriteHeader(output)
 	for vcfRecord := range answer {
-		fmt.Println(time.Since(lastProgressReport).Seconds())
 		if time.Since(lastProgressReport).Seconds() > 10 {
 			lastProgressReport = time.Now()
 			log.Printf("Current Position: %s\t%d", vcfRecord.Chr, vcfRecord.Pos)
