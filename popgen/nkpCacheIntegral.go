b4bbdc5002b18a3a3f79da0c9ebc9c768674c651
rimangan <riley.mangan@duke.edu>
2020-10-21 16:49:28 -0400

    new cacheIntegral from Craig

--- popgen/nkpCacheIntegral.go
+++ popgen/nkpCacheIntegral.go
@@ -5,10 +5,24 @@ import (
 	"math"
 )
 
+func LogIntegrateStationarityCache(alpha float64, n int, k int, stepSize int, alleleFrequencyCache []float64, nkpCache [][][]float64) float64 {
+        var bins int = len(nkpCache[n][k])-1
+        var deltaX float64 = (RightBound - LeftBound) / float64(bins / stepSize)
+        var logDeltaX float64 = math.Log(deltaX)
+        var answer float64
+        answer = numbers.DivideLog(f(alpha, n, k, 0, alleleFrequencyCache, nkpCache), math.Ln2)
+        for i := stepSize; i < bins; i += stepSize {
+                answer = numbers.AddLog(answer, f(alpha, n, k, i, alleleFrequencyCache, nkpCache))
+        }
+        answer = numbers.AddLog(answer, numbers.DivideLog(f(alpha, n, k, bins, alleleFrequencyCache, nkpCache), math.Ln2))
+        return numbers.MultiplyLog(answer, logDeltaX)
+}
+
+/*
 //LogIntegrateStationarityCache is a specialized numerical integration function for logspace integrals over the stationarity distribution, used for MCMC.
 func LogIntegrateStationarityCache(alpha float64, n int, k int, stepSize int, alleleFrequencyCache []float64, nkpCache [][][]float64) float64 {
 	var bins int = len(nkpCache[n][k])-1
-	var deltaX float64 = (float64(stepSize) * (RightBound - LeftBound)) / float64(bins)
+	var deltaX float64 = (RightBound - LeftBound) / float64(bins / stepSize)
 	var logDeltaX float64 = math.Log(deltaX)
 	var currLeftIndex int = 0
 	var currRightIndex int = stepSize
@@ -28,7 +42,7 @@ func LogIntegrateStationarityCache(alpha float64, n int, k int, stepSize int, al
 		nextLeftEval = rightEval
 	}
 	return numbers.MultiplyLog(sumHeights, logDeltaX)
-}
+}*/
 
 //helper function of LogIntegrateStationarityCache, returns the function evaluation for the unadjusted stationarity distribution.
 func f(alpha float64, n int, k int, index int, alleleFrequencyCache []float64, nkpCache [][][]float64) float64 {
