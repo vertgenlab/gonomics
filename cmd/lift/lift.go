ffe9b395c132c6ca4b9a2ec9574124e3b9d89fd2
rimangan <riley.mangan@duke.edu>
2020-11-23 12:44:29 -0600

    First draft of lift with fasta

--- cmd/lift/lift.go
+++ cmd/lift/lift.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 	"github.com/vertgenlab/gonomics/chain"
 	"github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/fasta"
+	"github.com/vertgenlab/gonomics/dna"
 	"github.com/vertgenlab/gonomics/interval"
 	"log"
 )
 
-func lift(chainFile string, inFile string, outFile string) {
+const Verbose int = 0
+
+func lift(chainFile string, inFile string, outFile string, faFile string) {
 	//first task, make tree from chainFile
 	chainChan, _ := chain.GoReadToChan(chainFile)
 	var chainIntervals []interval.Interval
@@ -18,6 +22,14 @@ func lift(chainFile string, inFile string, outFile string) {
 	}
 	tree := interval.BuildTree(chainIntervals)
 	out := fileio.EasyCreate(outFile)
+	var records []*fasta.Fasta
+	var currVcf *vcf.Vcf
+	var currRef, currAlt, faBases []dna.Base
+
+	if faFile != "" {
+		records = fasta.Read(faFile)
+	}
+
 	//second task, read in intervals, find chain, and convert to new interval
 	inChan := interval.GoReadToLiftChan(inFile)
 	var overlap []interval.Interval
@@ -27,6 +39,24 @@ func lift(chainFile string, inFile string, outFile string) {
 			log.Fatalf("Multiple overlaps???")
 		}
 		i.UpdateLift(interval.LiftIntervalWithChain(overlap[0].(*chain.Chain), i))
+		//special check for lifting over VCF files
+		if faFile != "" {
+			//faFile will be given if we are lifting over VCF data.
+			currVcf = i.(*vcf.Vcf)
+
+			//first question: does the "Ref" match the destination fa at this position.
+			if fasta.QuerySeq(records, currVcf.Chr, currVcf.Pos - 1, dna.StringToBases(currVcf.Ref)) {
+				//second question: does the "Alt" also match. Can occur in corner cases such as Ref=A, Alt=AAA. Currently we don't invert but write a verbose log print.
+				if fasta.QuerySeq(records, currVcf.Chr, currVcf.Pos - 1, dna.StringToBases(currVcf.Alt)) && Verbose > 0 {
+					log.Printf("For VCF on %s at position %d, Alt and Ref both match the fasta. Ref: %s. Alt: %s.", currVcf.Chr, currVcf.Pos, currVcf.Ref, currVcf.Alt)
+				}
+			//the third case handles when the alt matches but not the ref, in which case we invert the VCF.
+			} else fasta.QuerySeq(records, currVcf.Chr, currVcf.Pos - 1, dna.StringToBases(currVcf.Alt)) {
+				InvertVcf(currVcf)
+			} else {
+				log.Fatalf("Neither the Ref nor the Alt allele matched the bases in the corresponding destination fasta location.")
+			}
+		}
 		i.WriteToFileHandle(out)
 	}
 }
@@ -42,6 +72,7 @@ func usage() {
 
 func main() {
 	var expectedNumArgs int = 2
+	var faFile *string = flag.String("faFile", "", "Specify a fasta file for Lifting VCF format files. This fasta should correspond to the destination assembly.")
 
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
@@ -56,5 +87,5 @@ func main() {
 	chainFile := flag.Arg(0)
 	inFile := flag.Arg(1)
 	outFile := flag.Arg(2)
-	lift(chainFile, inFile, outFile)
+	lift(chainFile, inFile, outFile, *faFile)
 }
