555e543c690bafef8bc417bf8fcdc2d2b0d9634f
DanielSnellings <das106@duke.edu>
2021-01-18 10:03:11 -0500

    response to comments

--- gene/fromGtf.go
+++ gene/fromGtf.go
@@ -14,115 +14,15 @@ func GtfToGene(g *gtf.Gene, ref []*fasta.Fasta) *Gene {
 	transcript := g.Transcripts[0]
 
 	answer.id = g.GeneID
+	answer.symbol = g.GeneName
 	answer.posStrand = transcript.Strand
 
 	if transcript.Strand { // If on the positive posStrand
-		answer.startPos = transcript.Start - 1
-
-		fastaMap := fasta.FastaMap(ref)
-		answer.genomeSeq = make([]dna.Base, transcript.End-(transcript.Start-1))
-		copy(answer.genomeSeq, fastaMap[transcript.Chr][transcript.Start-1:transcript.End])
-		answer.cdnaSeq = make([]dna.Base, 0, len(answer.genomeSeq))
-		answer.featureArray = make([]Feature, len(answer.genomeSeq))
-		answer.cdsStarts = make([]int, 0, len(transcript.Exons))
-		answer.cdsEnds = make([]int, 0, len(transcript.Exons))
-
-		var i int
-		var prevExonEnd int = answer.startPos
-		var currCdsPos int32
-		for _, val := range transcript.Exons {
-			for i = prevExonEnd - answer.startPos; i < val.Start-1-answer.startPos; i++ {
-				answer.featureArray[i] = Intron
-			}
-			prevExonEnd = val.End
-
-			if val.FiveUtr != nil {
-				for i = val.FiveUtr.Start - 1 - answer.startPos; i < val.FiveUtr.End-answer.startPos; i++ {
-					answer.featureArray[i] = UtrFive
-				}
-				answer.cdnaSeq = append(answer.cdnaSeq, fastaMap[transcript.Chr][val.FiveUtr.Start-1:val.FiveUtr.End]...)
-				if val.FiveUtr.End-answer.startPos > answer.utrFive.end {
-					answer.utrFive.end = val.FiveUtr.End - answer.startPos
-				}
-			}
-
-			if val.Cds != nil {
-				answer.cdsStarts = append(answer.cdsStarts, val.Cds.Start-1-answer.startPos)
-				answer.cdsEnds = append(answer.cdsEnds, val.Cds.End-1-answer.startPos)
-				answer.cdnaSeq = append(answer.cdnaSeq, fastaMap[transcript.Chr][val.Cds.Start-1:val.Cds.End]...)
-
-				for i = val.Cds.Start - 1 - answer.startPos; i < val.Cds.End-answer.startPos; i++ {
-					answer.featureArray[i] = Feature(currCdsPos)
-					currCdsPos++
-				}
-			}
-
-			if val.ThreeUtr != nil {
-				for i = val.ThreeUtr.Start - 1 - answer.startPos; i < val.ThreeUtr.End-answer.startPos; i++ {
-					answer.featureArray[i] = UtrThree
-				}
-				if answer.utrThree.start == 0 {
-					answer.utrThree.start = len(answer.cdnaSeq)
-				}
-				answer.cdnaSeq = append(answer.cdnaSeq, fastaMap[transcript.Chr][val.ThreeUtr.Start-1:val.ThreeUtr.End]...)
-			}
-		}
-
+		processExonsPos(transcript, ref, answer)
 	} else { // If on the negative posStrand
-		answer.startPos = transcript.End - 1
-
-		fastaMap := fasta.FastaMap(ref)
-		answer.genomeSeq = make([]dna.Base, transcript.End-(transcript.Start-1))
-		copy(answer.genomeSeq, fastaMap[transcript.Chr][transcript.Start-1:transcript.End])
-		dna.ReverseComplement(answer.genomeSeq)
-		answer.cdnaSeq = make([]dna.Base, 0, len(answer.genomeSeq))
-		answer.featureArray = make([]Feature, len(answer.genomeSeq))
-		answer.cdsStarts = make([]int, 0, len(transcript.Exons))
-		answer.cdsEnds = make([]int, 0, len(transcript.Exons))
-
-		var i, k int
-		var prevExonEnd int = answer.startPos
-		var val *gtf.Exon
-		var currCdsPos int32
-		for k = len(transcript.Exons) - 1; k >= 0; k-- {
-			val = transcript.Exons[k]
-			for i = answer.startPos - prevExonEnd; i < answer.startPos-(val.End-1); i++ {
-				answer.featureArray[i] = Intron
-			}
-			prevExonEnd = val.Start - 2
-
-			if val.FiveUtr != nil {
-				for i = answer.startPos - (val.FiveUtr.End - 1); i < answer.startPos-(val.FiveUtr.Start-2); i++ {
-					answer.featureArray[i] = UtrFive
-				}
-				answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answer.startPos-(val.FiveUtr.End-1):answer.startPos-(val.FiveUtr.Start-2)]...)
-				if answer.utrFive.end < answer.startPos-(val.FiveUtr.Start-2) {
-					answer.utrFive.end = answer.startPos - (val.FiveUtr.Start - 2)
-				}
-			}
-
-			if val.Cds != nil {
-				answer.cdsStarts = append(answer.cdsStarts, answer.startPos-(val.Cds.End-1))
-				answer.cdsEnds = append(answer.cdsEnds, answer.startPos-(val.Cds.Start-1))
-				answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answer.startPos-(val.Cds.End-1):answer.startPos-(val.Cds.Start-2)]...)
-
-				for i = answer.startPos - (val.Cds.End - 1); i < answer.startPos-(val.Cds.Start-2); i++ {
-					answer.featureArray[i] = Feature(currCdsPos)
-					currCdsPos++
-				}
-			}
-
-			if val.ThreeUtr != nil {
-				for i = answer.startPos - (val.ThreeUtr.End - 1); i < answer.startPos-(val.ThreeUtr.Start-2); i++ {
-					answer.featureArray[i] = UtrThree
-				}
-				if answer.utrThree.start == 0 {
-					answer.utrThree.start = len(answer.cdnaSeq)
-				}
-				answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answer.startPos-(val.ThreeUtr.End-1):answer.startPos-(val.ThreeUtr.Start-2)]...)
-			}
-		}
+		processExonsNeg(transcript, ref, answer)
 	}
+
 	answer.utrFive.start = 0
 	answer.utrThree.end = len(answer.cdnaSeq)
 	answer.utrFive.seq = answer.cdnaSeq[answer.utrFive.start:answer.utrFive.end]
@@ -153,4 +53,144 @@ func GtfToGene(g *gtf.Gene, ref []*fasta.Fasta) *Gene {
 	answer.orig.codingSeq.end = answer.utrThree.start
 	answer.orig.codingSeq.seq = answer.orig.cdnaSeq[answer.codingSeq.start:answer.codingSeq.end]
 	return answer
+}
+
+func processExonsPos(transcript *gtf.Transcript, ref []*fasta.Fasta, answer *Gene) {
+	// set simple values and initialize slices
+	answer.startPos = transcript.Start - 1
+	fastaMap := fasta.FastaMap(ref)
+	answer.genomeSeq = make([]dna.Base, transcript.End-(transcript.Start-1))
+	copy(answer.genomeSeq, fastaMap[transcript.Chr][transcript.Start-1:transcript.End])
+	answer.cdnaSeq = make([]dna.Base, 0, len(answer.genomeSeq))
+	answer.featureArray = make([]Feature, len(answer.genomeSeq))
+	answer.cdsStarts = make([]int, 0, len(transcript.Exons))
+	answer.cdsEnds = make([]int, 0, len(transcript.Exons))
+
+	// loop through exons (these are read in order of 5' UTR < CDS < 3' UTR
+	var i int
+	var prevExonEnd int = answer.startPos
+	var currCdsPos int32
+	var answerIdxStart, answerIdxEnd int
+	for _, val := range transcript.Exons {
+		for i = prevExonEnd - answer.startPos; i < val.Start-1-answer.startPos; i++ {
+			answer.featureArray[i] = Intron
+		}
+		prevExonEnd = val.End
+
+		// PROCESS 5' UTR
+		if val.FiveUtr != nil {
+			answerIdxStart = val.FiveUtr.Start - 1 - answer.startPos // pos relative to answer.genomicSeq
+			answerIdxEnd = val.FiveUtr.End - answer.startPos         // pos relative to answer.genomicSeq
+
+			for i = answerIdxStart; i < answerIdxEnd; i++ {
+				answer.featureArray[i] = UtrFive
+			}
+
+			answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answerIdxStart:answerIdxEnd]...) // get the seq from the ref fasta
+			answer.utrFive.end = answerIdxEnd                                                         // answer is in cDNA coordinates (i.e. first base of 5' UTR is zero).
+		}
+
+		// PROCESS CDS
+		if val.Cds != nil {
+			answerIdxStart = val.Cds.Start - 1 - answer.startPos
+			answerIdxEnd = val.Cds.End - answer.startPos
+			answer.cdsStarts = append(answer.cdsStarts, answerIdxStart)
+			answer.cdsEnds = append(answer.cdsEnds, answerIdxEnd-1) // end pos is closed
+			answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answerIdxStart:answerIdxEnd]...)
+
+			for i = answerIdxStart; i < answerIdxEnd; i++ {
+				answer.featureArray[i] = Feature(currCdsPos)
+				currCdsPos++
+			}
+		}
+
+		// PROCESS 3' UTR
+		if val.ThreeUtr != nil {
+			answerIdxStart = val.ThreeUtr.Start - 1 - answer.startPos
+			answerIdxEnd = val.ThreeUtr.End - answer.startPos
+
+			for i = answerIdxStart; i < answerIdxEnd; i++ {
+				answer.featureArray[i] = UtrThree
+			}
+
+			// if unset, then define the start
+			if answer.utrThree.start == 0 { // by the time the read get to 3' UTR, the start will be defined as the current len of cDNA (5' UTR + CDS)
+				answer.utrThree.start = len(answer.cdnaSeq)
+			}
+
+			answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answerIdxStart:answerIdxEnd]...)
+		}
+	}
+}
+
+func processExonsNeg(transcript *gtf.Transcript, ref []*fasta.Fasta, answer *Gene) {
+	// set simple values and initialize slices
+	answer.startPos = transcript.End - 1
+	fastaMap := fasta.FastaMap(ref)
+	answer.genomeSeq = make([]dna.Base, transcript.End-(transcript.Start-1))
+	copy(answer.genomeSeq, fastaMap[transcript.Chr][transcript.Start-1:transcript.End])
+	dna.ReverseComplement(answer.genomeSeq)
+	answer.cdnaSeq = make([]dna.Base, 0, len(answer.genomeSeq))
+	answer.featureArray = make([]Feature, len(answer.genomeSeq))
+	answer.cdsStarts = make([]int, 0, len(transcript.Exons))
+	answer.cdsEnds = make([]int, 0, len(transcript.Exons))
+
+	// loop through exons backwards (these are read in order of 5' UTR < CDS < 3' UTR
+	var i, k int
+	var prevExonEnd int = answer.startPos
+	var val *gtf.Exon
+	var currCdsPos int32
+	var answerIdxStart, answerIdxEnd int
+	for k = len(transcript.Exons) - 1; k >= 0; k-- {
+		val = transcript.Exons[k]
+
+		for i = answer.startPos - prevExonEnd; i < answer.startPos-(val.End-1); i++ {
+			answer.featureArray[i] = Intron
+		}
+		prevExonEnd = val.Start - 2
+
+		// PROCESS 5' UTR
+		if val.FiveUtr != nil {
+			answerIdxStart = answer.startPos - (val.FiveUtr.End - 1)
+			answerIdxEnd = answer.startPos - (val.FiveUtr.Start - 2)
+
+			for i = answerIdxStart; i < answerIdxEnd; i++ {
+				answer.featureArray[i] = UtrFive
+			}
+
+			answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answerIdxStart:answerIdxEnd]...)
+			answer.utrFive.end = answerIdxEnd
+		}
+
+		// PROCESS CDS
+		if val.Cds != nil {
+			answerIdxStart = answer.startPos - (val.Cds.End - 1)
+			answerIdxEnd = answer.startPos - (val.Cds.Start - 2)
+
+			answer.cdsStarts = append(answer.cdsStarts, answerIdxStart)
+			answer.cdsEnds = append(answer.cdsEnds, answerIdxEnd-1) // end pos is closed
+			answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answerIdxStart:answerIdxEnd]...)
+
+			for i = answerIdxStart; i < answerIdxEnd; i++ {
+				answer.featureArray[i] = Feature(currCdsPos)
+				currCdsPos++
+			}
+		}
+
+		// PROCESS 3' UTR
+		if val.ThreeUtr != nil {
+			answerIdxStart = answer.startPos - (val.ThreeUtr.End - 1)
+			answerIdxEnd = answer.startPos - (val.ThreeUtr.Start - 2)
+
+			for i = answerIdxStart; i < answerIdxEnd; i++ {
+				answer.featureArray[i] = UtrThree
+			}
+
+			if answer.utrThree.start == 0 { // if start is unset, then this is the first instance of a 3' UTR
+				answer.utrThree.start = len(answer.cdnaSeq) // therefore the utrThreeStart is = curr len of cDNA encoding
+			}
+
+			answer.cdnaSeq = append(answer.cdnaSeq, answer.genomeSeq[answerIdxStart:answerIdxEnd]...)
+		}
+	}
 }
