bbad01d60541a780bfb6f2e1ade6292f5914517d
DanielSnellings <das106@duke.edu>
2021-01-25 13:16:31 -0500

    copy from old branch

---
+++ dna/examine_test.go
@@ -0,0 +1,50 @@
+package dna
+
+import "testing"
+
+var alpha = StringToBases("ACTGacgtAAACC--ACacgnnnNNNactg")
+var beta = StringToBases("ACGGacgtAATCC--ACacgnnnNCNaCtg")
+
+func TestCount(t *testing.T) {
+	ACount, CCount, GCount, TCount, NCount, aCount, cCount, gCount, tCount, nCount, gapCount := Count(alpha)
+	if ACount != 5 || CCount != 4 || GCount != 1 || TCount != 1 || NCount != 3 || aCount != 3 || cCount != 3 ||
+		gCount != 3 || tCount != 2 || nCount != 3 || gapCount != 2 {
+		t.Errorf("problem with Count")
+	}
+}
+
+func TestCountMask(t *testing.T) {
+	umC, mC, gC := CountMask(alpha)
+	if umC != 14 || mC != 14 || gC != 2 {
+		t.Errorf("problem with CountMask")
+	}
+}
+
+func TestCountGaps(t *testing.T) {
+	if CountGaps(alpha) != 2 {
+		t.Errorf("problem with CountGaps")
+	}
+}
+
+func TestDist(t *testing.T) {
+	actual := Dist(alpha, beta)
+	if actual != 4 {
+		t.Errorf("problem with Dist")
+	}
+}
+
+func TestCountBase(t *testing.T) {
+	actual := CountBase(alpha, N)
+	if actual != 3 {
+		t.Errorf("problem with CountBase")
+	}
+}
+
+func TestDefineBase(t *testing.T) {
+	if !DefineBase(A) || !DefineBase(C) || !DefineBase(G) || !DefineBase(T) ||
+		!DefineBase(LowerA) || !DefineBase(LowerC) || !DefineBase(LowerG) ||
+		!DefineBase(LowerT) || DefineBase(N) || DefineBase(LowerN) ||
+		DefineBase(Gap) || DefineBase(Dot) || DefineBase(Nil) {
+		t.Errorf("problem with DefineBase")
+	}
+}
--- dna/modify.go
+++ dna/modify.go
@@ -1,53 +1,84 @@
 package dna
 
 import (
-	"fmt"
-	"github.com/vertgenlab/gonomics/common"
+	"errors"
 	"log"
 )
 
+var (
+	ErrInvalidInterval          = errors.New("deletion interval is not valid")
+	ErrInvalidInsertionPosition = errors.New("insertion position is not valid")
+	ErrUnrecognizedBase         = errors.New("input base was not recognized")
+)
+
+// ToUpper changes the input base to uppercase.
 func ToUpper(b Base) Base {
-	if b == Gap || b == Dot {
-		return b
-	} else if b > N {
-		return b - 5
-	} else {
+	switch b {
+	case LowerA:
+		return A
+	case LowerC:
+		return C
+	case LowerG:
+		return G
+	case LowerT:
+		return T
+	case LowerN:
+		return N
+	default:
 		return b
 	}
 }
 
+// ToLower changes the input base to lowercase.
 func ToLower(b Base) Base {
-	if b == Gap || b == Dot {
-		return b
-	} else if b < a {
-		return b + 5
-	} else {
+	switch b {
+	case A:
+		return LowerA
+	case C:
+		return LowerC
+	case G:
+		return LowerG
+	case T:
+		return LowerT
+	case N:
+		return LowerN
+	default:
 		return b
 	}
 }
 
-// start is closed, end is open, both are zero-based
+// RangeToUpper changes the bases in a set range to uppercase.
+// start is closed, end is open, both are zero-based.
 func RangeToUpper(bases []Base, start int, end int) {
 	for i := start; i < end; i++ {
 		bases[i] = ToUpper(bases[i])
 	}
 }
 
-// start is closed, end is open, both are zero-based
+// RangeToLower changes the bases in a set range to lowercase.
+// start is closed, end is open, both are zero-based.
 func RangeToLower(bases []Base, start int, end int) {
 	for i := start; i < end; i++ {
 		bases[i] = ToLower(bases[i])
 	}
 }
 
+// AllToUpper changes all bases in a sequence to uppercase.
 func AllToUpper(bases []Base) {
 	RangeToUpper(bases, 0, len(bases))
 }
 
+// AllToLower changes all bases in a sequence to lowercase.
 func AllToLower(bases []Base) {
 	RangeToLower(bases, 0, len(bases))
 }
 
+// complementArray is an efficient lookup for the complement of the input base.
+// intended to remain as a private array to help the Complement functions.
+// panics if value input is not a valid Base.
+var complementArray = []Base{T, G, C, A, N, LowerT, LowerG, LowerC, LowerA, LowerN, Gap, Dot, Nil}
+
+// ComplementSingleBase returns the nucleotide complementary to the input base.
 func ComplementSingleBase(b Base) Base {
 	switch b {
 	case A:
@@ -60,80 +91,81 @@ func ComplementSingleBase(b Base) Base {
 		return A
 	case N:
 		return N
-	case a:
-		return t
-	case c:
-		return g
-	case g:
-		return c
-	case t:
-		return a
-	case n:
-		return n
+	case LowerA:
+		return LowerT
+	case LowerC:
+		return LowerG
+	case LowerG:
+		return LowerC
+	case LowerT:
+		return LowerA
+	case LowerN:
+		return LowerN
 	case Gap:
 		return Gap
 	case Dot:
 		return Dot
+	case Nil:
+		return Nil
 	default:
-		common.ExitIfError(fmt.Errorf("Error: trying to reverse ComplementSingleBase an unexpected base %d", b))
-		return N
+		log.Panicf("unrecognized base %v", b)
+		return Nil
 	}
 }
 
-func swap(alpha Base, beta Base) (Base, Base) {
-	return beta, alpha
-}
-
+// ReverseComplement reverses a sequence of bases and complements each base.
+// Used to switch strands and maintain 5' -> 3' orientation.
 func ReverseComplement(bases []Base) {
 	for i, j := 0, len(bases)-1; i <= j; i, j = i+1, j-1 {
-		bases[i], bases[j] = swap(ComplementSingleBase(bases[i]), ComplementSingleBase(bases[j]))
+		bases[i], bases[j] = complementArray[bases[j]], complementArray[bases[i]]
 	}
 }
 
+// Complement all bases in a sequence of bases.
 func Complement(bases []Base) {
-	for i := 0; i < len(bases); i++ {
-		bases[i] = ComplementSingleBase(bases[i])
+	for i := range bases {
+		bases[i] = complementArray[bases[i]]
 	}
 }
 
+// RemoveGaps returns a sequence of bases with no gaps.
 func RemoveGaps(bases []Base) []Base {
-	var ans []Base
-	for i := 0; i < len(bases); i++ {
-		if bases[i] != Gap {
+	return RemoveBase(bases, Gap)
+}
+
+// RemoveBase returns a sequence of bases without any of the designated base.
+func RemoveBase(bases []Base, baseToRemove Base) []Base {
+	ans := make([]Base, 0, len(bases))
+	for i := range bases {
+		if bases[i] != baseToRemove {
 			ans = append(ans, bases[i])
 		}
 	}
 	return ans
 }
 
-func RemoveBase(bases []Base, b Base) []Base {
-	var ans []Base
-	for i := 0; i < len(bases); i++ {
-		if bases[i] != b {
-			ans = append(ans, bases[i])
-		}
-	}
-	return ans
-}
-
-// all base positions are zero based and left closed, right open
-func Delete(seq []Base, delStart int64, delEnd int64) []Base {
-	if delStart >= delEnd || delStart < 0 || delEnd > int64(len(seq)) {
-		log.Fatalf("Error: deletion interval from %d to %d is not valid.\n", delStart, delEnd)
+// Delete removes bases from a sequence of bases.
+// all base positions are zero based and left closed, right open.
+func Delete(seq []Base, delStart int, delEnd int) []Base {
+	if delStart >= delEnd || delStart < 0 || delEnd > len(seq) {
+		log.Panicf("a deletion on sequence of length %d with start of %d and length of %d is invalid", len(seq), delStart, delEnd)
 	}
 	return append(seq[:delStart], seq[delEnd:]...)
 }
 
+// Insert adds bases to a sequence of bases.
 // base position is zero-based, insertion happens before specified base
-// giving the length of the sequence puts the insertion at the end
-func Insert(seq []Base, insPos int64, insSeq []Base) []Base {
-	if insPos < 0 || insPos > int64(len(seq)) {
-		log.Fatalf("Error: insertion location of %d is not valid.\n", insPos)
+// giving the length of the sequence puts the insertion at the end.
+func Insert(seq []Base, insPos int, insSeq []Base) []Base {
+	if insPos < 0 || insPos > len(seq) {
+		log.Panicf("an insertion on sequence of length %d with start of %d is invalid", len(seq), insPos)
 	}
 	return append(seq[:insPos], append(insSeq, seq[insPos:]...)...)
 }
 
-// all base positions are zero based and left closed, right open
-func Replace(seq []Base, start int64, end int64, insSeq []Base) []Base {
+// Replace performs both a deletion and an insertion,
+// replacing the input interval with the input insSeq.
+// all base positions are zero based and left closed, right open.
+func Replace(seq []Base, start int, end int, insSeq []Base) []Base {
 	return append(seq[:start], append(insSeq, seq[end:]...)...)
 }
