2ae97f4f873a2cf800a066b9717bd51325924ac5
RavenYL <yanting.raven.luo@gmail.com>
2021-02-01 16:58:20 -0500

    changed k-1 to 0 for ref/target/ins lines. added a 2nd set of testdata

--- cmd/mafIndels/mafIndels.go
+++ cmd/mafIndels/mafIndels.go
@@ -16,13 +16,13 @@ func mafIndels(in_maf string, species_ins string, species_del string, outIns_bed
 
 	//go through each line
 	for i, _ := range mafRecords { //each i is a block
-		for k := 1; k < len(mafRecords[i].Species); k++ { //each k is a line. Start loop at k=1, so that checking line k-1 starts at 0 and does not index out of range
+		for k := 1; k < len(mafRecords[i].Species); k++ { //each k is a line. Start loop at k=1 because that is the lowest possible index to find species_del, which is query
 
 			//convert maf to bed, start with getting assembly because it is needed to verify species_ins and species_del
 			//here I assume only pairwise alignment, not >2 species
-			//here I assume species_ins is target (because the 1st line in the block is a line, target is 2nd line in the block, k=1); species_del is query (3rd line in the block, k=2)
+			//here I assume species_ins is target (the 1st line in the block is a line but not included in mafRecords[i].Species, target is 2nd line in the block but 1st line in mafRecords[i].Species, index 0); species_del is query (3rd line in the block but 2nd line in mafRecords[i].Species, index 1 or higher)
 			assembly_del, chrom_del := maf.SrcToAssemblyAndChrom(mafRecords[i].Species[k].Src)   //start with species_del, get assembly (e.g. rheMac10), chrom (e.g. chrI)
-			assembly_ins, chrom_ins := maf.SrcToAssemblyAndChrom(mafRecords[i].Species[k-1].Src) //then find corresponding species_ins line, same block, 1 line above since pairwise maf
+			assembly_ins, chrom_ins := maf.SrcToAssemblyAndChrom(mafRecords[i].Species[0].Src) //then find corresponding species_ins line, which is target, index 0
 			//TODO: to improve clarity, consider using assembly_k first, and then setting assembly_del:=assembly_k if in fact the k line is a del line
 
 			//get eC species_del lines
@@ -30,14 +30,14 @@ func mafIndels(in_maf string, species_ins string, species_del string, outIns_bed
 				if mafRecords[i].Species[k].ELine.Status == 'C' && assembly_del == species_del { //I decided to check for both Status and Src here because they are on the same data level
 
 					//get corresponding s species_ins lines
-					if assembly_ins != species_ins { //verify line k-1 is indeed species_ins
+					if assembly_ins != species_ins { //verify line 0 is indeed species_ins
 						log.Fatalf("species_ins was incorrect. Please check you have a pairwise maf file, and entered species_ins and species_del correctly") //otherwise fatal
 					}
-					if mafRecords[i].Species[k-1].SLine != nil { //if corresponding species_ins line is an s line
+					if mafRecords[i].Species[0].SLine != nil { //if corresponding species_ins line is an s line
 
 						//convert maf to bed, continued
 						current_del := bed.Bed{Chrom: chrom_del, ChromStart: mafRecords[i].Species[k].ELine.Start, ChromEnd: mafRecords[i].Species[k].ELine.Start + mafRecords[i].Species[k].ELine.Size, Name: "del", Score: int64(mafRecords[i].Score)} //get chrom,start,end,name,score
-						current_ins := bed.Bed{Chrom: chrom_ins, ChromStart: mafRecords[i].Species[k-1].SLine.Start, ChromEnd: mafRecords[i].Species[k-1].SLine.Start + mafRecords[i].Species[k-1].SLine.Size, Name: "ins", Score: int64(mafRecords[i].Score)}
+						current_ins := bed.Bed{Chrom: chrom_ins, ChromStart: mafRecords[i].Species[0].SLine.Start, ChromEnd: mafRecords[i].Species[0].SLine.Start + mafRecords[i].Species[0].SLine.Size, Name: "ins", Score: int64(mafRecords[i].Score)}
 						bedList_del = append(bedList_del, &current_del) //append to growing bed
 						bedList_ins = append(bedList_ins, &current_ins)
 					}
