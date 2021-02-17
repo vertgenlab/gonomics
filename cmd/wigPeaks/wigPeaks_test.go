51098b1dac45187e62167fd8f452d970ff0a6592
RavenYL <yanting.raven.luo@gmail.com>
2021-02-16 20:05:21 -0500

    fixed bugs, go test passed, added fileio.EasyCreate

--- cmd/wigPeaks/wigPeaks_test.go
+++ cmd/wigPeaks/wigPeaks_test.go
@@ -14,7 +14,7 @@ var wigPeaksTests = []struct {
 	threshold   float64
 }{
 	{"testdata/in_wig_1.wig", "testdata/out_bed_1.bed", 20},
-	//{"testdata/in_wig_2.wig", "testdata/out_bed_2.bed", 50},
+	{"testdata/in_wig_2.wig", "testdata/out_bed_2.bed", 50},
 }
 
 func TestWigPeaks(t *testing.T) {
@@ -26,6 +26,7 @@ func TestWigPeaks(t *testing.T) {
 		if !bed.AllAreEqual(records, expected) {
 			t.Errorf("Error in wigPeaks.")
 		}
+		//DEBUG: don't delete out_bed_tmp.bed, unimport common, os
 		err := os.Remove("out_bed_tmp.bed")
 		if err != nil {
 			common.ExitIfError(err)
