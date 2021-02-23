3b1dd30c284d16245194786bfe3eb45c59f6a707
Daniel <daniel.snellings@duke.edu>
2021-02-23 09:51:55 -0500

    fasta.go

--- fasta/fasta.go
+++ fasta/fasta.go
@@ -3,13 +3,11 @@ package fasta
 
 import (
 	"fmt"
-	"github.com/vertgenlab/gonomics/common"
 	"github.com/vertgenlab/gonomics/dna"
 	"github.com/vertgenlab/gonomics/exception"
 	"github.com/vertgenlab/gonomics/fileio"
 	"io"
 	"log"
-	"os"
 	"strings"
 )
 
@@ -19,12 +17,15 @@ type Fasta struct {
 	Seq  []dna.Base
 }
 
-// Read in a fasta file to a []*Fasta struct. File must begin with
+// Read in a fasta file to a []*Fasta struct. All sequence records must be preceded by a name line starting with '>'.
+// Each record must have a unique sequence name
 func Read(filename string) []*Fasta {
 	var line string
+	var name string
 	var answer []*Fasta
 	var seqIdx int = -1
 	var doneReading bool = false
+	usedSeqNames := make(map[string]bool)
 
 	file := fileio.EasyOpen(filename)
 
@@ -35,8 +36,14 @@ func Read(filename string) []*Fasta {
 
 	for line, doneReading = fileio.EasyNextRealLine(file); !doneReading; line, doneReading = fileio.EasyNextRealLine(file) {
 		if strings.HasPrefix(line, ">") {
-			answer = append(answer, &Fasta{Name: line[1:]})
-			seqIdx++
+			name = line[1:]
+			if usedSeqNames[name] {
+				log.Fatalf("ERROR: %s is used as the name for multiple records. Names must be unique.", name)
+			} else {
+				usedSeqNames[name] = true
+				answer = append(answer, &Fasta{Name: name})
+				seqIdx++
+			}
 		} else {
 			answer[seqIdx].Seq = append(answer[seqIdx].Seq, dna.StringToBases(line)...)
 		}
@@ -46,90 +53,67 @@ func Read(filename string) []*Fasta {
 	return answer
 }
 
-//Dictionary/hash map look up of sequence by name
+// FastaMap converts the output of the Read function to a map of sequences keyed to the sequences name.
 func FastaMap(ref []*Fasta) map[string][]dna.Base {
 	m := make(map[string][]dna.Base)
-	var curr *Fasta
-	for i := 0; i < len(ref); i++ {
-		curr = ref[i]
-		_, ok := m[curr.Name]
+	for i := range ref {
+		_, ok := m[ref[i].Name]
 		if !ok {
-			m[curr.Name] = curr.Seq
+			m[ref[i].Name] = ref[i].Seq
 		} else {
-			log.Fatalf("Fasta slice has duplicate names")
+			log.Panicf("%s used for multiple fasta records. record names must be unique.", ref[i].Name)
 		}
 	}
 	return m
 }
 
-func WriteToSplitChr(filename string, records []*Fasta) {
-	for _, rec := range records {
-		Write(filename+rec.Name+".fa", []*Fasta{rec})
-	}
-}
-
-func WriteToFileHandle(file io.Writer, records []*Fasta, lineLength int) {
-	var err error
-	for _, rec := range records {
-		_, err = fmt.Fprintf(file, ">%s\n", rec.Name)
-		common.ExitIfError(err)
-		for i := 0; i < len(rec.Seq); i += lineLength {
-			if i+lineLength > len(rec.Seq) {
-				_, err = fmt.Fprintf(file, "%s\n", dna.BasesToString(rec.Seq[i:]))
-				common.ExitIfError(err)
-			} else {
-				_, err = fmt.Fprintf(file, "%s\n", dna.BasesToString(rec.Seq[i:i+lineLength]))
-				common.ExitIfError(err)
-			}
-		}
-	}
-}
-
+// Write a fasta to input filename. Output fastas have line length of 50.
 func Write(filename string, records []*Fasta) {
 	lineLength := 50
 	file := fileio.EasyCreate(filename)
-	defer file.Close()
-
 	WriteToFileHandle(file, records, lineLength)
+	exception.WarningOnErr(file.Close())
 }
 
+// WriteToFileHanle writes a slice of fasta records to a given io.Writer instead of creating
+// a new io.Writer as is done in the Write function.
+func WriteToFileHandle(file io.Writer, records []*Fasta, lineLength int) {
+	for _, rec := range records {
+		WriteFasta(file, rec, lineLength)
+	}
+}
+
+// WriteFasta writes a single fasta record to an io.Writer.
 func WriteFasta(file io.Writer, rec *Fasta, lineLength int) {
 	var err error
 	_, err = fmt.Fprintf(file, ">%s\n", rec.Name)
-	common.ExitIfError(err)
+	exception.PanicOnErr(err)
 	for i := 0; i < len(rec.Seq); i += lineLength {
 		if i+lineLength > len(rec.Seq) {
 			_, err = fmt.Fprintf(file, "%s\n", dna.BasesToString(rec.Seq[i:]))
-			common.ExitIfError(err)
+			exception.PanicOnErr(err)
 		} else {
 			_, err = fmt.Fprintf(file, "%s\n", dna.BasesToString(rec.Seq[i:i+lineLength]))
-			common.ExitIfError(err)
+			exception.PanicOnErr(err)
 		}
 	}
 }
 
-func WriteGroups(filename string, groups [][]*Fasta) error {
-	lineLength := 50
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
+// SplitWrite writes each fasta record to its own file.
+func SplitWrite(filename string, records []*Fasta) {
+	for _, rec := range records {
+		Write(filename+"_"+rec.Name+"_"+".fasta", []*Fasta{rec})
 	}
-	defer file.Close()
-
-	for i, _ := range groups {
-		WriteToFileHandle(file, groups[i], lineLength)
-		_, err = fmt.Fprint(file, "\n")
-		common.ExitIfError(err)
-	}
-	return nil
 }
 
-func CreateAllGaps(name string, numGaps int64) *Fasta {
+// CreateAllGaps creates a fasta record where the sequence is all gaps of length numGaps.
+func CreateAllGaps(name string, numGaps int) *Fasta {
 	answer := Fasta{Name: name, Seq: dna.CreateAllGaps(numGaps)}
 	return &answer
 }
 
-func CreateAllNs(name string, numGaps int64) *Fasta {
-	answer := Fasta{Name: name, Seq: dna.CreateAllNs(numGaps)}
+// CreateAllNs creates a fasta record where the sequence is all Ns of length numN
+func CreateAllNs(name string, numN int) *Fasta {
+	answer := Fasta{Name: name, Seq: dna.CreateAllNs(numN)}
 	return &answer
 }
