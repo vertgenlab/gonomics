ff8965d4710b6096410e5a47972a46ac4fa00622
Christi Fauci <christiana.fauci@duke.edu>
2021-03-03 16:41:00 -0500

    correction to test

--- expandedTree/expandedTree_test.go
+++ expandedTree/expandedTree_test.go
@@ -9,15 +9,18 @@ var actualName = "root"
 
 func TestReadTree(t *testing.T) {
 	tree, err := ReadTree("testdata/newick.txt", "testdata/fasta.fasta")
+	treeNodes := GetTree(tree)
 	if err != nil {
-		log.Print("Problem in ReadTree")
+		log.Fatal("Problem in ReadTree")
 	} else {
 		if tree.Name != actualName {
 			log.Fatal("Incorrect name in tree")
 		}
-		if tree.Name == "A" {
-			if tree.BranchLength != 0.10 {
-				log.Fatal("Wrong branch length for leaf A")
+		for b := 0; b < len(treeNodes); b++ {
+			if treeNodes[b].Name == "A" {
+				if treeNodes[b].BranchLength != 0.20 {
+					log.Fatal("incorrect branch length assignment")
+				}
 			}
 		}
 	}
