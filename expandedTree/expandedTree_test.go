247b70f58743fefcfb16b04a7625586ce489589e
Christi Fauci <christiana.fauci@duke.edu>
2021-03-03 16:35:53 -0500

    created test for expandedTree.ReadTree

--- expandedTree/expandedTree_test.go
+++ expandedTree/expandedTree_test.go
@@ -1,1 +1,24 @@
 package expandedTree
+
+import (
+	"log"
+	"testing"
+)
+
+var actualName = "root"
+
+func TestReadTree(t *testing.T) {
+	tree, err := ReadTree("testdata/newick.txt", "testdata/fasta.fasta")
+	if err != nil {
+		log.Print("Problem in ReadTree")
+	} else {
+		if tree.Name != actualName {
+			log.Fatal("Incorrect name in tree")
+		}
+		if tree.Name == "A" {
+			if tree.BranchLength != 0.10 {
+				log.Fatal("Wrong branch length for leaf A")
+			}
+		}
+	}
+}
