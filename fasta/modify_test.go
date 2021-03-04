d4d5ed03e7098f9493e72a8a78e13c4e7d3971af
Daniel <daniel.snellings@duke.edu>
2021-03-03 19:12:15 -0500

    added testdata

--- fasta/modify_test.go
+++ fasta/modify_test.go
@@ -1,6 +1,22 @@
 package fasta
 
-import "testing"
+import (
+	"github.com/vertgenlab/gonomics/dna"
+	"testing"
+)
+
+var seqThreeA = dna.StringToBases("ACGTacgTCATCATCATTACTACTAC")
+var seqThreeB = dna.StringToBases("acgtACGTACGT")
+var seqThreeC = dna.StringToBases("ACGTACGTACGTT")
+var rcSeqThreeA = dna.StringToBases("GTAGTAGTAATGATGATGAcgtACGT")
+var rcSeqThreeB = dna.StringToBases("ACGTACGTacgt")
+var rcSeqThreeC = dna.StringToBases("AACGTACGTACGT")
+var allRevCompTests = []struct {
+	input    []Fasta
+	expected []Fasta
+}{
+	{[]Fasta{{"apple", seqThreeA}, {"banana", seqThreeB}, {"carrot", seqThreeC}}, []Fasta{{"apple", rcSeqThreeA}, {"banana", rcSeqThreeB}, {"carrot", rcSeqThreeC}}},
+}
 
 func TestRemove(t *testing.T) {
 	for _, test := range readWriteTests {
