d8e8acec0d34ca66b654dfe3e2ca85817004ddfc
Christi Fauci <christiana.fauci@duke.edu>
2021-03-04 10:19:09 -0500

    added failure if node was not found

--- expandedTree/expandedTree_test.go
+++ expandedTree/expandedTree_test.go
@@ -5,23 +5,27 @@ import (
 	"testing"
 )
 
-var actualName = "root"
-
 func TestReadTree(t *testing.T) {
+	var actualName = "root"
+	var aFound = false
 	tree, err := ReadTree("testdata/newick.txt", "testdata/fasta.fasta")
 	treeNodes := GetTree(tree)
 	if err != nil {
 		log.Fatal("Problem in ReadTree")
 	} else {
 		if tree.Name != actualName {
-			log.Fatal("Incorrect name in tree")
+			log.Fatal("Incorrect name at root")
 		}
 		for b := 0; b < len(treeNodes); b++ {
 			if treeNodes[b].Name == "A" {
+				aFound = true
 				if treeNodes[b].BranchLength != 0.20 {
 					log.Fatal("incorrect branch length assignment")
 				}
 			}
+		}
+		if !aFound {
+			log.Fatal("A node not found in tree")
 		}
 	}
 }
