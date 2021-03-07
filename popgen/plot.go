3d0b15e642127618367b80e20098f9ef94884c3a
rimangan <riley.mangan@duke.edu>
2021-03-06 19:00:48 -0500

    A new plot emerges

---
+++ popgen/plot.go
@@ -0,0 +1,36 @@
+package popgen
+
+import (
+	"fmt"
+	"github.com/vertgenlab/gonomics/fileio"
+)
+
+func PlotAfsF(alpha float64, n int, outFile string) {
+	out := fileio.EasyCreate(outFile)
+	allN := []int{n}
+	binomCache := BuildBinomCache(allN)
+
+	fmt.Fprintf(out, "Frequency\tF\n")
+
+	for i := 1; i < n; i++ {
+		fmt.Fprintf(out, "%v\t%e\n", i, AFSSampleDensity(n, i, alpha, binomCache))
+	}
+	out.Close()
+}
+
+func PlotAfsPmf(alpha float64, n int, outFile string) {
+	out := fileio.EasyCreate(outFile)
+
+	allN := []int{n}
+	binomCache := BuildBinomCache(allN)
+	//DEBUG:fmt.Printf("Len BinomCache:%v. Entry at index n is of length: %v.\n", len(binomCache), len(binomCache[n]))
+
+	//a short header is written to the file
+	fmt.Fprintf(out, "Frequency\tProbability\n")
+	//for each possible allele frequency for a segregating site, we calculate the probability to the file
+	for i := 1; i < n; i++ {
+		fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbability(i, n, alpha, binomCache))
+	}
+
+	out.Close()
+}
