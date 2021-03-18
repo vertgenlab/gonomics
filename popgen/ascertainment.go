04153d284f16be669b4f0003fc3cebe807496ffc
rimangan <riley.mangan@duke.edu>
2021-03-17 23:16:31 -0400

    runtime optimization

--- popgen/ascertainment.go
+++ popgen/ascertainment.go
@@ -1,7 +1,50 @@
 package popgen
 
-import "github.com/vertgenlab/gonomics/numbers"
+import (
+	"github.com/vertgenlab/gonomics/numbers"
+	"math"
+)
 
+//BuildFCache builds a slice of len(n) where each index i contains F(i | n, alpha), where F is popgen.AFSSampleDensity
+func BuildFCache(n int, alpha float64, binomCache [][]float64) []float64 {
+	var answer []float64 = make([]float64, n, n)
+	for j := 1; j < n; j++ {
+		answer[j] = AFSSampleDensity(n, j, alpha, binomCache)
+	}
+	return answer
+}
+
+//GetFCacheSum uses the fCAche built in BuildFCache and calculates the sum from j=1 to n-1
+func GetFCacheSum(fCache []float64) float64 {
+	var answer float64 = math.Inf(-1)
+	for j := 1; j < len(fCache); j++ {
+		answer = numbers.AddLog(answer, fCache[j])
+	}
+	return answer
+}
+
+//AncestralAscertainmentDenominator is P(Asc | alpha), a constant normalizing factor for P(i | n, alpha) with the ancestral ascertainment correction.
+func AncestralAscertainmentDenominator(fCache []float64, FCS float64, d int) float64 {
+	var answer float64 = math.Inf(-1)
+	var current float64
+	for j := 1; j < len(fCache); j++ {
+		current = numbers.MultiplyLog(numbers.DivideLog(fCache[j], FCS), AncestralAscertainmentProbability(len(fCache), j, d))
+		answer = numbers.AddLog(answer, current)
+	}
+	return answer
+}
+
+func DerivedAscertainmentDenominator(fCache []float64, FCS float64, d int) float64 {
+	var answer float64 = math.Inf(-1)
+	var current float64
+	for j := 1; j < len(fCache); j++ {
+		current = numbers.MultiplyLog(numbers.DivideLog(fCache[j], FCS), DerivedAscertainmentProbability(len(fCache), j, d))
+		answer = numbers.AddLog(answer, current)
+	}
+	return answer
+}
+
+//AncestralAscertainmentProbability returns P(Asc | i, alpha) for ancestral allele ascertainment corrections.
 func AncestralAscertainmentProbability(n int, i int, d int) float64 {
 	return numbers.DivideLog(numbers.BinomCoefficientLog(n-i, d), numbers.BinomCoefficientLog(n, d))
 }
@@ -10,35 +53,20 @@ func DerivedAscertainmentProbability(n int, i int, d int) float64 {
 	return numbers.DivideLog(numbers.BinomCoefficientLog(i, d), numbers.BinomCoefficientLog(n, d))
 }
 
-func AncestralAscertainmentDenominator(alpha float64, n int, d int, binomCache [][]float64) float64 {
-	var answer float64 = 1.0 //0 in logSpace
-	var iteration float64
+func AlleleFrequencyProbabilityAncestralAscertainment(alpha float64, i int, n int, d int, binomCache [][]float64) float64 {
+	fCache := BuildFCache(n, alpha, binomCache)
+	FCS := GetFCacheSum(fCache)
+	pIgivenAlpha := numbers.DivideLog(fCache[i], FCS)
 
-	for j := 1; j < n; j++ {//sum from j=1 to n-1
-		iteration = numbers.MultiplyLog(AlleleFrequencyProbability(j, n, alpha, binomCache), AncestralAscertainmentProbability(n, j, d))
-		answer = numbers.AddLog(answer, iteration)
-	}
-
-	return answer
-}
-
-func DerivedAscertainmentDenominator(alpha float64, n int, d int, binomCache [][]float64) float64 {
-	var answer float64 = 0.0
-	var iteration float64
-
-	for j := 1; j < n; j++ {//sum from j=1 to n-1
-		iteration = numbers.MultiplyLog(AlleleFrequencyProbability(j, n, alpha, binomCache), DerivedAscertainmentProbability(n, j, d))
-		answer = numbers.AddLog(answer, iteration)
-	}
-	return answer
+	return numbers.DivideLog(numbers.MultiplyLog(pIgivenAlpha, AncestralAscertainmentProbability(n, i, d)), AncestralAscertainmentDenominator(fCache, FCS, d))
 }
 
 func AlleleFrequencyProbabilityDerivedAscertainment(alpha float64, i int, n int, d int, binomCache [][]float64) float64 {
-	return numbers.DivideLog(numbers.MultiplyLog(AlleleFrequencyProbability(i, n, alpha, binomCache), DerivedAscertainmentProbability(n, i, d)), DerivedAscertainmentDenominator(alpha, n, d, binomCache))
-}
+	fCache := BuildFCache(n, alpha, binomCache)
+	FCS := GetFCacheSum(fCache)
+	pIgivenAlpha := numbers.DivideLog(fCache[i], FCS)
 
-func AlleleFrequencyProbabilityAncestralAscertainment(alpha float64, i int, n int, d int, binomCache [][]float64) float64 {
-	return numbers.DivideLog(numbers.MultiplyLog(AlleleFrequencyProbability(i, n, alpha, binomCache), AncestralAscertainmentProbability(n, i, d)), AncestralAscertainmentDenominator(alpha, n, d, binomCache))
+	return numbers.DivideLog(numbers.MultiplyLog(pIgivenAlpha, DerivedAscertainmentProbability(n, i, d)), DerivedAscertainmentDenominator(fCache, FCS, d))
 }
 
 //AfsLikelihoodDerivedAscertainment is like AFSLikelihood, but makes a correction for divergence-based ascertainment when variant sets were selected for derived alleles between two groups of d individuals.
