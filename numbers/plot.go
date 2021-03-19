3a27598c26ba2d520cf1a076f85bb035f1157b0e
rimangan <riley.mangan@duke.edu>
2021-03-19 12:48:17 -0400

    small fix

--- numbers/plot.go
+++ numbers/plot.go
@@ -10,7 +10,6 @@ import (
 //Half-closed interval [left, right)
 func Plot(f func(float64) float64, left float64, right float64, bins int, outFile string) {
 	out := fileio.EasyCreate(outFile)
-	defer out.Close()
 	var step float64 = (right - left) / float64(bins)
 	var current float64 = left
 
@@ -20,6 +19,7 @@ func Plot(f func(float64) float64, left float64, right float64, bins int, outFil
 		fmt.Fprintf(out, "%f\t%f\n", current, f(current))
 		current = current + step
 	}
+	out.Close()
 }
 
 //PlotBinomCoefficient writes binomial coefficients (n choose k) from k=1 to k=n-1 to an output file for downstream visualization.
@@ -30,6 +30,5 @@ func PlotBinomCoefficient(n int, outFile string) {
 	for i := 1; i < n; i++ { //all possible choose, not including 0 and n as i arguments.
 		fmt.Fprintf(out, "%v\t%v\n", i, BinomCoefficientLog(n, i))
 	}
-
 	out.Close()
 }
