79961d06af1a5d3b3338049a6a26c801710b2f18
rimangan <riley.mangan@duke.edu>
2021-04-24 17:51:53 -0400

    A new plot unfolds

--- popgen/plot.go
+++ popgen/plot.go
@@ -32,6 +32,42 @@ func PlotAfsFCareless(alpha float64, n int, outFile string) {
 	exception.PanicOnErr(err)
 }
 
+func PlotAfsPmfAncestral(alpha float64, n int, outFile string, integralError float64) {
+	out := fileio.EasyCreate(outFile)
+
+	allN := []int{n}
+	binomCache := BuildBinomCache(allN)
+	//DEBUG:fmt.Printf("Len BinomCache:%v. Entry at index n is of length: %v.\n", len(binomCache), len(binomCache[n]))
+
+	//a short header is written to the file
+	fmt.Fprintf(out, "Frequency\tProbability\n")
+	//for each possible allele frequency for a segregating site, we calculate the probability to the file
+	for i := 1; i < n; i++ {
+		fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbabilityAncestralAscertainment(alpha, i, n, 1, binomCache, integralError))
+	}
+
+	err := out.Close()
+	exception.PanicOnErr(err)
+}
+
+func PlotAfsPmfDerived(alpha float64, n int, outFile string, integralError float64) {
+	out := fileio.EasyCreate(outFile)
+
+	allN := []int{n}
+	binomCache := BuildBinomCache(allN)
+	//DEBUG:fmt.Printf("Len BinomCache:%v. Entry at index n is of length: %v.\n", len(binomCache), len(binomCache[n]))
+
+	//a short header is written to the file
+	fmt.Fprintf(out, "Frequency\tProbability\n")
+	//for each possible allele frequency for a segregating site, we calculate the probability to the file
+	for i := 1; i < n; i++ {
+		fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbabilityDerivedAscertainment(alpha, i, n, 1, binomCache, integralError))
+	}
+
+	err := out.Close()
+	exception.PanicOnErr(err)
+}
+
 //PlotAfsPmf writes the allele frequency probability mass function (AlleleFrequencyProbability) to an output file for downstream visualization.
 func PlotAfsPmf(alpha float64, n int, outFile string, integralError float64) {
 	out := fileio.EasyCreate(outFile)
@@ -45,6 +81,64 @@ func PlotAfsPmf(alpha float64, n int, outFile string, integralError float64) {
 	//for each possible allele frequency for a segregating site, we calculate the probability to the file
 	for i := 1; i < n; i++ {
 		fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbability(i, n, alpha, binomCache, integralError))
+	}
+
+	err := out.Close()
+	exception.PanicOnErr(err)
+}
+
+func PlotDerivedAscertainmentProbability(outFile string, n int, d int) {
+	out := fileio.EasyCreate(outFile)
+	fmt.Fprintf(out, "Frequency\tProbability\n")
+
+	for i := 1; i < n; i++ {
+		fmt.Fprintf(out, "%v\t%e\n", i, DerivedAscertainmentProbability(n, i , d))
+	}
+
+	err := out.Close()
+	exception.PanicOnErr(err)
+}
+
+func PlotAncestralAscertainmentProbability(outFile string, n int, d int) {
+	out := fileio.EasyCreate(outFile)
+	fmt.Fprintf(out, "Frequency\tProbability\n")
+
+	for i := 1; i < n; i++ {
+		fmt.Fprintf(out, "%v\t%e\n", i, AncestralAscertainmentProbability(n, i , d))
+	}
+
+	err := out.Close()
+	exception.PanicOnErr(err)
+}
+
+func PlotDerivedAscertainmentDenominator(outFile string, n int, d int, alpha float64, integralError float64) {
+	allN := []int{n}
+	binomCache := BuildBinomCache(allN)
+	fCache := BuildFCache(n, alpha, binomCache, integralError)
+	fCacheSum := GetFCacheSum(fCache)
+
+	out := fileio.EasyCreate(outFile)
+	fmt.Fprintf(out, "Frequency\tProbability\n")
+
+	for i := 1; i < n; i++ {
+		fmt.Fprintf(out, "%v\t%e\n", i, DerivedAscertainmentDenominator(fCache, fCacheSum, d))
+	}
+
+	err := out.Close()
+	exception.PanicOnErr(err)
+}
+
+func PlotAncestralAscertainmentDenominator(outFile string, n int, d int, alpha float64, integralError float64) {
+	allN := []int{n}
+	binomCache := BuildBinomCache(allN)
+	fCache := BuildFCache(n, alpha, binomCache, integralError)
+	fCacheSum := GetFCacheSum(fCache)
+
+	out := fileio.EasyCreate(outFile)
+	fmt.Fprintf(out, "Frequency\tProbability\n")
+
+	for i := 1; i < n; i++ {
+		fmt.Fprintf(out, "%v\t%e\n", i, AncestralAscertainmentDenominator(fCache, fCacheSum, d))
 	}
 
 	err := out.Close()
