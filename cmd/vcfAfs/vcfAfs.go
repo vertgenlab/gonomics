e2d8f8a80a93bbb4c0776147f000cdc8b0330778
rimangan <riley.mangan@duke.edu>
2021-06-22 13:00:58 -0400

    Plotting likelihood

--- cmd/vcfAfs/vcfAfs.go
+++ cmd/vcfAfs/vcfAfs.go
@@ -11,14 +11,30 @@ import (
 	"log"
 )
 
-func vcfAfs(vcfFile string, outFile string, UnPolarized bool) {
-	g, err := popgen.VcfToAfs(vcfFile, UnPolarized, false) //hardcoded DivergenceAscertainment to false
+type Settings struct {
+	UnPolarized bool
+	PlotSelectionLikelihood string
+	LeftBound float64
+	RightBound float64
+	NumberOfPoints int
+	IntegralError float64
+}
+
+func vcfAfs(vcfFile string, outFile string, s Settings) {
+	var err error
+	g, err := popgen.VcfToAfs(vcfFile, s.UnPolarized, false) //hardcoded DivergenceAscertainment to false
 	exception.FatalOnErr(err)
 	f := popgen.AfsToFrequency(*g)
 	out := fileio.EasyCreate(outFile)
-	defer out.Close()
 	for i := 0; i < len(f); i++ {
-		fmt.Fprintf(out, "%f\n", f[i])
+		_, err = fmt.Fprintf(out, "%f\n", f[i])
+		exception.PanicOnErr(err)
+	}
+	err = out.Close()
+	exception.PanicOnErr(err)
+
+	if s.PlotSelectionLikelihood != "" {
+		popgen.PlotAfsLikelihood(*g, s.PlotSelectionLikelihood, s.LeftBound, s.RightBound, s.NumberOfPoints, s.IntegralError)
 	}
 }
 
@@ -26,12 +42,18 @@ func usage() {
 	fmt.Print(
 		"vcfAfs - Returns allele frequency spectrum information in a text file for graphing.\n" +
 			"vcfAfs - vcfFile.vcf outFile.txt\n")
+	flag.PrintDefaults()
 }
 
 func main() {
-	var unPolarized *bool = flag.Bool("unPolarized", false, "vcfAfs creates polarized derived frequency spectra by default. When true, the cmd returns unpolarized site frequency spectra.")
-
 	var expectedNumArgs int = 2
+	var unPolarized *bool = flag.Bool("unPolarized", false, "vcfAfs creates polarized derived frequency spectra by default. When true, the cmd returns unpolarized site frequency spectra.")
+	var plotSelectionLikelihood *string = flag.String("plotSelectionLikelihood", "", "Plots the likelihood function for the selection parameter from this VCF to a specified output file.")
+	var leftBound *float64 = flag.Float64("leftBound", -10.0, "Left bound for plotting of the likelihood function.")
+	var rightBound *float64 = flag.Float64("rightBound", 10.0, "Right bound for plotting of the likelihood function.")
+	var numberOfPoints *int = flag.Int("numberOfPoints", 100, "Set the number of points at which to evaluate the likelihood function.")
+	var integralError *float64 = flag.Float64("integralError", 1e-5, "Sets the error threshold for integral calculations in the likelihood calculation.")
+
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flag.Parse()
@@ -42,7 +64,16 @@ func main() {
 			expectedNumArgs, len(flag.Args()))
 	}
 
+	s := Settings{
+		UnPolarized: *unPolarized,
+		PlotSelectionLikelihood: *plotSelectionLikelihood,
+		LeftBound: *leftBound,
+		RightBound: *rightBound,
+		NumberOfPoints: *numberOfPoints,
+		IntegralError: *integralError,
+	}
+
 	vcfFile := flag.Arg(0)
 	outFile := flag.Arg(1)
-	vcfAfs(vcfFile, outFile, *unPolarized)
+	vcfAfs(vcfFile, outFile, s)
 }
--- popgen/plot.go
+++ popgen/plot.go
@@ -8,31 +8,38 @@ import (
 
 //PlotAfsF writes the Allele Frequency F function (AfsSampleDensity) to an output file for downstream visualization.
 func PlotAfsF(alpha float64, n int, outFile string, integralError float64) {
+	var err error
 	out := fileio.EasyCreate(outFile)
 	allN := []int{n}
 	binomCache := BuildBinomCache(allN)
 
-	fmt.Fprintf(out, "Frequency\tF\n")
+	_, err = fmt.Fprintf(out, "Frequency\tF\n")
+	exception.PanicOnErr(err)
 
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, AfsSampleDensity(n, i, alpha, binomCache, integralError))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, AfsSampleDensity(n, i, alpha, binomCache, integralError))
+		exception.PanicOnErr(err)
 	}
-	err := out.Close()
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
 
 //PlotAfsFCareless plots the old version of AfsF, which lacks the "careful" integration. Used for plotting and debugging. TODO: Delete eventually.
 func PlotAfsFCareless(alpha float64, n int, outFile string) {
+	var err error
 	out := fileio.EasyCreate(outFile)
-	fmt.Fprintf(out, "Frequency\tF\n")
+	_, err = fmt.Fprintf(out, "Frequency\tF\n")
+	exception.PanicOnErr(err)
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, AfsSampleDensityCareless(n, i, alpha))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, AfsSampleDensityCareless(n, i, alpha))
+		exception.PanicOnErr(err)
 	}
-	err := out.Close()
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
 
 func PlotAfsPmfAncestral(alpha float64, n int, outFile string, integralError float64) {
+	var err error
 	out := fileio.EasyCreate(outFile)
 
 	allN := []int{n}
@@ -40,17 +47,20 @@ func PlotAfsPmfAncestral(alpha float64, n int, outFile string, integralError flo
 	//DEBUG:fmt.Printf("Len BinomCache:%v. Entry at index n is of length: %v.\n", len(binomCache), len(binomCache[n]))
 
 	//a short header is written to the file
-	fmt.Fprintf(out, "Frequency\tProbability\n")
+	_, err = fmt.Fprintf(out, "Frequency\tProbability\n")
+	exception.PanicOnErr(err)
 	//for each possible allele frequency for a segregating site, we calculate the probability to the file
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbabilityAncestralAscertainment(alpha, i, n, 1, binomCache, integralError))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbabilityAncestralAscertainment(alpha, i, n, 1, binomCache, integralError))
+		exception.PanicOnErr(err)
 	}
 
-	err := out.Close()
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
 
 func PlotAfsPmfDerived(alpha float64, n int, outFile string, integralError float64) {
+	var err error
 	out := fileio.EasyCreate(outFile)
 
 	allN := []int{n}
@@ -58,18 +68,21 @@ func PlotAfsPmfDerived(alpha float64, n int, outFile string, integralError float
 	//DEBUG:fmt.Printf("Len BinomCache:%v. Entry at index n is of length: %v.\n", len(binomCache), len(binomCache[n]))
 
 	//a short header is written to the file
-	fmt.Fprintf(out, "Frequency\tProbability\n")
+	_, err = fmt.Fprintf(out, "Frequency\tProbability\n")
+	exception.PanicOnErr(err)
 	//for each possible allele frequency for a segregating site, we calculate the probability to the file
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbabilityDerivedAscertainment(alpha, i, n, 1, binomCache, integralError))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbabilityDerivedAscertainment(alpha, i, n, 1, binomCache, integralError))
+		exception.PanicOnErr(err)
 	}
 
-	err := out.Close()
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
 
 //PlotAfsPmf writes the allele frequency probability mass function (AlleleFrequencyProbability) to an output file for downstream visualization.
 func PlotAfsPmf(alpha float64, n int, outFile string, integralError float64) {
+	var err error
 	out := fileio.EasyCreate(outFile)
 
 	allN := []int{n}
@@ -77,70 +90,103 @@ func PlotAfsPmf(alpha float64, n int, outFile string, integralError float64) {
 	//DEBUG:fmt.Printf("Len BinomCache:%v. Entry at index n is of length: %v.\n", len(binomCache), len(binomCache[n]))
 
 	//a short header is written to the file
-	fmt.Fprintf(out, "Frequency\tProbability\n")
+	_, err = fmt.Fprintf(out, "Frequency\tProbability\n")
 	//for each possible allele frequency for a segregating site, we calculate the probability to the file
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbability(i, n, alpha, binomCache, integralError))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, AlleleFrequencyProbability(i, n, alpha, binomCache, integralError))
 	}
 
-	err := out.Close()
+	err = out.Close()
+	exception.PanicOnErr(err)
+}
+
+//PlotAfsLikelihood
+func PlotAfsLikelihood(afs Afs, outFile string, leftBound float64, rightBound float64, numPoints int, integralError float64) {
+	var err error
+	var alpha float64
+	out := fileio.EasyCreate(outFile)
+
+	allN := findAllN(afs)
+	binomMap := BuildBinomCache(allN)
+
+	_, err = fmt.Fprintf(out,"Alpha\tLikelihood\n")
+	exception.PanicOnErr(err)
+
+	for i := 0; i <= numPoints; i++ {
+		alpha = leftBound + (float64(i) / float64(numPoints)) * (rightBound - leftBound)
+		_, err = fmt.Fprintf(out, "%e\t%e\n", alpha, AfsLikelihoodFixedAlpha(afs, alpha, binomMap, integralError))
+	}
+
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
 
 func PlotDerivedAscertainmentProbability(outFile string, n int, d int) {
+	var err error
 	out := fileio.EasyCreate(outFile)
-	fmt.Fprintf(out, "Frequency\tProbability\n")
+	_, err = fmt.Fprintf(out, "Frequency\tProbability\n")
+	exception.PanicOnErr(err)
 
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, DerivedAscertainmentProbability(n, i, d))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, DerivedAscertainmentProbability(n, i, d))
+		exception.PanicOnErr(err)
 	}
 
-	err := out.Close()
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
 
 func PlotAncestralAscertainmentProbability(outFile string, n int, d int) {
+	var err error
 	out := fileio.EasyCreate(outFile)
-	fmt.Fprintf(out, "Frequency\tProbability\n")
+	_, err = fmt.Fprintf(out, "Frequency\tProbability\n")
+	exception.PanicOnErr(err)
 
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, AncestralAscertainmentProbability(n, i, d))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, AncestralAscertainmentProbability(n, i, d))
+		exception.PanicOnErr(err)
 	}
 
-	err := out.Close()
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
 
 func PlotDerivedAscertainmentDenominator(outFile string, n int, d int, alpha float64, integralError float64) {
+	var err error
 	allN := []int{n}
 	binomCache := BuildBinomCache(allN)
 	fCache := BuildFCache(n, alpha, binomCache, integralError)
 	fCacheSum := GetFCacheSum(fCache)
 
 	out := fileio.EasyCreate(outFile)
-	fmt.Fprintf(out, "Frequency\tProbability\n")
+	_, err = fmt.Fprintf(out, "Frequency\tProbability\n")
+	exception.PanicOnErr(err)
 
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, DerivedAscertainmentDenominator(fCache, fCacheSum, d))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, DerivedAscertainmentDenominator(fCache, fCacheSum, d))
+		exception.PanicOnErr(err)
 	}
 
-	err := out.Close()
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
 
 func PlotAncestralAscertainmentDenominator(outFile string, n int, d int, alpha float64, integralError float64) {
+	var err error
 	allN := []int{n}
 	binomCache := BuildBinomCache(allN)
 	fCache := BuildFCache(n, alpha, binomCache, integralError)
 	fCacheSum := GetFCacheSum(fCache)
 
 	out := fileio.EasyCreate(outFile)
-	fmt.Fprintf(out, "Frequency\tProbability\n")
+	_, err = fmt.Fprintf(out, "Frequency\tProbability\n")
+	exception.PanicOnErr(err)
 
 	for i := 1; i < n; i++ {
-		fmt.Fprintf(out, "%v\t%e\n", i, AncestralAscertainmentDenominator(fCache, fCacheSum, d))
+		_, err = fmt.Fprintf(out, "%v\t%e\n", i, AncestralAscertainmentDenominator(fCache, fCacheSum, d))
+		exception.PanicOnErr(err)
 	}
 
-	err := out.Close()
+	err = out.Close()
 	exception.PanicOnErr(err)
 }
