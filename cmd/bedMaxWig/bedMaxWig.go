de3652eea272aa1ecea67a39fadf5fef9dcbd107
Chelsea Shoben <cshoben@users.noreply.github.com>
2021-06-27 16:40:13 -0400

    draft PR resolve Not enough arguments in call to 'bedMaxWig'

--- cmd/bedMaxWig/bedMaxWig.go
+++ cmd/bedMaxWig/bedMaxWig.go
@@ -12,7 +12,7 @@ import (
 	"log"
 )
 
-func bedMaxWig(infile string, database string, chromsizeFile string, outfile string) {
+func bedMaxWig(infile string, database string, chromsizeFile string, outfile string, norm *bool) {
 	var records []bed.Bed = bed.Read(infile)
 	var wigData []wig.Wig = wig.Read(database)
 	var sizes []chromInfo.ChromInfo = chromInfo.ReadToSlice(chromsizeFile)
@@ -20,14 +20,34 @@ func bedMaxWig(infile string, database string, chromsizeFile string, outfile str
 	var currentBed bed.Bed = records[0]
 	var chromSlice []float64
 	var i int
+	var wigTotal float64 = 0
+
+	if *norm == true {
+		for i := 0; i < len(wigData); i++ { //Goal here is to cycle through all the "chromosomes" of the wig: wig[0], wig[1], etc.
+			var wigCounterByChrom float64 = 0
+			for k := 0; k < len(wigData[i].Values); k++ { //Cycle through each value in the float64[]
+				var chromValueMultiplyByStep float64 = 0
+				chromValueMultiplyByStep = float64(wigData[i].Step) * wigData[i].Values[i] // multiply each value by the step for that chrom
+				wigCounterByChrom = wigCounterByChrom + chromValueMultiplyByStep
+			}
+			wigTotal = wigTotal + wigCounterByChrom
+		}
+	}
+
 
 	for i = 0; i < len(sizes); i++ {
 		chromSlice = WigChromToSlice(wigData, sizes[i].Size, sizes[i].Name)
 		for k := 0; k < len(records); k++ {
 			if records[k].Chrom == sizes[i].Name {
 				currentBed = records[k]
-				currentBed.Annotation = append(currentBed.Annotation, fmt.Sprintf("%f", bedRangeMax(chromSlice, records[k].ChromStart, records[k].ChromEnd)))
-				outlist = append(outlist, currentBed)
+				if *norm == true {
+					maxWig := bedRangeMax(chromSlice, records[k].ChromStart, records[k].ChromEnd)
+					normMaxWig := maxWig/wigTotal
+					currentBed.Annotation = append(currentBed.Annotation, fmt.Sprintf("%e", normMaxWig))
+				} else {
+					currentBed.Annotation = append(currentBed.Annotation, fmt.Sprintf("%f", bedRangeMax(chromSlice, records[k].ChromStart, records[k].ChromEnd)))
+					outlist = append(outlist, currentBed)
+				}
 			}
 		}
 	}
@@ -74,6 +94,7 @@ func usage() {
 
 func main() {
 	var expectedNumArgs int = 4
+	var norm *bool = flag.Bool("normalize", false, "When true, will normalize the bedMaxWig output by dividing value by total wig hits.")
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flag.Parse()
@@ -89,5 +110,10 @@ func main() {
 	chromsize := flag.Arg(2)
 	outfile := flag.Arg(3)
 
-	bedMaxWig(infile, database, chromsize, outfile)
+	if *norm {
+		bedMaxWig(infile, database, chromsize, outfile, norm)
+		return
+	}
+	bedMaxWig(infile, database, chromsize, outfile, norm)
+
 }
