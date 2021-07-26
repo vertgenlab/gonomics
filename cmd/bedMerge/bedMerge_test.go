d10058b7693d8beb9550ce1a9106148714b678b2
rimangan <riley.mangan@duke.edu>
2021-07-26 15:01:19 -0400

    bedMerge mergedAdjacent

--- cmd/bedMerge/bedMerge_test.go
+++ cmd/bedMerge/bedMerge_test.go
@@ -10,14 +10,16 @@ import (
 var BedMergeTests = []struct {
 	InFile       string
 	ExpectedFile string
+	MergeAdjacent bool
 }{
-	{"testdata/test.bed", "testdata/test.merged.bed"},
+	{"testdata/test.bed", "testdata/test.merged.bed", false},
+	{"testdata/test.bed", "testdata/test.adjacent.merged.bed", true},
 }
 
 func TestBedMerge(t *testing.T) {
 	var err error
 	for _, i := range BedMergeTests {
-		bedMerge(i.InFile, "tmp.txt")
+		bedMerge(i.InFile, "tmp.txt", i.MergeAdjacent)
 		if !fileio.AreEqual("tmp.txt", i.ExpectedFile) {
 			t.Errorf("Error in bedMerge.")
 		} else {
