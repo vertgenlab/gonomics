bf25da368d0e4ef0da6bea4886bd43bd2aa447e5
rimangan <riley.mangan@duke.edu>
2021-08-03 21:42:06 -0400

    bedFilter Update

--- cmd/bedFilter/bedFilter.go
+++ cmd/bedFilter/bedFilter.go
@@ -7,75 +7,85 @@ import (
 	"fmt"
 	"github.com/vertgenlab/gonomics/bed"
 	"github.com/vertgenlab/gonomics/common"
+	"github.com/vertgenlab/gonomics/exception"
 	"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/numbers"
 	"log"
-	"strings"
+	"math"
 )
 
-func bedFilter(infile string, outfile string, minScore int, maxScore int, minLength int, maxLength int, minStart int, maxStart int, minEnd int, maxEnd int, chrom string) {
-	var outlist []bed.Bed
-	var line string
-	var startNum, endNum, length int
-	var doneReading bool = false
-	var pass bool = false
-	var numFields int
-	var current bed.Bed
-	file := fileio.EasyOpen(infile)
-	defer file.Close()
+type Settings struct {
+	InFile string
+	OutFile string
+	MinScore int
+	MaxScore int
+	MinLength int
+	MaxLength int
+	MinStart int
+	MaxStart int
+	MinEnd int
+	MaxEnd int
+	MinNameFloat float64
+	MaxNameFloat float64
+	Chrom string
+}
 
-	for line, doneReading = fileio.EasyNextRealLine(file); !doneReading; line, doneReading = fileio.EasyNextRealLine(file) {
+func bedFilter(s Settings) {
+	var pass bool = false
+	var length int
+	out := fileio.EasyCreate(s.OutFile)
+
+	bedChan := bed.GoReadToChan(s.InFile)
+	for i := range bedChan {
 		pass = true
-		words := strings.Split(line, "\t")
-		startNum = common.StringToInt(words[1])
-		endNum = common.StringToInt(words[2])
-		length = endNum - startNum
-		if len(words) > 4 {
-			if common.StringToInt(words[4]) < minScore {
+		length = i.ChromEnd - i.ChromStart
+		if i.Score < s.MinScore {
+			pass = false
+		}
+		if i.Score > s.MaxScore {
+			pass = false
+		}
+		if length < s.MinLength {
+			pass = false
+		}
+		if length > s.MaxLength {
+			pass = false
+		}
+		if i.ChromStart < s.MinStart {
+			pass = false
+		}
+		if i.ChromStart > s.MaxStart {
+			pass = false
+		}
+		if i.ChromEnd < s.MinEnd {
+			pass = false
+		}
+		if i.ChromEnd > s.MaxEnd {
+			pass = false
+		}
+		if s.MinNameFloat != -1 * math.MaxFloat64 {
+			if common.StringToFloat64(i.Name) < s.MinNameFloat {
 				pass = false
 			}
-			if common.StringToInt(words[4]) > maxScore {
+		}
+		if s.MaxNameFloat != math.MaxFloat64 {
+			if common.StringToFloat64(i.Name) > s.MaxNameFloat {
 				pass = false
 			}
 		}
-		if length < minLength {
-			pass = false
-		}
-		if length > maxLength {
-			pass = false
-		}
-		if startNum < minStart {
-			pass = false
-		}
-		if startNum > maxStart {
-			pass = false
-		}
-		if endNum < minEnd {
-			pass = false
-		}
-		if endNum > maxEnd {
-			pass = false
-		}
-		if chrom != "" {
-			if words[0] != chrom {
+		if s.Chrom != "" {
+			if s.Chrom != i.Chrom {
 				pass = false
 			}
 		}
 		if pass {
-			if len(words) == 3 {
-				current = bed.Bed{Chrom: words[0], ChromStart: startNum, ChromEnd: endNum}
-				numFields = numbers.Max(3, numFields)
-			} else if len(words) == 4 {
-				current = bed.Bed{Chrom: words[0], ChromStart: startNum, ChromEnd: endNum, Name: words[3]}
-				numFields = numbers.Max(4, numFields)
-			} else {
-				current = bed.Bed{Chrom: words[0], ChromStart: startNum, ChromEnd: endNum, Name: words[3], Score: common.StringToInt(words[4])}
-				numFields = numbers.Max(5, numFields)
-			}
-			outlist = append(outlist, current)
+			bed.WriteBed(out, i)
 		}
 	}
-	bed.Write(outfile, outlist)
+
+	var err error
+	err = out.Close()
+	exception.PanicOnErr(err)
 }
 
 func usage() {
@@ -89,7 +99,7 @@ func usage() {
 
 func main() {
 	var expectedNumArgs int = 2
-	var minScore *int = flag.Int("minScore", 0, "Specifies the minimum score in the fourth field.")
+	var minScore *int = flag.Int("minScore", -1 * numbers.MaxInt, "Specifies the minimum score in the fourth field.")
 	var maxScore *int = flag.Int("maxScore", numbers.MaxInt, "Specifies the maximum score in the fourth field.")
 	var minLength *int = flag.Int("minLength", 0, "Specifies the minimum length of the region.")
 	var maxLength *int = flag.Int("maxLength", numbers.MaxInt, "Specifies the maximum length of the region.")
@@ -97,6 +107,8 @@ func main() {
 	var maxStart *int = flag.Int("maxStart", numbers.MaxInt, "Specifies the maximum starting position of the region.")
 	var minEnd *int = flag.Int("minEnd", 0, "Specifies the minimum ending position of the region.")
 	var maxEnd *int = flag.Int("maxEnd", numbers.MaxInt, "Specifies the maximum ending position of the region.")
+	var minNameFloat *float64 = flag.Float64("minNameFloat", -1 * math.MaxFloat64, "Specifies the minimum floating point number value for bed entries where floating point numbers are stored in the name field.")
+	var maxNameFloat *float64 = flag.Float64("maxNameFloat", math.MaxFloat64, "Specifies the maximum floating point number value for bed entries where floating point numbers are stored in the name field.")
 	var chrom *string = flag.String("chrom", "", "Specifies the chromosome name.")
 
 	flag.Usage = usage
@@ -112,5 +124,20 @@ func main() {
 	infile := flag.Arg(0)
 	outfile := flag.Arg(1)
 
-	bedFilter(infile, outfile, *minScore, *maxScore, *minLength, *maxLength, *minStart, *maxStart, *minEnd, *maxEnd, *chrom)
+	s := Settings{
+		InFile: infile,
+		OutFile: outfile,
+		MinScore: *minScore,
+		MinLength: *minLength,
+		MaxScore: *maxScore,
+		MaxLength: *maxLength,
+		MinStart: *minStart,
+		MaxStart: *maxStart,
+		MinEnd: *minEnd,
+		MaxEnd: *maxEnd,
+		MinNameFloat: *minNameFloat,
+		MaxNameFloat: *maxNameFloat,
+		Chrom: *chrom,
+	}
+	bedFilter(s)
 }
---
+++ cmd/bedFilter/bedFilter_test.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/numbers"
+	"math"
+	"os"
+	"testing"
+)
+
+var bedFilterTests = []struct {
+	InFile string
+	OutFile string
+	ExpectedFile string
+	MinScore int
+	MaxScore int
+	MinLength int
+	MaxLength int
+	MinStart int
+	MaxStart int
+	MinEnd int
+	MaxEnd int
+	MinNameFloat float64
+	MaxNameFloat float64
+	Chrom string
+}{
+	{InFile: "testdata/test.bed",
+	OutFile: "testdata/tmp.bed",
+	ExpectedFile: "testdata/testOut.bed",
+	MinScore: 2,
+	MaxScore: 10,
+	MinLength: 0,
+	MaxLength: numbers.MaxInt,
+	MinStart: 5,
+	MaxStart: 250,
+	MinEnd: 15,
+	MaxEnd: 800,
+	MinNameFloat: -1 * math.MaxFloat64,
+	MaxNameFloat: math.MaxFloat64,
+	Chrom: "chr10",
+	},
+	{InFile: "testdata/test2.bed",
+		OutFile: "testdata/tmp2.bed",
+		ExpectedFile: "testdata/testOut2.bed",
+		MinScore: 2,
+		MaxScore: 10,
+		MinLength: 0,
+		MaxLength: numbers.MaxInt,
+		MinStart: 5,
+		MaxStart: 250,
+		MinEnd: 15,
+		MaxEnd: 800,
+		MinNameFloat: 2.5,
+		MaxNameFloat: 50.0,
+		Chrom: "chr10",
+	},
+}
+
+func TestBedFilter(t *testing.T) {
+	var s Settings
+	var err error
+	for _, i := range bedFilterTests {
+		s = Settings{
+			InFile: i.InFile,
+			OutFile: i.OutFile,
+			MinScore: i.MinScore,
+			MaxScore: i.MaxScore,
+			MinLength: i.MinLength,
+			MaxLength: i.MaxLength,
+			MinStart: i.MinStart,
+			MaxStart: i.MaxStart,
+			MinEnd: i.MinEnd,
+			MaxEnd: i.MaxEnd,
+			MinNameFloat: i.MinNameFloat,
+			MaxNameFloat: i.MaxNameFloat,
+			Chrom: i.Chrom,
+		}
+		bedFilter(s)
+		if !fileio.AreEqual(i.OutFile, i.ExpectedFile) {
+			t.Errorf("Error in bedFilter.")
+		} else {
+			err = os.Remove(i.OutFile)
+			exception.PanicOnErr(err)
+		}
+	}
+}
