2c79d67087d495d7186c128e1b74023622d48f3d
rimangan <riley.mangan@duke.edu>
2021-08-05 10:31:29 -0400

    bug fix

--- cmd/multiFaAcceleration/multiFaAcceleration.go
+++ cmd/multiFaAcceleration/multiFaAcceleration.go
@@ -90,8 +90,8 @@ func multiFaAcceleration(s Settings) {
 					piS2S3 = fasta.PairwiseMutationDistanceInRange(records[2], records[3], alignmentCounter, alnEnd)
 				}
 				b1 = numbers.MaxFloat64(0.0, float64(piS0S1+piS0S2-piS1S2)/2.0)
-				//b3 = numbers.MaxFloat64(0.0, (float64(piS1S2+piS0S3+piS2S3-piS0S1) / 2.0) - float64(piS2S3))
-				b3 = (float64(piS1S2+piS0S3+piS2S3-piS0S1) / 2.0) - float64(piS2S3)
+				b3 = numbers.MaxFloat64(0.0, (float64(piS1S2+piS0S3+piS2S3-piS0S1) / 2.0) - float64(piS2S3))
+				//b3 = (float64(piS1S2+piS0S3+piS2S3-piS0S1) / 2.0) - float64(piS2S3)
 				if s.Verbose && (b1 < 0 || b3 < 0) {
 					log.Printf("b1: %e. b3: %e. piS0S1: %v. piS0S2: %v. piS1S2: %v. piS0S3: %v. piS2S3: %v.", b1, b3, piS0S1, piS0S2, piS1S2, piS0S3, piS2S3)
 				}
