892f0a9ad4b37cae870eaaaa3bea34998701db77
rimangan <riley.mangan@duke.edu>
2021-08-21 11:09:02 -0400

    DocumentationUpdateRemoveShell

--- cmd/multiFaAcceleration/multiFaAcceleration_test.go
+++ cmd/multiFaAcceleration/multiFaAcceleration_test.go
@@ -7,6 +7,11 @@ import (
 	"testing"
 )
 
+//Tests were made with the following cmds.
+//~/go/bin/multiFaAcceleration -windowSize 50 chr1 test.fa test.vel.expected.bed test.accel.expected.bed test.initialVel.expected.bed
+//~/go/bin/multiFaAcceleration -windowSize 50 -searchSpaceBed test.searchspace.bed chr1 test.fa test.vel.searchspace.expected.bed test.accel.searchspace.expected.bed test.initialVel.searchspace.expected.bed
+//~/go/bin/multiFaAcceleration -windowSize 50 -searchSpaceBed test.searchspace.bed -useSnpDistance chr1 test.fa test.vel.snpDistance.expected.bed test.accel.snpDistance.expected.bed test.initialVel.snpDistance.expected.bed
+
 var multiFaAccelerationTests = []struct {
 	InFile                     string
 	ChromName                  string
