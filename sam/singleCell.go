7f331b12352f07118c2fd581d89705a29a57df0e
rimangan <riley.mangan@duke.edu>
2021-09-06 12:30:36 -0400

    ParseSingleCellFromSam

---
+++ sam/singleCell.go
@@ -0,0 +1,49 @@
+package sam
+
+import (
+	"github.com/vertgenlab/gonomics/dna"
+	"log"
+	"strings"
+)
+
+type SingleCellAlignment struct {
+	Aln Sam
+	Bx	[]dna.Base
+	Umi	[]dna.Base
+}
+
+func ToSingleCellAlignment(s Sam) SingleCellAlignment {
+	Bx, Umi := parseBxAndUmiFromAln(s)
+	return SingleCellAlignment{
+		Aln: s,
+		Bx: Bx,
+		Umi: Umi,
+	}
+}
+
+func parseBxAndUmiFromAln(s Sam) ([]dna.Base, []dna.Base) {
+	var Bx, Umi []dna.Base
+	var foundBx, foundUmi bool = false, false
+	nameFields := strings.Split(s.QName, "_")
+	var words []string
+	for i := range nameFields {
+		if strings.HasPrefix(nameFields[i], "UMI:") {
+			foundUmi = true
+			words = strings.Split(nameFields[i], ":")
+			Umi = dna.StringToBases(words[1])
+		}
+		if strings.HasPrefix(nameFields[i], "BX:") {
+			foundBx = true
+			words = strings.Split(nameFields[i], ":")
+			Bx = dna.StringToBases(words[1])
+		}
+	}
+	if !foundBx {
+		log.Fatalf("Failed to parse Barcode from single-cell read name.")
+	}
+	if !foundUmi {
+		log.Fatalf("Failed to parse Umi from single-cell read name.")
+	}
+	return Bx, Umi
+}
+
---
+++ sam/singleCell_test.go
@@ -0,0 +1,29 @@
+package sam
+
+import (
+	"github.com/vertgenlab/gonomics/dna"
+	"testing"
+)
+
+var singleCellTests = []struct {
+	InFile string
+	ExpectedBx []string
+	ExpectedUmi []string
+}{
+	{"testdata/singleCell.sam", []string{"AGTGCACGTGGAGGCT", "AGTGCACGTGGAGGCT", "AGTGCACGTGGAGGCT", "GGACAGTTAGACAAGT", "GCTAGGACAATGAGTA", "CAGGACTGAGTGACCA"}, []string{"AGGTGGACGCTA", "AGGTGGACGCTA", "AGGTGGACGCTA", "GGACAGTGATGA", "CAGTGAGCAGTA", "AGGTGGACGCTA"}},
+}
+
+func TestToSingleCellAlignment(t *testing.T) {
+	var scAln SingleCellAlignment
+	for _, v := range singleCellTests {
+		records, _ := Read(v.InFile)
+		for i := range records {
+			scAln = ToSingleCellAlignment(records[i])
+			if dna.BasesToString(scAln.Umi) != v.ExpectedUmi[i] {
+				t.Errorf("Error in ToSingleCellAlignment. Expected Umi: %s. Observed: %s.\n", dna.BasesToString(scAln.Umi), v.ExpectedUmi[i])
+			} else if dna.BasesToString(scAln.Bx) != v.ExpectedBx[i] {
+				t.Errorf("Error in ToSingleCellAlignment. Expected Bx: %s. Observed: %s.\n", dna.BasesToString(scAln.Bx), v.ExpectedUmi[i])
+			}
+		}
+	}
+}
