1764da7f5f337580fbc63073c19a70d2c9066d18
cshoben <cshoben@users.noreply.github.com>
2021-09-13 13:34:09 -0400

    renamed files for consistency, handle field intialized not at 5, add test for when multibed2 match to bed1.

---
+++ cmd/compareBedDistanceBasedOnName/compareBedDistanceBasedOnName.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/vertgenlab/gonomics/bed"
+	"github.com/vertgenlab/gonomics/fileio"
+	"log"
+)
+
+func compareBedDistanceBasedOnName (inputBed string, genomeBed string, outBed string) {
+	out := fileio.EasyCreate(outBed)
+	input := bed.Read(inputBed)
+	genome := bed.Read(genomeBed)
+
+	genomeMap := make(map[string][]bed.Bed)
+	var i int
+
+<<<<<<< HEAD
+	for i = range genome {
+		genomeMap[genome[i].Name] = append(genomeMap[genome[i].Name], genome[i])
+		}
+=======
+	for i = range geneInfo {
+		geneInfoMap[geneInfo[i].Name] = append(geneInfoMap[geneInfo[i].Name], geneInfo[i])
+	}
+>>>>>>> b5e923e7c080f84de875f0c3452de83a9debe3f3
+
+	var inputNameField []bed.Bed
+	var found bool
+	var distance int
+	var lowestDistance int
+	var err error
+	var j int
+
+	for i := 0; i < len(input); i++ {
+		lowestDistance = 0
+		inputNameField, found = genomeMap[input[i].Name]
+		if found != true {
+			log.Fatalf("Did not find %s", input[i].Name)
+		}
+		for j = range inputNameField {
+			distance, err = bed.CompareDistance(inputNameField[j], input[i])
+			if j == 0 {
+				lowestDistance = distance
+			} else {
+				if distance < lowestDistance {
+					lowestDistance = distance
+				}
+			}
+		}
+
+		if err != nil {
+			log.Fatalf("Unable to compare distance, error message: %s", err)
+		}
+		input[i].Score = lowestDistance
+		if input[i].FieldsInitialized < 5 {
+			input[i].FieldsInitialized = 5
+		}
+		bed.WriteToFileHandle(out, input[i])
+	}
+	err = out.Close()
+	if err != nil {
+		log.Fatalf("File unable to close properly, error is: %s", err)
+	}
+}
+
+func usage() {
+	fmt.Print(
+		"compareBedDistanceBasedOnName - Compares the Name fields between \n" +
+			"two bed files, and if they match, the minimum distance between those\n" +
+			"coordinates is calculated.\n" +
+			"The first bed is iterated over line by line and the second bed is\n" +
+			"searched for the entries that match the Name field.\n" +
+			"Every entry in the first bed is expected to have at least one corresponding\n" +
+			"bed entry in the second bed, where more than one matching bed entry is allowed\n" +
+			"If there is more than one bed entry in the second bed that matches an entry in" +
+			"the first bed, the distance between the coordinates will be calculated pairwise\n" +
+			"and the lowest value will be reported (first bed paired with each matching entry from the second bed).\n" +
+			"Thus, the second bed must include at least every Name included in the first bed.\n" +
+			"For example the second bed might be the coordinates for every gene, genome wide (genomeBed)\n" +
+			"while the first bed includes a list of bed coordinates you are interested in studying (inputBed).\n" +
+			"The output file will be the inputBed file with the minimum distance reported in the Score column.\n" +
+			"No headers allowed in input files.\n" +
+			"This program was originally written for comparing eSTR coordinates to gene coordinates.\n" +
+			"Usage:\n" +
+			"input.bed genome.bed out.bed\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	var expectedNumArgs = 3
+	flag.Usage = usage
+	flag.Parse()
+
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Error: expecting %d arguments, but got %d\n",
+			expectedNumArgs, len(flag.Args()))
+	}
+
+	inputBed := flag.Arg(0)
+	genomeBed := flag.Arg(1)
+	outBed := flag.Arg(2)
+
+	compareBedDistanceBasedOnName(inputBed, genomeBed, outBed)
+
+}
---
+++ cmd/compareBedDistanceBasedOnName/compareBedDistanceBasedOnName_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"os"
+	"testing"
+)
+
+var CompareBedDistanceOnNameTests = []struct {
+	inputBed    string
+	genomeBed   string
+	outputBed   string
+	expectedBed string
+}{
+	{"testdata/inputBed1.bed", "testdata/genomeBed1.bed", "testdata/outputBed1.bed", "testdata/expectedBed1.bed"},
+	{"testdata/inputBed2.bed", "testdata/genomeBed2.bed", "testdata/outputBed2.bed", "testdata/expectedBed2.bed"},
+	}
+
+func TestCompareBedDistanceOnName(t *testing.T) {
+	var err error
+	for _, v := range CompareBedDistanceOnNameTests {
+		compareBedOnName(v.inputBed, v.genomeBed, v.outputBed)
+		if !fileio.AreEqual(v.expectedBed, v.outputBed) {
+			t.Errorf("Error in compareBedDistanceBasedOnName")
+		} else {
+			err = os.Remove(v.outputBed)
+			exception.PanicOnErr(err)
+		}
+	}
+}
