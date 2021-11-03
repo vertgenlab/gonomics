43ea4179d32ed279e67d93f7032283eb686191ec
rimangan <riley.mangan@duke.edu>
2021-11-03 15:34:52 -0400

    bug fix

--- phylo/acceleration_test.go
+++ phylo/acceleration_test.go
@@ -7,6 +7,7 @@ var BranchLengthsAlternatingLeastSquaresTests = []struct {
 	AllowNegative         bool
 	Epsilon               float64
 	ExpectedBranchLengths AccelBranchLengths
+	CavalliSforzaQ	bool
 }{
 	{AccelDistancesAndWeights{ //This particular test was one that on genome-wide data failed to converge at 1000 iterations. Slowly descends on minimum, achieves in 10k iterations.
 		62,
@@ -15,19 +16,19 @@ var BranchLengthsAlternatingLeastSquaresTests = []struct {
 		31,
 		63,
 		71,
-		calculateWeight(62, 1000),
-		calculateWeight(67, 1000),
-		calculateWeight(37, 1000),
-		calculateWeight(31, 1000),
-		calculateWeight(63, 1000),
-		calculateWeight(71, 1000),
-	}, false, 1e-8, AccelBranchLengths{24.179037634841176, 19.55544803664856, 0, 18.710259085171323, 21.978125070026614}},
+		calculateWeight(62, 1000, false),
+		calculateWeight(67, 1000, false),
+		calculateWeight(37, 1000, false),
+		calculateWeight(31, 1000, false),
+		calculateWeight(63, 1000, false),
+		calculateWeight(71, 1000, false),
+	}, false, 1e-8, AccelBranchLengths{24.179037634841176, 19.55544803664856, 0, 18.710259085171323, 21.978125070026614}, false},
 }
 
 func TestBranchLengthsAlternatingLeastSquares(t *testing.T) {
 	var currAnswer AccelBranchLengths
 	for _, v := range BranchLengthsAlternatingLeastSquaresTests {
-		currAnswer = BranchLengthsAlternatingLeastSquares(v.D, v.AllowNegative, false, 1000, v.Epsilon)
+		currAnswer = BranchLengthsAlternatingLeastSquares(v.D, v.AllowNegative, false, 1000, v.Epsilon, v.CavalliSforzaQ)
 		if currAnswer != v.ExpectedBranchLengths {
 			t.Errorf("Error in BranchLengthsAlternatingLeastSquares.\nAnswer: %v.\nExpected: %v.", currAnswer, v.ExpectedBranchLengths)
 		}
