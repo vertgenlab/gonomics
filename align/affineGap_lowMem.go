5c1df594792290ca5287fbc396ffaf127bcd85ea
RavenYL <yanting.raven.luo@gmail.com>
2021-11-04 00:00:20 -0400

    merged conflicts from go fmt

--- align/affineGap_lowMem.go
+++ align/affineGap_lowMem.go
@@ -34,13 +34,9 @@ func initAffineScoring(firstSeqLen int, secondSeqLen int, checkersize_i int, che
 	return mRowCurrent, mRowPrevious, mColumn, trace_prep_i, trace_prep_j
 }
 
-<<<<<<< HEAD
 //"Step 0"
 //make data structures for tracing
 func initAffineTrace(firstSeqLen int, secondSeqLen int, checkersize_i int, checkersize_j int) ([][][]ColType) {
-=======
-func initAffineTrace(firstSeqLen int, secondSeqLen int, checkersize_i int, checkersize_j int) [][][]ColType {
->>>>>>> bff15de1450eed1706137655ef793c3862fce2db
 	trace_size_i := numbers.Min(firstSeqLen, checkersize_i) //make trace a matrix of size checkersize_i*checkersize_j, unless alpha or beta are shorter, in which case there is no need to allocate a full checkersize of memory
 	trace_size_j := numbers.Min(secondSeqLen, checkersize_j)
 	trace := make([][][]ColType, 3)
@@ -58,7 +54,7 @@ func AffineGap(alpha []dna.Base, beta []dna.Base, scores [][]int64, gapOpen int6
 	var checkersize_i, checkersize_j int
 	checkersize_i = 10000
 	checkersize_j = 10000
-	
+
 	//Step 1: find highest score, as well as get the position (i and j) of the highest score, and materials needed to fill traceback and write cigar in checkerboards
 	score_highest, score_highest_i, score_highest_j, trace_prep_i, trace_prep_j := highestScore_affineGap(alpha, beta, scores, gapOpen, gapExtend, checkersize_i, checkersize_j)
 
@@ -228,87 +224,11 @@ func highestScore_affineGap(alpha []dna.Base, beta []dna.Base, scores [][]int64,
 		}
 	}
 	score_highest_i := mColumn - 1
-<<<<<<< HEAD
 	score_highest_j := len(mRowCurrent[0]) - 1 //score_highest_j was denoted with "last J" in affineGap_highMem.go
 	score_highest, _ := tripleMaxTrace(mRowCurrent[0][score_highest_j], mRowCurrent[1][score_highest_j], mRowCurrent[2][score_highest_j]) //denoted by "maxScore" in affineGap_highMem.go. The 2nd output variable is its direction (0,1,2)
 	return score_highest, score_highest_i, score_highest_j, trace_prep_i, trace_prep_j
 }
 
-=======
-	score_highest_j := len(mRowCurrent[0]) - 1                                                                                            //TODO: remove this note to self after debugging - the "last J" in affineGap_highMem.go
-	score_highest, _ := tripleMaxTrace(mRowCurrent[0][score_highest_j], mRowCurrent[1][score_highest_j], mRowCurrent[2][score_highest_j]) //TODO: remove this note to self after debugging - "maxScore" in affineGap_highMem.go, thd 2nd value tells you direction of "maxScore"
-	//TODO: remove these prints
-	//fmt.Printf("score_highest: %d\n", score_highest)
-	//fmt.Printf("score_highest_i, score_highest_j: %d, %d\n", score_highest_i, score_highest_j)
-	//fmt.Printf("trace_prep_i: %v\n", trace_prep_i)
-	//fmt.Printf("trace_prep_j: %v\n", trace_prep_j)
-	return score_highest, score_highest_i, score_highest_j, trace_prep_i, trace_prep_j
-}
-
-func AffineGap_step234_testing(alpha []dna.Base, beta []dna.Base, scores [][]int64, gapOpen int64, gapExtend int64) []Cigar { //input=same as AffineGap_step1 for now, output=route
-	//initialize checkersizes here. TODO: make default 10000, and have version where we can change checkersize
-	checkersize_i := 3
-	checkersize_j := 3
-
-	//Step 1
-	//for affineGap, may need a switch here to get the right highest-score output set depending on which variation of affineGap is used
-	_, score_highest_i, score_highest_j, trace_prep_i, trace_prep_j := AffineGap_step1(alpha, beta, scores, gapOpen, gapExtend) //the first output score_highest isn't used in this function, so omit
-
-	//Make variables needed for checkerboards
-	//k1: the i-index (row-index) of the current checkerboard
-	//k2: the j-index (column-index) of the current checkerboard
-	//i_inChecker_max: the max i-index (row-index) in the current checkerboard during Step 2 (fillTraceback), either checkersize-1 or a smaller number, e.g. if the alpha/beta sequence to be aligned is not divisible into perfect checkerboards, and the remainder checkerboard is not completely filled
-	//j_inChecker_max: the max j-index (column-index) in the current checkerboard during Step 2 (fillTraceback)
-	//i_inChecker_min: the min i-index (row-index) in the current checkerboard during Step 3 (writeCigar), either 0 or another number, e.g. if cigar route leaves the checkerboard at a position that is not i_inChecker==0, j_inChecker==0
-	//j_inChecker_min: the min j-index (column-index) in the current checkerboard during Step 3 (writeCigar)
-	var k1, k2, i_inChecker_max, j_inChecker_max, i_inChecker_min, j_inChecker_min int
-	i_inChecker_min = -2                                                          //initialize i_inChecker_min != 0, so that the first ever Step 3 (writeCigar) will not interfere with i_inChecker_max
-	j_inChecker_min = -2                                                          //ditto for j
-	trace := initAffineTrace(len(alpha), len(beta), checkersize_i, checkersize_j) //for affineGap, use initAffineTrace function to initialize trace
-	route := make([]Cigar, 1)                                                     //initialie cigar route and routeIdx
-	var routeIdx_current int = 0
-	//for affineGap, make a variable to hold k_inChecker_max, outside of for loop
-	var k_inChecker_max, k_inChecker_min ColType
-
-	for k1, k2 = int((score_highest_i-1)/checkersize_i), int((score_highest_j-1)/checkersize_j); k1 >= 0 && k2 >= 0; { //use a function of score_highest_i, score_highest_j, and checkersize to initialize the right k1 and k2, go to the correct checkerboard to start traceback
-
-		//Step 2: for a checkerboard, fill traceback, as well as find the max i and j when the checkerboard traceback ends. Since the trace matrix was initialized above, pass it back and forth to recycle the memory instead of creating another trace in Step 2 in every iteration
-		//for affineGap, step 2 can be almost the same as constGap, just have 3 dimensions, use gapOpen and gapExtend instead of gapPen, add k_inChecker_max
-		trace, i_inChecker_max, j_inChecker_max, k_inChecker_max = fillTraceback_affineGap(alpha, beta, scores, gapOpen, gapExtend, checkersize_i, checkersize_j, score_highest_i, score_highest_j, trace_prep_i, trace_prep_j, k1, k2, i_inChecker_min, j_inChecker_min, trace)
-		//fmt.Printf("k_inChecker_max: %d\n", k_inChecker_max) //TODO: remove after debugging
-
-		//Step 3: for a checkerboard, use traceback to write cigar (update route and routeIdx), as well as find the min i and j when the checkerboard cigar ends
-		//for affineGap, rewrite step 3 to deal with 3D trace
-		route, routeIdx_current, i_inChecker_min, j_inChecker_min, k_inChecker_min = writeCigar_affineGap(trace, i_inChecker_max, j_inChecker_max, k_inChecker_max, route, routeIdx_current, i_inChecker_min, j_inChecker_min, k_inChecker_min)
-
-		//TODO: remove after debugging
-		//fmt.Printf("k1, k2, route: %d, %d, %v\n", k1, k2, route)
-
-		//Use Step 3's i_inChecker_min and j_inChecker_min to find the next checkerboard to go to and where to start in that checkerboard (update k1, k2)
-		if i_inChecker_min < 0 && j_inChecker_min < 0 {
-			k1, k2 = k1-1, k2-1 //go to the next checkerboard with lower i, lower j
-		} else if i_inChecker_min < 0 { //but j_inChecker != 0
-			k1 -= 1 //go to the next checkerboard with lower i, same j
-		} else if j_inChecker_min < 0 { //but i_inChecker != 0
-			k2 -= 1 //go to the next checkerboard with lower j, same i
-		}
-
-	}
-
-	//Step 4: write the last cigar entry
-	//This step is necessary because the row i=0 and the column j=0 are always stored in trace_prep, and never filled in in any checkerboard, but the cigar ends by the route going into 1 box/entry in either the row i=0 or the column j=0
-	if i_inChecker_min != -1 && j_inChecker_min == -1 { //indicating that Step 3 ended when k1 and k2 reached the smallest combination, and reached j=0, so the last cigar is a "2", e.g. if last cigar entry is the i=1, j=0 square
-		route, routeIdx_current = lastCigar_affineGap(route, routeIdx_current, 2)
-	} else if i_inChecker_min == -1 && j_inChecker_min != -1 { //indicating that Step 3 ended when k1 and k2 reached the smallest combination, and reached i=0, so the last cigar is a "1", e.g. if last cigar entry is the i=0, j=1 square
-		route, routeIdx_current = lastCigar_affineGap(route, routeIdx_current, 1)
-	} //no more "else" because the only situation left is if Step 3 ended when k1 and k2 reached the smallest combination, and reached both i=0 and j=0, aka the i=0 j=0 square, and there is no sequence there, so no cigar
-
-	//Final processing (reverse route) and return outputs
-	reverseCigar(route)
-	return route
-}
-
->>>>>>> bff15de1450eed1706137655ef793c3862fce2db
 //Step 2
 //inputs: the sequences to be aligned alpha and beta, scoring matrix for base matches, penalty for opening and extending gaps in alignment, checkerboard size for row (i) and column (j), row (i) and column (j) positions of the highest score, trace prep matrices for rows (i) and columns (j)
 //				coordinates specifying the current checkerboard in rows (k1) and columns (k2), row (i) and column (j) positions of inChecker_min_Previous which describe where Step 3 (writeCigar) stopped in the previous checkerboard that just went through Step 3 (writeCigar)
