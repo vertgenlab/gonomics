044f3de7d490979308042dc0d8cdbe7e65a38bf5
Daniel <daniel.snellings@duke.edu>
2021-11-13 11:03:34 -0500

    adjusted defaults

--- cmd/vcfWebAnnotate/vcfWebAnnotate.go
+++ cmd/vcfWebAnnotate/vcfWebAnnotate.go
@@ -231,7 +231,7 @@ func vcfWebAnnotate(data <-chan vcf.Vcf, header vcf.Header, outfile io.Writer, b
 
 func main() {
 	var outfile *string = flag.String("o", "stdout", "output to vcf file")
-	var batchSize *int = flag.Int("batchSize", 200, "number of variants to pool before querying web")
+	var batchSize *int = flag.Int("batchSize", 1000, "number of variants to pool before querying web")
 	var numBuffer *int = flag.Int("bufferSize", 2, "number of batchSize buffers to keep in memory")
 	//TODO species
 	//TODO assembly
