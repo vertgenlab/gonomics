eb6fa3ebd339b9b7d0ed8b1151a4e4203e58f97b
cshoben <cshoben@users.noreply.github.com>
2021-12-13 21:04:39 -0500

    update usage statement, remove loop for genomeBed if more than one entry matches, add test coverage that fields beyond the strand column are retained in the outputBed

--- cmd/bedMinimumDistanceName/bedMinimumDistanceName.go
+++ cmd/bedMinimumDistanceName/bedMinimumDistanceName.go
@@ -9,8 +9,7 @@ import (
 	"log"
 )
 
-// TODO: change func name
-// TODO add description for the online thing of gonomics.
+
 func bedMinimumDistanceName(inputBed string, genomeBed string, outBed string) {
 	out := fileio.EasyCreate(outBed)
 	input := bed.Read(inputBed)
@@ -18,42 +17,37 @@ func bedMinimumDistanceName(inputBed string, genomeBed string, outBed string) {
 
 	genomeMap := make(map[string][]bed.Bed)
 	var i int
+	var ok bool
 
-	//Create a map of all the locations given in the genomeBed for easy reference.
-	//Assumes location name defined by the "Name" field of the genomeBed.
+	//Create a map of all the "Names" in the genomeBed for easy reference.
+	//Assumes all genomeBed entries have unique "Name" fields.
 	for i = range genome {
+		if _ , ok = genomeMap[genome[i].Name]; ok {
+			log.Fatalf("The following entry (Name field) is found twice in the bed: %s", genome[i].Name )
+		}
 		genomeMap[genome[i].Name] = append(genomeMap[genome[i].Name], genome[i])
 	}
 
 	var genomeBedMatchingNameField []bed.Bed
 	var found bool
-	var distance int
 	var lowestDistance int
 	var upstreamDownstreamIndicator bed.Strand
 	var err error
-	var j int
 
 	for i = range input { //range over the inputBed
-		//lowestDistance = 0
-		genomeBedMatchingNameField, found = genomeMap[input[i].Name] //grab all gene bed entries that match on the "Name" field.
+		genomeBedMatchingNameField, found = genomeMap[input[i].Name] //grab the genomeBed entry that matches on the "Name" field.
 		if found != true {
-			log.Fatalf("Did not find %s", input[i].Name)
+			log.Fatalf("Did not find genomeBed match for: %s", input[i].Name)
 		}
-		for j = range genomeBedMatchingNameField {
-			distance, err = bed.MinimumDistance(genomeBedMatchingNameField[j], input[i])
-			if err != nil {
-				log.Fatalf("Unable to compare distance, error message: %s", err)
-			}
-			if j == 0 {
-				lowestDistance = distance //after this would grab the gene sense and the upstream downstream.
-				upstreamDownstreamIndicator = determineUpstreamDownstream(input[i], genomeBedMatchingNameField[j])
-			} else {
-				if distance < lowestDistance {
-					lowestDistance = distance
-					upstreamDownstreamIndicator = determineUpstreamDownstream(input[i], genomeBedMatchingNameField[j])
-				}
-			}
+		if len(genomeBedMatchingNameField) > 1 { //check that there is only one genomeBed entry that matches the current inputBed[i].Name
+			log.Fatalf("There is more than one genomeBed.Name entry that matches %s", input[i])
 		}
+		lowestDistance, err = bed.MinimumDistance(genomeBedMatchingNameField[0], input[i])
+		if err != nil {
+			log.Fatalf("Unable to compare distance, error message: %s", err)
+		}
+		upstreamDownstreamIndicator = determineUpstreamDownstream(input[i], genomeBedMatchingNameField[0])
+
 		input[i].Score = lowestDistance
 		input[i].Strand = upstreamDownstreamIndicator
 		if input[i].FieldsInitialized < 6 {
@@ -65,7 +59,7 @@ func bedMinimumDistanceName(inputBed string, genomeBed string, outBed string) {
 	exception.PanicOnErr(err)
 }
 
-func determineUpstreamDownstream(inputBed bed.Bed, genomeBed bed.Bed) bed.Strand {
+func determineUpstreamDownstream(inputBed bed.Bed, genomeBed bed.Bed) (bed.Strand) {
 	var outputStrand bed.Strand
 	if genomeBed.Strand == '+' {
 		if (inputBed.ChromStart < genomeBed.ChromStart) || (inputBed.ChromStart == genomeBed.ChromStart) {
@@ -82,7 +76,7 @@ func determineUpstreamDownstream(inputBed bed.Bed, genomeBed bed.Bed) bed.Strand
 			outputStrand = '+'
 		}
 	} else {
-		outputStrand = '.'
+		log.Fatalf("problem with genomeBed strand: %s", genomeBed)
 	}
 	return outputStrand
 }
@@ -90,22 +84,25 @@ func determineUpstreamDownstream(inputBed bed.Bed, genomeBed bed.Bed) bed.Strand
 //TODO: update usage statement. Explain that genomeBed needs strand.
 func usage() {
 	fmt.Print(
-		"compareBedDistanceBasedOnName - Compares the Name fields between \n" +
-			"two bed files, and if they match, the minimum distance between those\n" +
-			"coordinates is calculated.\n" +
-			"The first bed is iterated over line by line and the second bed is\n" +
-			"searched for the entries that match the Name field.\n" +
-			"Every entry in the first bed is expected to have at least one corresponding\n" +
-			"bed entry in the second bed, where more than one matching bed entry is allowed\n" +
-			"If there is more than one bed entry in the second bed that matches an entry in" +
-			"the first bed, the distance between the coordinates will be calculated pairwise\n" +
-			"and the lowest value will be reported (first bed paired with each matching entry from the second bed).\n" +
-			"Thus, the second bed must include at least every Name included in the first bed.\n" +
-			"For example the second bed might be the coordinates for every gene, genome wide (genomeBed)\n" +
-			"while the first bed includes a list of bed coordinates you are interested in studying (inputBed).\n" +
-			"The output file will be the inputBed file with the minimum distance reported in the Score column.\n" +
-			"No headers allowed in input files.\n" +
-			"This program was originally written for comparing eSTR coordinates to gene coordinates.\n" +
+		"bedMinimumDistanceName - For all entries in bed A (inputBed), look at the Name field and\n" +
+			"find the matching Name field in bed B (genomeBed). Calculates the minimum distance between\n" +
+			"the two bed entries and determines if the inputBed entry is upstream or downstream of the matching\n" +
+			"genomeBed entry. Upstream is denoted as '-' and downstream is denoted as '+'. \n" +
+			"The outputBed is the inputBed, with minimum distance reported in the 'Score' field and the \n" +
+			"upstream/downstream labeling reported in the 'Strand' field. \n" +
+			"Both 'Score' and 'Strand' fields will be overwritten from the inputBed if these \n" +
+			"fields are not blank.\n"+
+			"inputBed expectations: \n" +
+			"\t - must have Chr, ChromStart, ChromEnd, and Name field\n" +
+			"\t - optional to have Score and Strand but these will be rewritten\n" +
+			"\t - all subsequent fields will be retained in the outputBed\n" +
+			"\t - Name fields do not need to be unique" +
+			"genomeBed expectations:\n" +
+			"\t - must have Chr, ChromStart, ChromEnd, Name, and Strand\n" +
+			"\t - all Name field entries must be unique.\n" +
+			"\t - must include at least every Name from the inputBed, but it can have more.\n" +
+			"No headers allowed in either input files.\n" +
+			"This program was originally written for comparing eSTR coordinates to gene TSS coordinates.\n" +
 			"Usage:\n" +
 			"input.bed genome.bed out.bed\n")
 	flag.PrintDefaults()
