f22f49f6bb0dcac2fb3c463ab5db8d55c096c509
Daniel <daniel.snellings@duke.edu>
2021-12-15 16:34:46 -0500

    removed unecessary function

--- cmd/sortGraph/sortGraph_test.go
+++ cmd/sortGraph/sortGraph_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestGetSortOrder(t *testing.T) {
+func TestSortOrder(t *testing.T) {
 	sortedGraph := test(MakeTestGraph())
 	correctOrder := []uint32{0, 1, 2, 3, 4}
 
@@ -39,90 +39,6 @@ func test(graph *genomeGraph.GenomeGraph) (sortedGraph *genomeGraph.GenomeGraph)
 	err = os.Remove(output.Name())
 	exception.PanicOnErr(err)
 	return answer
-}
-
-// TestGraph Structure
-//             n2          e0 = 1
-//         e1     \e2
-//      e0     e3  \       e2 = 1
-//  n0 --- n1       n4
-//                 /
-//         e4     /e5      e5 = 1
-//             n3
-//
-//               A
-//                  \
-//                   \
-//  ATG --- CG 		 TAA
-//                   /
-//                  /
-//               T
-func MakeDisContigTestGraph() *genomeGraph.GenomeGraph {
-	graph := genomeGraph.EmptyGraph()
-
-	var n0, n1, n2, n3, n4 genomeGraph.Node
-	var f0, f2, f5, r0, r2, r5 genomeGraph.Edge
-
-	// Make Nodes
-	n0 = genomeGraph.Node{
-		Id:  0,
-		Seq: dna.StringToBases("ATG")}
-
-	n1 = genomeGraph.Node{
-		Id:  1,
-		Seq: dna.StringToBases("CG")}
-
-	n2 = genomeGraph.Node{
-		Id:  2,
-		Seq: dna.StringToBases("A")}
-
-	n3 = genomeGraph.Node{
-		Id:  3,
-		Seq: dna.StringToBases("T")}
-
-	n4 = genomeGraph.Node{
-		Id:  4,
-		Seq: dna.StringToBases("TAA")}
-
-	// Make Edges
-	f0 = genomeGraph.Edge{
-		Dest: &n1,
-		Prob: 1}
-
-	f2 = genomeGraph.Edge{
-		Dest: &n4,
-		Prob: 1}
-
-	f5 = genomeGraph.Edge{
-		Dest: &n4,
-		Prob: 1}
-
-	r0 = genomeGraph.Edge{
-		Dest: &n0,
-		Prob: 1}
-
-	r2 = genomeGraph.Edge{
-		Dest: &n2,
-		Prob: 1}
-
-	r5 = genomeGraph.Edge{
-		Dest: &n3,
-		Prob: 1}
-
-	// Define Paths
-	n0.Next = append(n0.Next, f0)
-	n1.Prev = append(n1.Prev, r0)
-	n2.Next = append(n2.Next, f2)
-	n3.Next = append(n3.Next, f5)
-	n4.Prev = append(n4.Prev, r2, r5)
-
-	graph.Nodes = append(graph.Nodes, n0, n1, n2, n3, n4)
-
-	for i := 0; i < len(graph.Nodes); i++ {
-		graph.Nodes[i].SeqTwoBit = dnaTwoBit.NewTwoBit(graph.Nodes[i].Seq)
-	}
-
-	return graph
 }
 
 // TestGraph Structure
