89bc56930435d62bb9fdbb7136aac9f092f64d00
RavenYL <yanting.raven.luo@gmail.com>
2021-12-16 17:20:59 -0500

    added testing coverage for mafToFa

---
+++ cmd/mafToFa/mafToFa_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"os"
+	"testing"
+)
+
+var mafToFaTests = []struct {
+	mafFile	string
+	refFile	string
+	speciesFile string
+	outFile_expected string
+	noMask bool
+}{
+	{"testdata/test1.maf", "testdata/test.ref.fa", "testdata/test.species.list", "testdata/test.mfa", false},
+	{"testdata/test2.maf", "testdata/test.ref.fa", "testdata/test.species.list", "testdata/test.mfa", true},
+}
+
+func TestMafToFa(t *testing.T) {
+	var err error
+	for _, v := range mafToFaTests {
+		mafToFa(v.mafFile, v.refFile, v.speciesFile, "outFile_tmp.mfa", v.noMask)
+
+		if !fileio.AreEqual("outFile_tmp.mfa", v.outFile_expected) {
+			t.Errorf("Error in mafToFa")
+		} else {
+			err = os.Remove("outFile_tmp.mfa")
+		}
+		exception.PanicOnErr(err)
+	}
+}
