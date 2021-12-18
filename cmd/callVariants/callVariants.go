2aa749b0d05b3ab834fe004f8dfe4a6a9896e1a0
Daniel <daniel.snellings@duke.edu>
2021-12-17 20:22:05 -0500

    bug in pileup

--- cmd/callVariants/callVariants.go
+++ cmd/callVariants/callVariants.go
@@ -3,14 +3,16 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/vertgenlab/gonomics/exception"
 	"github.com/vertgenlab/gonomics/sam"
+	"log"
 	"strings"
 )
 
-func callVariants(experimentalFiles, normalFiles []string) {
+func callVariants(experimentalFiles, normalFiles []string, outfile string) {
 	expHeaders, expPiles := startPileup(experimentalFiles)
 	normHeaders, normPiles := startPileup(normalFiles)
 	isExperimental := make([]bool, len(experimentalFiles)+len(normalFiles))
@@ -34,13 +36,26 @@ func startPileup(files []string) (headers []sam.Header, piles []<-chan sam.Pile)
 	var samChan <-chan sam.Sam
 	for i := range files {
 		samChan, headers[i] = sam.GoReadToChan(files[i])
+		if len(headers[i].Text) == 0 {
+			log.Fatal("ERROR: sam/bam files must have headers")
+		}
 		piles[i] = sam.GoPileup(samChan, headers[i], false, nil, nil)
 	}
 	return
 }
 
 func checkHeadersMatch(headers []sam.Header) error {
-	// TODO check chrom order
+	ref := headers[0].Chroms
+	for i := 1; i < len(headers); i++ {
+		if len(headers[i].Chroms) != len(ref) {
+			return errors.New("ERROR: reference chromosomes in input files must match")
+		}
+		for j := range headers[i].Chroms {
+			if headers[i].Chroms[j] != ref[j] {
+				return errors.New("ERROR: reference chromosomes in input files must match and be in the same order")
+			}
+		}
+	}
 	return nil
 }
 
@@ -59,7 +74,9 @@ func main() {
 	var experimentalFiles, normalFiles inputFiles
 	flag.Var(&experimentalFiles, "i", "Input experimental files , may be declared more than once (.bam, .sam)")
 	flag.Var(&normalFiles, "n", "Input normal files, may be declared more than once (.bam, .sam)")
+
+	outfile := flag.String("o", "stdout", "Output file (.vcf)")
 	flag.Parse()
 
-	callVariants(experimentalFiles, normalFiles)
+	callVariants(experimentalFiles, normalFiles, *outfile)
 }
--- sam/pileup.go
+++ sam/pileup.go
@@ -286,19 +286,19 @@ func getPile(start *Pile, refidx int, pos uint32) *Pile {
 func sendPassedLinked(start *Pile, s Sam, includeNoData bool, refmap map[string]chromInfo.ChromInfo, send chan<- Pile, pileFilters []func(p Pile) bool) (newStart *Pile) {
 	var lastRefIdx int
 	var lastPos uint32
-	for start.RefIdx != refmap[s.RName].Order || start.Pos != s.Pos {
+	for start.RefIdx != refmap[s.RName].Order || start.Pos < s.Pos {
 		if start.RefIdx == -1 {
 			break
 		}
-
 		if (start.touched || includeNoData) && passesPileFilters(*start, pileFilters) {
 			lastRefIdx = start.RefIdx
 			lastPos = start.Pos
 
 			send <- *start
-			resetPile(start)
-			start = start.next
 		}
+
+		resetPile(start)
+		start = start.next
 	}
 
 	// if we have not returned by this point, there are positions with no data
