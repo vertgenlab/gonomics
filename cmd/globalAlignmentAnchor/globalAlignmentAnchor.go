c683054bfa384ed655d686ce99592909503f57ff
RavenYL <yanting.raven.luo@gmail.com>
2021-12-29 00:41:25 -0500

    started by duplicating cmd/globalAlignment to make cmd/globalAlignmentAnchor

---
+++ cmd/globalAlignmentAnchor/globalAlignmentAnchor.go
@@ -0,0 +1,135 @@
+// Command Group: "Linear Alignment Tools"
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/vertgenlab/gonomics/align"
+	"github.com/vertgenlab/gonomics/exception" //raven added this line for file Close, exception.PanicOnErr
+	"github.com/vertgenlab/gonomics/fasta"
+	"github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/genomeGraph"
+	"log"
+	"os"      //raven added this line for MSA Fasta output
+	"strings" //raven added this line for CountSeqIdx
+)
+
+//raven did not put this helper function into the globalAlignment function because it is used twice within the globalAlignment function
+//raven wrote this block to count sequences based on the Read function in gonomics/fasta/fasta.go
+//raven changed the input variable from filename string to inputFile EasyReader, so that the file is only opened 1 time for 2 purposes: faDone and CountSeqIdx
+func CountSeqIdx(inputFile *fileio.EasyReader) int {
+	var line string
+	var seqIdx int = 1 //I know in Read seqIdx is int64 and starts with -1, but I am using it differently here. EasyReader comes in having read the first fasta, so seqIdx starts with 1
+	var doneReading bool = false
+	for line, doneReading = fileio.EasyNextRealLine(inputFile); !doneReading; line, doneReading = fileio.EasyNextRealLine(inputFile) {
+		if strings.HasPrefix(line, ">") {
+			seqIdx++
+		}
+	}
+	err := inputFile.Close()
+	exception.PanicOnErr(err)
+	return seqIdx
+}
+
+//raven did not put this helper function into the globalAlignment function because it is used in the globalAlignment function
+//faONe is target, faTwo is query
+func cigarToGraph(target fasta.Fasta, query fasta.Fasta, aln []align.Cigar) *genomeGraph.GenomeGraph {
+	answer := genomeGraph.EmptyGraph()
+	//use targetEnd and queryEnd to track position number for each fasta sequence as we move along.
+	var targetEnd, queryEnd int = 0, 0
+	var curr *genomeGraph.Node
+	//TODO: Make sure it can handle non-match at node 0. Fill out annotations for each node (knowing which allele/haplotype).
+	//TODO: Handle multi-entry fasta?
+
+	//Creating the first node. This is done independent of all other number because this node has no 'previous' nodes.  All following code leverages the cigar output (second number printed in the Op position of the struct {}) to determine if the alignment returned a 0:match, 1:insertion, or 2:deletion. All indels are relative to the target sequence.
+	curr, targetEnd, queryEnd = genomeGraph.FaSeqToNode(target, query, targetEnd, queryEnd, aln[0], 0)
+	curr = genomeGraph.AddNode(answer, curr)
+	//Drawing the remaining nodes and all edges. Method for adding edges is based on previous nodes.
+	for i := 1; i < len(aln); i++ {
+		curr, targetEnd, queryEnd = genomeGraph.FaSeqToNode(target, query, targetEnd, queryEnd, aln[i], i)
+		curr = genomeGraph.AddNode(answer, curr)
+		if aln[i].Op == align.ColM {
+			genomeGraph.AddEdge(&answer.Nodes[i-1], curr, 1)
+			genomeGraph.AddEdge(&answer.Nodes[i-2], curr, 0.5)
+		} else if aln[i].Op == align.ColI || aln[i].Op == align.ColD {
+			genomeGraph.AddEdge(&answer.Nodes[i-1], curr, 0.5)
+		} else {
+			log.Fatalf("Error: cigar.Op = %d is unrecognized...\n", aln[i].Op)
+		}
+	}
+	return answer
+}
+
+//raven moved helper functions from main and non-usage functions into this function
+func globalAlignment(inputFileOne *fileio.EasyReader, inputFileTwo *fileio.EasyReader, outFileName string) {
+	//make sure files meet the usage requirements of globalAlignment.go
+	faOne, faDoneOne := fasta.NextFasta(inputFileOne)
+	faTwo, faDoneTwo := fasta.NextFasta(inputFileTwo)
+	if faDoneOne || faDoneTwo {
+		log.Fatalf("error, unable to read .fa files, check for > symbol before each name and that each fasta entry is only one line. Check to make sure there are only four bases or N, globalAlignment is unable to use anything outside A-T-G-C-N.\n")
+	}
+	numSeqOne := CountSeqIdx(inputFileOne)
+	numSeqTwo := CountSeqIdx(inputFileTwo)
+	if numSeqOne > 1 || numSeqTwo > 1 {
+		log.Fatalf("multiple sequnces detected in .fa files: %v sequences in the first .fa file and %v sequences in the second .fa file. This program is designed for .fa files with only 1 sequence in them\n", numSeqOne, numSeqTwo)
+	}
+
+	//needleman wunsch (global alignment)
+	bestScore, aln := align.ConstGap(faOne.Seq, faTwo.Seq, align.HumanChimpTwoScoreMatrix, -430)
+	fmt.Printf("Alignment score is %d, cigar is %v \n", bestScore, aln)
+
+	//visualize
+	visualize := align.View(faOne.Seq, faTwo.Seq, aln)
+	fmt.Println(visualize)
+
+	//raven added this block to put visualized alignment output into MSA Fasta
+	if outFileName != "" { //only write to file if a filename is given in the faOut command line option
+		outFile, err := os.Create(outFileName) //fileio.EasyCreate and other fileio tools should work, but I don't really know how to use them
+		if err != nil {
+			log.Fatalf("Write to file failed on step 1\n")
+		}
+		visualizeOutput := ">" + faOne.Name + "\n" + strings.Split(visualize, "\n")[0] + "\n" + ">" + faTwo.Name + "\n" + strings.Split(visualize, "\n")[1] + "\n"
+		_, err = outFile.WriteString(visualizeOutput)
+		if err != nil {
+			log.Fatalf("Write to file failed on step 2\n")
+		}
+		outFile.Close() //commented out defer outFile.Close()
+	}
+
+	//cigar to graph
+	//genomeGraph := cigarToGraph(faOne, faTwo, aln)
+	//genomeGraph.PrintGraph(genomeGraph)
+}
+
+//raven edited this block to specify only 1 sequnce is expected in each fasta file and add Usage nad options
+func usage() {
+	fmt.Print(
+		"./globalAlignment - chelsea's global alignment\n" +
+			" Align 2 .fasta files, each with only 1 sequence\n" +
+			"Usage:\n" +
+			"	globalAlignment target.fasta query.fasta\n" +
+			"options:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	var expectedNum int = 2
+
+	flag.Usage = usage
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	faOut := flag.String("faOut", "", "fasta MSA output filename")
+	flag.Parse()
+
+	if len(flag.Args()) != expectedNum {
+		flag.Usage()
+		log.Fatalf("error, expecting 2 .fasta files to be able to align, only found %d files...\n", len(flag.Args()))
+	}
+
+	//read in sequences that should be put in as fasta type files.
+	//raven edited this block to save fileio.EasyOpen as file handles, so that the file is only opened 1 time for 2 purposes: faDone and CountSeqIdx
+	inputFileOne := fileio.EasyOpen(flag.Arg(0)) //raven's note: EasyOpen returns the type EasyReader
+	inputFileTwo := fileio.EasyOpen(flag.Arg(1))
+
+	globalAlignment(inputFileOne, inputFileTwo, *faOut)
+}
---
+++ cmd/globalAlignmentAnchor/globalAlignmentAnchor_test.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/align"
+	"github.com/vertgenlab/gonomics/dna"
+	"github.com/vertgenlab/gonomics/fasta"
+	"github.com/vertgenlab/gonomics/genomeGraph"
+	"testing"
+)
+
+//initiate fake fasta files for testing the graph function
+var toad = fasta.Fasta{Name: "toad", Seq: dna.StringToBases("TTGTTATTC")}
+var ahsoka = fasta.Fasta{Name: "ahsoka", Seq: dna.StringToBases("TTGTTC")}
+
+//make needed cigar and graph using the graph function being tested
+var _, aln = align.ConstGap(toad.Seq, ahsoka.Seq, align.HumanChimpTwoScoreMatrix, -430)
+var graph = cigarToGraph(toad, ahsoka, aln)
+
+//make a manual graph using the fake fastas
+var manual = buildManualGraph(toad, ahsoka)
+
+//Did graph and manual end up with the same number of nodes?
+func TestAddNode(t *testing.T) {
+	if len(graph.Nodes) != len(aln) {
+		t.Errorf("Number of nodes does not match with the cigar: %d %d", len(graph.Nodes), len(aln))
+	}
+}
+
+//test if graph and manual are equivalent.
+func TestCigarToGraphMatchManual(t *testing.T) {
+	compareScore := 0
+	seqCounter := compareSeq(manual, graph)
+	edgeCounter := compareEdges(manual, graph)
+	compareScore = seqCounter + edgeCounter
+
+	if compareScore != 0 {
+		t.Errorf("'Graph' was not equivalent to 'manual'. Expect counter value to be zero. Use location of non-zero output to determine the location of the issue.  compareSeq() counter was %v, compareEdge() counter was %v, giving a total score of %v", seqCounter, edgeCounter, compareScore)
+	}
+}
+
+//functions below this line were used to execute test functions
+
+//this function makes the manually created graph (called 'manual')
+func buildManualGraph(toad fasta.Fasta, ahsoka fasta.Fasta) *genomeGraph.GenomeGraph {
+	manual := genomeGraph.EmptyGraph()
+	nodeOne := &genomeGraph.Node{
+		Id:  0,
+		Seq: toad.Seq[0:3]}
+	nodeTwo := &genomeGraph.Node{
+		Id:  1,
+		Seq: toad.Seq[3:6]}
+	nodeThree := &genomeGraph.Node{
+		Id:  2,
+		Seq: toad.Seq[6:9]}
+
+	nodeOne = genomeGraph.AddNode(manual, nodeOne)
+	nodeTwo = genomeGraph.AddNode(manual, nodeTwo)
+	genomeGraph.AddEdge(nodeOne, nodeTwo, 0.5)
+	nodeThree = genomeGraph.AddNode(manual, nodeThree)
+	genomeGraph.AddEdge(nodeTwo, nodeThree, 1.0)
+	genomeGraph.AddEdge(nodeOne, nodeThree, 0.5)
+
+	return manual
+}
+
+//this function compares the sequences at each node, going base by base.
+func compareSeq(manual *genomeGraph.GenomeGraph, graph *genomeGraph.GenomeGraph) int {
+	var seqCounter int = 0
+	for i := 0; i < len(manual.Nodes); i++ {
+		for j := 0; j < len(manual.Nodes[i].Seq); j++ {
+			if manual.Nodes[i].Seq[j] == graph.Nodes[i].Seq[j] {
+				seqCounter += 0
+			} else {
+				seqCounter += 1
+			}
+		}
+	}
+	return seqCounter
+}
+
+//this function compares the edges (both previous and next) by comparing the name of the node within the prev[] and next[] for each node. As a reminder, the prev[] and next[] are type *Node, so they have all the information of the connecting nodes.
+//TODO: Compare the sequences within each edge between the 'graph' and 'manual'.
+//TODO: Compare the probability of each edge between the 'graph' and 'manual'.
+func compareEdges(manual *genomeGraph.GenomeGraph, graph *genomeGraph.GenomeGraph) int {
+	totalManualPrevEdges := 0
+	totalManualNextEdges := 0
+
+	for i := 0; i < len(manual.Nodes); i++ {
+		totalManualPrevEdges += len(manual.Nodes[i].Prev)
+	}
+	for i := 0; i < len(manual.Nodes); i++ {
+		totalManualNextEdges += len(manual.Nodes[i].Next)
+	}
+
+	prevNodeMatchCounter := 0
+	for i := 1; i < len(manual.Nodes); i++ {
+		for j := 0; j < len(manual.Nodes[i].Prev); j++ {
+			if dna.CompareSeqsCaseSensitive(manual.Nodes[i].Prev[j].Dest.Seq, graph.Nodes[i].Prev[j].Dest.Seq) == 0 {
+				prevNodeMatchCounter += 0
+			} else {
+				prevNodeMatchCounter += 1
+			}
+		}
+	}
+
+	nextNodeMatchCounter := 0
+	for i := 0; i < len(manual.Nodes)-1; i++ {
+		for j := 0; j < len(manual.Nodes[i].Next); j++ {
+			if dna.CompareSeqsCaseSensitive(manual.Nodes[i].Next[j].Dest.Seq, graph.Nodes[i].Next[j].Dest.Seq) == 0 {
+				nextNodeMatchCounter += 0
+			} else {
+				nextNodeMatchCounter += 1
+			}
+		}
+	}
+
+	totalEdgeCounter := prevNodeMatchCounter + nextNodeMatchCounter
+
+	return totalEdgeCounter
+}
