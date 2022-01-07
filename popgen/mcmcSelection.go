9322a76d0fcebb89079cd41f2a068ba65d040cc1
Craig Lowe <craiglowe@@users.noreply.github.com>
2022-01-07 16:58:20 -0500

    adding prior to mcmc

--- popgen/mcmcSelection.go
+++ popgen/mcmcSelection.go
@@ -33,28 +33,31 @@ type McmcSettings struct {
 	Verbose                 int
 }
 
-//The Theta struct stores parameter sets, including the alpha vector, mu, and sigma parameters, along with the likelihood of a particular parameter set for MCMC.
+// The Theta struct stores parameter sets, including the alpha vector, mu, and sigma parameters, along with the likelihood of a particular parameter set for MCMC.
 type Theta struct {
-	alpha []float64
-	mu    float64
-	sigma float64
-	//probability float64
+	alpha      []float64
+	mu         float64
+	sigma      float64
+	prior      float64
 	likelihood float64
 }
 
-//MetropolisAccept is a helper function of MetropolisHastings that determines whether to accept or reject a candidate parameter set.
+// MetropolisAccept is a helper function of MetropolisHastings that determines whether to accept or reject a candidate parameter set.
 func MetropolisAccept(old Theta, thetaPrime Theta, s McmcSettings) bool {
 	var pAccept, yRand float64
 	yRand = math.Log(rand.Float64())
 	var decision bool
+
+	/* I think the prior calculations can now handle this
 	if thetaPrime.sigma < 0 || thetaPrime.sigma > 0.5 { //if sigma dips below zero or above 0.5, the candidate set is automatically discarded.
 		return false
-	}
-	pAccept = BayesRatio(old, thetaPrime, s)
+	}*/
+	pAccept = PosteriorOdds(old, thetaPrime)
 	decision = pAccept > yRand
 
 	if s.Verbose == 1 {
-		log.Printf("%e\t%e\t%e\t%e\t%e\t%e\t%t\n", old.mu, thetaPrime.mu, old.likelihood, thetaPrime.likelihood, pAccept, yRand, decision)
+		log.Printf("%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%t\n", old.mu, thetaPrime.mu, old.likelihood, thetaPrime.likelihood, old.prior, thetaPrime.prior, pAccept, yRand, decision)
+		//log.Printf("%g\t%g\t%g\t%g\t%g\t%g\t%t\n", old.mu, thetaPrime.mu, math.Exp(numbers.DivideLog(old.likelihood, thetaPrime.likelihood)), math.Exp(numbers.DivideLog(old.prior, thetaPrime.prior)), math.Exp(pAccept), math.Exp(yRand), decision)
 	}
 	return decision
 }
@@ -68,25 +71,49 @@ func HastingsRatio(tOld Theta, tNew Theta, sigmaStep float64) float64 {
 	return math.Log(oldGivenNew / newGivenOld)
 }*/
 
-//BayesRatio is a helper function of MetropolisAccept taht returns the ratio of likelihoods of parameter sets
-func BayesRatio(old Theta, thetaPrime Theta, s McmcSettings) float64 {
-	like := numbers.DivideLog(thetaPrime.likelihood, old.likelihood)
-	//prob := numbers.DivideLog(thetaPrime.probability, old.probability)
-	//prob = 0 //trick for debug
-	if s.Verbose > 1 {
-		log.Printf("Old log(like): %e, New log(like): %e, likeRatio: %f", old.likelihood, thetaPrime.likelihood, math.Exp(like))
-	}
-	return like
+// PosteriorOdds is a helper function of MetropolisAccept that returns the Bayes Factor times the Prior Odds
+// this should be the probability of accepting (can be greater than 1) if the Hastings Ratio is one.
+func PosteriorOdds(old Theta, thetaPrime Theta) float64 {
+	bayesFactor := numbers.DivideLog(thetaPrime.likelihood, old.likelihood)
+	priorOdds := numbers.DivideLog(thetaPrime.prior, old.prior)
+	posteriorOdds := numbers.MultiplyLog(bayesFactor, priorOdds)
+	return posteriorOdds
 }
 
-//GenerateCandidateThetaPrime is a helper function of Metropolis Hastings that picks a new set of parameters based on the state of the current parameter set t.
+// prior returns log(probability) of having meanAlpha and sigma as mean
+// and standard deviation of the function that will be generating the individual
+// alpha values
+func priorProb(mu float64, sigma float64) float64 {
+	var sigmaPrior, muPrior float64
+
+	//prior on sigma is a uniform distribution between zero and 0.5
+	if sigma < 0 || sigma > 0.5 {
+		return math.Inf(-1) // prior probability is zero
+	} else {
+		sigmaPrior = 2
+	}
+
+	//prior on alpha is normal with a mean of zero and a stdev of 4
+	muPrior = numbers.NormalDist(mu, 0, 2)
+
+	return math.Log(muPrior * sigmaPrior)
+}
+
+// GenerateCandidateThetaPrime is a helper function of Metropolis Hastings that picks a new set of
+// parameters based on the state of the current parameter set t.
+// TODO: We could avoid some memory allocations by passing in an "old" theta and overwriting the values
 func GenerateCandidateThetaPrime(t Theta, data Afs, binomCache [][]float64, s McmcSettings) Theta {
 	//sample from uninformative gamma
 	var alphaPrime []float64
-	//var p float64 = 0.0
-	var likelihood, muPrime, sigmaPrime float64
+	var prior, likelihood, muPrime, sigmaPrime float64
 	alphaPrime = make([]float64, len(t.alpha))
-	sigmaPrime = numbers.SampleInverseNormal(t.sigma, s.SigmaStep)
+
+	if s.FixedSigma {
+		sigmaPrime = t.sigma
+	} else {
+		sigmaPrime = numbers.SampleInverseNormal(t.sigma, s.SigmaStep)
+	}
+
 	muPrime = numbers.SampleInverseNormal(t.mu, s.MuStep)
 	for i := range t.alpha {
 		alphaPrime[i] = numbers.SampleInverseNormal(muPrime, sigmaPrime)
@@ -97,10 +124,13 @@ func GenerateCandidateThetaPrime(t Theta, data Afs, binomCache [][]float64, s Mc
 	} else {
 		likelihood = AfsLikelihood(data, alphaPrime, binomCache, s.IntegralError)
 	}
+
+	prior = priorProb(muPrime, sigmaPrime)
+
 	if s.Verbose > 1 {
 		log.Printf("Candidate Theta. Mu: %f. Sigma:%f. LogLikelihood: %e.\n", muPrime, sigmaPrime, likelihood)
 	}
-	return Theta{alphaPrime, muPrime, sigmaPrime, likelihood}
+	return Theta{alphaPrime, muPrime, sigmaPrime, prior, likelihood}
 }
 
 //InitializeTheta is a helper function of Metropolis Hastings that generates the initial value of theta based on argument values.
@@ -115,6 +145,7 @@ func InitializeTheta(m float64, sig float64, data Afs, binomCache [][]float64, s
 	} else {
 		answer.likelihood = AfsLikelihood(data, answer.alpha, binomCache, s.IntegralError)
 	}
+	answer.prior = priorProb(answer.mu, answer.sigma)
 	return answer
 }
 
@@ -149,7 +180,7 @@ func MetropolisHastings(data Afs, outFile string, s McmcSettings) {
 		log.Printf("Initial Theta: mu: %f. sigma: %f. LogLikelihood: %e.", t.mu, t.sigma, t.likelihood)
 	}
 	if s.Verbose == 1 {
-		log.Printf("OldMu\tNewMu\tOldLikelihood\tNewLikelihood\tpAccept\tlogRand\tDecision\n")
+		log.Printf("OldMu\tNewMu\tOldLikelihood\tNewLikelihood\tOldPrior\tNewPrior\tpAccept\tlogRand\tDecision\n")
 	}
 	_, err = fmt.Fprintf(out, "Iteration\tMu\tSigma\tAccept\n")
 	exception.PanicOnErr(err)
