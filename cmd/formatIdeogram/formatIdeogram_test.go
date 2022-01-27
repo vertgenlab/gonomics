cc58c71a47b5adf922e6e4e0de053233a169a588
rimangan <riley.mangan@duke.edu>
2022-01-27 15:07:27 -0500

    formatIdeogram Testing

---
+++ cmd/formatIdeogram/formatIdeogram_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"os"
+	"testing"
+)
+
+var FormatIdeogramTests = []struct {
+	InFile string
+	OutFile string
+	ExpectedFile string
+	NoScore bool
+}{
+	{"testdata/test.bed", "testdata/tmp.txt", "testdata/expected.Score.txt", false},
+	{"testdata/test.bed", "testdata/tmp.txt", "testdata/expected.NoScore.txt", true},
+}
+
+func TestFormatIdeogram(t *testing.T) {
+	var err error
+	for _, v := range FormatIdeogramTests {
+		formatIdeogram(v.InFile, v.OutFile, v.NoScore)
+		if !fileio.AreEqual(v.OutFile, v.ExpectedFile) {
+			t.Errorf("Error in formatIdeogram. Output does not match expected.")
+		} else {
+			err = os.Remove(v.OutFile)
+			exception.PanicOnErr(err)
+		}
+	}
+}
