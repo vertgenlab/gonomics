84911c82b6fa6133991f93e6aaead71a2b5a2f34
rimangan <riley.mangan@duke.edu>
2022-02-04 14:54:45 -0500

    dotToNewick Testing

---
+++ cmd/dotToNewick/dotToNewick_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"os"
+	"testing"
+)
+
+var DotToNewickTests = []struct {
+	InFile string
+	OutFile string
+	ExpectedFile string
+	Verbose bool
+}{
+	{"testdata/primate.dot", "testdata/tmp.nh", "testdata/expected.nh", false},
+}
+
+func TestDotToNewick(t *testing.T) {
+	var err error
+	for _, v := range DotToNewickTests {
+		dotToNewick(v.InFile, v.OutFile, v.Verbose)
+		if !fileio.AreEqual(v.OutFile, v.ExpectedFile) {
+			t.Errorf("Error in dotToNewick. Output did not match expected.")
+		} else {
+			err = os.Remove(v.OutFile)
+			exception.PanicOnErr(err)
+		}
+	}
+}
