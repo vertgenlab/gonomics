d2553fab87eb9acf7ce5c6171bfc10d467f48dd3
RavenYL <yanting.raven.luo@gmail.com>
2022-02-11 12:25:34 -0500

    added test for SrcToAssemblyAndChrom

--- maf/maf_test.go
+++ maf/maf_test.go
@@ -7,10 +7,24 @@ import (
 )
 
 var readWriteTests = []struct {
-	filename string // input
+	filename string
 }{
-	//{"testdata/chr7.test.maf"},
 	{"testdata/chr22.test.maf"},
+}
+
+var srcToAssemblyAndChromTests = []struct {
+	src string
+	assembly_expected string
+	chrom_expected string
+}{
+	{"hg38.chr22", "hg38", "chr22"},
+}
+
+var parseTests = []struct {
+	line string
+	expected Maf
+}{
+	{"a score=407709.000000", Maf{Score: 407709.000000, Species: nil}},
 }
 
 func TestReadWrite(t *testing.T) {
@@ -23,5 +37,16 @@ func TestReadWrite(t *testing.T) {
 			t.Errorf("File has changed after reading and writing.")
 		}
 		fileio.EasyRemove(tempFile)
+	}
+}
+
+func TestSrcToAssemblyAndChrom(t *testing.T) {
+	var assembly_actual string
+	var chrom_actual string
+	for _, test := range srcToAssemblyAndChromTests {
+		assembly_actual, chrom_actual = SrcToAssemblyAndChrom(test.src)
+		if assembly_actual != test.assembly_expected || chrom_actual != test.chrom_expected {
+			t.Errorf("SrcToAssemblyAndChrom failed.")
+		}
 	}
 }
