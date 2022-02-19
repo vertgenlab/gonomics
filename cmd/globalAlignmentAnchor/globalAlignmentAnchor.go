2a0efd09ae369ad2d0cfb947cb9fdf273b675997
RavenYL <yanting.raven.luo@gmail.com>
2022-02-19 09:01:47 -0500

    got step 3 coordinatesToAlignment to work

--- cmd/globalAlignmentAnchor/globalAlignmentAnchor.go
+++ cmd/globalAlignmentAnchor/globalAlignmentAnchor.go
@@ -5,7 +5,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	//"github.com/vertgenlab/gonomics/align"
+	"github.com/vertgenlab/gonomics/align"
+	"github.com/vertgenlab/gonomics/dna"
 	"github.com/vertgenlab/gonomics/exception"
 	"github.com/vertgenlab/gonomics/fasta"
 	"github.com/vertgenlab/gonomics/fileio"
@@ -70,7 +71,7 @@ func mafToAnchor(in_maf string, species1 string, species2 string) {
 //Step 2: Use anchors to calculate coordinates that still need to be aligned
 func anchorToCoordinates(species1_match_bed_filename string, species2_match_bed_filename string, species1_genome_fa string, species2_genome_fa string) {
 	//read bed files
-	species1_bed := bed.Read(species1_match_bed_filename)
+	species1_match_bed := bed.Read(species1_match_bed_filename)
 	species2_match_bed := bed.Read(species2_match_bed_filename)
 	//read genome files, which are fastas containing each chromosome
 	species1_genome_fasta := fasta.Read(species1_genome_fa)
@@ -92,8 +93,8 @@ func anchorToCoordinates(species1_match_bed_filename string, species2_match_bed_
 	defer out_species2.Close()
 
 	//loop through bed. species1_bed and species2_match_bed should have the same number of records
-	for i := range species1_bed {
-		chr_curr = species1_bed[i].Chrom //set chr_curr to the new record
+	for i := range species1_match_bed {
+		chr_curr = species1_match_bed[i].Chrom //set chr_curr to the new record
 		//calculate the unaligned/gap chunk before we get to the aligned s line
 		if i != 0 && chr_curr != chr_prev { //if this is not the first entry, but we encounter new chr
 			//first finish off the previous chr
@@ -108,12 +109,12 @@ func anchorToCoordinates(species1_match_bed_filename string, species2_match_bed_
 			pos_species2 = 1
 		} // "else" encompasses "first entry", and "if we have existing chr"
 		current_species2 := bed.Bed{Chrom: species2_match_bed[i].Chrom, ChromStart: pos_species2, ChromEnd: species2_match_bed[i].ChromStart, Name: "species2_gap", FieldsInitialized: 4}
-		current_species1 := bed.Bed{Chrom: chr_curr, ChromStart: pos_species1, ChromEnd: species1_bed[i].ChromStart, Name: "species1_gap", FieldsInitialized: 4}
+		current_species1 := bed.Bed{Chrom: chr_curr, ChromStart: pos_species1, ChromEnd: species1_match_bed[i].ChromStart, Name: "species1_gap", FieldsInitialized: 4}
 		bed.WriteBed(out_species1.File, current_species1)
 		bed.WriteBed(out_species2.File, current_species2)
 
 		//update variables at the end of each iteration
-		pos_species1 = species1_bed[i].ChromEnd
+		pos_species1 = species1_match_bed[i].ChromEnd
 		pos_species2 = species2_match_bed[i].ChromEnd
 		chr_prev = chr_curr
 		//copy is for slices, so just use equal
@@ -129,98 +130,37 @@ func anchorToCoordinates(species1_match_bed_filename string, species2_match_bed_
 	bed.WriteBed(out_species2.File, current_species2)
 }
 
-/*
 //Step 3: globalAlignment lowMem for non-anchor sequences
-//raven did not put this helper function into the globalAlignment function because it is used twice within the globalAlignment function
-//raven wrote this block to count sequences based on the Read function in gonomics/fasta/fasta.go
-//raven changed the input variable from filename string to inputFile EasyReader, so that the file is only opened 1 time for 2 purposes: faDone and CountSeqIdx
-//resources
-	species1_genome_fastaMap[i].Name
-	records[i].Seq[start:end] // I believe Seq starts index at 0, according to fasta.go WriteFasta function
-func CountSeqIdx(inputFile *fileio.EasyReader) int {
-	var line string
-	var seqIdx int = 1 //I know in Read seqIdx is int64 and starts with -1, but I am using it differently here. EasyReader comes in having read the first fasta, so seqIdx starts with 1
-	var doneReading bool = false
-	for line, doneReading = fileio.EasyNextRealLine(inputFile); !doneReading; line, doneReading = fileio.EasyNextRealLine(inputFile) {
-		if strings.HasPrefix(line, ">") {
-			seqIdx++
-		}
-	}
-	err := inputFile.Close()
-	exception.PanicOnErr(err)
-	return seqIdx
-}
-
-//raven did not put this helper function into the globalAlignment function because it is used in the globalAlignment function
 //faONe is target, faTwo is query
-func cigarToGraph(target fasta.Fasta, query fasta.Fasta, aln []align.Cigar) *genomeGraph.GenomeGraph {
-	answer := genomeGraph.EmptyGraph()
-	//use targetEnd and queryEnd to track position number for each fasta sequence as we move along.
-	var targetEnd, queryEnd int = 0, 0
-	var curr *genomeGraph.Node
-	//TODO: Make sure it can handle non-match at node 0. Fill out annotations for each node (knowing which allele/haplotype).
-	//TODO: Handle multi-entry fasta?
+func coordinatesToAlignment(species1_gap_bed_filename string, species2_gap_bed_filename string, species1_genome_fa string, species2_genome_fa string) {
+	species1_gap_bed := bed.Read(species1_gap_bed_filename)
+	species2_gap_bed := bed.Read(species2_gap_bed_filename)
+	species1_genome_fasta := fasta.Read(species1_genome_fa)
+	species1_genome_fastaMap := fasta.ToMap(species1_genome_fasta)
+	species2_genome_fasta := fasta.Read(species2_genome_fa)
+	species2_genome_fastaMap := fasta.ToMap(species2_genome_fasta)
 
-	//Creating the first node. This is done independent of all other number because this node has no 'previous' nodes.  All following code leverages the cigar output (second number printed in the Op position of the struct {}) to determine if the alignment returned a 0:match, 1:species1ertion, or 2:species2etion. All inspecies2s are relative to the target sequence.
-	curr, targetEnd, queryEnd = genomeGraph.FaSeqToNode(target, query, targetEnd, queryEnd, aln[0], 0)
-	curr = genomeGraph.AddNode(answer, curr)
-	//Drawing the remaining nodes and all edges. Method for adding edges is based on previous nodes.
-	for i := 1; i < len(aln); i++ {
-		curr, targetEnd, queryEnd = genomeGraph.FaSeqToNode(target, query, targetEnd, queryEnd, aln[i], i)
-		curr = genomeGraph.AddNode(answer, curr)
-		if aln[i].Op == align.ColM {
-			genomeGraph.AddEdge(&answer.Nodes[i-1], curr, 1)
-			genomeGraph.AddEdge(&answer.Nodes[i-2], curr, 0.5)
-		} else if aln[i].Op == align.ColI || aln[i].Op == align.ColD {
-			genomeGraph.AddEdge(&answer.Nodes[i-1], curr, 0.5)
-		} else {
-			log.Fatalf("Error: cigar.Op = %d is unrecognized...\n", aln[i].Op)
-		}
+	var species1_seq, species2_seq []dna.Base
+
+	for i := range species1_gap_bed {
+		species1_seq = species1_genome_fastaMap[species1_gap_bed[i].Chrom][species1_gap_bed[i].ChromStart:species1_gap_bed[i].ChromEnd] // I believe Seq starts index at 0, according to fasta.go WriteFasta function
+		//species1_seq = dna.StringToBases("ACGT") //TODO: remove after debugging
+		dna.AllToUpper(species1_seq)
+		species2_seq = species2_genome_fastaMap[species2_gap_bed[i].Chrom][species2_gap_bed[i].ChromStart:species2_gap_bed[i].ChromEnd]
+		//species2_seq = dna.StringToBases("ACG") //TODO: remove after debugging
+		dna.AllToUpper(species2_seq)
+
+		//alignment with affineGap
+		fmt.Printf("species1_seq: %v", species1_seq)
+		fmt.Printf("species2_seq: %v", species2_seq)
+		bestScore, aln := align.AffineGap_customizeCheckersize(species1_seq, species2_seq, align.DefaultScoreMatrix, -400, -30, 10000, 10000)
+		fmt.Printf("Alignment score is %d, cigar is %v \n", bestScore, aln)
+
+		//visualize
+		visualize := align.View(species1_seq, species2_seq, aln)
+		fmt.Println(visualize)
 	}
-	return answer
 }
-
-//raven moved helper functions from main and non-usage functions into this function
-func globalAlignment(inputFileOne *fileio.EasyReader, inputFileTwo *fileio.EasyReader, outFileName string) {
-	//make sure files meet the usage requirements of globalAlignment.go
-	faOne, faDoneOne := fasta.NextFasta(inputFileOne)
-	faTwo, faDoneTwo := fasta.NextFasta(inputFileTwo)
-	if faDoneOne || faDoneTwo {
-		log.Fatalf("error, unable to read .fa files, check for > symbol before each name and that each fasta entry is only one line. Check to make sure there are only four bases or N, globalAlignment is unable to use anything outside A-T-G-C-N.\n")
-	}
-	numSeqOne := CountSeqIdx(inputFileOne)
-	numSeqTwo := CountSeqIdx(inputFileTwo)
-	if numSeqOne > 1 || numSeqTwo > 1 {
-		log.Fatalf("multiple sequnces detected in .fa files: %v sequences in the first .fa file and %v sequences in the second .fa file. This program is designed for .fa files with only 1 sequence in them\n", numSeqOne, numSeqTwo)
-	}
-
-	//needleman wunsch (global alignment)
-	bestScore, aln := align.ConstGap(faOne.Seq, faTwo.Seq, align.HumanChimpTwoScoreMatrix, -430)
-	fmt.Printf("Alignment score is %d, cigar is %v \n", bestScore, aln)
-
-	//visualize
-	visualize := align.View(faOne.Seq, faTwo.Seq, aln)
-	fmt.Println(visualize)
-
-	//raven added this block to put visualized alignment output into MSA Fasta
-	if outFileName != "" { //only write to file if a filename is given in the faOut command line option
-		outFile, err := os.Create(outFileName) //fileio.EasyCreate and other fileio tools should work, but I don't really know how to use them
-		if err != nil {
-			log.Fatalf("Write to file failed on step 1\n")
-		}
-		visualizeOutput := ">" + faOne.Name + "\n" + strings.Split(visualize, "\n")[0] + "\n" + ">" + faTwo.Name + "\n" + strings.Split(visualize, "\n")[1] + "\n"
-		_, err = outFile.WriteString(visualizeOutput)
-		if err != nil {
-			log.Fatalf("Write to file failed on step 2\n")
-		}
-		outFile.Close() //commented out defer outFile.Close()
-	}
-
-	//cigar to graph
-	//genomeGraph := cigarToGraph(faOne, faTwo, aln)
-	//genomeGraph.PrintGraph(genomeGraph)
-}
-*/
 
 //raven edited this block to specify only 1 sequnce is expected in each fasta file and add Usage nad options
 func usage() {
@@ -263,4 +203,5 @@ func main() {
 
 	mafToAnchor(in_maf, species1, species2)
 	anchorToCoordinates("testdata/species1ForStep2.bed", "testdata/species2ForStep2.bed", "testdata/species1.fa", "testdata/species2.fa")
+	coordinatesToAlignment("species1ForStep3.bed", "speices2ForStep3.bed", "testdata/species1.fa", "testdata/species2.fa")
 }
