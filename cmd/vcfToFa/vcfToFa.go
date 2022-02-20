4c2ad2a602e2bdcb8c5b4c94626a2a84866d5939
rimangan <riley.mangan@duke.edu>
2022-02-20 18:04:56 -0500

    vcfToFa MultiFa Compatibility

--- cmd/vcfToFa/vcfToFa.go
+++ cmd/vcfToFa/vcfToFa.go
@@ -11,31 +11,75 @@ import (
 	"log"
 )
 
-func vcfToFa(inVcfFilename string, inFaFilename string, outFaFilename string, useAlt bool) {
+func vcfToFa(inVcfFilename string, inFaFilename string, outFaFilename string, useAlt bool, multiFaMode bool, multiFaChromName string) {
 	var seqsOrdered []fasta.Fasta
 	var seqsLookup fasta.FastaMap
 	var vcfRecords <-chan vcf.Vcf
+	var firstTime bool = true
+	var prevPos int//position of the previous variant. Used to enforce sorted order in multiFaMode.
+	var multiFaAppendName string//name of the appended record in multiFaMode.
+	var currAlnPos int
+	var outIndex int
+	var prevRefPos, prevAlnPos int = 0, 0
 
 	// We hang on to the ordered version of the fasta sequences so that we could
 	// output the edited sequences (those in the map) in the same order that
 	// we originally read them, but we do not have that currently
 	seqsOrdered = fasta.Read(inFaFilename)
+	if multiFaMode && useAlt {
+		var refSeqCopy []dna.Base
+		if len(seqsOrdered) == 0 {
+			log.Fatalf("Input fasta must contain at least one record in multiFaMode.")
+		}
+		refSeqCopy = make([]dna.Base, len(seqsOrdered[0].Seq))
+		copy(refSeqCopy, seqsOrdered[0].Seq)
+		multiFaAppendName = seqsOrdered[0].Name + "alt"
+		seqsOrdered = append(seqsOrdered, fasta.Fasta{Name: multiFaAppendName, Seq: refSeqCopy})
+		outIndex = len(seqsOrdered) - 1//index of the output sequence.
+	}
 	seqsLookup = fasta.ToMap(seqsOrdered)
 
 	// we could do some sort of header check here to make sure the fa matches the vcf
 	vcfRecords, _ = vcf.GoReadToChan(inVcfFilename)
+
 	for v := range vcfRecords {
+		//first a block of code to enforce sorted order in multiFaMode.
+		if firstTime && multiFaMode && v.Chr == multiFaChromName {//only check if in multiFaMode and we have the right chrom
+			firstTime = false
+			prevPos = v.Pos
+		} else if multiFaMode {
+			if v.Pos < prevPos && v.Chr == multiFaChromName {
+				log.Fatalf("Input VCF variants must be in sorted order in multiFaMode.")
+			}
+		}
+
 		if !(vcf.IsBiallelic(v) && vcf.IsSubstitution(v)) {
 			log.Fatal("Error: currently we only handle biallelic substitutions\n")
 		}
 
-		// check to make sure the fa matches the vcf before we edit
-		if seqsLookup[v.Chr][v.Pos-1] != dna.StringToBase(v.Ref) {
-			log.Fatal("Error: base in fasta didn't match ref base from VCF record\n")
-		}
+		if multiFaMode {
+			if v.Chr == multiFaChromName && useAlt {//only consider variants with the correct chrom name.
+				currAlnPos = fasta.RefPosToAlnPosCounter(seqsOrdered[outIndex], v.Pos-1, prevRefPos, prevAlnPos)
+				if seqsLookup[multiFaAppendName][currAlnPos] != dna.StringToBase(v.Ref) {
+					log.Fatal("Error: base in fasta didn't match ref base from VCF record\n")
+				}
+				if useAlt {
+					seqsLookup[multiFaAppendName][currAlnPos] = dna.StringToBase(v.Alt[0])
+				}
 
-		if useAlt {
-			seqsLookup[v.Chr][v.Pos-1] = dna.StringToBase(v.Alt[0])
+				//update the ref/aln pos pair
+				prevRefPos = v.Pos - 1 //move to 0 base from 1-based vcf
+				prevAlnPos = currAlnPos
+			}
+		} else {
+			// check to make sure the fa matches the vcf before we edit
+			if seqsLookup[v.Chr][v.Pos-1] != dna.StringToBase(v.Ref) {
+				log.Fatal("Error: base in fasta didn't match ref base from VCF record\n")
+			}
+
+			if useAlt {
+				seqsLookup[v.Chr][v.Pos-1] = dna.StringToBase(v.Alt[0])
+			}
 		}
 	}
 
@@ -58,6 +102,10 @@ func usage() {
 func main() {
 	var expectedNumArgs int = 3
 	var useAlt *bool = flag.Bool("useAlt", false, "Retains only biallelic variants in the output file.")
+	var multiFaMode *bool = flag.Bool("multiFaMode", false, "Use a multiFa reference chromosome. Variants will be placed in reference" +
+		"positions with respect to gaps. Input vcf must be sorted when using multiFaMode. Appends a new sequence with the updated sequence from the reference sequence," +
+		"defined as the first sequence in the multiFa. Currently only supports useAlt.")
+	var multiFaChromName *string = flag.String("multiFaChromName", "", "Identifies the chrom of the multiFa file. Only variants matching this chrom name will be used.")
 
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
@@ -77,5 +125,5 @@ func main() {
 		log.Fatal("Error: you must use at least one option or the fasta file will have no edits\n")
 	}
 
-	vcfToFa(inVcfFilename, inFaFilename, outFaFilename, *useAlt)
+	vcfToFa(inVcfFilename, inFaFilename, outFaFilename, *useAlt, *multiFaMode, *multiFaChromName)
 }
--- cmd/vcfToFa/vcfToFa_test.go
+++ cmd/vcfToFa/vcfToFa_test.go
@@ -12,14 +12,21 @@ var VcfToFaTests = []struct {
 	inputFaFile        string
 	actualOutputFile   string
 	expectedOutputFile string
+	useAlt bool
+	multiFaMode bool
+	multiFaChromName string
 }{
-	{"testdata/testInput.vcf", "testdata/testInput.fa", "testdata/actual.fa", "testdata/expected.fa"},
+	//{"testdata/testInput.vcf", "testdata/testInput.fa", "testdata/actual.fa", "testdata/expected.fa", true, false, ""},
+	//{"testdata/testInput.vcf", "testdata/testInput.fa", "testdata/actualNoAlt.fa", "testdata/testInput.fa", false, false, ""},//should just produce the input file.
+	//{"testdata/testMultiInput.vcf", "testdata/testMultiInput.fa", "testdata/actualMultiNoAlt.fa", "testdata/expectedMultiNoAlt.fa", false, true, "chr1"},//should also produce the input file
+	//{"testdata/testMultiInput.vcf", "testdata/testMultiInput.fa", "testdata/actualMultiAlt.fa", "testdata/expectedMultiAlt.fa", true, true, "chr1"},
+	{"testdata/testMultiInput.vcf", "testdata/testMultiInput.fa", "testdata/actualMultiAltchr2.fa", "testdata/expectedMultiAltchr2.fa", true, true, "chr2"},
 }
 
 func TestVcfToFa(t *testing.T) {
 	var err error
 	for _, v := range VcfToFaTests {
-		vcfToFa(v.inputVcfFile, v.inputFaFile, v.actualOutputFile, true)
+		vcfToFa(v.inputVcfFile, v.inputFaFile, v.actualOutputFile, v.useAlt, v.multiFaMode, v.multiFaChromName)
 		if !fileio.AreEqual(v.actualOutputFile, v.expectedOutputFile) {
 			t.Errorf("VcfToFa output did not match expected output.")
 		} else {
