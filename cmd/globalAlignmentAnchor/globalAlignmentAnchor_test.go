9403694653c0c301e0b9646c3c95c9ae57168b8c
RavenYL <yanting.raven.luo@gmail.com>
2022-02-20 23:31:27 -0500

    added go test

--- cmd/globalAlignmentAnchor/globalAlignmentAnchor_test.go
+++ cmd/globalAlignmentAnchor/globalAlignmentAnchor_test.go
@@ -1,120 +1,79 @@
 package main
 
 import (
-	"github.com/vertgenlab/gonomics/align"
-	"github.com/vertgenlab/gonomics/dna"
-	"github.com/vertgenlab/gonomics/fasta"
-	"github.com/vertgenlab/gonomics/genomeGraph"
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"os"
+	"path"
+	"strings"
 	"testing"
 )
 
-//initiate fake fasta files for testing the graph function
-var toad = fasta.Fasta{Name: "toad", Seq: dna.StringToBases("TTGTTATTC")}
-var ahsoka = fasta.Fasta{Name: "ahsoka", Seq: dna.StringToBases("TTGTTC")}
-
-//make needed cigar and graph using the graph function being tested
-var _, aln = align.ConstGap(toad.Seq, ahsoka.Seq, align.HumanChimpTwoScoreMatrix, -430)
-var graph = cigarToGraph(toad, ahsoka, aln)
-
-//make a manual graph using the fake fastas
-var manual = buildManualGraph(toad, ahsoka)
-
-//Did graph and manual end up with the same number of nodes?
-func TestAddNode(t *testing.T) {
-	if len(graph.Nodes) != len(aln) {
-		t.Errorf("Number of nodes does not match with the cigar: %d %d", len(graph.Nodes), len(aln))
-	}
+var globalAlignmentAnchorTests = []struct {
+	in_maf string
+	species1 string
+	species2 string
+	species1_genome string
+	species2_genome string
+	out_maf_expected string
+	species1_match_bed_expected string
+	species2_match_bed_expected string
+	species1_gap_bed_expected string
+	species2_gap_bed_expected string
+	out_alignment_expected string
+}{
+	{"testdata/in_hg38_vs_rheMac10.toy.maf", "hg38", "rheMac10", "testdata/hg38.toy.fa", "testdata/rheMac10.toy.fa", "testdata/in_hg38_vs_rheMac10.toy.filtered.expected.maf", "testdata/out_hg38_match.expected.bed", "testdata/out_rheMac10_match.expected.bed", "testdata/out_hg38_gap.expected.bed", "testdata/out_rheMac10_gap.expected.bed", "testdata/out_alignment.expected.tsv"},
 }
 
-//test if graph and manual are equivalent.
-func TestCigarToGraphMatchManual(t *testing.T) {
-	compareScore := 0
-	seqCounter := compareSeq(manual, graph)
-	edgeCounter := compareEdges(manual, graph)
-	compareScore = seqCounter + edgeCounter
+func TestGlobalAlignmentAnchorTests(t *testing.T) {
+	var out_maf, species1_match, species2_match, species1_gap, species2_gap, out_alignment string
+	var err error
 
-	if compareScore != 0 {
-		t.Errorf("'Graph' was not equivalent to 'manual'. Expect counter value to be zero. Use location of non-zero output to determine the location of the issue.  compareSeq() counter was %v, compareEdge() counter was %v, giving a total score of %v", seqCounter, edgeCounter, compareScore)
-	}
-}
+	for _, test := range globalAlignmentAnchorTests {
+		out_maf = strings.Replace(test.in_maf, ".maf", ".filtered.maf", 1)
+		species1_match = strings.Replace(test.in_maf, path.Base(test.in_maf), "out_" + test.species1 + "_match.bed", 1)
+		species2_match = strings.Replace(test.in_maf, path.Base(test.in_maf), "out_" + test.species2 + "_match.bed", 1)
+		species1_gap = strings.Replace(test.in_maf, path.Base(test.in_maf), "out_" + test.species1 + "_gap.bed", 1)
+		species2_gap = strings.Replace(test.in_maf, path.Base(test.in_maf), "out_" + test.species2 + "_gap.bed", 1)
+		out_alignment = strings.Replace(test.in_maf, path.Base(test.in_maf), "out_alignment.tsv", 1)
+		globalAlignmentAnchor(test.in_maf, test.species1, test.species2, test.species1_genome, test.species2_genome)
 
-//functions below this line were used to execute test functions
-
-//this function makes the manually created graph (called 'manual')
-func buildManualGraph(toad fasta.Fasta, ahsoka fasta.Fasta) *genomeGraph.GenomeGraph {
-	manual := genomeGraph.EmptyGraph()
-	nodeOne := &genomeGraph.Node{
-		Id:  0,
-		Seq: toad.Seq[0:3]}
-	nodeTwo := &genomeGraph.Node{
-		Id:  1,
-		Seq: toad.Seq[3:6]}
-	nodeThree := &genomeGraph.Node{
-		Id:  2,
-		Seq: toad.Seq[6:9]}
-
-	nodeOne = genomeGraph.AddNode(manual, nodeOne)
-	nodeTwo = genomeGraph.AddNode(manual, nodeTwo)
-	genomeGraph.AddEdge(nodeOne, nodeTwo, 0.5)
-	nodeThree = genomeGraph.AddNode(manual, nodeThree)
-	genomeGraph.AddEdge(nodeTwo, nodeThree, 1.0)
-	genomeGraph.AddEdge(nodeOne, nodeThree, 0.5)
-
-	return manual
-}
-
-//this function compares the sequences at each node, going base by base.
-func compareSeq(manual *genomeGraph.GenomeGraph, graph *genomeGraph.GenomeGraph) int {
-	var seqCounter int = 0
-	for i := 0; i < len(manual.Nodes); i++ {
-		for j := 0; j < len(manual.Nodes[i].Seq); j++ {
-			if manual.Nodes[i].Seq[j] == graph.Nodes[i].Seq[j] {
-				seqCounter += 0
-			} else {
-				seqCounter += 1
-			}
+		if !fileio.AreEqual(out_maf, test.out_maf_expected) {
+			t.Errorf("Error in out_maf")
+		} else {
+			err = os.Remove(out_maf)
 		}
-	}
-	return seqCounter
-}
 
-//this function compares the edges (both previous and next) by comparing the name of the node within the prev[] and next[] for each node. As a reminder, the prev[] and next[] are type *Node, so they have all the information of the connecting nodes.
-//TODO: Compare the sequences within each edge between the 'graph' and 'manual'.
-//TODO: Compare the probability of each edge between the 'graph' and 'manual'.
-func compareEdges(manual *genomeGraph.GenomeGraph, graph *genomeGraph.GenomeGraph) int {
-	totalManualPrevEdges := 0
-	totalManualNextEdges := 0
-
-	for i := 0; i < len(manual.Nodes); i++ {
-		totalManualPrevEdges += len(manual.Nodes[i].Prev)
-	}
-	for i := 0; i < len(manual.Nodes); i++ {
-		totalManualNextEdges += len(manual.Nodes[i].Next)
-	}
-
-	prevNodeMatchCounter := 0
-	for i := 1; i < len(manual.Nodes); i++ {
-		for j := 0; j < len(manual.Nodes[i].Prev); j++ {
-			if dna.CompareSeqsCaseSensitive(manual.Nodes[i].Prev[j].Dest.Seq, graph.Nodes[i].Prev[j].Dest.Seq) == 0 {
-				prevNodeMatchCounter += 0
-			} else {
-				prevNodeMatchCounter += 1
-			}
+		if !fileio.AreEqual(species1_match, test.species1_match_bed_expected) {
+			t.Errorf("Error in species1_match_bed")
+		} else {
+			err = os.Remove(species1_match)
 		}
-	}
 
-	nextNodeMatchCounter := 0
-	for i := 0; i < len(manual.Nodes)-1; i++ {
-		for j := 0; j < len(manual.Nodes[i].Next); j++ {
-			if dna.CompareSeqsCaseSensitive(manual.Nodes[i].Next[j].Dest.Seq, graph.Nodes[i].Next[j].Dest.Seq) == 0 {
-				nextNodeMatchCounter += 0
-			} else {
-				nextNodeMatchCounter += 1
-			}
+		if !fileio.AreEqual(species2_match, test.species2_match_bed_expected) {
+			t.Errorf("Error in species2_match_bed")
+		} else {
+			err = os.Remove(species2_match)
 		}
+
+		if !fileio.AreEqual(species1_gap, test.species1_gap_bed_expected) {
+			t.Errorf("Error in species1_gap_bed")
+		} else {
+			err = os.Remove(species1_gap)
+		}
+
+		if !fileio.AreEqual(species2_gap, test.species2_gap_bed_expected) {
+			t.Errorf("Error in species2_gap_bed")
+		} else {
+			err = os.Remove(species2_gap)
+		}
+
+		if !fileio.AreEqual(out_alignment, test.out_alignment_expected) {
+			t.Errorf("Error in out_alignment")
+		} else {
+			err = os.Remove(out_alignment)
+		}
+
+		exception.PanicOnErr(err)
 	}
-
-	totalEdgeCounter := prevNodeMatchCounter + nextNodeMatchCounter
-
-	return totalEdgeCounter
 }
