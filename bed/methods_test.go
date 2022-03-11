d6c10ba5572e113aa59845b8261a7ea97cf264c4
cshoben <cshoben@users.noreply.github.com>
2022-03-11 13:33:13 -0500

    stuck when trying to create a *[]Bed

--- bed/methods_test.go
+++ bed/methods_test.go
@@ -5,44 +5,56 @@ import (
 )
 
 var BedStructTests = []struct {
-	A                Bed
-	chrom			string
-	chromStart		int
-	chromEnd		int
+	bed        Bed
+	chrom      string
+	chromStart int
+	chromEnd   int
 }{
-	{A: Bed{Chrom: "chr1", ChromStart: 10, ChromEnd: 20, Name: "", Score:0, Strand:Positive}, chrom: "chr1", chromStart: 10, chromEnd: 20},
-	{A: Bed{Chrom: "chr10", ChromStart: 11, ChromEnd: 18, Name: "", Score:0, Strand:Positive}, chrom: "chr10", chromStart: 11, chromEnd: 18},
-	{A: Bed{Chrom: "chr12", ChromStart: 12, ChromEnd: 16, Name: "", Score:0, Strand:Positive}, chrom: "chr12", chromStart: 11, chromEnd: 16},
+	{bed: Bed{Chrom: "chr1", ChromStart: 10, ChromEnd: 20, Name: "", Score: 0, Strand: Positive}, chrom: "chr1", chromStart: 10, chromEnd: 20},
+	{bed: Bed{Chrom: "chr10", ChromStart: 11, ChromEnd: 18, Name: "", Score: 0, Strand: Positive}, chrom: "chr10", chromStart: 11, chromEnd: 18},
+	{bed: Bed{Chrom: "chr12", ChromStart: 12, ChromEnd: 16, Name: "", Score: 0, Strand: Positive}, chrom: "chr12", chromStart: 11, chromEnd: 16},
 }
-
 
 func TestBed_GetChrom(t *testing.T) {
 	for _, v := range BedStructTests {
-		if v.chrom != v.A.GetChrom() {
-			t.Errorf("Error in bed methods.go GetChrom(); Expected chrom: %v. Actual chrom: %v.", v.chrom, v.A.GetChrom())
+		if v.chrom != v.bed.GetChrom() {
+			t.Errorf("Error in bed methods.go GetChrom(); Expected chrom: %v. Actual chrom: %v.", v.chrom, v.bed.GetChrom())
 		}
 	}
 }
 
-
 func TestBed_GetChromStart(t *testing.T) {
 	for _, v := range BedStructTests {
-		if v.chromStart != v.A.GetChromStart() {
-			t.Errorf("Error in bed methods.go GetChromStart(); Expected chromStart: %v. Actual chromStart: %v.", v.chrom, v.A.GetChrom())
+		if v.chromStart != v.bed.GetChromStart() {
+			t.Errorf("Error in bed methods.go GetChromStart(); Expected chromStart: %v. Actual chromStart: %v.", v.chromStart, v.bed.GetChromStart())
 		}
 	}
 }
 
 func TestBed_GetChromEnd(t *testing.T) {
-
+	for _, v := range BedStructTests {
+		if v.chromStart != v.bed.GetChromEnd() {
+			t.Errorf("Error in bed methods.go GetChromEnd(); Expected chromEnd: %v. Actual chromEnd: %v.", v.chromEnd, v.bed.GetChromEnd())
+		}
+	}
 }
 
-
-
-
+// I can't figure out how to handle a slice of *Bed. Below are my two attempts.
 
 func TestBedSlice_Len(t *testing.T) {
-
+	bedFile := *Read("bedFileTest.bed") // this is a type []Bed and I want []*Bed
+	if bedFile.Len() != 3 {
+		//error here
+	}
 }
 
- */
+func TestBedSlice_Len(t *testing.T) {
+	var allBed BedSlice
+	for _, v := range BedStructTests {
+		pointer := *v.bed
+		append(allBed, pointer)
+		if allBed.Len() != 3 {
+			//error here
+		}
+	}
+}
