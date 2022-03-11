0ca9ffd82b1f9e80f760eb4e7ab3c14cb84b8994
cshoben <cshoben@users.noreply.github.com>
2022-03-11 13:56:04 -0500

    resolved the Len() method testing

--- bed/methods_test.go
+++ bed/methods_test.go
@@ -12,7 +12,7 @@ var BedStructTests = []struct {
 }{
 	{bed: Bed{Chrom: "chr1", ChromStart: 10, ChromEnd: 20, Name: "", Score: 0, Strand: Positive}, chrom: "chr1", chromStart: 10, chromEnd: 20},
 	{bed: Bed{Chrom: "chr10", ChromStart: 11, ChromEnd: 18, Name: "", Score: 0, Strand: Positive}, chrom: "chr10", chromStart: 11, chromEnd: 18},
-	{bed: Bed{Chrom: "chr12", ChromStart: 12, ChromEnd: 16, Name: "", Score: 0, Strand: Positive}, chrom: "chr12", chromStart: 11, chromEnd: 16},
+	{bed: Bed{Chrom: "chr12", ChromStart: 12, ChromEnd: 16, Name: "", Score: 0, Strand: Positive}, chrom: "chr12", chromStart: 12, chromEnd: 16},
 }
 
 func TestBed_GetChrom(t *testing.T) {
@@ -33,28 +33,33 @@ func TestBed_GetChromStart(t *testing.T) {
 
 func TestBed_GetChromEnd(t *testing.T) {
 	for _, v := range BedStructTests {
-		if v.chromStart != v.bed.GetChromEnd() {
+		if v.chromEnd != v.bed.GetChromEnd() {
 			t.Errorf("Error in bed methods.go GetChromEnd(); Expected chromEnd: %v. Actual chromEnd: %v.", v.chromEnd, v.bed.GetChromEnd())
 		}
 	}
 }
 
-// I can't figure out how to handle a slice of *Bed. Below are my two attempts.
 
+
+
+
+
+/*
 func TestBedSlice_Len(t *testing.T) {
-	bedFile := *Read("bedFileTest.bed") // this is a type []Bed and I want []*Bed
+	bedFile := Read("bedFileTest.bed") // this is a type []Bed and I want []*Bed
 	if bedFile.Len() != 3 {
 		//error here
 	}
 }
+*/
 
 func TestBedSlice_Len(t *testing.T) {
 	var allBed BedSlice
 	for _, v := range BedStructTests {
-		pointer := *v.bed
-		append(allBed, pointer)
+		pointer := &v.bed
+		allBed = append(allBed, pointer)
 		if allBed.Len() != 3 {
-			//error here
+			t.Errorf("Error in bed methods.go Len(); Expected %v Actual %v.", 3, allBed.Len())
 		}
 	}
 }
