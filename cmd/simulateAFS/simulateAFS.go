22737eab8c9a4778ec183bbd07f718546e25cfef
rpornmon <82060975+rpornmon@users.noreply.github.com>
2022-03-18 12:25:25 -0400

    bug fixing

--- cmd/simulateAFS/simulateAFS.go
+++ cmd/simulateAFS/simulateAFS.go
@@ -6,23 +6,15 @@ import (
 	"flag"
 	"fmt"
 	"github.com/vertgenlab/gonomics/fasta"
-	//"github.com/vertgenlab/gonomics/exception"
-	//"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/simulate"
 	"log"
 	"math/rand"
 )
 
-func simulateAFS(popSize uint, mutRate int, genTime uint, genomeSize int, outFile string, setSeed int64) {
+func simulateAFS(popSize int, mutRate float64, genTime int, genomeSize int, outFile string, setSeed int64) {
 	rand.Seed(setSeed)
 	c := simulate.GoSimulateAFS(popSize, mutRate, genTime, genomeSize)
-	//var err error
-
 	fasta.Write(outFile, c)
-
-	
-
-	fmt.Println(c)
 }
 
 func usage() {
@@ -36,10 +28,10 @@ func usage() {
 
 func main() {
 	var expectedNumArgs int = 1
-	var popSize *uint = flag.Uint("N", 10, "Specifies the population size in the simulation.")
-	var mutRate *int = flag.Int("m", 1, "Specifies the genome-wide mutation rate in the simulation.")
-	var genTime *uint = flag.Uint("t", 10, "Specifies the generation time in the simulation")
-	var genomeSize *int = flag.Int("g", 100, "Specifies the genome size in base-pair in the simulation")
+	var popSize *int = flag.Int("N", 5, "Specifies the population size in the simulation.")
+	var mutRate *float64 = flag.Float64("m", 5e-1, "Specifies the genome-wide mutation rate in the simulation.")
+	var genTime *int = flag.Int("t", 10, "Specifies the generation time in the simulation")
+	var genomeSize *int = flag.Int("g", 10, "Specifies the genome size in base-pair in the simulation")
 	var setSeed *int64 = flag.Int64("setSeed", -1, "Use a specific seed for the RNG.")
 
 	flag.Usage = usage
