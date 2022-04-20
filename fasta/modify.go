a65a836e566c64dc31bceade3746217eafb698b6
Craig Lowe <craiglowe@users.noreply.github.com>
2022-04-20 16:19:47 -0400

    comment for fasta copy

--- fasta/modify.go
+++ fasta/modify.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+// Copy makes a copy of the fasta struct and the associated slice of dna so that
+// modifying one will not also modify the other
 func Copy(a Fasta) Fasta {
 	var ans Fasta
 	ans = Fasta{a.Name, make([]dna.Base, len(a.Seq))}
