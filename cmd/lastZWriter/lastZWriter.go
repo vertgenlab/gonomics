c2b0a05c57282b0ca04e4b78ac44e7bdd5d8329a
Christi Fauci <christiana.fauci@duke.edu>
2022-07-12 15:40:18 -0400

    changed variables, added a todo

--- cmd/lastZWriter/lastZWriter.go
+++ cmd/lastZWriter/lastZWriter.go
@@ -6,34 +6,45 @@ import (
 	"github.com/vertgenlab/gonomics/lastZWriter"
 )
 
-func main(speciesListFile string, refListFile string, allDists string, outText string, outCsv string) {
+func MakeArray(speciesListFile string, refListFile string, allDists string, outText string, outCsv string) {
 	speciesList := fileio.EasyOpen(speciesListFile)
 	refList := fileio.EasyOpen(refListFile)
 	csvFile := fileio.EasyCreate(outCsv)
-	textFile := fileio.EasyCreate(outText)
+	fileio.EasyCreate(outText)
 	var spec, ref string
 	var speciesDone, refDone bool
 	var parameters []string
 	var matrix string
 	var dist int
+	var allLines []string
+	csvOut := csv.NewWriter(csvFile)
 	for ref, refDone = fileio.EasyNextRealLine(refList); !refDone; ref, refDone = fileio.EasyNextRealLine(refList) {
 		for spec, speciesDone = fileio.EasyNextRealLine(speciesList); !speciesDone; spec, speciesDone = fileio.EasyNextRealLine(speciesList) {
 			if spec != ref {
 				parameters, matrix, dist = lastZWriter.AlignSetUp(spec, ref, allDists)
-				writeFiles(ref, spec, parameters, matrix, dist, textFile, csvFile)
+				allLines := writeFiles(ref, spec, parameters, matrix, dist, allLines, csvOut)
 			}
 		}
 	}
+	fileio.Write(outText, allLines)
+
 }
 
-func writeFiles(reference string, species string, parameters []string, matrix string, dist int, text *fileio.EasyWriter, csvFile *fileio.EasyWriter) {
-	//TODO: write CSV with directories involved?
-	//TODO: write into text file
+func writeFiles(reference string, species string, parameters []string, matrix string, dist int, allLines []string, csvOut *csv.Writer) (lines []string) {
+	var currLine string
+	lastZ := "/hpc/group/vertgenlab/softwareShared/lastz-master/src/lastz"
+
 	allVars := []string{
 		reference, species, string(dist), matrix}
 
-	csvOut := csv.NewWriter(csvFile)
 	csvOut.Write(allVars)
 	csvOut.Write(parameters)
+
+	currLine = lastZ
+	allLines = append(allLines, currLine)
+
+	return allLines
+
+	//TODO:add lines here but write and update files in the wrapper (make array)
 
 }
