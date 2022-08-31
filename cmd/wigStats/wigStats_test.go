072e69232702610a3371dfe8f161d256c7e51ff7
seth weaver <sethweaver@seths-MacBook-Pro.local>
2022-08-31 12:29:40 -0400

    wig stats testing

---
+++ cmd/wigStats/wigStats_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"os"
+	"testing"
+)
+
+var WigStatsTests = []struct {
+	inputNoGapBed    string
+	inputWig         string
+	outputFile       string
+	expectedFile     string
+	missingDataValue float64
+}{
+	{"testdata/test.noGap.bed", "testdata/test.wig", "testdata/tmp.tsv", "testdata/expected.tsv", -10},
+}
+
+func TestWigStats(t *testing.T) {
+	var err error
+	for _, v := range WigStatsTests {
+		wigStats(v.inputWig, v.inputNoGapBed, v.outputFile, v.missingDataValue)
+		if !fileio.AreEqual(v.outputFile, v.expectedFile) {
+			t.Errorf("Error in wigStats.")
+		} else {
+			err = os.Remove(v.outputFile)
+			exception.PanicOnErr(err)
+		}
+	}
+}
