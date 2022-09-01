d871e9bb61eb768c1f51480fa2c72843dad9e76e
Christi Fauci <christiana.fauci@duke.edu>
2022-09-01 14:41:20 -0400

    added testing and squashed bugs

--- fasta/bin.go
+++ fasta/bin.go
@@ -2,23 +2,30 @@ package fasta
 
 import (
 	"github.com/vertgenlab/gonomics/dna"
+	"log"
 )
 
 func BinFasta(genome []Fasta, binNum int) map[int][]Fasta {
-	var remainingBases, totalBases, lastPos int
+	var totalBases, lastPos int
 	var lastName string
-	var answer map[int][]Fasta
+	answer := make(map[int][]Fasta, binNum)
 	for c := range genome {
 		totalBases = totalBases + len(genome[c].Seq)
 	}
+
 	//TODO: chance that there's a situation where the final bin with leftOvers is empty or very small, what I have with baseCap,
 	//	or chance that the last bin could be almost double the size of everything else (totalBases/BinNum and remainder goes in last bin)
 	//	could do a check to see if remainder is super large and then handle as necessary
 	equalBinNum := binNum - 1
-	baseCap := totalBases / equalBinNum
+	remainder := totalBases % binNum
+	splitBases := totalBases - remainder //number of bases to split between equalBins
+	baseCap := splitBases / equalBinNum
 
-	for j := range answer {
-		for i := range genome {
+	for j := 0; j < binNum; j++ {
+		for i := 0; i < len(genome); i++ {
+
+			log.Print(answer)
+			var remainingBases int
 			var done int
 			var currFa Fasta
 			var currBases []dna.Base
@@ -28,29 +35,79 @@ func BinFasta(genome []Fasta, binNum int) map[int][]Fasta {
 			}
 
 			value, exists := answer[j]
-			if value != nil { //old bin
+
+			log.Printf("evaluating answer[%d]", j)
+			log.Print(value, exists)
+
+			if j == 0 && i == 0 {
+				currBases = appendRangeOfBases(genome[i].Seq, 0, baseCap)
+				currFa.Name = genome[i].Name
+				currFa.Seq = currBases
+				answer[j] = append(answer[j], currFa)
+				lastPos = baseCap
+				continue
+			} else if value != nil { //old bin
+
+				log.Print("old bin new contig")
+
 				filledBases := calcNumBasesInBin(answer[j])
 				if filledBases < baseCap { //bin not full
 					remainingBases = baseCap - filledBases
-					currBases = appendRangeOfBases(genome[i].Seq, 0, remainingBases-1)
-				}
-			} else if !exists || value == nil { //new bin
-				lastName, lastPos = findLastBinnedPos(answer[j-1])
-				if lastName == genome[i].Name && lastPos < len(genome[i].Seq) { //new bin and old contig
-					remainingBases = len(genome[i].Seq) - lastPos
-					currBases = appendRangeOfBases(genome[i].Seq, lastPos+1, lastPos+remainingBases)
+
+					log.Print(remainingBases)
+
+					currBases = appendRangeOfBases(genome[i].Seq, 0, remainingBases)
 					currFa.Name = genome[i].Name
 					currFa.Seq = currBases
 					answer[j] = append(answer[j], currFa)
+					lastPos = len(currBases)
+
+					log.Print("bin not full", lastPos)
+
+				} else {
+					continue
+				}
+			} else if !exists || value == nil { //new bin
+
+				log.Print("making new bin")
+				log.Print(i)
+
+				lastName = findLastBinnedName(answer[j-1])
+
+				log.Print(lastPos, len(genome[i].Seq))
+
+				if lastName == genome[i].Name && lastPos < len(genome[i].Seq) { //new bin and old contig
+
+					log.Print("new bin old contig")
+
+					remainingBases = len(genome[i].Seq) - lastPos
+					currBases = appendRangeOfBases(genome[i].Seq, lastPos, lastPos+remainingBases)
+					currFa.Name = genome[i].Name
+					currFa.Seq = currBases
+					answer[j] = append(answer[j], currFa)
+					lastPos = lastPos + len(currBases)
 				} else if lastName == genome[i].Name && lastPos == len(genome[i].Seq) { //new bin and new contig
+
+					log.Print("new bin new contig")
+					lastPos = 0
+
+					//currBases = appendRangeOfBases(genome[i].Seq, 0, baseCap)
+					//currFa.Name = genome[i].Name
+					//currFa.Seq = currBases
+					//answer[j] = append(answer[j], currFa)
+				} else if lastName != "" && lastPos == 0 {
 					currBases = appendRangeOfBases(genome[i].Seq, 0, baseCap)
 					currFa.Name = genome[i].Name
 					currFa.Seq = currBases
 					answer[j] = append(answer[j], currFa)
+					lastPos = baseCap
 				}
 			}
 		}
 	}
+
+	log.Print(answer)
+
 	return answer
 }
 
@@ -62,16 +119,14 @@ func calcNumBasesInBin(f []Fasta) int {
 	return totalBases
 }
 
-func findLastBinnedPos(f []Fasta) (string, int) {
-	var lastPos int
+func findLastBinnedName(f []Fasta) string {
 	var lastFasta string
 
 	for i := range f {
 		lastFasta = f[i].Name
-		lastPos = len(f[i].Seq)
 	}
 
-	return lastFasta, lastPos
+	return lastFasta
 }
 
 func appendRangeOfBases(bases []dna.Base, start int, stop int) []dna.Base {
@@ -85,7 +140,7 @@ func appendRangeOfBases(bases []dna.Base, start int, stop int) []dna.Base {
 		}
 	} else {
 		for b := range bases {
-			if b >= start && b <= stop {
+			if b >= start && b <= stop-1 {
 				answer = append(answer, bases[b])
 			}
 		}
