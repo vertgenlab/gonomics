528f4f870d0dc015952319ab9772e641a8734763
sweavs111 <seth.weaver@duke.edu>
2022-10-13 11:58:23 -0400

    revisions to bedPeOverlap.go

--- cmd/bedPeOverlap/bedPeOverlap.go
+++ cmd/bedPeOverlap/bedPeOverlap.go
@@ -12,10 +12,8 @@ import (
 
 func bedpeOverlap(bedpeSelectFile string, bedpeInFile string, contactOutFile string) {
 	var inIntervals = make([]interval.Interval, 0)
-	var leftOverlaps []interval.Interval
-	var rightOverlaps []interval.Interval
-	var rightHalf bedpe.BedPeHalf
-	var leftHalf bedpe.BedPeHalf
+	var leftOverlaps, rightOverlaps []interval.Interval
+	var rightHalf, leftHalf bedpe.BedPeHalf
 	var err error
 
 	out := fileio.EasyCreate(contactOutFile)
@@ -24,18 +22,21 @@ func bedpeOverlap(bedpeSelectFile string, bedpeInFile string, contactOutFile str
 
 	inBedPe := bedpe.Read(bedpeInFile)
 
+	var left, right bedpe.BedPeHalf
 	for _, i := range inBedPe {
-		left, right := bedpe.SplitBedPe(i)
+		left, right = bedpe.SplitBedPe(i)
 		inIntervals = append(inIntervals, left, right)
 	}
 	inTree := interval.BuildTree(inIntervals)
 
+	var found bool
+	var j, k int
 	for _, i := range contactRecords {
+		found = false
 		leftOverlaps = interval.Query(inTree, i.A, "any")
 		rightOverlaps = interval.Query(inTree, i.B, "any")
-		found := false
-		for j := range leftOverlaps {
-			for k := range rightOverlaps {
+		for j = range leftOverlaps {
+			for k = range rightOverlaps {
 				leftHalf = leftOverlaps[j].(bedpe.BedPeHalf)
 				rightHalf = rightOverlaps[k].(bedpe.BedPeHalf)
 				if leftHalf.Home == rightHalf.Home {
@@ -54,7 +55,7 @@ func bedpeOverlap(bedpeSelectFile string, bedpeInFile string, contactOutFile str
 }
 
 func usage() {
-	fmt.Print("bedpeOverlap - Returns all bedpe entries that fully overlap another bedpe entry. " +
+	fmt.Print("bedpeOverlap - Returns all bedpe entries where both bed region pairs overlap a bedpe entry in the selectFile \n" +
 		"bedpe entries from in the inFile will be selected based on overlaps with the selectFile.\n" +
 		"Usage:\n" +
 		"	bedpeOverlap [options] selectFile.bedpe inFile.bedPe out.bedpe\n\n")
