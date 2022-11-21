c372ad93ca952c79aecce8b8d32f01c625a3fbd7
rimangan <riley.mangan@duke.edu>
2022-11-20 20:53:52 -0500

    multiFaToAxt

---
+++ cmd/multiFaToAxt/multiFaToAxt.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/vertgenlab/gonomics/axt"
+	"github.com/vertgenlab/gonomics/fasta"
+	"log"
+)
+
+func multiFaToAxt(inFile string, rName string, qName string, outFile string) {
+	var answer []axt.Axt = make([]axt.Axt, 0)
+	var currAxt axt.Axt
+	records := fasta.Read(inFile)
+	if len(records) != 2 {
+		log.Fatalf("multiFaToAxt accepts only a pairwise multiFa alignment. Expected 2 entries, found %d.\n", len(records))
+	}
+	var recordsCopy []fasta.Fasta = fasta.RemoveGaps(records)
+
+	currAxt = axt.Axt{
+		RName: rName,
+		RStart: 0,
+		REnd: len(recordsCopy[0].Seq),
+		QName: qName,
+		QStart: 0,
+		QEnd: len(recordsCopy[1].Seq),
+		QStrandPos: true,
+		Score: 100,//dummy value, hardcoded
+		RSeq: records[0].Seq,
+		QSeq: records[1].Seq,
+	}
+
+	answer = append(answer, currAxt)
+	axt.Write(outFile, answer)
+}
+
+func usage() {
+	fmt.Print(
+		"multiFaToAxt - Convert a multiFa format alignment to an AXT format alignment file.\n" +
+			"Usage:\n" +
+			" multiFaToAxt input.fa RName QName output.axt\n" +
+			"options:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	var expectedNumArgs = 4
+	flag.Usage = usage
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	flag.Parse()
+
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Error: expecting %d arguments, but got %d\n", expectedNumArgs, len(flag.Args()))
+	}
+
+	inFile := flag.Arg(0)
+	RName := flag.Arg(1)
+	QName := flag.Arg(2)
+	outFile := flag.Arg(3)
+
+	multiFaToAxt(inFile, RName, QName, outFile)
+}
+
+
+
---
+++ cmd/multiFaToAxt/multiFaToAxt_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"os"
+	"testing"
+)
+
+var MultiFaToAxtTests = []struct {
+	InFile string
+	RName string
+	QName string
+	OutFile string
+	ExpecctedFile string
+}{
+	{"testdata/test.fa", "chr1", "chr1", "testdata/tmp.axt", "testdata/expected.axt"},
+}
+
+func TestMultiFaToAxt(t *testing.T) {
+	var err error
+	for _, v := range MultiFaToAxtTests {
+		multiFaToAxt(v.InFile, v.RName, v.QName, v.OutFile)
+		if !fileio.AreEqual(v.OutFile, v.ExpecctedFile) {
+			t.Errorf("Error in multiFatoAxt. Output was not as expected.")
+		} else {
+			err = os.Remove(v.OutFile)
+			exception.PanicOnErr(err)
+		}
+	}
+}
