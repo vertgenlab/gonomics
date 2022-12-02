752bed6928b86cd05a9e2ee3a755d069c9ed1f3c
rimangan <riley.mangan@duke.edu>
2022-12-02 16:09:54 -0500

    new wigPeaks option

--- cmd/wigPeaks/wigPeaks_test.go
+++ cmd/wigPeaks/wigPeaks_test.go
@@ -2,33 +2,35 @@ package main
 
 import (
 	"github.com/vertgenlab/gonomics/bed"
-	"github.com/vertgenlab/gonomics/common"
+	"github.com/vertgenlab/gonomics/exception"
 	"os"
 	"testing"
 )
 
 //use struct to specify testdata
 var wigPeaksTests = []struct {
-	in_wig    string
-	out_bed   string
+	inWig    string
+	outBed string
+	expectedBed   string
 	threshold float64
+	findMinima bool
 }{
-	{"testdata/in_wig_1.wig", "testdata/out_bed_1.bed", 20},
-	{"testdata/in_wig_2.wig", "testdata/out_bed_2.bed", 50},
+	//{"testdata/in_wig_1.wig", "testdata/out_bed_tmp.bed", "testdata/out_bed_1.bed", 20, false},
+	//{"testdata/in_wig_2.wig", "testdata/out_bed_tmp.bed", "testdata/out_bed_2.bed", 50, false},
+	{"testdata/in_wig_1.wig", "testdata/tmp.Minima.bed", "testdata/expected.minima.bed", 50, true},
 }
 
 func TestWigPeaks(t *testing.T) {
+	var err error
 	for _, v := range wigPeaksTests {
-		wigPeaks(v.in_wig, "out_bed_tmp.bed", v.threshold)
-
-		records := bed.Read("out_bed_tmp.bed")
-		expected := bed.Read(v.out_bed)
+		wigPeaks(v.inWig, v.outBed, v.threshold, v.findMinima)
+		records := bed.Read(v.outBed)
+		expected := bed.Read(v.expectedBed)
 		if !bed.AllAreEqual(records, expected) {
 			t.Errorf("Error in wigPeaks, created bed and test bed are not equal.")
-		}
-		err := os.Remove("out_bed_tmp.bed")
-		if err != nil {
-			common.ExitIfError(err)
+		} else {
+			err = os.Remove(v.outBed)
+			exception.PanicOnErr(err)
 		}
 	}
 }
