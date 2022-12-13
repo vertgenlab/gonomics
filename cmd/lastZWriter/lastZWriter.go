c0dafc96c1d4f70b1aa155e02b5c95902ce55a3d
Christi Fauci <christiana.fauci@duke.edu>
2022-12-13 14:11:40 -0500

    tracking bug

--- cmd/lastZWriter/lastZWriter.go
+++ cmd/lastZWriter/lastZWriter.go
@@ -76,9 +76,9 @@ func fastaFinder(lastZ string, pairwise string, reference string, species string
 		}
 	}
 
-	if theseLines == nil {
-		log.Fatal("No lines to write to file")
-	}
+	//if theseLines == nil {
+	//	log.Fatal("No lines to write to file")
+	//}
 
 	return theseLines
 }
