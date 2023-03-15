ff341b61da7d0900aa1bb44b0d864253d3fcbe6d
sweavs111 <seth.weaver@duke.edu>
2023-03-15 15:11:08 -0400

    one more save

--- cmd/liftCoordinates/liftCoordinates.go
+++ cmd/liftCoordinates/liftCoordinates.go
@@ -18,6 +18,7 @@ import (
 	"github.com/vertgenlab/gonomics/vcf"
 	"io"
 	"log"
+	"path"
 	"strings"
 	"unicode/utf8"
 )
@@ -26,11 +27,27 @@ type fileWriter interface {
 	WriteToFileHandle(io.Writer)
 }
 
-func liftCoordinates(chainFile string, inFile string, outFile string, faFile string, unMapped string, minMatch float64, swapAlleleAB bool, verbose int, bedpe bool) {
+func liftCoordinates(chainFile string, inFile string, outFile string, faFile string, unMapped string, minMatch float64, swapAlleleAB bool, verbose int) {
 	var err error
 	if minMatch < 0.0 || minMatch > 1.0 {
 		log.Fatalf("minMatch must be between 0 and 1. User input: %f.\n", minMatch)
 	}
+
+	//determine filetype
+	filetype := path.Ext(inFile)
+	if filetype == ".gz" {
+		// If terminal extension is ".gz" then trim off the gz and get the next extension
+		filetype = path.Ext(inFile[0 : len(inFile)-len(filetype)])
+	}
+
+	var bdpe bool
+	bdpe = false
+
+	switch filetype {
+	case ".bed":
+		bdpe = true
+	}
+
 	//first task, make tree from chainFile
 	chainChan, _ := chain.GoReadToChan(chainFile)
 	var chainIntervals []interval.Interval
@@ -62,48 +79,47 @@ func liftCoordinates(chainFile string, inFile string, outFile string, faFile str
 	// bedpe error message map
 	mp := map[int]string{
 		0:  "Record below has no ortholog in new assembly.",
-		1:  "Record below mapped correctly",
+		1:  "Record below mapped correctly.",
 		-1: "Record below falls below min-match threshold.",
 	}
 
 	//second task, read in intervals, find chain, and convert to new interval
 	var A, B string
 	var found1, found2 bool
-	var halfA, halfB bed.Bed
-	inChan := lift.GoReadToChan(inFile)
+	var bedpeEntry bedpe2.BedPe
+	var iOut, jOut lift.Lift
+	var mappedI, mappedJ int
 	var overlap []interval.Interval
+
+	inChan := lift.GoReadToChan(inFile)
 	for i := range inChan {
-		if bedpe {
+		if bdpe {
 			j := <-inChan
-			iOut, mappedI := liftAndCheck(i, tree, faFile, minMatch)
-			jOut, mappedJ := liftAndCheck(j, tree, faFile, minMatch)
-			halfA = bed.Bed{iOut.GetChrom(), iOut.GetChromStart(), iOut.GetChromEnd()}
-			halfB = bed.Bed{jOut.GetChrom(), jOut.GetChromStart(), jOut.GetChromEnd()}
-			bedpeEntry := bedpe2.BedPe{halfA, halfB}
+			iOut, mappedI = liftAndCheckBedpe(i, tree, minMatch)
+			jOut, mappedJ = liftAndCheckBedpe(j, tree, minMatch)
+			bedpeEntry = bedpe2.BedPe{A: bed.Bed{Chrom: iOut.GetChrom(), ChromStart: iOut.GetChromStart(), ChromEnd: iOut.GetChromEnd()}, B: bed.Bed{Chrom: jOut.GetChrom(), ChromStart: jOut.GetChromStart(), ChromEnd: jOut.GetChromEnd()}}
 
 			if mappedI == 1 && mappedJ == 1 {
-				bedpeEntry.(fileWriter).WriteToFileHandle(out)
+				bedpe2.WriteToFileHandle(out, bedpeEntry)
 			} else {
 				A, found1 = mp[mappedI]
 				B, found2 = mp[mappedJ]
 				if found1 && found2 {
 					_, err = fmt.Fprintf(un, "A: %s  B: %s\n", A, B)
 					exception.PanicOnErr(err)
-					bedpeEntry.(fileWriter).WriteToFileHandle(out)
+					bedpe2.WriteToFileHandle(un, bedpeEntry)
 				} else if found1 && !found2 {
 					_, err = fmt.Fprintf(un, "A: %s  B: Record below maps to multiple chains\n", A)
 					exception.PanicOnErr(err)
-					bedpeEntry.(fileWriter).WriteToFileHandle(out)
+					bedpe2.WriteToFileHandle(un, bedpeEntry)
 				} else if !found1 && found2 {
 					_, err = fmt.Fprintf(un, "A: Record below maps to multiple chains  B: %s\n", B)
 					exception.PanicOnErr(err)
-					bedpeEntry.(fileWriter).WriteToFileHandle(out)
+					bedpe2.WriteToFileHandle(un, bedpeEntry)
 				}
-
 			}
-
+		} else {
 			overlap = interval.Query(tree, i, "any")
-
 			if len(overlap) > 1 {
 				_, err = fmt.Fprintf(un, "Record below maps to multiple chains:\n")
 				exception.PanicOnErr(err)
@@ -112,15 +128,20 @@ func liftCoordinates(chainFile string, inFile string, outFile string, faFile str
 				_, err = fmt.Fprintf(un, "Record below has no ortholog in new assembly:\n")
 				exception.PanicOnErr(err)
 				i.WriteToFileHandle(un)
-			} else if !minMatchPass(overlap[0].(chain.Chain), i, minMatch) {
+			} else if !minMatchPass(overlap[0].(chain.Chain), i, s.MinMatch) {
 				a, b = lift.MatchProportion(overlap[0].(chain.Chain), i)
 				_, err = fmt.Fprintf(un, "Record below fails minMatch with a proportion of %f. Here's the corresponding chain: %d.\n", numbers.Min(a, b), overlap[0].(chain.Chain).Score)
 				exception.PanicOnErr(err)
 				i.WriteToFileHandle(un)
+			} else if s.StrictBorders && !lift.StrictBorderCheck(overlap[0].(chain.Chain), i) {
+				_, err = fmt.Fprintf(un, "Record below failed the strict border check:\n")
+				exception.PanicOnErr(err)
+				i.WriteToFileHandle(un)
 			} else {
 				i = i.UpdateCoord(lift.LiftCoordinatesWithChain(overlap[0].(chain.Chain), i)).(lift.Lift) //now i is 1-based and we can assert VCF.
+
 				//special check for lifting over VCF files
-				if faFile != "" {
+				if s.FaFile != "" {
 					//faFile will be given if we are lifting over VCF data.
 					currVcf = i.(vcf.Vcf)
 					if utf8.RuneCountInString(currVcf.Ref) > 1 || utf8.RuneCountInString(currVcf.Alt[0]) > 1 {
@@ -134,7 +155,7 @@ func liftCoordinates(chainFile string, inFile string, outFile string, faFile str
 						i.WriteToFileHandle(un)
 					} else if QuerySeq(ref, currVcf.Chr, int(currVcf.Pos-1), dna.StringToBases(currVcf.Ref)) { //first question: does the "Ref" match the destination fa at this position.
 						//second question: does the "Alt" also match. Can occur in corner cases such as Ref=A, Alt=AAA. Currently we don't invert but write a verbose log print.
-						if QuerySeq(ref, currVcf.Chr, int(currVcf.Pos-1), dna.StringToBases(currVcf.Alt[0])) && verbose > 0 {
+						if QuerySeq(ref, currVcf.Chr, int(currVcf.Pos-1), dna.StringToBases(currVcf.Alt[0])) && s.Verbose > 0 {
 							_, err = fmt.Fprintf(un, "For VCF on %s at position %d, Alt and Ref both match the fasta. Ref: %s. Alt: %s.", currVcf.Chr, currVcf.Pos, currVcf.Ref, currVcf.Alt)
 							exception.PanicOnErr(err)
 						}
@@ -145,7 +166,7 @@ func liftCoordinates(chainFile string, inFile string, outFile string, faFile str
 						exception.PanicOnErr(err)
 						i.WriteToFileHandle(un)
 						currVcf = vcf.InvertVcf(currVcf)
-						if swapAlleleAB {
+						if s.SwapAB {
 							swapInfoAlleles(&currVcf)
 						}
 						i = &currVcf
@@ -159,11 +180,11 @@ func liftCoordinates(chainFile string, inFile string, outFile string, faFile str
 					i.(fileWriter).WriteToFileHandle(out)
 				}
 			}
+			err = out.Close()
+			exception.PanicOnErr(err)
+			err = un.Close()
+			exception.PanicOnErr(err)
 		}
-		err = out.Close()
-		exception.PanicOnErr(err)
-		err = un.Close()
-		exception.PanicOnErr(err)
 	}
 }
 
@@ -215,10 +236,12 @@ func swapInfoAlleles(v *vcf.Vcf) {
 	v.Info = string(newInfo)
 }
 
-func liftAndCheck(in lift.Lift, inChain map[string]*interval.IntervalNode, faFile string, matchPerc float64) (lift.Lift, int) {
+func liftAndCheckBedpe(in lift.Lift, inChain map[string]*interval.IntervalNode, matchPerc float64) (lift.Lift, int) {
 	var overlap []interval.Interval
 	var a, b float64
 	var currVcf vcf.Vcf
+
+	a, b = 0, 0
 
 	overlap = interval.Query(inChain, in, "any")
 	mapped := len(overlap)
@@ -229,50 +252,6 @@ func liftAndCheck(in lift.Lift, inChain map[string]*interval.IntervalNode, faFil
 			a, b = lift.MatchProportion(overlap[0].(chain.Chain), in)
 		} else {
 			in = in.UpdateCoord(lift.LiftCoordinatesWithChain(overlap[0].(chain.Chain), in)).(lift.Lift)
-			//special check for lifting over VCF files
-			if faFile != "" {
-				//faFile will be given if we are lifting over VCF data.
-				currVcf = in.(vcf.Vcf)
-			}
-			//special check for lifting over VCF files
-			if faFile != "" {
-				//faFile will be given if we are lifting over VCF data.
-				currVcf = in.(vcf.Vcf)
-				if utf8.RuneCountInString(currVcf.Ref) > 1 || utf8.RuneCountInString(currVcf.Alt[0]) > 1 {
-					_, err = fmt.Fprintf(un, "The following record did not lift as VCF lift is not currently supported for INDEL records.\n")
-					exception.PanicOnErr(err)
-					i.WriteToFileHandle(un)
-					//log.Fatalf("VCF liftOver is currently not supported for INDEL records. Please filter the input VCF for substitutions and try again.") //Currently we're causing INDEL records to fatal.
-				} else if len(currVcf.Alt) > 1 {
-					_, err = fmt.Fprintf(un, "The following record did not lift as VCF lift is not currently supported for multiallelic sites.\n")
-					exception.PanicOnErr(err)
-					i.WriteToFileHandle(un)
-				} else if QuerySeq(ref, currVcf.Chr, int(currVcf.Pos-1), dna.StringToBases(currVcf.Ref)) { //first question: does the "Ref" match the destination fa at this position.
-					//second question: does the "Alt" also match. Can occur in corner cases such as Ref=A, Alt=AAA. Currently we don't invert but write a verbose log print.
-					if QuerySeq(ref, currVcf.Chr, int(currVcf.Pos-1), dna.StringToBases(currVcf.Alt[0])) && verbose > 0 {
-						_, err = fmt.Fprintf(un, "For VCF on %s at position %d, Alt and Ref both match the fasta. Ref: %s. Alt: %s.", currVcf.Chr, currVcf.Pos, currVcf.Ref, currVcf.Alt)
-						exception.PanicOnErr(err)
-					}
-					i.(fileWriter).WriteToFileHandle(out)
-					//the third case handles when the alt matches but not the ref, in which case we invert the VCF.
-				} else if QuerySeq(ref, currVcf.Chr, int(currVcf.Pos-1), dna.StringToBases(currVcf.Alt[0])) {
-					_, err = fmt.Fprintf(un, "Record below was lifted, but the ref and alt alleles are inverted:\n")
-					exception.PanicOnErr(err)
-					i.WriteToFileHandle(un)
-					currVcf = vcf.InvertVcf(currVcf)
-					if swapAlleleAB {
-						swapInfoAlleles(&currVcf)
-					}
-					i = &currVcf
-					i.(fileWriter).WriteToFileHandle(out)
-				} else {
-					_, err = fmt.Fprintf(un, "For the following record, neither the Ref nor the Alt allele matched the bases in the corresponding destination fasta location.\n")
-					exception.PanicOnErr(err)
-					i.WriteToFileHandle(un)
-				}
-			} else {
-				i.(fileWriter).WriteToFileHandle(out)
-			}
 		}
 	}
 	return in, mapped
