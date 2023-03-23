69871a7c834ffa7e1813dd6c2a051d1aaf48cfee
Daniel <daniel.snellings@duke.edu>
2023-03-23 11:37:50 -0400

    ignore, testing workflow

---
+++ cmd/testCmd/testCmd.go
@@ -0,0 +1,9 @@
+// Command Group: "Congratulations on digging deep into my testing spiral... As a reward I'll share some secret knowledge with you ;)"
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
