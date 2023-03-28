d449f09d5b693ae672a9ec53927ff29e573c4e53
sweavs111 <seth.weaver@duke.edu>
2023-03-28 13:44:14 -0400

    Testing plus additional "keepMatching" option

--- cmd/geneIdToName/geneIdToName_test.go
+++ cmd/geneIdToName/geneIdToName_test.go
@@ -13,16 +13,18 @@ var geneIdToNameTests = []struct {
 	out           string
 	ncbi          bool
 	ensembl       bool
+	keepMatching  bool
 	expectedFile  string
 }{
-	{"testdata/testConversionTable.txt", "testdata/in.txt", "testdata/tmp.out.txt", false, false, "testdata/expected.txt"},
-	{"", "testdata/in.txt", "testdata/tmp.out.txt", true, false, "testdata/expected.txt"},
+	{"testdata/testConversionTable.txt", "testdata/in.txt", "testdata/tmp.out.txt", false, false, false, "testdata/expected.txt"},
+	{"", "testdata/in.txt", "testdata/tmp.out.txt", true, false, false, "testdata/expected.txt"},
+	{"", "testdata/in.txt", "testdata/tmp.keepMatching.out.txt", true, false, true, "testdata/expectedKeepMatching.txt"},
 }
 
 func TestBedPeOverlap(t *testing.T) {
 	var err error
 	for _, v := range geneIdToNameTests {
-		geneIdToName(v.geneNameTable, v.in, v.out, v.ncbi, v.ensembl)
+		geneIdToName(v.geneNameTable, v.in, v.out, v.ncbi, v.ensembl, v.keepMatching)
 		if !fileio.AreEqual(v.out, v.expectedFile) {
 			t.Errorf("Error in geneIdToName.")
 		} else {
