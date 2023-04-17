b0fcc633e34bc744bfeedde50ef7425a150e73ac
Daniel <daniel.snellings@duke.edu>
2023-04-17 15:37:45 -0400

    added GODEBUG init in exception package

--- exception/exception.go
+++ exception/exception.go
@@ -1,7 +1,40 @@
 // Package exception provides globally available functions to simplify exit after unrecoverable errors are encountered.
 package exception
 
-import "log"
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+// init is run as the exception package is imported and modifies the GODEBUG variable to contain
+// randautoseed=0 so that functions in gonomics that call the rand package are deterministic by default.
+// The seed for the rand package can still be changed in individual packages or cmds by calling rand.Seed(num).
+func init() {
+	dbgVar := os.Getenv("GODEBUG")
+
+	if dbgVar == "" {
+		err := os.Setenv("GODEBUG", "randautoseed=0")
+		PanicOnErr(err)
+		return
+	}
+
+	variables := strings.Split(dbgVar, ",")
+	var found bool
+	for i := range variables {
+		if strings.HasPrefix(variables[i], "randautoseed=") {
+			variables[i] = "randautoseed=0"
+			found = true
+		}
+	}
+
+	if !found {
+		variables = append(variables, "randautoseed=0")
+	}
+
+	err := os.Setenv("GODEBUG", strings.Join(variables, ","))
+	PanicOnErr(err)
+}
 
 // PanicOnErr will panic if input error is not nil.
 // Panic is the preferred kill method for packages in the codebase (i.e. everything not in the cmd folder).
