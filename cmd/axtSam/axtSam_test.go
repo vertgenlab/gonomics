2734ca62d05c6ec8ee3d1b5612633a5342d14b40
Eric Au <ericdotau@gmail.com>
2023-04-19 16:38:46 -0400

    fix: axtSam test

--- cmd/axtSam/axtSam_test.go
+++ cmd/axtSam/axtSam_test.go
@@ -11,21 +11,18 @@ import (
 	"github.com/vertgenlab/gonomics/sam"
 )
 
-var sequence []dna.Base = dna.StringToBases("GCTTGAAGAGGAGCTGTAGATCAGGCGGAAGATTGGGACCCCCCTTCCCT")
-var testSamFile string = "testdata/test.sam"
-
 var axtToSamTests = []struct {
 	axtFile   string
 	samHeader sam.Header
 	output    string
 }{
-	{"testdata/test.axt", sam.GenerateHeader(fasta.ToChromInfo([]fasta.Fasta{{Name: "chrI", Seq: sequence}}), nil, sam.Unsorted, sam.None), testSamFile},
+	{"testdata/test.axt", sam.GenerateHeader(fasta.ToChromInfo([]fasta.Fasta{{Name: "chrI", Seq: dna.StringToBases("GCTTGAAGAGGAGCTGTAGATCAGGCGGAAGATTGGGACCCCCCTTCCCT")}}), nil, sam.Unsorted, sam.None), "testdata/test.sam"},
 }
 
 func TestAxtToSamCmd(t *testing.T) {
 	for _, v := range axtToSamTests {
 		axtToSam(v.axtFile, v.samHeader, v.output)
-		testSam, _ := sam.Read(testSamFile)
+		testSam, _ := sam.Read(v.output)
 		var expectedSam sam.Sam = sam.Sam{
 			QName: "contig_1",
 			Flag:  0,
@@ -36,13 +33,13 @@ func TestAxtToSamCmd(t *testing.T) {
 			RNext: "*",
 			PNext: 0,
 			TLen:  49,
-			Seq:   sequence,
+			Seq:   dna.StringToBases("GCTTGAAGAGGAGCTGTAGATCAGGCGGAAGATTGGGACCCTTCCCT"),
 			Qual:  "*",
 			Extra: fmt.Sprintf("AS:i:%d\tXS:i:%d\tXE:i:%d", 4021, 1, 47),
 		}
 		if !sam.Equal(testSam[0], expectedSam) {
 			t.Errorf("Error: testing samFile=%v and expectedSam=%v are not equal...", testSam[0], expectedSam)
 		}
-		os.Remove(testSamFile)
+		os.Remove(v.output)
 	}
 }
