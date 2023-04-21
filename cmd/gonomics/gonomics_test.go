1629b52c90f78bf5d1b239e2c19378b08243eb84
Daniel <daniel.snellings@duke.edu>
2023-04-21 10:29:07 -0400

    only run gonomics cmd test if binary is installed

--- cmd/gonomics/gonomics_test.go
+++ cmd/gonomics/gonomics_test.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"github.com/vertgenlab/gonomics/exception"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
 func TestGonomics(t *testing.T) {
-	//buildCmdCache("") // tests everything is formatted correctly and updates cache file
+	// check to see if binary is already installed, if so buildCmdCache, else return
+
+	// Find binary location. Preference is gonomics exec path > GOBIN > GOPATH > Default go install location
+	execPath, err := os.Executable() // path to gonomics executable
+	exception.PanicOnErr(err)
+	execPath, err = filepath.EvalSymlinks(execPath) // follow any symlinks to get true executable location
+
+	// options to choose from
+	execPath = strings.TrimSuffix(execPath, ".test") // avoid errors when testing function
+	execPath = strings.TrimSuffix(execPath, "/gonomics")
+	gobin := os.Getenv("GOBIN")
+	gopath := os.Getenv("GOPATH") + "/bin"     // default to $GOPATH/bin
+	godefault := os.Getenv("HOME") + "/go/bin" // default install directory
+
+	switch {
+	case tryPathFile(execPath):
+	case gobin != "" && tryPathFile(gobin):
+	case gopath != "" && tryPathFile(gopath):
+	case godefault != "" && tryPathFile(godefault):
+	default:
+		return // exit test if binary not found
+	}
+	buildCmdCache("") // tests everything is formatted correctly and updates cache file
 }
