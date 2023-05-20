a44bbfd534afa7d28bdd314dd3c15b2380297272
Eric Au <ericdotau@gmail.com>
2023-05-19 23:23:27 -0700

    fix: small opt changes

--- fileio/byteio.go
+++ fileio/byteio.go
@@ -3,7 +3,6 @@ package fileio
 import (
 	"bufio"
 	"bytes"
-	"fmt"
 	"io"
 	"log"
 	"os/exec"
@@ -18,14 +17,9 @@ const (
 )
 
 // ByteReader implements the io.Reader interface by providing the Read(b []byte) method.
-// - The primary advantage of ByteReader is data read into a shared bytes.Buffer rather than allocating memory.
-// - The drawback is that ByteReader is not as easy to use as EasyReader nd should be reserved for performance intensive tasks.
-// The struct contains:
-// - *bufio.Reader, embedded
-// - GunzipReader which implements io.ReadCloser, a builtin io.Reader and io.Closer interface.
-// - []byte
-// - *bytes.Buffer
-// - close function from os.File
+// It wraps a *bufio.Reader and optionally a GunzipReader for reading gzipped files.
+// The primary advantage of ByteReader is that it allows data to be read into a shared bytes.Buffer
+// instead of allocating individual strings.
 type ByteReader struct {
 	*bufio.Reader
 	internalGzip *GunzipReader
@@ -34,33 +28,33 @@ type ByteReader struct {
 	close        func() error
 }
 
-// GunzipReader is implements the io.ReadCloser interface, which is a golang buildin interface that groups the basic Read and Close methods.
+// GunzipReader implements the io.ReadCloser interface and is used to read gzipped files.
+// It wraps an io.ReadCloser and a command for executing the gunzip command.
 type GunzipReader struct {
 	io.ReadCloser
 	Cmd *exec.Cmd
 }
 
-// Read reads data into p and is a method required by ByteReader implement the io.Reader interface.
+// Read reads data into p and implements the io.Reader interface for ByteReader.
 func (reader *ByteReader) Read(b []byte) (n int, err error) {
-	return reader.Read(b)
+	return reader.Reader.Read(b)
 }
 
-// Read reads data into p and is a method required by GunzipReader implement the io.Reader interface.
-func (reader GunzipReader) Read(b []byte) (n int, err error) {
+// Read reads data into p and implements the io.Reader interface for GunzipReader.
+func (reader *GunzipReader) Read(b []byte) (n int, err error) {
 	return reader.ReadCloser.Read(b)
 }
 
-// NewByteReader will process a given file and performs error handling if an error occurs and process gzipped files accordingly by suffix of the provided file.
+// NewByteReader creates a new ByteReader for the given filename.
+// It automatically detects if the file is gzipped and handles it accordingly.
 func NewByteReader(filename string) *ByteReader {
-	var answer ByteReader = ByteReader{
-		Buffer: &bytes.Buffer{},
-		line:   make([]byte, defaultBufSize),
-	}
-	switch true {
+	var answer ByteReader
+	answer.Buffer = &bytes.Buffer{}
+	switch {
 	case strings.HasSuffix(filename, ".gz"):
 		answer.internalGzip = NewGunzipReader(filename)
 		answer.Reader = bufio.NewReader(answer.internalGzip)
-		answer.close = answer.internalGzip.ReadCloser.Close
+		answer.close = answer.internalGzip.Close
 	default:
 		file := MustOpen(filename)
 		answer.Reader = bufio.NewReader(file)
@@ -69,11 +63,11 @@ func NewByteReader(filename string) *ByteReader {
 	return &answer
 }
 
-// NewGunzipReader takes a file name and returns an implementation of io.Reader.
+// NewGunzipReader creates a new GunzipReader for the given filename.
+// It executes the gunzip command to read the gzipped file.
 func NewGunzipReader(filename string) *GunzipReader {
-	var answer GunzipReader = GunzipReader{
-		Cmd: exec.Command("gunzip", "-c", filename),
-	}
+	var answer GunzipReader
+	answer.Cmd = exec.Command("gunzip", "-c", filename)
 	stdout, err := answer.Cmd.StdoutPipe()
 	exception.PanicOnErr(err)
 	answer.ReadCloser = stdout
@@ -81,10 +75,9 @@ func NewGunzipReader(filename string) *GunzipReader {
 	return &answer
 }
 
-// ReadLine will return a bytes.Buffer pointing to the internal slice of bytes.
-// Important to note:
-// - Provided this function is called within a loop, the function will read one line at a time, and return bool to continue reading.
-// - the buffer return points to the internal slice belonging to the reader, meaning the slice will be overridden if the data is not copied.
+// ReadLine reads one line from the ByteReader and returns it as a bytes.Buffer.
+// It also returns a bool indicating whether there are more lines to read.
+// The returned buffer points to the internal slice belonging to the reader, so it should be copied if needed.
 func ReadLine(reader *ByteReader) (*bytes.Buffer, bool) {
 	var err error
 	reader.line, err = reader.ReadSlice('\n')
@@ -100,13 +93,14 @@ func ReadLine(reader *ByteReader) (*bytes.Buffer, bool) {
 			reader.line = recursiveRead(reader)
 			return bytesToBuffer(reader), false
 		} else {
-			CatchErrThrowEOF(err)
+			catchErrThrowEOF(err)
 		}
 	}
 	return nil, true
 }
 
-// recursiveRead is a private helper function to deal with very long lines to in an effort to avoid alocating too much memory upfront and only resize the size of the buffer when necessary.
+// recursiveRead is a helper function to deal with very long lines and avoid allocating too much memory upfront.
+// It recursively reads the next bytes until reaching the end of line character.
 func recursiveRead(reader *ByteReader) []byte {
 	_, err := reader.Buffer.Write(reader.line)
 	exception.PanicOnErr(err)
@@ -117,15 +111,14 @@ func recursiveRead(reader *ByteReader) []byte {
 	if err == bufio.ErrBufferFull {
 		_, err = reader.Buffer.Write(reader.line)
 		exception.PanicOnErr(err)
-		// recursive call to read next bytes until reaching end of line character
 		return recursiveRead(reader)
 	}
 	exception.PanicOnErr(err)
 	return reader.line
 }
 
-// CatchErrThrowEOF will silently handles and throws the EOF error and will log and exit any other errors.
-func CatchErrThrowEOF(err error) {
+// catchErrThrowEOF silently handles and throws the EOF error, and logs and exits on any other error.
+func catchErrThrowEOF(err error) {
 	if err == io.EOF {
 		return
 	} else {
@@ -133,7 +126,7 @@ func CatchErrThrowEOF(err error) {
 	}
 }
 
-// bytesToBuffer will parse []byte and return a pointer to the same underlying bytes.Buffer.
+// bytesToBuffer converts the internal byte slice of the ByteReader to a bytes.Buffer and returns it.
 func bytesToBuffer(reader *ByteReader) *bytes.Buffer {
 	var err error
 	if reader.line[len(reader.line)-2] == '\r' {
@@ -145,7 +138,7 @@ func bytesToBuffer(reader *ByteReader) *bytes.Buffer {
 	return reader.Buffer
 }
 
-// Close is called by ByteReader to closes files and render unusable for I/O. On files that support SetDeadline, any pending I/O operations will be canceled and return immediately with an error.
+// Close closes the ByteReader, rendering it unusable for I/O.
 func (br *ByteReader) Close() error {
 	if br.internalGzip != nil {
 		exception.PanicOnErr(br.internalGzip.Close())
@@ -155,42 +148,43 @@ func (br *ByteReader) Close() error {
 	return nil
 }
 
-// Close is called by GunzipReader to closes files and render unusable for I/O. On files that support SetDeadline, any pending I/O operations will be canceled and return immediately with an error.
+// Close closes the GunzipReader, rendering it unusable for I/O.
 func (unzip *GunzipReader) Close() error {
 	exception.PanicOnErr(unzip.ReadCloser.Close())
 	exception.PanicOnErr(unzip.Cmd.Wait())
 	return nil
 }
 
-// StringToIntSlice will process a row of data separated by commas, convert the slice into a slice of type int. PSL and genePred formats have a trailing comma we need to account for and the check at the beginning will adjust the length of the working slice.
+// StringToIntSlice converts a comma-separated string to a slice of integers.
+// It handles formats like PSL and genePred that may have a trailing comma.
 func StringToIntSlice(line string) []int {
 	work := strings.Split(line, ",")
-	var sliceSize int = len(work)
+	sliceSize := len(work)
 	if line[len(line)-1] == ',' {
 		sliceSize--
 	}
-	var answer []int = make([]int, sliceSize)
-	var err error
+	answer := make([]int, sliceSize)
 	for i := 0; i < sliceSize; i++ {
-		answer[i], err = strconv.Atoi(work[i])
+		num, err := strconv.Atoi(work[i])
 		exception.PanicOnErr(err)
+		answer[i] = num
 	}
 	return answer
 }
 
-// IntListToString will process a slice of type int as an input and return a each value separated by a comma as a string.
-// - Important Note: string will include a trailing comma to satisfy UCSC's anomalies.
+// IntSliceToString converts a slice of integers to a comma-separated string.
+// The string will include a trailing comma to satisfy UCSC's anomalies.
 func IntSliceToString(nums []int) string {
 	ans := strings.Builder{}
 	ans.Grow(2 * len(nums))
 	for i := 0; i < len(nums); i++ {
-		ans.WriteString(fmt.Sprintf("%d", nums[i]))
+		ans.WriteString(strconv.Itoa(nums[i]))
 		ans.WriteByte(',')
 	}
 	return ans.String()
 }
 
-// IntToString a function that converts a number of type int and return a string.
+// IntToString converts an integer to a string.
 func IntToString(i int) string {
-	return fmt.Sprintf("%d", i)
+	return strconv.Itoa(i)
 }
