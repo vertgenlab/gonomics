2066a3e8c9153abc6719c508b1ace12cae3456c2
Christi Fauci <christiana.fauci@duke.edu>
2023-06-12 13:27:54 -0400

    altered gene input settings. Now allows for user to input a bed instead of a gtf assuming that bed is made up of single point genes with names as the gtf would be after processing. This is based on a bool argument option, SinglePointGeneBed.

--- cmd/great3d/great3d.go
+++ cmd/great3d/great3d.go
@@ -11,17 +11,23 @@ import (
 )
 
 type Settings struct {
-	GtfFile        string
-	ContactFile    string
-	SizesFile      string
-	NearestGeneBed string
+	GeneFile           string
+	ContactFile        string
+	SizesFile          string
+	NearestGeneBed     string
+	SinglePointGeneBed bool
 }
 
 func great3d(s Settings) {
-	genes := gtf.Read(s.GtfFile)
+	var tss []bed.Bed
 	contacts := bedpe.Read(s.ContactFile)
 	sizes := chromInfo.ReadToMap(s.SizesFile)
-	tss := gtf.GenesToTssBed(genes, sizes)
+	if s.SinglePointGeneBed {
+		tss = bed.Read(s.GeneFile)
+	} else {
+		genes := gtf.Read(s.GeneFile)
+		tss = gtf.GenesToTssBed(genes, sizes)
+	}
 	nearestGenes := bedpe.Fill3dSpace(contacts, tss, sizes)
 	if s.NearestGeneBed != "" {
 		bed.Write(s.NearestGeneBed, nearestGenes)
@@ -41,6 +47,7 @@ func usage() {
 func main() {
 	var expectedNumArgs = 4
 	var nearestGeneBed *string = flag.String("nearestGeneBed", "", "Write a bed representing the nearest genes in 3d space to every position in the genome.")
+	var geneBed *bool = flag.Bool("geneBed", false, "User has provided a bed in place of a gtf for te first argument that contains information about a gene TSS already spanning a single bp distance, and therefore GTF processing can be skipped.")
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flag.Parse()
@@ -49,16 +56,16 @@ func main() {
 		flag.Usage()
 		log.Fatalf("Error: expecting %d arguments, but got %d\n", expectedNumArgs, len(flag.Args()))
 	}
-	gtfFile := flag.Arg(0)
+	geneFile := flag.Arg(0)
 	contactFile := flag.Arg(1)
 	sizesFile := flag.Arg(2)
-	//TODO other args
 
 	s := Settings{
-		GtfFile:        gtfFile,
-		ContactFile:    contactFile,
-		SizesFile:      sizesFile,
-		NearestGeneBed: *nearestGeneBed,
+		GeneFile:           geneFile,
+		ContactFile:        contactFile,
+		SizesFile:          sizesFile,
+		NearestGeneBed:     *nearestGeneBed,
+		SinglePointGeneBed: *geneBed,
 	}
 
 	great3d(s)
