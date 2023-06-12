7f4afef959f7a7306dbe56bbd70352e96318d9a2
Christi Fauci <christiana.fauci@duke.edu>
2023-06-12 13:48:27 -0400

    added functionality to output 1d closest gene file

--- cmd/great3d/great3d.go
+++ cmd/great3d/great3d.go
@@ -16,6 +16,7 @@ type Settings struct {
 	SizesFile          string
 	NearestGeneBed     string
 	SinglePointGeneBed bool
+	Output1d           string
 }
 
 func great3d(s Settings) {
@@ -27,6 +28,10 @@ func great3d(s Settings) {
 	} else {
 		genes := gtf.Read(s.GeneFile)
 		tss = gtf.GenesToTssBed(genes, sizes)
+	}
+	if s.Output1d != "" {
+		proximityFile := bed.FillSpaceNoHiddenValue(tss, sizes)
+		bed.Write(s.Output1d, proximityFile)
 	}
 	nearestGenes := bedpe.Fill3dSpace(contacts, tss, sizes)
 	if s.NearestGeneBed != "" {
@@ -48,6 +53,7 @@ func main() {
 	var expectedNumArgs = 4
 	var nearestGeneBed *string = flag.String("nearestGeneBed", "", "Write a bed representing the nearest genes in 3d space to every position in the genome.")
 	var geneBed *bool = flag.Bool("geneBed", false, "User has provided a bed in place of a gtf for te first argument that contains information about a gene TSS already spanning a single bp distance, and therefore GTF processing can be skipped.")
+	var proximityFile *string = flag.String("proximity", "", "If given a file name the program will output a file that contains the closest gene to a bp based only on proximity as well as any other file requested.")
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flag.Parse()
@@ -66,6 +72,7 @@ func main() {
 		SizesFile:          sizesFile,
 		NearestGeneBed:     *nearestGeneBed,
 		SinglePointGeneBed: *geneBed,
+		Output1d:           *proximityFile,
 	}
 
 	great3d(s)
