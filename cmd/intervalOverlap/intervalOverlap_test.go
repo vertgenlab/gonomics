6ab96c1012abffc9ec10d97c9fb356bfe427c715
Daniel <daniel.snellings@duke.edu>
2023-07-05 10:09:41 -0400

    fix

--- cmd/intervalOverlap/intervalOverlap_test.go
+++ cmd/intervalOverlap/intervalOverlap_test.go
@@ -19,14 +19,14 @@ func TestSequelOverlap(t *testing.T) {
 		BaseOverlap:     0,
 		Aggregate:       false,
 		Relationship:    "any",
-		MergedOutput:    false,
+		MergedOutput:    true,
 		SwapTargetQuery: false,
 	}
 
 	answer := sequelOverlap(options)
 
 	for val := range answer {
-		b := val.answer[0].(*bed.Bed)
+		b := val.answer[0].(bed.Bed)
 		if b.Chrom != "chr1" || b.ChromStart != 100 || b.ChromEnd != 200 {
 			t.Errorf("ERROR: Problem with intervalOverlap cmd")
 		}
