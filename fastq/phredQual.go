97fda0075c68531fb5e24ade08078bd0c972ab7f
Craig Lowe <craiglowe@users.noreply.github.com>
2023-07-05 23:48:16 -0400

    fastq done

--- fastq/phredQual.go
+++ fastq/phredQual.go
@@ -11,6 +11,7 @@ import (
 // without adding 33 you get values like spaces and newlines.
 const asciiOffset uint8 = 33
 
+// ToQualUint8 converts a slice of runes to uint8 and substracts a 33 offset to the values.
 func ToQualUint8(qual []rune) []uint8 {
 	var answer []uint8 = make([]uint8, len(qual))
 	for i := 0; i < len(qual); i++ {
@@ -28,7 +29,7 @@ func ToQual(qual []byte) []uint8 {
 	return answer
 }
 
-// qualScore.
+// ReverseQualUint8Record reverses the elements of the slice
 func ReverseQualUint8Record(qualScore []uint8) {
 	var i, off int
 	for i = len(qualScore)/2 - 1; i >= 0; i-- {
@@ -49,12 +50,14 @@ func QualString(qual []uint8) string {
 	return str.String()
 }
 
+// PhredToPError converts a base quality in uint8 format to an error rate
 func PhredToPError(baseQual uint8) float32 {
 	q := float64(baseQual)
 	p := math.Pow(10, -q/10)
 	return float32(p)
 }
 
+// ErrorRate converts a slice of base qualities in uint8 format to an error rate
 func ErrorRate(baseQual []uint8) []float32 {
 	var answer []float32 = make([]float32, len(baseQual))
 	for i := 0; i < len(baseQual); i++ {
