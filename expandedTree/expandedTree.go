5143b7663f9475dc22fa093de1c67fc318b8d159
Christi Fauci <christiana.fauci@duke.edu>
2023-07-11 12:08:44 -0400

    expandedTree documentation

--- expandedTree/expandedTree.go
+++ expandedTree/expandedTree.go
@@ -1,3 +1,4 @@
+// Package expandedTree contains structs and functions that build and utilize information in a binary tree format that can be used for genome simulationa nd reconstruction
 package expandedTree
 
 import (
