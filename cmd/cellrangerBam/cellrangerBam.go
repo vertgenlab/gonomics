97a91a4d88bc0576b0d78cd9f76558d69fd78d6d
sweavs111 <seth.weaver@duke.edu>
2023-07-28 13:56:37 -0400

    push

--- cmd/cellrangerBam/cellrangerBam.go
+++ cmd/cellrangerBam/cellrangerBam.go
@@ -7,28 +7,25 @@ import (
 )
 
 func main() {
-	var j int = 0
 	var k int = 0
 	var bit int32
 	var umis []string
 
-	ch, _ := sam.GoReadToChan("/Users/sethweaver/Downloads/2dayBrainIueOut/lowe/possorted_genome_bam.chrSS.bam")
+	//ch, _ := sam.GoReadToChan("/Users/sethweaver/Downloads/2dayBrainIueOut/lowe/possorted_genome_bam.chrSS.bam") //lowe library analysis
+	//ch, _ := sam.GoReadToChan("/Users/sethweaver/Downloads/2dayBrainIueOut/silver/silver_possorted_genome_bam.chrSS.bam") //silver library analysis
+	ch, _ := sam.GoReadToChan("/Users/sethweaver/Downloads/2dayBrainIueOut/haqer/haqer_possorted_genome_bam.chrSS.bam") //haqer reanalysis
 
 	for i := range ch {
-		if j >= 10 {
-			break
-		}
-		num, _, _ := sam.QueryTag(i, "xf")
+		num, _, _ := sam.QueryTag(i, "xf") //xf: extra flags
 		bit = num.(int32)
 
-		if bit&8 == 8 {
-			construct, _, _ := sam.QueryTag(i, "GX")
+		if bit&8 == 8 { // bit 8 is the flag for UMI used in final count.
+			construct, _, _ := sam.QueryTag(i, "GX") // gene associated with read
 			name := construct.(string)
 			umis = append(umis, name)
 			k++
 		}
-		//j++
 	}
 	fmt.Println(k)
-	fileio.Write("/Users/sethweaver/Downloads/2dayBrainIueOut/lowe/cellrangerUsedUMIs.txt", umis)
+	fileio.Write("/Users/sethweaver/Downloads/2dayBrainIueOut/haqer/cellrangerUsedUMIs.txt", umis)
 }
