55fdaff6c1497619d7fa199b0e479f704327c1a7
rimangan <riley.mangan@duke.edu>
2023-07-29 12:09:31 -0400

    deprecatePrint

--- cmd/DEPRECATED/deprecated.go
+++ cmd/DEPRECATED/deprecated.go
@@ -1,7 +1,10 @@
 package main
 
-/*
-This directory contains deprecated commands, preserved for backwards compatability.
-Each deprecated command should point to an intended replacement program, and should
-describe how the usage of a deprecated program can be transferred to usage in the new program.
-*/
+import "fmt"
+
+func main() {
+	fmt.Print(
+		"This directory contains deprecated commands, preserved for backwards compatability.\n" +
+			"Each deprecated command should point to an intended replacement program, and should\n" +
+			"describe how the usage of a deprecated program can be transferred to usage in the new program.\n")
+}
