599c69b3d7dc4365dcfedfb4d7df1d6f748b323e
sweavs111 <seth.weaver@duke.edu>
2023-07-30 23:21:20 -0400

    cellrangerBam command

--- cmd/cellrangerBam/cellrangerBam.go
+++ cmd/cellrangerBam/cellrangerBam.go
@@ -1,31 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/sam"
+	"log"
 )
 
-func main() {
+func parseBam(inSam string, outTable string) {
 	var k int = 0
 	var bit int32
-	var umis []string
+	var constructName, cellString string
+	var output []string
 
-	//ch, _ := sam.GoReadToChan("/Users/sethweaver/Downloads/2dayBrainIueOut/lowe/possorted_genome_bam.chrSS.bam") //lowe library analysis
-	//ch, _ := sam.GoReadToChan("/Users/sethweaver/Downloads/2dayBrainIueOut/silver/silver_possorted_genome_bam.chrSS.bam") //silver library analysis
-	ch, _ := sam.GoReadToChan("/Users/sethweaver/Downloads/2dayBrainIueOut/haqer/haqer_possorted_genome_bam.chrSS.bam") //haqer reanalysis
+	ch, _ := sam.GoReadToChan(inSam)
+	out := fileio.EasyCreate(outTable)
 
 	for i := range ch {
 		num, _, _ := sam.QueryTag(i, "xf") //xf: extra flags
 		bit = num.(int32)
 
 		if bit&8 == 8 { // bit 8 is the flag for UMI used in final count.
-			construct, _, _ := sam.QueryTag(i, "GX") // gene associated with read
-			name := construct.(string)
-			umis = append(umis, name)
+			construct, _, _ := sam.QueryTag(i, "GX") // gene associated with UMI
+			constructName = construct.(string)
+			cell, _, _ := sam.QueryTag(i, "CB") // cell associated with UMI
+			cellString = cell.(string)
+			write := fmt.Sprintf("%s\t%s", cellString, constructName)
+			fileio.WriteToFileHandle(out, write)
 			k++
 		}
 	}
-	fmt.Println(k)
-	fileio.Write("/Users/sethweaver/Downloads/2dayBrainIueOut/haqer/cellrangerUsedUMIs.txt", umis)
+	fmt.Println("Found this many valid UMIs: ", k)
+	fileio.Write(outTable, output)
+}
+
+func usage() {
+	fmt.Print("cellrangerBam -- Takes in a cellranger bam file of STARR-seq reads and parses the extra flag field to pull out the" +
+		"representitive read for each UMI and which construct it belongs to. It will also report the cell which it was found in.\n" +
+		"Usage: \n" +
+		"cellrangerBam inFile outFile\n\n")
+	flag.PrintDefaults()
+}
+func main() {
+	var expectedNumArgs int = 2
+
+	flag.Usage = usage
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	flag.Parse()
+
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Error: expecting %d arguments, but got %d\n",
+			expectedNumArgs, len(flag.Args()))
+	}
+
+	a := flag.Arg(0)
+	b := flag.Arg(1)
+	parseBam(a, b)
 }
