0e92d75ff3a7af05204bc42161f417648a36c2ec
RavenYL <yanting.raven.luo@gmail.com>
2023-08-30 11:24:42 -0400

    added posRefName option

--- cmd/faFindFast/faFindFast.go
+++ cmd/faFindFast/faFindFast.go
@@ -15,21 +15,29 @@ import (
 	"github.com/vertgenlab/gonomics/fileio"
 )
 
-func faFindFast(inFile string, outFile string, referenceName string, queryName string, windowSize int, refChromName string, removeN bool, longOutput bool, divergenceRate float64) {
+func faFindFast(inFile string, outFile string, referenceName string, queryName string, posRefName string, windowSize int, posRefChromName string, removeN bool, longOutput bool, divergenceRate float64) {
 	records := fasta.Read(inFile)
 
-	var reference, query []dna.Base
+	var reference, query, posRef []dna.Base
 	referenceCount := 0
 	queryCount := 0
+	posRefCount := 0
 
 	//if reference and query names were not specified in the command, then use 1st and 2nd fasta records' names, respectively
-	//will still proceed to check for non=unique names
 	if referenceName == "" {
 		referenceName = records[0].Name
 	}
 	if queryName == "" {
 		queryName = records[1].Name
 	}
+
+	//if position reference name was not specified in the command, then use reference fasta record's name
+	//TODO: add 1 more test where reference and posRef have different sequences (not just different names)
+	if posRefName == "" {
+		posRefName = referenceName
+	}
+
+	//proceed to check for non-unique names and get any specified fasta record names
 
 	for i := 0; i < len(records); i++ { //for each fasta record, check if name matches reference or query
 		if records[i].Name == referenceName { //if name matches reference, extract reference sequence
@@ -45,14 +53,21 @@ func faFindFast(inFile string, outFile string, referenceName string, queryName s
 			query = records[i].Seq
 			queryCount += 1
 		}
+		if records[i].Name == posRefName { //separate check for posRefName. Else if will not be able to obtain posRef if records[i].Name == referenceName == posRefName
+			if posRefCount == 1 {
+				log.Fatalf("Position reference sequence name is not unique in the input file")
+			}
+			posRef = records[i].Seq
+			posRefCount += 1
+		}
 	}
 
-	if len(reference) != len(query) {
+	if !(len(reference) == len(query) && len(reference) == len(posRef)) {
 		log.Fatalf("Reference and query sequences are not of equal length")
 	}
 
 	file := fileio.EasyCreate(outFile)
-	speedyWindowDifference(windowSize, reference, query, refChromName, removeN, longOutput, divergenceRate, file)
+	speedyWindowDifference(windowSize, reference, query, posRefChromName, removeN, longOutput, divergenceRate, file) //TODO: change variables here
 	err := file.Close()
 	exception.PanicOnErr(err)
 }
@@ -70,8 +85,9 @@ func main() {
 	var expectedNumArgs int = 2
 	var referenceName *string = flag.String("referenceName", "", "Specify the name of the reference sequence")
 	var queryName *string = flag.String("queryName", "", "Specify the name of the query sequence")
+	var posRefName *string = flag.String("posRefName", "", "Specify the name of the position reference sequence. Set to reference sequence by default")
 	var windowSize *int = flag.Int("windowSize", 1000, "Specify the window size")
-	var refChromName *string = flag.String("chrom", "", "Specify a chrom name of the reference sequence")
+	var posRefChromName *string = flag.String("chrom", "", "Specify a chrom name of the position reference sequence")
 	var removeN *bool = flag.Bool("removeN", false, "Excludes bed regions with Ns in the reference from the output.")
 	var longOutput *bool = flag.Bool("longOutput", false, "Print percent diverged and raw -Log10PValue in output. Requires the 'divergenceRate' argument.")
 	var divergenceRate *float64 = flag.Float64("divergenceRate", math.MaxFloat64, "Set the null divergence rate for p value calculations with 'longOutput'.")
@@ -99,5 +115,5 @@ func main() {
 	inFile := flag.Arg(0)
 	outFile := flag.Arg(1)
 
-	faFindFast(inFile, outFile, *referenceName, *queryName, *windowSize, *refChromName, *removeN, *longOutput, *divergenceRate)
+	faFindFast(inFile, outFile, *referenceName, *queryName, *posRefName, *windowSize, *posRefChromName, *removeN, *longOutput, *divergenceRate)
 }
--- cmd/faFindFast/faFindFast_test.go
+++ cmd/faFindFast/faFindFast_test.go
@@ -9,83 +9,90 @@ import (
 )
 
 var FaFindFastTests = []struct {
-	InFile         string
-	OutFile        string
-	ExpectedFile   string
-	ReferenceName  string
-	QueryName      string
-	WindowSize     int
-	ChromName      string
-	RemoveN        bool
-	DivergenceRate float64
-	LongOutput     bool
+	InFile          string
+	OutFile         string
+	ExpectedFile    string
+	ReferenceName   string
+	QueryName       string
+	PosRefName      string
+	WindowSize      int
+	posRefChromName string
+	RemoveN         bool
+	DivergenceRate  float64
+	LongOutput      bool
 }{
 	{InFile: "testdata/test_indel.fa",
-		OutFile:        "testdata/tmp.out.bed",
-		ExpectedFile:   "testdata/expected.bed",
-		ReferenceName:  "Human", //also test for extra species here
-		QueryName:      "Chimp",
-		WindowSize:     10,
-		ChromName:      "chr1",
-		RemoveN:        false,
-		DivergenceRate: -1,
-		LongOutput:     false},
+		OutFile:         "testdata/tmp.out.bed",
+		ExpectedFile:    "testdata/expected.bed",
+		ReferenceName:   "Human", //also test for extra species here
+		QueryName:       "Chimp",
+		PosRefName:      "Human",
+		WindowSize:      10,
+		posRefChromName: "chr1",
+		RemoveN:         false,
+		DivergenceRate:  -1,
+		LongOutput:      false},
 	{InFile: "testdata/test_indel.fa",
-		OutFile:        "testdata/tmp.noN.bed",
-		ExpectedFile:   "testdata/expected.noN.bed",
-		ReferenceName:  "", //also test for ReferenceName and QueryName defaults here
-		QueryName:      "",
-		WindowSize:     10,
-		ChromName:      "chr1",
-		RemoveN:        true,
-		DivergenceRate: -1,
-		LongOutput:     false},
+		OutFile:         "testdata/tmp.noN.bed",
+		ExpectedFile:    "testdata/expected.noN.bed",
+		ReferenceName:   "", //also test for ReferenceName, QueryName and PosRefName defaults here
+		QueryName:       "",
+		PosRefName:      "",
+		WindowSize:      10,
+		posRefChromName: "chr1",
+		RemoveN:         true,
+		DivergenceRate:  -1,
+		LongOutput:      false},
 	{InFile: "testdata/test.endDoubleGaps.fa",
-		OutFile:        "testdata/tmp.doubleGaps.bed",
-		ExpectedFile:   "testdata/expected.bed",
-		ReferenceName:  "Human",
-		QueryName:      "Gorilla", //also test for a different QueryName here
-		WindowSize:     10,
-		ChromName:      "chr1",
-		RemoveN:        false,
-		DivergenceRate: -1,
-		LongOutput:     false},
+		OutFile:         "testdata/tmp.doubleGaps.bed",
+		ExpectedFile:    "testdata/expected.bed",
+		ReferenceName:   "Human",
+		QueryName:       "Gorilla", //also test for a different QueryName here
+		PosRefName:      "",
+		WindowSize:      10,
+		posRefChromName: "chr1",
+		RemoveN:         false,
+		DivergenceRate:  -1,
+		LongOutput:      false},
 	{InFile: "testdata/test.endGapsQuery.fa",
-		OutFile:        "testdata/tmp.queryGaps.bed",
-		ExpectedFile:   "testdata/expected.endGapsQuery.bed",
-		ReferenceName:  "Human",
-		QueryName:      "Chimp",
-		WindowSize:     10,
-		ChromName:      "chr1",
-		RemoveN:        false,
-		DivergenceRate: -1,
-		LongOutput:     false},
+		OutFile:         "testdata/tmp.queryGaps.bed",
+		ExpectedFile:    "testdata/expected.endGapsQuery.bed",
+		ReferenceName:   "Human",
+		QueryName:       "Chimp",
+		PosRefName:      "",
+		WindowSize:      10,
+		posRefChromName: "chr1",
+		RemoveN:         false,
+		DivergenceRate:  -1,
+		LongOutput:      false},
 	{InFile: "testdata/test.endGapsRef.fa",
-		OutFile:        "testdata/tmp.refGaps.bed",
-		ExpectedFile:   "testdata/expected.endGapsRef.bed",
-		ReferenceName:  "Human", //also test for finding a ReferenceName which is not the 1st sequence
-		QueryName:      "Chimp",
-		WindowSize:     10,
-		ChromName:      "chr1",
-		RemoveN:        false,
-		DivergenceRate: -1,
-		LongOutput:     false},
+		OutFile:         "testdata/tmp.refGaps.bed",
+		ExpectedFile:    "testdata/expected.endGapsRef.bed",
+		ReferenceName:   "Human", //also test for finding a ReferenceName that is not the 1st sequence
+		QueryName:       "Chimp",
+		PosRefName:      "HumanPosRef", //also test for finding a PosRefName that is not the same as ReferenceName
+		WindowSize:      10,
+		posRefChromName: "chr1",
+		RemoveN:         false,
+		DivergenceRate:  -1,
+		LongOutput:      false},
 	{InFile: "testdata/test.endGapsRef.fa",
-		OutFile:        "testdata/tmp.longOutput.bed",
-		ExpectedFile:   "testdata/expected.longOutput.bed",
-		ReferenceName:  "Human",
-		QueryName:      "Chimp",
-		WindowSize:     10,
-		ChromName:      "chr1",
-		RemoveN:        false,
-		DivergenceRate: 0.01,
-		LongOutput:     true},
+		OutFile:         "testdata/tmp.longOutput.bed",
+		ExpectedFile:    "testdata/expected.longOutput.bed",
+		ReferenceName:   "Human",
+		QueryName:       "Chimp",
+		PosRefName:      "",
+		WindowSize:      10,
+		posRefChromName: "chr1",
+		RemoveN:         false,
+		DivergenceRate:  0.01,
+		LongOutput:      true},
 }
 
 func TestFaFindFast(t *testing.T) {
 	var err error
 	for _, v := range FaFindFastTests {
-		faFindFast(v.InFile, v.OutFile, v.ReferenceName, v.QueryName, v.WindowSize, v.ChromName, v.RemoveN, v.LongOutput, v.DivergenceRate)
+		faFindFast(v.InFile, v.OutFile, v.ReferenceName, v.QueryName, v.PosRefName, v.WindowSize, v.posRefChromName, v.RemoveN, v.LongOutput, v.DivergenceRate)
 		if !fileio.AreEqual(v.OutFile, v.ExpectedFile) {
 			t.Errorf("Error in faFindFast. Output did not match expected.")
 		} else {
