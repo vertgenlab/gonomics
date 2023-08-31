0cb11c998fbf6b11eccfd07240ac7394a6c01631
RavenYL <yanting.raven.luo@gmail.com>
2023-08-30 20:36:03 -0400

    more notes in efficient.go, changed query to secondQuery

--- cmd/faFindFast/efficient.go
+++ cmd/faFindFast/efficient.go
@@ -18,27 +18,31 @@ import (
 // outputs:
 //  1. alnIdx: the next alignment position where a reference base exists, which is usually alignmentIndex+1, but can be greater due to gaps in the reference sequence
 //     this value will be equal to len(reference) if we were unable to find another reference base, and this would mean we have reached the end of the reference sequence
-//  2. gapOpenCloseFirstQuery: gaps opened in the firstQuery sequence (0 or 1). Only 1 variable is needed for firstQuery gapOpenClose, but 2 variables for secondQuery gapOpened and gapClosed // TODO: make into 2 variables?
-//  3. gapOpenedSecondQuery: gaps opened in the secondQuery sequence (0 or 1)
-//  4. gapClosedSecondQuery: gaps closed in the secondQuery sequence (0 or 1)
-//  5. numFirstQueryNs: the number of Ns in the firstQuery after this increment (0 or 1)
-//  6. numSecondQueryNsGap: the number of Ns in the secondQuery sequence after this increment from matches with firstQuery
-//  7. numSecondQueryNsMatch: the number of Ns in the secondQuery sequence after this increment from gaps in the firstQuery
-//  8. numSubst: the number of substitutions/mismatches (0 or 1) between firstQuery and secondQuery
-func incrementWindowEdge(reference []dna.Base, firstQuery []dna.Base, secondQuery []dna.Base, alnIdxOrig int) (alnIdx, gapOpenCloseFirstQuery, gapOpenedSecondQuery, gapClosedSecondQuery, numFirstQueryNs, numSecondQueryNsGap, numSecondQueryNsMatch, numSubst int) {
+//  2. gapOpenedFirstQuery: gaps opened in the firstQuery sequence (0 or 1)
+//  3. gapClosedFirstQuery: gaps closed in the firstQuery sequence (0 or 1)
+//  4. gapOpenedSecondQuery: gaps opened in the secondQuery sequence (0 or 1)
+//  5. gapClosedSecondQuery: gaps closed in the secondQuery sequence (0 or 1)
+//  6. numFirstQueryNs: the number of Ns in the firstQuery after this increment (0 or 1)
+//  7. numSecondQueryNsGap: the number of Ns in the secondQuery sequence after this increment from matches with firstQuery
+//  8. numSecondQueryNsMatch: the number of Ns in the secondQuery sequence after this increment from gaps in the firstQuery
+//  9. numSubst: the number of substitutions/mismatches (0 or 1) between firstQuery and secondQuery
+func incrementWindowEdge(reference []dna.Base, firstQuery []dna.Base, secondQuery []dna.Base, alnIdxOrig int) (alnIdx, gapOpenedFirstQuery, gapClosedFirstQuery, gapOpenedSecondQuery, gapClosedSecondQuery, numFirstQueryNs, numSecondQueryNsGap, numSecondQueryNsMatch, numSubst int) {
 	alnIdx = alnIdxOrig
 	// at initialization, lastAlnIdxOfWindow == -1, which is assigned to alnIdxOrig when incrementWindowEdge is called, so alnIdxOrig == -1, so alnIdx = -1. For loop will start at alnIdx++ which is 0, which is the first position in the alignment
 
 	// increment alnIdx to the next ref position (next non-gap position in the reference sequence)
 	// for loop start condition: start at alnIdx++, which is the position after the current alignment position
-	// for loop end condition: ends either when we reach the end of the reference sequence or when we reach a gap in the reference sequence
+	// for loop end condition: ends when we reach the end of the reference sequence, or when we reach a gap in the reference sequence, or when we reach a gap in the firstQuery sequence, whichever happens first
 	// for loop incrementation: alnIdx++
-	for alnIdx++; alnIdx < len(reference) && reference[alnIdx] == dna.Gap; alnIdx++ {
+	for alnIdx++; alnIdx < len(reference) && reference[alnIdx] == dna.Gap && firstQuery[alnIdx] == dna.Gap; alnIdx++ {
+		if firstQuery[alnIdx] == dna.N {
+			numFirstQueryNsGap++ //TODO: think more about this
+		}
 		if secondQuery[alnIdx] == dna.N {
 			numSecondQueryNsGap++
 		}
 		if secondQuery[alnIdx] != dna.Gap {
-			gapOpenCloseFirstQuery = 1 // TODO: when reference is the same as firstQuery, it defines the window, so only 1 variable open/close. Should this change?
+			gapOpenedFirstQuery = 1 //TODO: think more about this, OpenClose or 2 variables Opened and Closed?
 		}
 	}
 
@@ -72,14 +76,14 @@ func incrementWindowEdge(reference []dna.Base, firstQuery []dna.Base, secondQuer
 	return
 }
 
-// speedyWindowDifference is a helper function of faFindFast that calculates the divergence between two input sequences for every position using a sliding window.
+// speedyWindowDifference is a helper function of faFindFast that calculates the divergence between two input sequences (firstQuery, secondQuery sequences) for every reference position (in the reference sequence) using a sliding window.
 // optional arguments longOutput and divergenceRate allow the user to report a -log10pValue corresponding to the p value of observing a level of divergence for a given
 // window under a null binomial model of neutral evolution.
-func speedyWindowDifference(reference []dna.Base, firstQuery []dna.Base, secondQuery []dna.Base, s Settings) {
-	var alnIdxBeforeWindow, lastAlnIdxOfWindow int = -1, -1                                                   // these are the two edges of the sliding window in "alignment coordinates"
-	var refIdxBeforeWindow, lastRefIdxOfWindow int = -1, -1                                                   // these are the two edges of the sliding window in "reference (no gaps) coordinates"
-	var totalGaps, totalNs, totalSubst int                                                                    // this is the data we need to keep track of that describes the current window
-	var gapOpenCloseRef, gapOpenQuery, gapClosedQuery, numRefNs, numQueryNsGap, numQueryNsMatch, numSubst int // ints we will get back when moving the window one ref base. TODO: pos ref?
+func speedyWindowDifference(reference []dna.Base, firstQuery []dna.Base, secondQuery []dna.Base, noPrintIfN bool, s Settings) {
+	var alnIdxBeforeWindow, lastAlnIdxOfWindow int = -1, -1                                                                           // these are the two edges of the sliding window in "alignment positions"
+	var refIdxBeforeWindow, lastRefIdxOfWindow int = -1, -1                                                                           // these are the two edges of the sliding window in "reference (no gaps) positions"
+	var totalGaps, totalNs, totalSubst int                                                                                            // this is the data we need to keep track of that describes the current window
+	var gapOpenCloseRef, gapOpenSecondQuery, gapClosedSecondQuery, numRefNs, numSecondQueryNsGap, numSecondQueryNsMatch, numSubst int // ints we will get back when moving the window one ref base.
 	var err error
 	var percentDiverged, rawPValue float64
 
@@ -98,43 +102,43 @@ func speedyWindowDifference(reference []dna.Base, firstQuery []dna.Base, secondQ
 	for lastAlnIdxOfWindow < len(firstQuery) { // this check could also be "!done", I am not sure what is more clear
 		// we always move the lastBaseOfTheWindow (right side) and add on what we find to the counters because
 		// all this stuff is now inside the current window
-		lastAlnIdxOfWindow, gapOpenCloseRef, gapOpenQuery, _, numRefNs, numQueryNsGap, numQueryNsMatch, numSubst = incrementWindowEdge(reference, firstQuery, secondQuery, lastAlnIdxOfWindow)
+		lastAlnIdxOfWindow, gapOpenCloseRef, gapOpenSecondQuery, _, numRefNs, numSecondQueryNsGap, numSecondQueryNsMatch, numSubst = incrementWindowEdge(reference, firstQuery, secondQuery, lastAlnIdxOfWindow)
 		lastRefIdxOfWindow++
-		totalGaps += gapOpenCloseRef + gapOpenQuery
-		totalNs += numRefNs + numQueryNsGap + numQueryNsMatch
+		totalGaps += gapOpenCloseRef + gapOpenSecondQuery
+		totalNs += numRefNs + numSecondQueryNsGap + numSecondQueryNsMatch
 		totalSubst += numSubst
 
 		// usually increment the baseBeforeWindow, but not at the beginning when we have not yet incremented the end
 		// enough to have a full "windowSize" of bases in the window
-		if lastRefIdxOfWindow-refIdxBeforeWindow > windowSize {
-			alnIdxBeforeWindow, _, _, _, numRefNs, _, numQueryNsMatch, numSubst = incrementWindowEdge(reference, query, alnIdxBeforeWindow)
+		if lastRefIdxOfWindow-refIdxBeforeWindow > s.WindowSize {
+			alnIdxBeforeWindow, _, _, _, numRefNs, _, numSecondQueryNsMatch, numSubst = incrementWindowEdge(reference, firstQuery, secondQuery, alnIdxBeforeWindow)
 			refIdxBeforeWindow++
 			//totalGaps -= gapOpenCloseRef + gapClosedQuery
-			totalNs -= numRefNs + numQueryNsMatch
+			totalNs -= numRefNs + numSecondQueryNsMatch
 			totalSubst -= numSubst
 		}
 
 		// the trailing window needs to "look ahead" to see what happens before the next ref base
-		if lastRefIdxOfWindow-refIdxBeforeWindow == windowSize {
-			_, gapOpenCloseRef, _, gapClosedQuery, _, numQueryNsGap, _, _ = incrementWindowEdge(reference, query, alnIdxBeforeWindow)
-			totalGaps -= gapOpenCloseRef + gapClosedQuery
-			totalNs -= numQueryNsGap
+		if lastRefIdxOfWindow-refIdxBeforeWindow == s.WindowSize {
+			_, gapOpenCloseRef, _, gapClosedSecondQuery, _, numSecondQueryNsGap, _, _ = incrementWindowEdge(reference, firstQuery, secondQuery, alnIdxBeforeWindow)
+			totalGaps -= gapOpenCloseRef + gapClosedSecondQuery
+			totalNs -= numSecondQueryNsGap
 		}
 
 		// we check to make sure we are not at the very beginning or end, where we would have partial or illegal windows
-		if lastRefIdxOfWindow-refIdxBeforeWindow == windowSize && lastAlnIdxOfWindow < len(reference) {
+		if lastRefIdxOfWindow-refIdxBeforeWindow == s.WindowSize && lastAlnIdxOfWindow < len(reference) {
 			// an option/flag can tell us not to print if there are Ns in the query or target
 			if !noPrintIfN || totalNs == 0 {
-				if longOutput {
-					percentDiverged = 100 * (float64(totalSubst+totalGaps) / float64(windowSize))
-					if totalSubst+totalGaps > windowSize {
+				if s.LongOutput {
+					percentDiverged = 100 * (float64(totalSubst+totalGaps) / float64(s.WindowSize))
+					if totalSubst+totalGaps > s.WindowSize {
 						log.Fatalf("Error: total number of mutations exceeds windowSize. This may or may not be a bug, but your sequence has deviated from our use case.")
 					}
 					rawPValue = scorePValueCache[totalSubst+totalGaps]
-					_, err = fmt.Fprintf(out, "%s\t%d\t%d\t%s_%d\t%d\t%s\t%e\t%e\n", posRefChrName, refIdxBeforeWindow+1, lastRefIdxOfWindow+1, posRefChrName, refIdxBeforeWindow+1, totalSubst+totalGaps, "+", percentDiverged, rawPValue)
+					_, err = fmt.Fprintf(file, "%s\t%d\t%d\t%s_%d\t%d\t%s\t%e\t%e\n", s.RefChromName, refIdxBeforeWindow+1, lastRefIdxOfWindow+1, s.RefChromName, refIdxBeforeWindow+1, totalSubst+totalGaps, "+", percentDiverged, rawPValue)
 					exception.PanicOnErr(err)
 				} else {
-					_, err = fmt.Fprintf(out, "%s\t%d\t%d\t%s_%d\t%d\n", posRefChrName, refIdxBeforeWindow+1, lastRefIdxOfWindow+1, posRefChrName, refIdxBeforeWindow+1, totalSubst+totalGaps)
+					_, err = fmt.Fprintf(file, "%s\t%d\t%d\t%s_%d\t%d\n", s.RefChromName, refIdxBeforeWindow+1, lastRefIdxOfWindow+1, s.RefChromName, refIdxBeforeWindow+1, totalSubst+totalGaps)
 					exception.PanicOnErr(err)
 				}
 			}
