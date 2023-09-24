57882c17a963108ac472bdce2cb313cbb6624b01
sweavs111 <seth.weaver@duke.edu>
2023-09-24 15:01:15 -0400

    edits

--- cmd/haqerBedComps/haqerBedComps.go
+++ cmd/haqerBedComps/haqerBedComps.go
@@ -13,6 +13,8 @@ func main() {
 	fmt.Println("asymetric Jaccard: ", answer)
 	answer = kulczynski2(small, large)
 	fmt.Println("kulczynski2: ", answer)
+	answer = kulczynski2point0(small, large)
+	fmt.Println("kulczynski2.0: ", answer)
 }
 
 func asymetricJaccard(small []bed.Bed, big []bed.Bed, smallWindowSize int) float64 {
@@ -53,7 +55,7 @@ func kulczynski2(small []bed.Bed, large []bed.Bed) float64 {
 		if len(overlap) == 0 {
 			c++
 		} else {
-			a++
+			//a++
 		}
 	}
 
@@ -64,8 +66,38 @@ func kulczynski2(small []bed.Bed, large []bed.Bed) float64 {
 			b++
 		}
 	}
-	fmt.Printf("a: %d\nb: %d\nc: %d\n", a, b, c)
 	return 0.5 * ((float64(a) / float64(a+b)) + (float64(a) / float64(a+c)))
+}
+
+func kulczynski2point0(small []bed.Bed, large []bed.Bed) float64 {
+	var overlapSmall, overlapLarge, allLargeOverlaps, allSmallOverlaps []interval.Interval
+
+	smallIntervalMap := makeIntervalMap(small)
+	largeIntervalMap := makeIntervalMap(large)
+	for _, bd := range large {
+		overlapSmall = interval.Query(smallIntervalMap, bd, "any")
+		for _, i := range overlapSmall {
+			allSmallOverlaps = append(allSmallOverlaps, i)
+		}
+	}
+	for _, bd := range small {
+		overlapLarge = interval.Query(largeIntervalMap, bd, "any")
+		for _, i := range overlapLarge {
+			allLargeOverlaps = append(allLargeOverlaps, i)
+		}
+	}
+
+	allSmallOverlapsBed := sliceIntervalToBed(allSmallOverlaps)
+	allLargeOverlapsBed := sliceIntervalToBed(allLargeOverlaps)
+	bed.SortByCoord(allSmallOverlapsBed)
+	bed.SortByCoord(allLargeOverlapsBed)
+	uniqueSmallOverlapBeds := unique(allSmallOverlapsBed)
+	uniqueLargeOverlapBeds := unique(allLargeOverlapsBed)
+
+	smallOverlapPerc := float64(len(uniqueSmallOverlapBeds)) / float64(len(small))
+	bigOverlapPerc := float64(len(uniqueLargeOverlapBeds)) / float64(len(large))
+
+	return (smallOverlapPerc + bigOverlapPerc) / 2
 }
 
 func makeIntervalMap(inBed []bed.Bed) map[string]*interval.IntervalNode {
@@ -94,4 +126,27 @@ func overlapSize(a bed.Bed, b bed.Bed) int {
 		return a.ChromEnd - b.ChromStart
 	}
 	return 0
+}
+
+func sliceIntervalToBed(intervals []interval.Interval) []bed.Bed {
+	var bedSlice []bed.Bed
+	for _, i := range intervals {
+		bedSlice = append(bedSlice, i.(bed.Bed))
+	}
+	return bedSlice
+}
+
+func unique(regions []bed.Bed) []bed.Bed {
+	var uniqueRegions []bed.Bed
+
+	currRegion := regions[0]
+	for _, i := range regions {
+		if bed.Equal(currRegion, i) {
+			continue
+		}
+		uniqueRegions = append(uniqueRegions, currRegion)
+		currRegion = i
+	}
+	uniqueRegions = append(uniqueRegions, currRegion)
+	return uniqueRegions
 }
