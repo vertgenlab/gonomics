724394fc67ae06d8b25fe1bd3c2de16fa944537d
RavenYL <yanting.raven.luo@gmail.com>
2023-10-04 10:08:24 -0400

    address comments on interval/tools.go

--- interval/tools.go
+++ interval/tools.go
@@ -10,17 +10,18 @@ func CoordsToString(i Interval) string {
 	return fmt.Sprintf("%s:%v-%v", i.GetChrom(), i.GetChromStart(), i.GetChromEnd())
 }
 
+// IntervalSize calculates the size of the interval
 func IntervalSize(i Interval) int {
 	return i.GetChromEnd() - i.GetChromStart()
 }
 
+// OverlapSize calculates the size of the overlap betwen 2 intervals, assuming they are from the same genome
 func OverlapSize(a, b Interval) int {
 	if a.GetChrom() != b.GetChrom() {
 		return 0
 	}
 	end := numbers.Min(a.GetChromEnd(), b.GetChromEnd())
 	start := numbers.Max(a.GetChromStart(), b.GetChromStart())
-	// overlapSize := end - start
 	if end <= start {
 		return 0
 	} else {
