dc17ba09579d58c3553a363cf1901ec74eb6e559
edotau <ericdotau@gmail.com>
2023-10-04 23:44:21 -0700

    fix: less power

--- openai/config.go
+++ openai/config.go
@@ -4,6 +4,6 @@ const (
 	ApiEndpoint       = "https://api.openai.com/v1/chat/completions"
 	gptThreeFiveTurbo = "gpt-3.5-turbo"
 	gptFour           = "gpt-4"
-	maxThreeTokens    = 4097
-	maxFourTokens     = 8192
+	maxThreeTokens    = 2480
+	maxFourTokens     = 4960
 )
