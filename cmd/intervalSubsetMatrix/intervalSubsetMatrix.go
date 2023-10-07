582a75d3e42d558892da789d5cb6197f14323d71
RavenYL <yanting.raven.luo@gmail.com>
2023-10-06 20:10:36 -0400

    added -markMultipleOverlaps option in response to comment

--- cmd/intervalSubsetMatrix/intervalSubsetMatrix.go
+++ cmd/intervalSubsetMatrix/intervalSubsetMatrix.go
@@ -14,7 +14,7 @@ import (
 	"github.com/vertgenlab/gonomics/interval"
 )
 
-func intervalSubsetMatrix(unionFile string, fileListFile string, outFile string, fraction bool) {
+func intervalSubsetMatrix(unionFile string, fileListFile string, outFile string, fraction bool, markMultipleOverlaps string) {
 	var err error
 	var i int
 	var j interval.Interval
@@ -36,8 +36,16 @@ func intervalSubsetMatrix(unionFile string, fileListFile string, outFile string,
 	//this will be our output feature matrix. mat[row] => col, where rows are genomic regions, cols are files.
 	var mat = make(map[string][]float64)
 
+	// if in markMultipleOverlaps mode, create output feature matrix for additional multiple overlap output file
+	var matMultipleOverlaps = make(map[string][]float64)
+
 	for _, u := range unionIntervals {
 		mat[interval.CoordsToString(u)] = make([]float64, len(files)) //initialize all rows to zeros based on the number of files. Map is indexed by coords of regions.
+
+		if markMultipleOverlaps != "" {
+			matMultipleOverlaps[interval.CoordsToString(u)] = make([]float64, len(files))
+		}
+
 	}
 
 	for i = range files {
@@ -45,12 +53,23 @@ func intervalSubsetMatrix(unionFile string, fileListFile string, outFile string,
 		for j = range currIntervalChan {
 			currOverlaps = interval.Query(unionTree, j, "any") // each j (1 bed region from the file of the column) can overlap 0 or 1 or more bed region of the row
 			if len(currOverlaps) > 0 {
-				for _, k := range currOverlaps {
+				for _, k := range currOverlaps { // k >= 1 would mean j overlaps more than 1 bed region of the row
 					if fraction {
 						overlapSize = interval.OverlapSize(k, j)
 						mat[interval.CoordsToString(k)][i] += float64(overlapSize) / float64(interval.IntervalSize(k)) // add number rather than assign number, in case 1 bed region of the row overlaps multiple bed regions in the files of the columns
+
+						// if in markMultipleOverlaps mode, mark whenever there is an overlap hit
+						if markMultipleOverlaps != "" && overlapSize > 0 {
+							matMultipleOverlaps[interval.CoordsToString(k)][i] += 1
+						}
+
 					} else {
 						mat[interval.CoordsToString(k)][i] = 1
+
+						// if in markMultipleOverlaps mode, mark whenever there is an overlap hit
+						if markMultipleOverlaps != "" && overlapSize > 0 {
+							matMultipleOverlaps[interval.CoordsToString(k)][i] += 1
+						}
 					}
 				}
 			}
@@ -80,6 +99,23 @@ func intervalSubsetMatrix(unionFile string, fileListFile string, outFile string,
 
 	err = out.Close()
 	exception.PanicOnErr(err)
+
+	// if in markMultipleOverlaps mode, create additional multiple overlap output file
+	if markMultipleOverlaps != "" {
+		outMultipleOverlaps := fileio.EasyCreate(markMultipleOverlaps)
+		_, err = fmt.Fprintln(outMultipleOverlaps, headerString)
+		exception.PanicOnErr(err)
+		for x := range keys {
+			currLineString = keys[x]
+			for y := range mat[keys[x]] {
+				currLineString = fmt.Sprintf("%s\t%v", currLineString, matMultipleOverlaps[keys[x]][y])
+			}
+			_, err = fmt.Fprintln(outMultipleOverlaps, currLineString)
+			exception.PanicOnErr(err)
+		}
+		err = outMultipleOverlaps.Close()
+		exception.PanicOnErr(err)
+	}
 }
 
 func usage() {
@@ -96,6 +132,7 @@ func usage() {
 func main() {
 	var expectedNumArgs int = 3
 	var fraction *bool = flag.Bool("fraction", false, "fraction mode outputs a fraction matrix (each entry is a fraction, the number of bases of overlap between the bed region of the row and the file of the column divided by the size of the bed region of the row), unlike the default mode, which outputs a binary matrix (each entry is 1 if the bed region of the row overlaps any bed region in the file of the column, and 0 if otherwise")
+	var markMultipleOverlaps *string = flag.String("markMultipleOverlaps", "", "markMultipleOverlaps mode creates an additional output file that indicates the number of overlaps each interval in union.interval has. Use this option to provide the multiple overlap output file name")
 
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
@@ -110,5 +147,5 @@ func main() {
 	fileListFile := flag.Arg(1)
 	outFile := flag.Arg(2)
 
-	intervalSubsetMatrix(unionFile, fileListFile, outFile, *fraction)
+	intervalSubsetMatrix(unionFile, fileListFile, outFile, *fraction, *markMultipleOverlaps)
 }
--- cmd/intervalSubsetMatrix/intervalSubsetMatrix_test.go
+++ cmd/intervalSubsetMatrix/intervalSubsetMatrix_test.go
@@ -9,29 +9,40 @@ import (
 )
 
 var BedSubsetMatrixTests = []struct {
-	UnionFile    string
-	FileListFile string
-	Fraction     bool
-	OutFile      string
-	ExpectedFile string
+	UnionFile                    string
+	FileListFile                 string
+	Fraction                     bool
+	MarkMultipleOverlaps         string
+	OutFile                      string
+	ExpectedFile                 string
+	ExpectedMultipleOverlapsFile string
 }{
-	{"testdata/union.bed", "testdata/files.list", false, "testdata/out1.txt", "testdata/expected.txt"},
-	{"testdata/union.bed", "testdata/files.fraction1.list", true, "testdata/out2.txt", "testdata/expected.fraction1.txt"},
-	{"testdata/union.bed", "testdata/files.fraction2.list", true, "testdata/out3.txt", "testdata/expected.fraction2.txt"},
-	{"testdata/union.bed", "testdata/files.fraction3.list", false, "testdata/out4.txt", "testdata/expected.noFraction2.txt"},
-	{"testdata/union.bed", "testdata/files.fraction3.list", true, "testdata/out5.txt", "testdata/expected.fraction3.txt"},
+	{"testdata/union.bed", "testdata/files.list", false, "", "testdata/out1.txt", "testdata/expected.txt", ""},
+	{"testdata/union.bed", "testdata/files.fraction1.list", true, "", "testdata/out2.txt", "testdata/expected.fraction1.txt", ""},
+	{"testdata/union.bed", "testdata/files.fraction2.list", true, "", "testdata/out3.txt", "testdata/expected.fraction2.txt", ""},
+	{"testdata/union.bed", "testdata/files.fraction3.list", false, "", "testdata/out4.txt", "testdata/expected.noFraction2.txt", ""},
+	{"testdata/union.bed", "testdata/files.fraction3.list", true, "", "testdata/out5.txt", "testdata/expected.fraction3.txt", ""},
+	{"testdata/union.bed", "testdata/files.fraction3.list", true, "testdata/out5.multipleOverlaps.txt", "testdata/out5.txt", "testdata/expected.fraction3.txt", "testdata/expected.fraction3.multipleOverlaps.txt"},
 }
 
 func TestBedSubsetMatrix(t *testing.T) {
 	var err error
 
 	for _, v := range BedSubsetMatrixTests {
-		intervalSubsetMatrix(v.UnionFile, v.FileListFile, v.OutFile, v.Fraction)
+		intervalSubsetMatrix(v.UnionFile, v.FileListFile, v.OutFile, v.Fraction, v.MarkMultipleOverlaps)
 		if !fileio.AreEqual(v.OutFile, v.ExpectedFile) {
 			t.Errorf("Error in bedSubsetMatrix. Output was not as expected.")
 		} else {
 			err = os.Remove(v.OutFile)
 			exception.PanicOnErr(err)
+		}
+		if v.MarkMultipleOverlaps != "" {
+			if !fileio.AreEqual(v.MarkMultipleOverlaps, v.ExpectedMultipleOverlapsFile) {
+				t.Errorf("Error in bedSubsetMatrix -markMultipleOverlaps. Multiple overlaps output file was not as expected.")
+			} else {
+				err = os.Remove(v.MarkMultipleOverlaps)
+				exception.PanicOnErr(err)
+			}
 		}
 	}
 }
