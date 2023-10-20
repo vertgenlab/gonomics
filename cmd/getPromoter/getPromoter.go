3024a4472b5a693a58958117c6ffe6599a653681
Christi Fauci <christiana.fauci@duke.edu>
2023-10-20 13:13:40 -0400

    getPromoter tool, all tests passing

---
+++ cmd/getPromoter/getPromoter.go
@@ -0,0 +1,57 @@
+// Command Group: "GTF Tools"
+
+// getPromoter will take a list of unique genes of interest and return a bed file of the promoter region proceeding
+// the position of the TSS by the amount specified with distance.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/vertgenlab/gonomics/bed"
+	"github.com/vertgenlab/gonomics/chromInfo"
+	"github.com/vertgenlab/gonomics/fileio"
+	gtf2 "github.com/vertgenlab/gonomics/gtf"
+	"log"
+)
+
+func getPromoter(genes string, info string, outBed string, chrom string, upstream int, downstream int) {
+	geneNames := fileio.Read(genes)
+	geneInfo := gtf2.Read(info)
+	chr := chromInfo.ReadToMap(chrom)
+
+	answer := gtf2.FindPromoter(geneNames, upstream, downstream, geneInfo, chr)
+
+	bed.Write(outBed, answer)
+}
+
+func usage() {
+	fmt.Print(
+		"getPromoter will take a list of unique genes of interest and return a bed file of the promoter region proceeding\n" +
+			"the position of the TSS by the amount specified with distance.\n" +
+			"Usage:\n" +
+			"getPromoter [options] uniqueGenes.txt geneInfo.gtf out.bed\n" +
+			"options:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	var expectedNumArgs int = 4
+
+	var upstream *int = flag.Int("upstream", 1000, "Specifies the amount of bases to return in the bed entry before the TSS")
+	var downstream *int = flag.Int("downstream", 200, "Specifies the amount of bases to return in the bed entry after the TSS")
+
+	flag.Usage = usage
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	flag.Parse()
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Error: expecting %d arguments, but got %d\n",
+			expectedNumArgs, len(flag.Args()))
+	}
+	uniqueGenes := flag.Arg(0)
+	geneInfo := flag.Arg(1)
+	outFile := flag.Arg(2)
+	chromInfoName := flag.Arg(3)
+
+	getPromoter(uniqueGenes, geneInfo, outFile, chromInfoName, *upstream, *downstream)
+}
--- gtf/filter.go
+++ gtf/filter.go
@@ -1,7 +1,9 @@
 package gtf
 
 import (
+	"github.com/vertgenlab/gonomics/bed"
 	"github.com/vertgenlab/gonomics/chromInfo"
+	"github.com/vertgenlab/gonomics/numbers"
 	"github.com/vertgenlab/gonomics/vcf"
 )
 
@@ -58,4 +60,33 @@ func FilterVariantThreeUtr(v *vcf.Vcf, g map[string]*Gene, c map[string]*chromIn
 func FilterVariantFiveUtr(v *vcf.Vcf, g map[string]*Gene, c map[string]*chromInfo.ChromInfo) bool {
 	a := FiveUtrBoolArray(g, c)
 	return VariantArrayOverlap(v, a)
+}
+
+func FindPromoter(genes []string, upstream int, downstream int, gtf map[string]*Gene, size map[string]chromInfo.ChromInfo) []bed.Bed {
+	var answer []bed.Bed
+	var currGene, transcript int
+	var exists bool
+	var info *Gene
+	var name string
+	var trans *Transcript
+	var newBed = bed.Bed{Chrom: "", ChromStart: 0, ChromEnd: 0, FieldsInitialized: 4}
+
+	for currGene = range genes {
+		name = genes[currGene]
+
+		info, exists = gtf[name]
+		if exists {
+			for transcript = range info.Transcripts {
+				trans = info.Transcripts[transcript]
+				if trans.Strand {
+					newBed = bed.Bed{Chrom: trans.Chr, ChromStart: numbers.Max(trans.Start-upstream, 0), ChromEnd: numbers.Min(trans.Start+downstream, size[trans.Chr].Size), Name: info.GeneName, FieldsInitialized: 4}
+				} else if !trans.Strand {
+					newBed = bed.Bed{Chrom: trans.Chr, ChromStart: trans.Start + downstream, ChromEnd: trans.Start + upstream}
+				}
+				answer = append(answer, newBed)
+			}
+		}
+	}
+
+	return answer
 }
