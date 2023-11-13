b3cb0ea703e8bc022fd920f580f56a5f5276ea7d
sweavs111 <seth.weaver@duke.edu>
2023-11-13 11:04:08 -0500

    more concise testing files

--- cmd/mergesort/mergesort_test.go
+++ cmd/mergesort/mergesort_test.go
@@ -29,7 +29,7 @@ var MergeSortTests = []struct {
 	{"testdata/test.axt", "", "testdata/out.axt", "", "testdata/expected.axt", "", "tmp", 1000000, "byGenomicCoordinates", false},
 	{"testdata/test_R1.fastq", "", "testdata/out_R1.fastq", "", "testdata/expected_R1.fastq", "", "tmp", 1000000, "byGenomicCoordinates", false},
 	{"testdata/test_R1.fastq", "testdata/test_R2.fastq", "testdata/out_R1.fastq", "testdata/out_R2.fastq", "testdata/expected_R1.fastq", "testdata/expected_R2.fastq", "tmp", 1000000, "byGenomicCoordinates", true},
-	{"testdata/small.readName.sam", "", "testdata/out.readName.sam", "", "testdata/expected.readName.sam", "", "tmp", 1000000, "readName", false},
+	{"testdata/small.sam", "", "testdata/out.readName.sam", "", "testdata/expected.readName.sam", "", "tmp", 1000000, "readName", false},
 
 	// TODO enable giraf sorting after pointers are removed
 	//{"testdata/test.giraf", "testdata/out.giraf", "testdata/expected.giraf", "tmp", 1000000, "byGenomicCoordinates"},
