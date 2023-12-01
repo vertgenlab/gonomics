512196eeab728c9cadb8fd6037bc860ffdd36116
sweavs111 <seth.weaver@duke.edu>
2023-12-01 18:14:31 -0500

    testing for samToWig with deletions

--- cmd/samToWig/samToWig_test.go
+++ cmd/samToWig/samToWig_test.go
@@ -8,25 +8,28 @@ import (
 )
 
 var SamToWigTests = []struct {
-	inFile           string
-	reference        string
-	outFile_expected string
-	fragLength       int
-	deletions        bool
+	inFile     string
+	reference  string
+	outFile    string
+	expected   string
+	fragLength int
+	deletions  bool
 }{
-	{"testdata/test1.sam", "testdata/test.chrom.sizes", "testdata/test1.wig", -1, false},
-	{"testdata/test2.sam", "testdata/test.chrom.sizes", "testdata/test2.wig", 30, false},
-	{"testdata/test1.bam", "testdata/test.chrom.sizes", "testdata/test1.wig", -1, false},
-	{"testdata/test2.bam", "testdata/test.chrom.sizes", "testdata/test2.wig", 30, false},
+	{"testdata/test1.sam", "testdata/test.chrom.sizes", "testdata/out.test1.wig", "testdata/test1.wig", -1, false},
+	{"testdata/test2.sam", "testdata/test.chrom.sizes", "testdata/out.test2.wig", "testdata/test2.wig", 30, false},
+	{"testdata/test1.bam", "testdata/test.chrom.sizes", "testdata/out.test1.wig", "testdata/test1.wig", -1, false},
+	{"testdata/test2.bam", "testdata/test.chrom.sizes", "testdata/out.test2.wig", "testdata/test2.wig", 30, false},
+	{"testdata/test1.sam", "testdata/test.chrom.sizes", "testdata/out.test1.withDel.wig", "testdata/test1.withDel.wig", -1, true},
 }
 
 func TestSamToWig(t *testing.T) {
+	var err error
 	for _, v := range SamToWigTests {
-		samToWig(v.inFile, v.reference, "outFile_tmp.wig", v.fragLength, v.deletions)
-		if !fileio.AreEqual("outFile_tmp.wig", v.outFile_expected) {
-			t.Errorf("Error in samToWig")
+		samToWig(v.inFile, v.reference, v.outFile, v.fragLength, v.deletions)
+		if !fileio.AreEqual(v.expected, v.outFile) {
+			t.Errorf("Error in samToWig: Exp: %s, actual: %s\n", v.expected, v.outFile)
 		}
-		err := os.Remove("outFile_tmp.wig")
+		err = os.Remove(v.outFile)
 		exception.PanicOnErr(err)
 	}
 }
