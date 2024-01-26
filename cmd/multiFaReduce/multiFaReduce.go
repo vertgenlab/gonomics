ffec8d5cfd4c769209bc4eec6b3a976d7966e5d2
RavenYL <yanting.raven.luo@gmail.com>
2024-01-25 21:55:15 -0500

    add -bedFilename option to multiFaReduce

--- cmd/multiFaReduce/multiFaReduce.go
+++ cmd/multiFaReduce/multiFaReduce.go
@@ -6,14 +6,27 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-
+	"github.com/vertgenlab/gonomics/bed"
 	"github.com/vertgenlab/gonomics/fasta"
+	"log"
 )
 
-func mfaReduce(inFilename, outFilename string) {
+func mfaReduce(inFilename, outFilename, bedFilename, chrom string) {
 	aln := fasta.Read(inFilename)
-	answer := fasta.SegregatingSites(aln)
+	var answer []fasta.Fasta
+	var answerBedPos [][]int // answerBedPos is [][]int not [][]bed.Bed to avoid circular dependencies, aka trying to import bed package in fasta package
+	if bedFilename != "" {
+		var answerBed []bed.Bed
+		var currentBed bed.Bed
+		answer, answerBedPos = fasta.SegregatingSitesWithBed(aln)
+		for i := 0; i < len(answerBedPos); i++ {
+			currentBed = bed.Bed{Chrom: chrom, ChromStart: answerBedPos[i][0], ChromEnd: answerBedPos[i][1], Name: aln[0].Name, FieldsInitialized: 4} // Name field is reference species name
+			answerBed = append(answerBed, currentBed)
+		}
+		bed.Write(bedFilename, answerBed)
+	} else {
+		answer = fasta.SegregatingSites(aln)
+	}
 	fasta.Write(outFilename, answer)
 }
 
@@ -28,6 +41,8 @@ func usage() {
 
 func main() {
 	var expectedNumArgs int = 2
+	var bedFilename *string = flag.String("bedFilename", "", "Output the positions of variable sites in the reference species into a bed file with this name")
+	var chrom *string = flag.String("chrom", "", "Required when using -bedFilename, to specify the chromosome name of the reference species in the output bed file")
 
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
@@ -38,8 +53,13 @@ func main() {
 		log.Fatalf("Error: expecting %d arguments, but got %d\n", expectedNumArgs, len(flag.Args()))
 	}
 
+	if *bedFilename != "" && *chrom == "" {
+		flag.Usage()
+		log.Fatalf("Error: using -bedFilename without -chrom\n")
+	}
+
 	inFile := flag.Arg(0)
 	outFile := flag.Arg(1)
 
-	mfaReduce(inFile, outFile)
+	mfaReduce(inFile, outFile, *bedFilename, *chrom)
 }
--- cmd/multiFaReduce/multiFaReduce_test.go
+++ cmd/multiFaReduce/multiFaReduce_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/vertgenlab/gonomics/bed"
 	"os"
 	"testing"
 
@@ -12,14 +13,20 @@ var MfaReduceTests = []struct {
 	inputFile    string
 	outputFile   string
 	expectedFile string
+	bedFilename  string
+	chrom        string
+	expectedBed  string
 }{
-	{"testdata/test.mfa", "testdata/output.mfa", "testdata/expected.mfa"},
+	{"testdata/test.mfa", "testdata/output.mfa", "testdata/expected.mfa", "", "", ""},
+	{"testdata/test.mfa", "testdata/output.mfa", "testdata/expected.mfa", "testdata/output.bed", "chrTest", "testdata/expected.bed"},
+	{"testdata/test2.mfa", "testdata/output2.mfa", "testdata/expected2.mfa", "testdata/output2.bed", "chrTest", "testdata/expected2.bed"},
+	{"testdata/test3.mfa", "testdata/output3.mfa", "testdata/expected3.mfa", "testdata/output3.bed", "chrTest", "testdata/expected3.bed"},
 }
 
 func TestMfaReduce(t *testing.T) {
 	var err error
 	for _, v := range MfaReduceTests {
-		mfaReduce(v.inputFile, v.outputFile)
+		mfaReduce(v.inputFile, v.outputFile, v.bedFilename, v.chrom)
 		records := fasta.Read(v.outputFile)
 		expected := fasta.Read(v.expectedFile)
 		if !fasta.AllAreEqual(records, expected) {
@@ -27,6 +34,16 @@ func TestMfaReduce(t *testing.T) {
 		} else {
 			err = os.Remove(v.outputFile)
 			exception.PanicOnErr(err)
+		}
+		if v.bedFilename != "" {
+			recordsBed := bed.Read(v.bedFilename)
+			expectedBed := bed.Read(v.expectedBed)
+			if !bed.AllAreEqual(recordsBed, expectedBed) {
+				t.Errorf("Error in mfaReduce bed output.")
+			} else {
+				err = os.Remove(v.bedFilename)
+				exception.PanicOnErr(err)
+			}
 		}
 	}
 }
