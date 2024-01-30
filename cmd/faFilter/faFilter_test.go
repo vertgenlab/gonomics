03e3bbb4cf38b70add1ab3325fc80deba51727ce
hcnapier <napier1@kenyon.edu>
2024-01-30 11:19:41 -0500

    Fix testing files

--- cmd/faFilter/faFilter_test.go
+++ cmd/faFilter/faFilter_test.go
@@ -19,15 +19,17 @@ var FaFilterTests = []struct {
 	start        int
 	end          int
 	minSize      int
+	maxGC        int
 }{
-	{"testdata/minSizeTest.fa", "testdata/minSizeOutput.fa", "testdata/minSizeExpected.fa", "", "", "", false, 0, -1, 10},
-	{"testdata/nameContainsTest.fa", "testdata/nameContainsOutput.fa", "testdata/nameContainsExpected.fa", "", "", "_maternal", false, 0, -1, 0},
+	{"testdata/minSizeTest.fa", "testdata/minSizeOutput.fa", "testdata/minSizeExpected.fa", "", "", "", false, 0, -1, 10, 100},
+	{"testdata/nameContainsTest.fa", "testdata/nameContainsOutput.fa", "testdata/nameContainsExpected.fa", "", "", "_maternal", false, 0, -1, 0, 100},
+	{"testdata/maxGCTest.fa", "testdata/maxGCOutput.fa", "testdata/maxGCExpected.fa", "", "", "", false, 0, -1, 0, 65},
 }
 
 func TestFaFilter(t *testing.T) {
 	var err error
 	for _, v := range FaFilterTests {
-		faFilter(v.inputFile, v.outputFile, v.name, v.notName, v.nameContains, v.refPositions, v.start, v.end, v.minSize)
+		faFilter(v.inputFile, v.outputFile, v.name, v.notName, v.nameContains, v.refPositions, v.start, v.end, v.minSize, v.maxGC)
 		records := fasta.Read(v.outputFile)
 		expected := fasta.Read(v.expectedFile)
 		if !fasta.AllAreEqual(records, expected) {
