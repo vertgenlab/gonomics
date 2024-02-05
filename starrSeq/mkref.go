ede2978959035a7f3452002f610701ac25be0aa9
sweavs111 <seth.weaver@duke.edu>
2024-02-05 18:21:34 -0500

    add makeref to starrSeqAnalysis

---
+++ starrSeq/mkref.go
@@ -0,0 +1,31 @@
+package starrSeq
+
+import (
+	"github.com/vertgenlab/gonomics/bed"
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fasta"
+	"github.com/vertgenlab/gonomics/fileio"
+)
+
+func Mkref(upstream, constructs, downstream, output string) {
+	var bds []bed.Bed
+	var bd bed.Bed
+	var ref fasta.Fasta
+	var idx int
+	o := fileio.EasyCreate(output)
+	ref.Name = "chrSS"
+	c := fasta.Read(constructs)
+	u := fasta.Read(upstream)
+	d := fasta.Read(downstream)
+	for i := range c {
+		ref.Seq = append(ref.Seq, u[0].Seq...)
+		idx += len(u[0].Seq)
+		ref.Seq = append(ref.Seq, c[i].Seq...)
+		bd = bed.Bed{Chrom: "chrSS", ChromStart: idx, ChromEnd: idx + len(c[i].Seq), Name: c[i].Name, FieldsInitialized: 4}
+		ref.Seq = append(ref.Seq, d[0].Seq...)
+		idx += len(c[i].Seq) + len(d[0].Seq)
+	}
+	fasta.WriteFasta(o, ref, 50)
+	err := o.Close()
+	exception.PanicOnErr(err)
+}
