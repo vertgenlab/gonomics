58fd0ff77855c011b0db268a24248130b4046cc4
hcnapier <napier1@kenyon.edu>
2024-02-05 14:39:49 -0500

    Address comments: update error messages for style.

--- dna/examine_test.go
+++ dna/examine_test.go
@@ -9,52 +9,52 @@ func TestCount(t *testing.T) {
 	ACount, CCount, GCount, TCount, NCount, aCount, cCount, gCount, tCount, nCount, gapCount := Count(alpha)
 	if ACount != 5 || CCount != 4 || GCount != 1 || TCount != 1 || NCount != 3 || aCount != 3 || cCount != 3 ||
 		gCount != 3 || tCount != 2 || nCount != 3 || gapCount != 2 {
-		t.Errorf("problem with Count")
+		t.Errorf("Error: Problem with Count")
 	}
 
 	ACount, CCount, GCount, TCount, NCount, aCount, cCount, gCount, tCount, nCount, gapCount = Count(beta)
 	if ACount != 4 || CCount != 6 || GCount != 2 || TCount != 1 || NCount != 2 ||
 		aCount != 3 || cCount != 2 || gCount != 3 || tCount != 2 || nCount != 3 ||
 		gapCount != 2 {
-		t.Errorf("problem with Count")
+		t.Errorf("Error: Problem with Count")
 	}
 }
 
 func TestCountMask(t *testing.T) {
 	umC, mC, gC := CountMask(alpha)
 	if umC != 14 || mC != 14 || gC != 2 {
-		t.Error("problem with CountMask(alpha)")
+		t.Error("Error: Problem with CountMask(alpha)")
 	}
 
 	umC, mC, gC = CountMask(beta)
 	if umC != 15 || mC != 13 || gC != 2 {
-		t.Error("problem with CountMask(beta)")
+		t.Error("Error: Problem with CountMask(beta)")
 	}
 }
 
 func TestCountGaps(t *testing.T) {
 	if CountGaps(alpha) != 2 {
-		t.Errorf("problem with CountGaps")
+		t.Errorf("Error: Problem with CountGaps")
 	}
 }
 
 func TestGCContent(t *testing.T) {
 	if GCContent(alpha) != 50 {
-		t.Errorf("problem with GCContent")
+		t.Errorf("Error: Problem with GCContent")
 	}
 }
 
 func TestDist(t *testing.T) {
 	actual := Dist(alpha, beta)
 	if actual != 4 {
-		t.Errorf("problem with Dist")
+		t.Errorf("Error: Problem with Dist")
 	}
 }
 
 func TestCountBase(t *testing.T) {
 	actual := CountBase(alpha, N)
 	if actual != 3 {
-		t.Errorf("problem with CountBase")
+		t.Errorf("Error: Problem with CountBase")
 	}
 }
 
@@ -63,6 +63,6 @@ func TestDefineBase(t *testing.T) {
 		!DefineBase(LowerA) || !DefineBase(LowerC) || !DefineBase(LowerG) ||
 		!DefineBase(LowerT) || DefineBase(N) || DefineBase(LowerN) ||
 		DefineBase(Gap) || DefineBase(Dot) || DefineBase(Nil) {
-		t.Errorf("problem with DefineBase")
+		t.Errorf("Error: Problem with DefineBase")
 	}
 }
