4e0b3ad4c74cc9355eed05afd460c5c91f33dba2
sweavs111 <seth.weaver@duke.edu>
2024-02-06 11:56:49 -0500

    respond to comments

--- sam/pairedSam.go
+++ sam/pairedSam.go
@@ -9,11 +9,11 @@ import (
 
 // SamPE is struct that contains 2 paired end Sam entries as well as a slice of Sam that represents all supplementary
 // alignments for the read-pair
-type SamPE struct {
-	A    Sam
-	B    Sam
-	SupA []Sam
-	SupB []Sam
+type SamPE struct { //TODO: rename from A/B to something more informative
+	R1    Sam
+	R2    Sam
+	SupR1 []Sam
+	SupR2 []Sam
 }
 
 // GoReadSamPeToChan takes a read-name sorted sam file and returns a chanel of SamPE and a Header
@@ -53,7 +53,8 @@ func combineEnds(data <-chan Sam, peChan chan<- SamPE) {
 			continue
 		} else {
 			peChan <- parseReads(tmpSlice)
-			tmpSlice = []Sam{tmp}
+			tmpSlice = tmpSlice[0:0]
+			tmpSlice = append(tmpSlice, tmp)
 		}
 	}
 	close(peChan)
@@ -62,72 +63,39 @@ func combineEnds(data <-chan Sam, peChan chan<- SamPE) {
 // parseReads is a helper function for GoReadSamPeToChan that parses a slice of Sam that all have the same read name into a SamPE struct
 func parseReads(tmpSlice []Sam) SamPE {
 	var pe SamPE
-	if len(tmpSlice) == 2 {
-		pe = SamPE{A: tmpSlice[0], B: tmpSlice[1]}
-		pe, _ = OrderSamPair(pe)
-		return pe
-	}
 	for i := range tmpSlice {
-		if tmpSlice[i].Flag&2048 == 2048 {
-			if tmpSlice[i].Flag&64 == 64 {
-				pe.SupA = append(pe.SupA, tmpSlice[i])
-			} else if tmpSlice[i].Flag&128 == 128 {
-				pe.SupB = append(pe.SupB, tmpSlice[i])
+		if tmpSlice[i].Flag&2048 == 2048 { // Flag 2048 == supplementary reads
+			if tmpSlice[i].Flag&64 == 64 { // Flag 64 == Read 1
+				pe.SupR1 = append(pe.SupR1, tmpSlice[i])
+			} else if tmpSlice[i].Flag&128 == 128 { // Flag 128 == Read 2
+				pe.SupR2 = append(pe.SupR2, tmpSlice[i])
 			}
-		} else if pe.A.QName == "" {
-			pe.A = tmpSlice[i]
-		} else if pe.B.QName == "" {
-			pe.B = tmpSlice[i]
-		} else {
-			pe.SupA = append(pe.SupA, tmpSlice[i])
+		} else if tmpSlice[i].Flag&64 == 64 { // is Read 1
+			pe.R1 = tmpSlice[i]
+		} else if tmpSlice[i].Flag&128 == 128 { // is Read 2
+			pe.R2 = tmpSlice[i]
 		}
 	}
-	pe, _ = OrderSamPair(pe)
 	return pe
-}
-
-// OrderSamPair returns R1, R2 in order based on Sam Flags and a bool if the pair had the proper flags. If the pair doesn't have the proper flags,
-// the pair will be returned in the same order with false. Sup Slices will not be reordered.
-func OrderSamPair(p SamPE) (SamPE, bool) {
-	if p.A.Flag&64 == 64 && p.B.Flag&128 == 128 {
-		return p, true
-	} else if p.A.Flag&128 == 128 && p.B.Flag&64 == 64 {
-		return SamPE{A: p.B, B: p.A, SupA: p.SupA, SupB: p.SupB}, true
-	}
-	return p, false
 }
 
 // WriteSamPeToFileHandle writes a both reads and any supplementary alignments from a SamPE struct to a fileio EasyWriter
 // TODO: Add bam writing functionality
 func WriteSamPeToFileHandle(file io.Writer, pe SamPE) {
-	_, err := fmt.Fprintln(file, ToString(pe.A))
+	_, err := fmt.Fprintln(file, ToString(pe.R1))
 	exception.PanicOnErr(err)
-	_, err = fmt.Fprintln(file, ToString(pe.B))
+	_, err = fmt.Fprintln(file, ToString(pe.R2))
 	exception.PanicOnErr(err)
-	if len(pe.SupA) != 0 {
-		for i := range pe.SupA {
-			_, err = fmt.Fprintln(file, ToString(pe.SupA[i]))
+	if len(pe.SupR1) != 0 {
+		for i := range pe.SupR1 {
+			_, err = fmt.Fprintln(file, ToString(pe.SupR1[i]))
 			exception.PanicOnErr(err)
 		}
 	}
-	if len(pe.SupB) != 0 {
-		for i := range pe.SupB {
-			_, err = fmt.Fprintln(file, ToString(pe.SupB[i]))
+	if len(pe.SupR2) != 0 {
+		for i := range pe.SupR2 {
+			_, err = fmt.Fprintln(file, ToString(pe.SupR2[i]))
 			exception.PanicOnErr(err)
 		}
 	}
 }
-
-/*		done = false
-		a, full = <-data
-		b, full = <-data
-		for !done {
-			switch {
-			case full, a.QName == b.QName:
-				peChan <- SamPE{a, b}
-				done = true
-			case a.QName != b.QName:
-				a = b
-				b, full = <-data
-			}
-		}*/
