56f838ffa08b9980a48b196ac37185b00bdfaf42
RavenYL <yanting.raven.luo@gmail.com>
2024-02-14 22:10:53 -0500

    added helper functions to pDNA

--- dna/pDna/pDna.go
+++ dna/pDna/pDna.go
@@ -4,11 +4,21 @@ import "math"
 
 // Float32Base encodes a DNA base as a probability vector using float32 precision.
 // Note that gap probabilities can be stored implicitly as 1 - (A + C + G + T).
+// The probabilities of A, C, G, and T should add up to 1
 type Float32Base struct {
 	A float32
 	C float32
 	G float32
 	T float32
+}
+
+// Float32Diff encodes a probability vector using float64 precision that describes the difference between 2 pDNA probability vectors
+// The probability of A, C, G, and T should add up to 0
+type Float64Diff struct {
+	A float64
+	C float64
+	G float64
+	T float64
 }
 
 // EqualBase returns true if two input Float32Base structs, p and q, are 'equal', defined
@@ -40,4 +50,27 @@ func equalFloatPrecision(a float32, b float32, precision float32) bool {
 		return float32(math.Abs(float64(a-b)))/b < precision
 	}
 	return float32(math.Abs(float64(a-b)))/a < precision
+}
+
+// Diff returns the vector that is the difference between 2 pDNA bases' probability vectors
+func Diff(p Float32Base, q Float32Base) Float64Diff {
+	return Float64Diff{
+		A: float64(p.A - q.A),
+		C: float64(p.C - q.C),
+		G: float64(p.G - q.G),
+		T: float64(p.T - q.T),
+	}
+}
+
+// Mag returns the magnitude of a difference probability vector
+// Mag is a float64
+func Mag(d Float64Diff) float64 {
+	return math.Sqrt(math.Pow(d.A, 2) + math.Pow(d.C, 2) + math.Pow(d.G, 2) + math.Pow(d.T, 2))
+}
+
+// Dist returns a score for the distance that separates 2 pDNA bases
+// The distance score is the magnitude of the vector that is the difference between the 2 pDNA bases' probability vectors
+// The distance score is a float64
+func Dist(p Float32Base, q Float32Base) float64 {
+	return Mag(Diff(p, q))
 }
--- dna/pDna/pDna_test.go
+++ dna/pDna/pDna_test.go
@@ -1,6 +1,8 @@
 package pDna
 
-import "testing"
+import (
+	"testing"
+)
 
 var EqualBaseTests = []struct {
 	P         Float32Base
@@ -46,6 +48,138 @@ func TestEqualBase(t *testing.T) {
 		observed = EqualBase(v.P, v.Q, v.Precision)
 		if observed != v.Expected {
 			t.Errorf("Error: in pDna, EqualBase. Output was not as expected.\n")
+		}
+	}
+}
+
+var DiffTests = []struct {
+	P        Float32Base
+	Q        Float32Base
+	Expected Float64Diff
+}{
+	{P: Float32Base{
+		A: 0.3,
+		C: 0.2,
+		G: 0.5,
+		T: 0,
+	},
+		Q: Float32Base{
+			A: 0.3,
+			C: 0.2,
+			G: 0.5,
+			T: 0,
+		},
+		Expected: Float64Diff{
+			A: 0,
+			C: 0,
+			G: 0,
+			T: 0,
+		},
+	},
+	{P: Float32Base{
+		A: 0.3,
+		C: 0.2,
+		G: 0.5,
+		T: 0,
+	},
+		Q: Float32Base{
+			A: 0.5,
+			C: 0.1,
+			G: 0.3,
+			T: 0.1,
+		},
+		Expected: Float64Diff{
+			A: -0.19999998807907104,
+			C: 0.10000000149011612,
+			G: 0.19999998807907104,
+			T: -0.10000000149011612,
+		},
+	},
+}
+
+func TestDiff(t *testing.T) {
+	var observed Float64Diff
+	for _, v := range DiffTests {
+		observed = Diff(v.P, v.Q)
+		if observed != v.Expected {
+			t.Errorf("Error: in pDna, Diff. Output was not as expected. observed: %v, expected: %v\n", observed, v.Expected)
+		}
+	}
+}
+
+var MagTests = []struct {
+	D        Float64Diff
+	Expected float64
+}{
+	{D: Float64Diff{
+		A: 0,
+		C: 0,
+		G: 0,
+		T: 0,
+	},
+		Expected: 0,
+	},
+	{D: Float64Diff{
+		A: -0.19999998807907104,
+		C: 0.10000000149011612,
+		G: 0.19999998807907104,
+		T: -0.10000000149011612,
+	},
+		Expected: 0.31622775188035535,
+	},
+}
+
+func TestMag(t *testing.T) {
+	var observed float64
+	for _, v := range MagTests {
+		observed = Mag(v.D)
+		if observed != v.Expected {
+			t.Errorf("Error: in pDna, Mag. Output was not as expected. observed: %v, expected: %v\n", observed, v.Expected)
+		}
+	}
+}
+
+var DistTests = []struct {
+	P        Float32Base
+	Q        Float32Base
+	Expected float64
+}{
+	{P: Float32Base{
+		A: 0.3,
+		C: 0.2,
+		G: 0.5,
+		T: 0,
+	},
+		Q: Float32Base{
+			A: 0.3,
+			C: 0.2,
+			G: 0.5,
+			T: 0,
+		},
+		Expected: 0,
+	},
+	{P: Float32Base{
+		A: 0.3,
+		C: 0.2,
+		G: 0.5,
+		T: 0,
+	},
+		Q: Float32Base{
+			A: 0.5,
+			C: 0.1,
+			G: 0.3,
+			T: 0.1,
+		},
+		Expected: 0.31622775188035535,
+	},
+}
+
+func TestDist(t *testing.T) {
+	var observed float64
+	for _, v := range DistTests {
+		observed = Dist(v.P, v.Q)
+		if observed != v.Expected {
+			t.Errorf("Error: in pDna, Dist. Output was not as expected. observed: %v, expected: %v\n", observed, v.Expected)
 		}
 	}
 }
