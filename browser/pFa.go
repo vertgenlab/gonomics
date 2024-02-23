d6d0f095594c5d5451e428cfab1aeedcdf545798
Sarah Zhao <sarahaz4240@gmail.com>
2024-02-22 20:44:24 -0500

    toy test for printSetOflines

--- browser/pFa.go
+++ browser/pFa.go
@@ -2,60 +2,63 @@ package browser
 
 import (
 	"fmt"
+	"unicode/utf8"
 
+	"github.com/vertgenlab/gonomics/dna/pDna"
+	"github.com/vertgenlab/gonomics/fasta/pFasta"
+	"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/numbers"
-	"github.com/vertgenlab/gonomics/fasta/pfasta"
 )
 
 // PFaVisualizer produces command line visualizations of pFasta format alignments from a specified start and end position.
 // Can be written to a file or to standard out. Includes noMask and lineLength formatting options as bools.
 // If 0 sig figs, returns full probability
-func PFaVisualizer(infile string, outfile string, start int, end int, sigFigs int, lineLength int, seqName string) {
-	//add 'end' as input in cmd (look at multfavisualiser)
-	if !(start < end) {
-		log.Fatalf("Error: Invalid arguments, start must be lower than end")
-	}
-
-	records := Read(infile) // for now, assuming only 1 seq in pfasta, will rewrite for multiple
-	out := fileio.EasyCreate(outfile)
-	lineA := make([]float32, lineLength)
-	lineC := make([]float32, lineLength)
-	lineG := make([]float32, lineLength)
-	lineT := make([]float32, lineLength)
-	setOfLinesIdx := 0
-
-	fmt.Fprintf(out, "%s", records[0].Name)
-	for setOfLinesIdx = 0; setOfLinesIdx < (end-start)/sigFigs; setOfLinesIdx++ {
-		printOneSetLines(lineLength, setOfLinesIdx, lineLength, lineA, lineC, lineG, lineT, start, records, out, sigFigs)
-	}
-	printOneSetLines(lineLength, setOfLinesIdx, (end-start)%sigFigs, lineA, lineC, lineG, lineT, start, records, out, sigFigs)
-}
+//func PFaVisualizer(infile string, outfile string, start int, end int, sigFigs int, lineLength int, seqName string) {
+//	//add 'end' as input in cmd (look at multfavisualiser)
+//	if !(start < end) {
+//		log.Fatalf("Error: Invalid arguments, start must be lower than end")
+//	}
+//
+//	records := pFasta.Read(infile) // for now, assuming only 1 seq in pfasta, will rewrite for multiple
+//	out := fileio.EasyCreate(outfile)
+//	lineA := make([]float32, lineLength)
+//	lineC := make([]float32, lineLength)
+//	lineG := make([]float32, lineLength)
+//	lineT := make([]float32, lineLength)
+//	setOfLinesIdx := 0
+//
+//	long := calculateLongestNamePFa(records)
+//
+//	fmt.Fprintf(out, "Start: %d. Sigfigs: %d.", start, sigFigs)
+//
+//	for setOfLinesIdx = 0; setOfLinesIdx < (end-start)/sigFigs; setOfLinesIdx++ {
+//		printOneSetLines(lineLength, setOfLinesIdx, lineLength, lineA, lineC, lineG, lineT, start, records, out, sigFigs, records[0].Name, long)
+//	}
+//	printOneSetLines(lineLength, setOfLinesIdx, (end-start)%sigFigs, lineA, lineC, lineG, lineT, start, records, out, sigFigs, records[0].Name, long)
+//}
 
 // printOneSetLines prints from init_pos =(setOfLinesIdx*lineLength + start) in pFasta to (init_pos + numIters )
-func printOneSetLines(lineLength int, setOfLinesIdx int, numIters int, lineA []float32, lineC []float32, lineG []float32, lineT []float32, start int, records []PFasta, out *fileio.EasyWriter, sigFigs int, name string) {
-	// add start position (look at mulfavisualiser), empty spacer lines
+func printOneSetLines(lineLength int, setOfLinesIdx int, numIters int, lineA []float32, lineC []float32, lineG []float32, lineT []float32, start int, records []pFasta.PFasta, out *fileio.EasyWriter, sigFigs int, name string, longestName int) {
 	recordIdx := setOfLinesIdx*lineLength + start
 	lineIdx := 0
 	for lineIdx = 0; lineIdx < numIters; lineIdx++ {
-		fmt.Printf("sigfigs:%v", sigFigs)
 		lineA[lineIdx], lineC[lineIdx], lineG[lineIdx], lineT[lineIdx] = getBaseProbsAtPos(records[0].Seq[recordIdx], sigFigs)
 		recordIdx++
 	}
 	fmt.Fprintf(out, "Position: %d\n", recordIdx)
-	fmt.Fprintf(out, "|%-*s | A | %v\n", lineA[0:lineIdx])
-	fmt.Fprintf(out, "C | %v\n", lineC[0:lineIdx])
-	fmt.Fprintf(out, "G | %v\n", lineG[0:lineIdx])
-	fmt.Fprintf(out, "T | %v\n\n", lineT[0:lineIdx])
+	fmt.Fprintf(out, "|%-*s | A | %v\n", longestName, records[0].Name, lineA[0:lineIdx])
+	fmt.Fprintf(out, "|%-*s | C | %v\n", longestName, "", lineC[0:lineIdx])
+	fmt.Fprintf(out, "|%-*s | G | %v\n", longestName, "", lineG[0:lineIdx])
+	fmt.Fprintf(out, "|%-*s | T | %v\n\n", longestName, "", lineT[0:lineIdx])
 }
 
 // getBaseProbsAtPos returns the four probabilities rounded to sigFigs for a specified base
 func getBaseProbsAtPos(base pDna.Float32Base, sigFigs int) (float32, float32, float32, float32) {
-	// return roundSigFigs(base.A, sigFigs), roundSigFigs(base.C, sigFigs), roundSigFigs(base.G, sigFigs), roundSigFigs(base.T, sigFigs)
 	return numbers.RoundSigFigs(float64(base.A), sigFigs), numbers.RoundSigFigs(float64(base.C), sigFigs), numbers.RoundSigFigs(float64(base.G), sigFigs), numbers.RoundSigFigs(float64(base.T), sigFigs)
 }
 
 // calculateLongestName is a helper function of MultiFaVisualizer that returns the length of the longest name in a slice of fasta.Fasta structs.
-func calculateLongestName(f []pFasta.PFasta) int {
+func calculateLongestNamePFa(f []pFasta.PFasta) int {
 	var ans int = 0
 	var temp int
 	for i := range f {
@@ -66,4 +69,3 @@ func calculateLongestName(f []pFasta.PFasta) int {
 	}
 	return ans
 }
-
--- browser/pFa_test.go
+++ browser/pFa_test.go
@@ -1,8 +1,12 @@
-
 package browser
 
 import (
+	"fmt"
+	"github.com/vertgenlab/gonomics/exception"
 	"testing"
+
+	"github.com/vertgenlab/gonomics/dna/pDna"
+	"github.com/vertgenlab/gonomics/fasta/pFasta"
 	"github.com/vertgenlab/gonomics/fileio"
 )
 
@@ -53,42 +57,114 @@ func TestGetBaseProbsAtPos(t *testing.T) {
 }
 
 var PrintOneSetLinesTests = []struct {
-	LineLength     int
-	SetOfLinesIdx  int
-	NumIters int
-	LineA	[]float32
-	LineC	[]float32
-	LineG	[]float32
-	LineT	[]float32
-	Start int
-	Records []PFasta
-	Out	*fileio.EasyWriter
-	SigFigs int
-	Expected	string
-}{{LineLength: 30,
+	LineLength    int
+	SetOfLinesIdx int
+	NumIters      int
+	LineA         []float32
+	LineC         []float32
+	LineG         []float32
+	LineT         []float32
+	Start         int
+	Records       []pFasta.PFasta
+	Out           string
+	SigFigs       int
+	LongestName   int
+	Expected      string
+}{{LineLength: 10,
 	SetOfLinesIdx: 0,
-	NumIters: 30,
-	LineA: make([]float32, 30),
-	LineC: make([]float32, 30),
-	LineG: make([]float32, 30),
-	LineT: make([]float32, 30),
-	Start: 2,
-	Records: ,
-	Out: fileio.EasyCreate('testdata_tools/printONeSetLinesTest_1.txt'),
-	SigFigs: 3,
-	Expected: 'testdata_tools/printONeSetLinesTest_1_Expected.txt',
-	}
+	NumIters:      7,
+	LineA:         make([]float32, 30),
+	LineC:         make([]float32, 30),
+	LineG:         make([]float32, 30),
+	LineT:         make([]float32, 30),
+	Start:         2,
+	Records: []pFasta.PFasta{
+		pFasta.PFasta{Name: "chr1",
+			Seq: []pDna.Float32Base{
+				pDna.Float32Base{
+					A: 0.23857,
+					C: 0.3323,
+					G: 0.44958,
+					T: 0.139448,
+				},
+				pDna.Float32Base{
+					A: 0.334,
+					C: 0.44239,
+					G: 0.134875,
+					T: 0.12394,
+				},
+				pDna.Float32Base{
+					A: 0.384398,
+					C: 0.59723,
+					G: 0.3289,
+					T: 0.8325,
+				},
+				pDna.Float32Base{
+					A: 0.488532,
+					C: 0.23858,
+					G: 0.548523,
+					T: 0.5473,
+				},
+				pDna.Float32Base{
+					A: 0.92323,
+					C: 0.237,
+					G: 0.1747,
+					T: 0.5839,
+				},
+				pDna.Float32Base{
+					A: 0.483284,
+					C: 0.123,
+					G: 0.25388,
+					T: 0.98243,
+				},
+				pDna.Float32Base{
+					A: 0.00834,
+					C: 0.5288,
+					G: 0.58001,
+					T: 0.4892,
+				},
+				pDna.Float32Base{
+					A: 0.5688,
+					C: 0.278,
+					G: 0.1937,
+					T: 0.1111,
+				},
+				pDna.Float32Base{
+					A: 0.42397,
+					C: 0.2358,
+					G: 0.984,
+					T: 0.3823,
+				},
+				pDna.Float32Base{
+					A: 0.042387,
+					C: 0.587,
+					G: 0.32871,
+					T: 0.73224,
+				}}},
+	},
+	Out:         "testdata/pfa_printOneSetLines_output_toy_1.txt",
+	SigFigs:     3,
+	LongestName: 15,
+	Expected:    "testdata/pfa_printOneSetLines_expected_toy_1.txt",
+},
 }
 
 // printOneSetLines(lineLength int, setOfLinesIdx int, numIters int, lineA []float32, lineC []float32, lineG []float32, lineT []float32, start int, records []PFasta, out *fileio.EasyWriter, sigFigs int) {
 
 func TestPrintOneSetLines(t *testing.T) {
 	for _, testCase := range PrintOneSetLinesTests {
-		printOneSetLines(testCase.lineLength, testCase.setOfLinesIdx, testCase.numIters, testCase.lineA, testCase.lineC, testCase.lineG, testCase.lineT, testCase.start, testCase.records, testCase.out, testCase.sigFigs)
+		pFaOut := "testdata/pfa_printOneSetLines_input_toy_1.pfa"
+		pFasta.Write(pFaOut, testCase.Records)
 
-		if resA != testCase.Expected[0] || resC != testCase.Expected[1] || resG != testCase.Expected[2] || resT != testCase.Expected[3] {
-			fmt.Printf("%v, %v, %v, %v\n", resA, resC, resG, resT)
+		testOut := fileio.EasyCreate(testCase.Out)
 
+		printOneSetLines(testCase.LineLength, testCase.SetOfLinesIdx, testCase.NumIters,
+			testCase.LineA, testCase.LineC, testCase.LineG, testCase.LineT, testCase.Start,
+			testCase.Records, testOut, testCase.SigFigs, testCase.Records[0].Name, testCase.LongestName)
+
+		err := testOut.Close()
+		exception.PanicOnErr(err)
+		if fileio.AreEqual(testCase.Out, testCase.Expected) {
 			t.Errorf("Error: in in pFasta. printOneSetLines test not as expected.")
 		}
 	}
