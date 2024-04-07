b7ba82c06dd65584ffddab60ad4ff9530ed2748e
RavenYL <yanting.raven.luo@gmail.com>
2024-04-07 10:18:25 -0400

    address PR comments. If find 1 CDS, retain all gene

--- cmd/gtfTools/filter.go
+++ cmd/gtfTools/filter.go
@@ -35,7 +35,7 @@ func parseFilterArgs() {
 	filterFlags := flag.NewFlagSet("filter", flag.ExitOnError)
 	var geneNameList *string = filterFlags.String("geneNameList", "", "Specify a new-line delimited file containing the geneNames for records to be retained.\n")
 	var chromFilter *string = filterFlags.String("chromFilter", "", "Specify a chromosome for which all transcript records will be in the output. All transcripts must be on the filtering chromosome in order to pass the filter. Can be used in combination with Gene Name Filter option, all records for a gene must be on the filtering chromosome.\n")
-	var codingTranscript *bool = filterFlags.Bool("codingTranscript", false, "In -codingTranscript mode, only transcripts that are coding, i.e. have 1 or more CDS, are retained.\n")
+	var codingTranscript *bool = filterFlags.Bool("codingTranscript", false, "In -codingTranscript mode, only genes that have at least 1 coding transcript, i.e. have 1 or more CDS, are retained. All of the gene's transcripts, exons, and CDSs are retained.\n")
 	err = filterFlags.Parse(os.Args[2:])
 	exception.PanicOnErr(err)
 	filterFlags.Usage = func() { filterUsage(filterFlags) }
@@ -78,22 +78,20 @@ func gtfFilter(s FilterSettings) {
 		pass = true
 
 		if s.CodingTranscript { // option to detect coding transcript
+			transcriptCoding := false                                      // in each gene, for each transcript, transcript is non-coding unless an exon with CDS is found later
 			for _, currTranscript := range records[currGene].Transcripts { // for each transcript
-				if len(currTranscript.Exons) == 0 { // if transcript has no exon, then transcript is non-coding
-					pass = false
-				} else { // if transcript has exon(s)
+				if len(currTranscript.Exons) != 0 { // if transcript has no exon, then transcript is non-coding. if transcript has exon(s),
 					// if we find at least 1 exon that has 1 CDS, then transcript is coding
-					foundCDS := false
 					for _, currExon := range currTranscript.Exons { // for each exon
 						if currExon.Cds != nil { // if exon has CDS, then found "at least 1 exon that has 1 CDS", transcript is coding
-							foundCDS = true
+							transcriptCoding = true
 							break
 						}
 					}
-					if !foundCDS { // after checking each exon, if no CDS, then transcript is non-coding
-						pass = false
-					}
 				}
+			}
+			if !transcriptCoding { // if after checking all transcripts, gene has no coding transcript, then don't let gene pass
+				pass = false
 			}
 		}
 
