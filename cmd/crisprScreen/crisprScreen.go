4c9257b4383d89a633e499bf3e325208b1ce8e22
sweavs111 <seth.weaver@duke.edu>
2024-04-15 15:56:37 -0400

    guideChar

---
+++ cmd/crisprScreen/crisprScreen.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/vertgenlab/gonomics/dna"
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fastq"
+	"github.com/vertgenlab/gonomics/fileio"
+	"strings"
+)
+
+type guide struct {
+	name   string
+	seq    string
+	counts int
+	cells  [][]dna.Base
+	proper bool
+}
+
+func checkCell(g guide, peFq fastq.SingleCellPair) guide {
+	var found bool = false
+	for i := range g.cells {
+		if !dna.SeqsAreSimilar(peFq.Bx, g.cells[i], 2) {
+			continue
+		} else {
+			found = true
+			break
+		}
+	}
+	if !found {
+		g.cells = append(g.cells, peFq.Bx)
+	}
+	return g
+}
+
+func readGuides(guidesFile string) (map[string]guide, []dna.Base) {
+	var cols []string
+	var g guide
+	mp := make(map[string]guide)
+	seqs := fileio.Read(guidesFile)
+	for i := 1; i < len(seqs); i++ {
+		cols = strings.Split(seqs[i], ",")
+		g = guide{name: cols[0], seq: cols[4], counts: 0, cells: [][]dna.Base{}, proper: true}
+		mp[g.seq] = g
+	}
+	return mp, dna.StringToBases(cols[3][0:20])
+}
+
+func analyze(mp map[string]guide, outDir string) {
+	var j int
+	var bx string
+	cellOut := fileio.EasyCreate(outDir + "guidesPerCell.txt")
+	guideOut := fileio.EasyCreate(outDir + "guideCount.txt")
+	cellMap := make(map[string][]string)
+	for i := range mp {
+		fileio.WriteToFileHandle(guideOut, fmt.Sprintf("%s\t%s\t%d", mp[i].name, i, mp[i].counts))
+		for j = range mp[i].cells {
+			bx = dna.BasesToString(mp[i].cells[j])
+			cellMap[bx] = append(cellMap[bx], mp[i].name)
+		}
+	}
+	err := guideOut.Close()
+	exception.PanicOnErr(err)
+	for i := range cellMap {
+		fileio.WriteToFileHandle(cellOut, fmt.Sprintf("%s\t%d\t%s", i, len(cellMap[i]), strings.Join(cellMap[i], ",")))
+	}
+	err = cellOut.Close()
+	exception.PanicOnErr(err)
+}
+
+func charGuides(r1, r2, guidesFile, outDir string) {
+	var noProto, c, u, p int
+	var seq string
+	var found bool
+	var g guide
+
+	mp, recogSeq := readGuides(guidesFile)
+	fqChan := make(chan fastq.SingleCellPair)
+	go fastq.ReadToChanSingleCellPair(r1, r2, 16, 10, fqChan)
+
+	for i := range fqChan {
+		c++
+		if !dna.SeqsAreSimilar(recogSeq, i.Reads.Rev.Seq[34:54], 2) {
+			noProto++
+			continue
+		}
+		seq = dna.BasesToString(i.Reads.Rev.Seq[54:74])
+		g, found = mp[seq]
+		if found {
+			g = checkCell(g, i)
+			g.counts++
+			mp[seq] = g
+			if g.proper {
+				p++
+			}
+		} else {
+			g = guide{name: fmt.Sprintf("unknown%d", u), seq: seq, cells: [][]dna.Base{i.Bx}, counts: 0, proper: false}
+			mp[seq] = g
+			u++
+		}
+	}
+	fmt.Println(noProto)
+	fmt.Println("total reads assessed: ", c)
+	fmt.Printf("Protospacer recognized in %f percent of cells\n", float64(c-noProto)/float64(c)*100)
+	fmt.Println("recognized guide seq: ", p)
+	analyze(mp, outDir)
+}
+
+func main() {
+	flag.Parse()
+	r1 := flag.Arg(0)
+	r2 := flag.Arg(1)
+	guidesFile := flag.Arg(2)
+	outDir := flag.Arg(3)
+
+	charGuides(r1, r2, guidesFile, outDir)
+}
