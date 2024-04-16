7d1f80bd4fce4a570c0194ee1afe65f8781eab8b
RavenYL <yanting.raven.luo@gmail.com>
2024-04-15 22:36:23 -0400

    get Craig's pFasta Read bug fix

--- fasta/pFasta/pFasta.go
+++ fasta/pFasta/pFasta.go
@@ -3,12 +3,14 @@ package pFasta
 import (
 	"bufio"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"github.com/vertgenlab/gonomics/dna/pDna"
 	"github.com/vertgenlab/gonomics/exception"
 	"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/numbers/parse"
 	"github.com/x448/float16"
+	"io"
 	"log"
 	"strings"
 )
@@ -106,68 +108,34 @@ func ReadPfaHeader(reader *bufio.Reader) []string {
 func Read(inFile string) []PFasta {
 	var err error
 	file := fileio.EasyOpen(inFile)
-	reader := bufio.NewReader(file)
-	records := makeEmptyRecords(reader)
+	records := makeEmptyRecords(file.BuffReader)
 	var currBase = make([]byte, 2) //8 bytes, or 64 bit slice. Enough for one pDna base probability.
 	var currSeq, currPos int
 
 	for currSeq = range records {
 		for currPos = range records[currSeq].Seq {
-			_, err = reader.Read(currBase)
+			_, err = io.ReadFull(file.BuffReader, currBase)
 			exception.PanicOnErr(err)
 			records[currSeq].Seq[currPos].A = float16.Frombits(binary.LittleEndian.Uint16(currBase)).Float32()
-			_, err = reader.Read(currBase)
+			_, err = io.ReadFull(file.BuffReader, currBase)
 			exception.PanicOnErr(err)
 			records[currSeq].Seq[currPos].C = float16.Frombits(binary.LittleEndian.Uint16(currBase)).Float32()
-			_, err = reader.Read(currBase)
+			_, err = io.ReadFull(file.BuffReader, currBase)
 			exception.PanicOnErr(err)
 			records[currSeq].Seq[currPos].G = float16.Frombits(binary.LittleEndian.Uint16(currBase)).Float32()
-			_, err = reader.Read(currBase)
+			_, err = io.ReadFull(file.BuffReader, currBase)
 			exception.PanicOnErr(err)
 			records[currSeq].Seq[currPos].T = float16.Frombits(binary.LittleEndian.Uint16(currBase)).Float32()
 		}
+	}
+	// check to make sure we are at the end of the file
+	currBase = currBase[0:1]
+	_, err = io.ReadFull(file.BuffReader, currBase)
+	if !errors.Is(err, io.EOF) {
+		log.Fatalf("Error: %s has more sequence than expected from the header\n", inFile)
 	}
 
 	err = file.Close()
 	exception.PanicOnErr(err)
 	return records
-}
-
-// ToMap converts a slice of pFasta records (e.g. the output of the Read function)
-// to a map of sequences keyed to the sequences name.
-func ToMap(ref []PFasta) map[string][]pDna.Float32Base {
-	m := make(map[string][]pDna.Float32Base)
-	for i := range ref {
-		_, ok := m[ref[i].Name]
-		if !ok {
-			m[ref[i].Name] = ref[i].Seq
-		} else {
-			log.Panicf("%s used for multiple pFasta records. record names must be unique.", ref[i].Name)
-		}
-	}
-	return m
-}
-
-// QC tests if each base in a single pFasta sequence is valid. Valid means either gap {0 0 0 0} or non-gap (bases' probabilities add up to 1)
-func QC(pfa []pDna.Float32Base) {
-	for i := range pfa {
-		if !pDna.IsGap(pfa[i]) && !pDna.IsNonGap(pfa[i]) {
-			log.Fatalf("Error: pFasta QC failed. Position %v has invalid base: %v\n", i, pfa[i])
-		}
-	}
-}
-
-// MakeValid makes each base in a single pFasta sequence valid
-func MakeValid(pfaIn []pDna.Float32Base) []pDna.Float32Base {
-	pfaOut := make([]pDna.Float32Base, len(pfaIn))
-	for i := range pfaIn {
-		if pDna.IsGap(pfaIn[i]) {
-			pfaOut[i] = pfaIn[i]
-		} else if pDna.IsNonGap(pfaIn[i]) {
-			pfaOut[i] = pfaIn[i]
-		} else { // already checked that the base is not gap, so convert it to valid non-gap
-			pfaOut[i] = pDna.MakeValid(pfaIn[i])
-		}
-	}
-	return pfaOut
 }
