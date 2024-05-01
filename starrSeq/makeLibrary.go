51ce2462191953106dcf8ab8f60f0559cd62616c
sweavs111 <seth.weaver@duke.edu>
2024-05-01 16:23:32 -0400

    add auto-pooling (WIP)

--- starrSeq/makeLibrary.go
+++ starrSeq/makeLibrary.go
@@ -7,6 +7,7 @@ import (
 	"github.com/vertgenlab/gonomics/fasta"
 	"github.com/vertgenlab/gonomics/fileio"
 	"math"
+	"sort"
 	"strings"
 )
 
@@ -19,8 +20,69 @@ type MakeLibSettings struct {
 	MinTemp     float64
 	MaxTemp     float64
 	MaxOligoLen int
-	OutFile     string
+	OutDir      string
 	SumFile     string
+}
+
+func writeOutPools(pools [][]fasta.Fasta, outDir string) {
+	var r int = 65
+	for i := 0; i < len(pools); i++ {
+		fasta.Write(fmt.Sprintf("%s/oligoLib.pool%c.fa", outDir, r+i), pools[i])
+	}
+}
+
+// searchConstruct returns true if no similar matches are found in the construct and false if a simiar match is found
+func searchConstruct(ref fasta.Fasta, query []dna.Base) bool {
+	var testSeq []dna.Base
+	for i := 0; i < len(ref.Seq)-len(query); i++ {
+		testSeq = ref.Seq[i : i+len(query)]
+		fmt.Println(len(testSeq), len(query))
+		if dna.SeqsAreSimilar(testSeq, query, 2) {
+			return false
+		}
+	}
+	return true
+}
+
+func findSearchSeq(overlap []dna.Base, minTemp float64) []dna.Base {
+	for i := 10; i < len(overlap); i++ {
+		if dna.MeltingTemp(overlap[0:i]) > minTemp {
+			return overlap[0:i]
+		}
+	}
+	return overlap
+}
+
+func orderPools(pools [][]fasta.Fasta) [][]int {
+	var ans [][]int
+	for i := range pools {
+		ans = append(ans, []int{i, len(pools[i])})
+	}
+	sort.Slice(ans, func(i, j int) bool {
+		return ans[i][1] < ans[j][1]
+	})
+	return ans
+}
+
+func makeNewPool(pools [][]fasta.Fasta) {
+	pools = append(pools, []fasta.Fasta{})
+}
+
+func distributeIntoPool(pools [][]fasta.Fasta, overlap []dna.Base, minTm float64) int {
+	idx := orderPools(pools)
+	searchSeq := findSearchSeq(overlap, minTm)
+	for i := range idx {
+		for j := range pools[idx[i][0]] {
+			if searchConstruct(pools[idx[i][0]][j], searchSeq) {
+				continue
+			} else {
+				break
+			}
+		}
+		return idx[i][0]
+	}
+	makeNewPool(pools)
+	return len(pools) + 1
 }
 
 func checkTm(fa fasta.Fasta, mid int, minTemp, maxTemp float64) (bool, float64) {
@@ -77,27 +139,26 @@ func extendOverlap(bestMid int, minTemp float64, maxOligoSize int, construct fas
 	return dna.MeltingTemp(construct.Seq[down:up]), up, down
 }
 
-func optimizeMeltingTemps(constructs []fasta.Fasta, minTemp, maxTemp float64, maxOligoSize int, summaryFile string) []fasta.Fasta {
-	var mid, c, bestMid, up, down int
+func optimizeMeltingTemps(constructs []fasta.Fasta, minTemp, maxTemp float64, maxOligoSize int, summaryFile string) [][]fasta.Fasta {
+	var mid, c, bestMid, up, down, idx int
 	var pass, odd, inRange bool = false, true, false
-	var oligos []fasta.Fasta
 	var Tm, bestDeltaTm, newTm float64
 	var o *fileio.EasyWriter
+	var oligoPools, pools [][]fasta.Fasta = [][]fasta.Fasta{{}}, [][]fasta.Fasta{{}}
 
-	if summaryFile != "" {
-		o = fileio.EasyCreate(summaryFile)
-		fileio.WriteToFileHandle(o, "construct\tTm\toverlapBases\toligoSizeF\toligoSizeR\twithinRange\toverlapSize\tconstructSize\tGC%")
-	}
+	o = fileio.EasyCreate(summaryFile)
+	fileio.WriteToFileHandle(o, "construct\tTm\toverlapBases\toligoSizeF\toligoSizeR\twithinRange\toverlapSize\tconstructSize\tGC%\tpool")
 
 	for i := range constructs {
 		mid = len(constructs[i].Seq) / 2
 		inRange, Tm = checkTm(constructs[i], mid, minTemp, maxTemp)
 
 		if inRange {
-			oligos = append(oligos, createOligos(constructs[i], mid+10, mid-10)...)
-			if summaryFile != "" {
-				fileio.WriteToFileHandle(o, fmt.Sprintf("%s\t%f\t%d-%d\t%d\t%d\ttrue\t20\t%d\t%f", constructs[i].Name, Tm, mid-10, mid+10, mid+10, len(constructs[i].Seq)-mid+10, len(constructs[i].Seq), dna.GCContent(constructs[i].Seq[mid-10:mid+10])))
-			}
+			idx = distributeIntoPool(pools, constructs[i].Seq[mid-10:mid+10], minTemp)
+			oligoPools[idx] = append(oligoPools[idx], createOligos(constructs[i], mid+10, mid-10)...)
+			pools[idx] = append(pools[idx], constructs[i])
+			fileio.WriteToFileHandle(o, fmt.Sprintf("%s\t%f\t%d-%d\t%d\t%d\ttrue\t20\t%d\t%f\t%c",
+				constructs[i].Name, Tm, mid-10, mid+10, mid+10, len(constructs[i].Seq)-mid+10, len(constructs[i].Seq), dna.GCContent(constructs[i].Seq[mid-10:mid+10]), idx+65))
 			continue
 		}
 
@@ -124,26 +185,28 @@ func optimizeMeltingTemps(constructs []fasta.Fasta, minTemp, maxTemp float64, ma
 						} else {
 							inRange = false
 						}
-						oligos = append(oligos, createOligos(constructs[i], up, down)...)
-						if summaryFile != "" {
-							fileio.WriteToFileHandle(o, fmt.Sprintf("%s\t%f\t%d-%d\t%d\t%d\t%t\t%d\t%d\t%f", constructs[i].Name, newTm, down, up, up, len(constructs[i].Seq)-down, inRange, up-down, len(constructs[i].Seq), dna.GCContent(constructs[i].Seq[down:up])))
-						}
+						idx = distributeIntoPool(pools, constructs[i].Seq[down:up], minTemp)
+						oligoPools[idx] = append(oligoPools[idx], createOligos(constructs[i], up, down)...)
+						pools[idx] = append(pools[idx], constructs[i])
+						fileio.WriteToFileHandle(o, fmt.Sprintf("%s\t%f\t%d-%d\t%d\t%d\t%t\t%d\t%d\t%f\t%c",
+							constructs[i].Name, newTm, down, up, up, len(constructs[i].Seq)-down, inRange, up-down, len(constructs[i].Seq), dna.GCContent(constructs[i].Seq[down:up]), idx+65))
 						break
 					}
 				}
-				fmt.Println("couldn't find an ideal mid. using this instead: ", bestMid, bestDeltaTm, constructs[i].Name)
-				oligos = append(oligos, createOligos(constructs[i], bestMid+10, bestMid-10)...)
-				if summaryFile != "" {
-					fileio.WriteToFileHandle(o, fmt.Sprintf("%s\t%f\t%d-%d\t%d\t%d\tfalse\t20\t%d\t%f", constructs[i].Name, bestDeltaTm, bestMid-10, bestMid+10, bestMid+10, len(constructs[i].Seq)-bestMid+10, len(constructs[i].Seq), dna.GCContent(constructs[i].Seq[bestMid-10:bestMid+10])))
-				}
+				idx = distributeIntoPool(pools, constructs[i].Seq[bestMid-10:bestMid+10], minTemp)
+				oligoPools[idx] = append(oligoPools[idx], createOligos(constructs[i], bestMid+10, bestMid-10)...)
+				pools[idx] = append(pools[idx], constructs[i])
+				fileio.WriteToFileHandle(o, fmt.Sprintf("%s\t%f\t%d-%d\t%d\t%d\tfalse\t20\t%d\t%f\t%c",
+					constructs[i].Name, bestDeltaTm, bestMid-10, bestMid+10, bestMid+10, len(constructs[i].Seq)-bestMid+10, len(constructs[i].Seq), dna.GCContent(constructs[i].Seq[bestMid-10:bestMid+10]), idx+65))
 				break
 			}
 			inRange, Tm = checkTm(constructs[i], mid, minTemp, maxTemp)
 			if inRange {
-				oligos = append(oligos, createOligos(constructs[i], mid+10, mid-10)...)
-				if summaryFile != "" {
-					fileio.WriteToFileHandle(o, fmt.Sprintf("%s\t%f\t%d-%d\t%d\t%d\ttrue\t20\t%d\t%f", constructs[i].Name, Tm, mid-10, mid+10, mid+10, len(constructs[i].Seq)-mid+10, len(constructs[i].Seq), dna.GCContent(constructs[i].Seq[mid-10:mid+10])))
-				}
+				idx = distributeIntoPool(pools, constructs[i].Seq[mid-10:mid+10], minTemp)
+				oligoPools[idx] = append(oligoPools[idx], createOligos(constructs[i], mid+10, mid-10)...)
+				pools[idx] = append(pools[idx], constructs[i])
+				fileio.WriteToFileHandle(o, fmt.Sprintf("%s\t%f\t%d-%d\t%d\t%d\ttrue\t20\t%d\t%f\t%c",
+					constructs[i].Name, Tm, mid-10, mid+10, mid+10, len(constructs[i].Seq)-mid+10, len(constructs[i].Seq), dna.GCContent(constructs[i].Seq[mid-10:mid+10]), idx+65))
 				pass = true
 				continue
 			} else {
@@ -154,15 +217,14 @@ func optimizeMeltingTemps(constructs []fasta.Fasta, minTemp, maxTemp float64, ma
 			}
 		}
 	}
-	if summaryFile != "" {
-		err := o.Close()
-		exception.PanicOnErr(err)
-	}
-	return oligos
+	err := o.Close()
+	exception.PanicOnErr(err)
+	return oligoPools
 }
 
 func MakeLibrary(s MakeLibSettings) {
 	var cs, constructs, oligos []fasta.Fasta
+	var pools [][]fasta.Fasta
 	var construct fasta.Fasta
 	var umi []dna.Base
 	var tmpUMI []string
@@ -192,12 +254,13 @@ func MakeLibrary(s MakeLibSettings) {
 		constructs = append(constructs, construct)
 	}
 	if s.MinTemp != -1 || s.MaxTemp != -1 {
-		oligos = optimizeMeltingTemps(constructs, s.MinTemp, s.MaxTemp, s.MaxOligoLen, s.SumFile)
+		pools = optimizeMeltingTemps(constructs, s.MinTemp, s.MaxTemp, s.MaxOligoLen, s.SumFile)
 	} else {
 		for i := range constructs {
 			mid = len(constructs[i].Seq) / 2
 			oligos = append(oligos, createOligos(constructs[i], mid+10, mid-10)...)
 		}
+		fasta.Write(s.OutDir+"/oligoLib.out.fa", oligos)
 	}
-	fasta.Write(s.OutFile, oligos)
+	writeOutPools(pools, s.OutDir)
 }
