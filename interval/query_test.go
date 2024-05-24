08ab85846202e2cdaadb1b8a102cc6eb99bcde52
Eric Au <ericdotau@gmail.com>
2024-05-24 05:21:51 -0700

    test: interval test unknown file case

---
+++ interval/query_test.go
@@ -0,0 +1,57 @@
+package interval
+
+import (
+	"os"
+	"testing"
+
+	"github.com/vertgenlab/gonomics/bed"
+	"github.com/vertgenlab/gonomics/fileio"
+)
+
+func TestReadToChanBed(t *testing.T) {
+	var b1 bed.Bed = bed.Bed{Chrom: "chr1", ChromStart: 100, ChromEnd: 200, Name: "First", Score: 1, Strand: '+', FieldsInitialized: 6}
+	var beds []bed.Bed = []bed.Bed{b1}
+	var bedFile string = "testdata/interval.bed"
+
+	bed.Write(bedFile, beds)
+
+	// Create the interval channel
+	interval := make(chan Interval, 1)
+	go ReadToChan(bedFile, interval)
+
+	// Check the intervals read from the channel
+	expectedBeds := []bed.Bed{b1}
+	for _, expected := range expectedBeds {
+		val, ok := <-interval
+		if !ok {
+			t.Fatalf("Expected more intervals, but channel was closed")
+		}
+		bedVal, ok := val.(*bed.Bed)
+		if !ok {
+			t.Fatalf("Expected *bed.Bed type, got %T", val)
+		}
+		if bedVal.Chrom != expected.Chrom || bedVal.ChromStart != expected.ChromStart || bedVal.ChromEnd != expected.ChromEnd {
+			t.Errorf("Error: %v != %v", bedVal, expected)
+		}
+	}
+
+	// Verify that no more intervals are left in the channel
+	if _, ok := <-interval; ok {
+		t.Errorf("Expected channel to be closed, but it still has values")
+	}
+	fileio.EasyRemove(bedFile)
+}
+
+func TestReadToChanUnknownFileType(t *testing.T) {
+	// Create a temporary file with an unknown extension
+	unknownData := "testdata/interval.unknown"
+	unknownFile := fileio.EasyCreate(unknownData)
+	defer unknownFile.Close()
+
+	if os.Getenv("TEST_FATAL") != "1" {
+		return
+	}
+	interval := make(chan Interval, 1)
+	go ReadToChan(unknownData, interval)
+	fileio.EasyRemove(unknownData)
+}
