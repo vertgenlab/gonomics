71b54db2147fd3b75a20ddadbb4b2f5c4f969d8e
Christi Fauci <christiana.fauci@duke.edu>
2024-06-05 11:24:31 -0400

    updated the usage to reflect changes

--- cmd/ontologyEnrichment/ontologyEnrichment.go
+++ cmd/ontologyEnrichment/ontologyEnrichment.go
@@ -29,14 +29,14 @@ func ontologyEnrichment(inputFile string, chromSizes string, geneFile string, an
 	ontology.ThreeDGreat(queries, sizes, geneFile, contacts, annotations, obos, enrichmentOut, geneProportions, termEnrichments)
 }
 
-// TODO: update usage for new functions
 func usage() {
 	fmt.Print(
 		"ontologyEnrichment will assign regions in the input data to their closest gene provided in the gene file input either " +
 			"using proximity (if no contact file is given) or by the " +
 			"closest gene in 3d space if a contact file is provided. With those assigned genes, and their corresponding GO terms, " +
 			"provided in the gaf and obo files, " +
-			"each query region will be assigned a GO term and then enrichment for each GO term assigned will be calculated." +
+			"each query region will be assigned a GO term and then enrichment for each GO term as it relates to the input data will " +
+			"be output along with the p-value of that enrichment calculated by approximating the binomial with a normal distribution." +
 			"Note: gene file may be a normal gtf file, or a bed file with each TSS listed as a single point" +
 			"Usage:\n" +
 			"ontologyEnrichment [options] inputFile.bed chromSizes geneFile.gtf/tssFile.bed annotationsFile.gaf oboFile.obo enrichmentOut.bed\n" +
@@ -49,7 +49,7 @@ func main() {
 	var force *bool = flag.Bool("force", false, "Default is set to false, this should only be set to true if the obo should be read ignoring empty fields besides the ID.")
 	var contactFile *string = flag.String("contactFile", "", "If the goal is to assign query regions to their closest 3d gene, then provide a contact file in the form of a bedpe.")
 	var geneProportions *bool = flag.Bool("geneEnrichments", true, "If the user doesn't want to output the gene proportions file listing how much of the genome each gene covers, then set to false.")
-	var termEnrichments *bool = flag.Bool("termEnrichments", true, "If user doesn't want to output the GO term enrichments for a set of input data, set to false.")
+	var termEnrichments *bool = flag.Bool("termEnrichments", true, "If user doesn't want to output the GO term enrichments and p-values for a set of input data, set to false.")
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flag.Parse()
