323c69b6560d07a24f70a4df0915a88f5f5378a2
Eric Au <ericdotau@gmail.com>
2024-06-12 02:03:25 -0400

    chore: remove unused code

--- genomeGraph/view.go
+++ genomeGraph/view.go
@@ -4,20 +4,11 @@ import (
 	"bytes"
 	"strings"
 
-	"github.com/vertgenlab/gonomics/align"
 	"github.com/vertgenlab/gonomics/cigar"
 	"github.com/vertgenlab/gonomics/dna"
 	"github.com/vertgenlab/gonomics/numbers/parse"
 	"github.com/vertgenlab/gonomics/sam"
 )
-
-func calcExtension(seq []dna.Base) int64 {
-	var maxScore int64 = 0
-	for bases := 0; bases < len(seq); bases++ {
-		maxScore += align.HumanChimpTwoScoreMatrix[seq[bases]][seq[bases]]
-	}
-	return maxScore/600 + int64(len(seq))
-}
 
 func LocalView(samLine *sam.Sam, ref []*Node) string {
 	var seqOne, seqTwo bytes.Buffer
