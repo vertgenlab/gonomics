3a87978b20a9e8134e4db724e01f72105813765f
Sarah Zhao <sarahaz4240@gmail.com>
2024-06-27 09:12:11 -0400

    notes on vcfToPfa

--- simulate/vcf.go
+++ simulate/vcf.go
@@ -4,8 +4,10 @@ import (
 	"github.com/vertgenlab/gonomics/dna"
 	"github.com/vertgenlab/gonomics/exception"
 	"github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/fasta"
 	"github.com/vertgenlab/gonomics/popgen"
 	"github.com/vertgenlab/gonomics/vcf"
+	"github.com/vertgenlab/gonomics/"
 )
 
 // VcfToFile generates simulated VCF data.  The inputs are alpha (the selection parameter), the number of sites,
@@ -46,13 +48,37 @@ func SingleVcf(alpha float64, numAlleles int, boundAlpha float64, boundBeta floa
 	return answer
 }
 
+// NEW VERSION
+// VcfToFile generates simulated VCF data.  The inputs are alpha (the selection parameter), the number of sites,
+// the output filename, along with parameters for the bounding function for sampling.  Reasonable parameters
+// choices for boundAlpha, boundBeta, and boundMultiplier are 0.001, 0.001, and 10000.
+func VcfToFileWithFasta(alpha float64, numAlleles int, numSites int, outFile string, boundAlpha float64, boundBeta float64, boundMultiplier float64, refFile fasta.Fasta) {
+	out := fileio.EasyCreate(outFile)
+	var current vcf.Vcf
+
+	//for each segregating site, we make a vcf entry and write out
+	for i := 0; i < numSites; i++ {
+		current = SingleVcf(alpha, numAlleles, boundAlpha, boundBeta, boundMultiplier, i+1)
+		vcf.WriteVcf(out, current)
+	}
+
+	// pick some number of positions in sequence, use fasta as reference
+	// mutate to some other base than ref, 
+	var err error
+	err = out.Close()
+	exception.PanicOnErr(err)
+}
+
 // we wanted to have fasta as a second input to vcfToFile?
+// andmake it not hardcoded? so the ref is from the fasta, and we randomly generate the alt based on the bound alpha/beta/multiplier?
 func SingleVcfNotHardCoded(alpha float64, numAlleles int, boundAlpha float64, boundBeta float64, boundMultiplier float64, pos int) vcf.Vcf {
 	var genotype []vcf.Sample
 	var divergent bool
 	var answer vcf.Vcf
 	genotype, divergent = popgen.SimulateGenotype(alpha, numAlleles, boundAlpha, boundBeta, boundMultiplier)
 	//most fields are hardcoded but can be filled in later
+	// already exists: ability to simulte llele frequencies based on allele distribution (boundalpha, boundbeta, boundmultiplier)
+	// using jukes-cantor (equal probability mutate to any other)
 	answer = vcf.Vcf{Chr: "chr1", Pos: pos, Id: ".", Ref: "A", Alt: []string{"T"}, Qual: 100, Filter: ".", Info: ".", Format: []string{"GT"}, Samples: genotype}
 	if divergent {
 		answer = vcf.AppendAncestor(answer, dna.StringToBases(answer.Alt[0]))
