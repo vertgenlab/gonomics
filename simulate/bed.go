1ac4d2872af65922806285d799c361cf33a2f3bb
Sarah Zhao <sarahaz4240@gmail.com>
2024-08-05 16:59:27 -0400

    forgot to save file

--- simulate/bed.go
+++ simulate/bed.go
@@ -5,50 +5,6 @@ import (
 	"github.com/vertgenlab/gonomics/numbers"
 )
 
-// GoSimulateBed takes a searchSpace (represented by a noGap.bed input file, here as a parsed struct) and generates a
-// number of regions (regionCount) of a specified length (regionLength) and sends the simulated regions to an output chan.
-func GoSimulateBed(searchSpace []bed.Bed, regionCount int, regionLength int) <-chan bed.Bed {
-	c := make(chan bed.Bed, 1000)
-
-	totalWindows := CountWindows(searchSpace, regionLength)
-
-	//this function generates new bed regions and sends them to a channel.
-	go func() {
-		for i := 0; i < regionCount; i++ {
-			c <- GenerateBedRegion(searchSpace, totalWindows, regionLength)
-		}
-		close(c)
-	}()
-	return c
-}
-
-// GenerateBedRegion searches the regions of searchSpace (a noGap.bed input file, as a parsed struct) and randomly selects a continuous region of length regionLength 
-func GenerateBedRegion(searchSpace []bed.Bed, totalWindows int, regionLength int) bed.Bed {
-	tmp := numbers.RandIntInRange(0, totalWindows)
-	var chromWindows int
-	var length int
-	var answer bed.Bed
-	// iterating through each ungapped window
-	for j := 0; j < len(searchSpace); j++ {
-		length = searchSpace[j].ChromEnd - searchSpace[j].ChromStart
-
-		// check that the window can fit a sequence of that length e.g. want 10 bp, but window is 5bp
-		chromWindows = length - regionLength + 1
-
-		if chromWindows < 1 {
-			continue
-		}
-		if tmp-chromWindows > 0 {
-			tmp = tmp - chromWindows
-		} else {
-			answer = bed.Bed{Chrom: searchSpace[j].Chrom, ChromStart: searchSpace[j].ChromStart + tmp - 1, ChromEnd: searchSpace[j].ChromStart + tmp - 1 + regionLength, Name: searchSpace[j].Name}
-			break
-		}
-	}
-	// TODO: I want it to fatal error if it can't generate answer, but when I had it as a return in the if-else above, it kept on giving "no return" error
-	return answer
-}
-
 // CountWindows counts the total viable windows of length regionLength in the sequence searchSpace
 func CountWindows(searchSpace []bed.Bed, regionLength int) int {
 	var length, totalWindows int
@@ -61,4 +17,54 @@ func CountWindows(searchSpace []bed.Bed, regionLength int) int {
 	}
 
 	return totalWindows
+}
+
+// GenerateBedRegion searches the regions of searchSpace (a noGap.bed input file, as a parsed struct) and randomly selects a continuous region of length regionLength 
+func GenerateBedRegion(searchSpace []bed.Bed, tmp int, regionLength int) (bed.Bed, bool) {
+	var chromWindows int
+	var length int
+	// iterating through each ungapped window
+	for j := 0; j < len(searchSpace); j++ {
+		length = searchSpace[j].ChromEnd - searchSpace[j].ChromStart
+
+		// check that the window can fit a sequence of that length e.g. want 10 bp, but window is 5bp
+		chromWindows = length - regionLength + 1
+
+		if chromWindows < 1 {
+			continue
+		}
+		if tmp-chromWindows > 0 {
+			tmp -= chromWindows
+		} else {
+			return bed.Bed{
+				Chrom: searchSpace[j].Chrom, 
+				ChromStart: searchSpace[j].ChromStart + tmp - 1, 
+				ChromEnd: searchSpace[j].ChromStart + tmp - 1 + regionLength, 
+				Name: searchSpace[j].Name,
+				FieldsInitialized: 4}, true
+		}
+	}
+	// TODO: I want it to fatal error if it can't generate answer, but when I had it as a return in the if-else above, it kept on giving "no return" error
+	return bed.Bed{}, false
+}
+
+// GoSimulateBed takes a searchSpace (represented by a noGap.bed input file, here as a parsed struct) and generates a
+// number of regions (regionCount) of a specified length (regionLength) and sends the simulated regions to an output chan.
+func GoSimulateBed(searchSpace []bed.Bed, regionCount int, regionLength int) <-chan bed.Bed {
+	var tmp int
+	c := make(chan bed.Bed, 1000)
+
+	totalWindows := CountWindows(searchSpace, regionLength)
+
+	//this function generates new bed regions and sends them to a channel.
+	go func() {
+		for i := 0; i < regionCount; i++ {
+			tmp = numbers.RandIntInRange(0, totalWindows)
+			if region, found := GenerateBedRegion(searchSpace, tmp, regionLength); found {
+				c <- region
+			}
+		}
+		close(c)
+	}()
+	return c
 }
