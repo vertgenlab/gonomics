894cde6e987f53aa775472d8d29db4d7ebc9dfed
sweavs111 <seth.weaver@duke.edu>
2024-08-21 15:05:21 -0400

    PR adding the L50 statistic to assemblyStats.go + misc testing files

--- fasta/assemblyStats_test.go
+++ fasta/assemblyStats_test.go
@@ -1,6 +1,7 @@
 package fasta
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 
@@ -28,4 +29,23 @@ func ContigAllAreEqual(input []int, expected []int) bool {
 		}
 	}
 	return true
+}
+
+func TestCalculateGenomeLength(t *testing.T) {
+	input := MakeContigList(inputFasta, false)
+	if calculateGenomeLength(input) != 49 {
+		t.Errorf("Error in calculateGenomeLength. Expected: 49. Observed: %d", calculateGenomeLength(input))
+	}
+}
+
+func TestCalculateN50L50(t *testing.T) {
+	exp := []int{30, 1}
+	input := MakeContigList(inputFasta, true)
+	sort.Ints(input)
+	fmt.Println(input)
+	N50, L50 := CalculateN50L50(input, calculateGenomeLength(input)/2)
+	if N50 != exp[0] || L50 != exp[1] {
+		t.Errorf("Error in CalculateN50L50: Expected N50: %d L50: %d. Observed N50: %d L50: %d", exp[0], exp[1], N50, L50)
+	}
+
 }
