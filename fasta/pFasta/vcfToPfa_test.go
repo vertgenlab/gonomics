c07c0f28ebf2d782140ee479552e48dc5d408440
Sarah Zhao <sarahaz4240@gmail.com>
2024-09-09 13:08:03 -0400

    fixing testing imports

--- fasta/pFasta/vcfToPfa_test.go
+++ fasta/pFasta/vcfToPfa_test.go
@@ -3,6 +3,7 @@ package pFasta
 import (
 	"testing"
 	"math/rand"
+	"github.com/vertgenlab/gonomics/fileio"
 )
 
 var vcfToPfaTests = []struct {
@@ -12,20 +13,26 @@ var vcfToPfaTests = []struct {
 	End	int
 	SetSeed  int64
 	Expected string
+	Output string
+	Precision	float32
 }{
 	{InputVcf: "testdata/test_vcftoPfa_input_1.vcf",
 		RefFa: "testdata/test_vcfToPfa_input_1.fa",
 		Start: 1,
 		End: 36,
 		SetSeed: 7,
-		Expected: "testdata/test_vcfToPfa_expected_1.vcf",
+		Expected: "testdata/test_vcfToPfa_expected_1.pfa",
+		Output: "testdata/test_vcfToPfa_observed_1.pfa",
+		Precision: 1e-3,
 	},
 	{InputVcf: "testdata/test_vcftoPfa_input_2.vcf",
 		RefFa: "testdata/test_vcfToPfa_input_1.fa",
 		Start: 1,
 		End: 44,
 		SetSeed: 7,
-		Expected: "testdata/test_vcfToPfa_expected_2.vcf",
+		Expected: "testdata/test_vcfToPfa_expected_2.pfa",
+		Output: "testdata/test_vcfToPfa_observed_2.pfa",
+		Precision: 1e-3,
 	},
 }
 
@@ -33,9 +40,12 @@ func TestVcfToPfa(t *testing.T) {
 	for _, tc := range vcfToPfaTests {
 		rand.Seed(tc.SetSeed)
 		observed := []PFasta{VcfToPfa(tc.InputVcf, tc.RefFa, tc.Start, tc.End)}
-		Write("testdata/test_vcfToPfa_output_2.pfa", observed)
-		if !fasta.IsEqual(observed, testCase.Expected) {
+		Write(tc.Output, observed)
+		expected := Read(tc.Expected)
+		if !AllAreEqual(observed, expected, tc.Precision) {
 			t.Errorf("Error: in pFasta. Sample valid input test not as expected.\n")
+		} else {
+			fileio.MustRemove(tc.Output)
 		}
 	}
 }
