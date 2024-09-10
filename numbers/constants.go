2266ce524bdb82f0332761109c0e3dc6cfd02dc0
Eric Au <ericdotau@gmail.com>
2024-09-10 03:13:14 -0400

    fix: lists and hash added to const

--- numbers/constants.go
+++ numbers/constants.go
@@ -7,3 +7,16 @@ import (
 const MaxInt = 1<<(bits.UintSize-1) - 1
 const MinInt = -MaxInt - 1
 const MaxUint = 1<<bits.UintSize - 1
+
+var numerals = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+var romans = []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+
+var symbols = map[byte]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
