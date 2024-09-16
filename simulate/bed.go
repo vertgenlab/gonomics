25586ad1b157299389b11db5900b5fc4c91b5f31
Sarah Zhao <sarahaz4240@gmail.com>
2024-09-16 14:49:56 -0400

    test cases

--- simulate/bed.go
+++ simulate/bed.go
@@ -24,17 +24,13 @@ func CountWindows(searchSpace []bed.Bed, regionLength int) int {
 func GenerateBedRegion(searchSpace []bed.Bed, randPos int, regionLength int) (bed.Bed, bool) {
 	var chromWindows int
 	var length int
-	totalWindows := CountWindows(searchSpace, regionLength)
-	log.Printf("total windows: %v\n", totalWindows)
 	// iterating through each ungapped window
 	for j := range searchSpace {
-		log.Print("this region: ", searchSpace[j])
 		length = searchSpace[j].ChromEnd - searchSpace[j].ChromStart
 
 		// number of windows of length regionLength in the specific bed region
 		// check that the window can fit a sequence of that length e.g. want 10 bp, but window is 5bp
 		chromWindows = length - regionLength + 1
-		log.Printf("\nchromWindows: %v, randPos: %v\n", chromWindows, randPos)
 
 		if chromWindows < 1 {
 			continue
@@ -42,18 +38,16 @@ func GenerateBedRegion(searchSpace []bed.Bed, randPos int, regionLength int) (be
 
 		// Decrement randomly generated overall position (corresponds to start of generated region) until it fits within a region
 		// must have randPos < chromWindows (randPos is 0-indexed, chromWindows is not), at most randPos + 1 = chromWindows
-		if randPos > chromWindows {
+		if randPos - chromWindows > -1 {
 			randPos -= chromWindows
 		} else {
-			log.Print("generated\n\n\n")
 			return bed.Bed{
 				Chrom: searchSpace[j].Chrom, 
-				ChromStart: searchSpace[j].ChromStart + randPos - 1, 
-				ChromEnd: searchSpace[j].ChromStart + randPos - 1 + regionLength, 
+				ChromStart: searchSpace[j].ChromStart + randPos, 
+				ChromEnd: searchSpace[j].ChromStart + randPos + regionLength, 
 				Name: searchSpace[j].Name,
 				FieldsInitialized: 4}, true
 		}
-		log.Print("looping\n")
 	}
 
 	log.Panic("Unable to generate region")
--- simulate/bed_test.go
+++ simulate/bed_test.go
@@ -5,11 +5,11 @@ import (
 	"testing"
 	"math/rand"
 
-	// "github.com/vertgenlab/gonomics/exception"
-	// "github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
 	"github.com/vertgenlab/gonomics/bed"
 	"github.com/vertgenlab/gonomics/fasta"
-	"log"
+	// "log"
 
 )
 
@@ -42,30 +42,66 @@ var GenerateBedRegionTests = []struct {
 	InputFa string
 	Pos int
 	RegionLength int
-	SetSeed int64
 	Expected string
 } {
-	{"testdata/ref_short.fasta", 51, 1, 8, "testdata/generateBedRegion_expected_1.bed"}, // end of region, len=1
-	{"testdata/ref_short.fasta", 49, 3, 3, "testdata/generateBedRegion_expected_2.bed"}, // end of region, len>1
-	{"testdata/ref_short.fasta", 0, 1, 22, "testdata/generateBedRegion_expected_3.bed"}, // beginning of region, len=1
-	{"testdata/ref_short.fasta", 52, 1, 5, "testdata/generateBedRegion_expected_4.bed"}, // beginning of new region, len=1
-	{"testdata/ref_short_2.fasta", 0, 50, 2, "testdata/generateBedRegion_expected_5.bed"}, // entire region
-	{"testdata/ref_short.fasta", 15, 13, 17, "testdata/generateBedRegion_expected_5.bed"}, // after gap, to end of region, len>1
+	{"testdata/ref_short.fasta", 49, 1, "testdata/generateBedRegion_expected_1.bed"}, // end of region, len=1
+	{"testdata/ref_short.fasta", 10, 3, "testdata/generateBedRegion_expected_2.bed"}, // end of region before gap, len>1
+	{"testdata/ref_short.fasta", 0, 1,"testdata/generateBedRegion_expected_3.bed"}, // beginning of region, len=1
+	{"testdata/ref_short.fasta", 50, 1,"testdata/generateBedRegion_expected_4.bed"}, // beginning of new region, len=1
+	{"testdata/ref_short_2.fasta", 0, 50, "testdata/generateBedRegion_expected_5.bed"}, // entire region
+	{"testdata/ref_short.fasta", 14, 13,"testdata/generateBedRegion_expected_6.bed"}, // after gap, to end of region, len>1
 }
 
 func TestGenerateBedRegion(t * testing.T) {
 	var searchSpace []bed.Bed
 	var inputFile []fasta.Fasta
-	// var region bed.Bed
+	var region bed.Bed
+	var generatedBool bool
+	var expected bed.Bed
 	for idx, v := range GenerateBedRegionTests {
+		inputFile = fasta.Read(v.InputFa)
+		searchSpace = bed.UngappedRegionsAllFromFa(inputFile)
+		region, generatedBool = GenerateBedRegion(searchSpace, v.Pos, v.RegionLength)
+		expected = bed.Read(v.Expected)[0]
+		if !generatedBool {
+			t.Errorf("No bed generated.")
+		}
+		if !bed.Equal(region, expected){
+			t.Errorf("Error in GenerateBedRegion testcase %v.", idx)
+		}
+	}
+}
+
+var GoSimulateBedTests = []struct {
+	InputFa string
+	RegionCount int
+	RegionLength int
+	SetSeed int64
+	Expected string
+	OutFile string
+} {
+	{"testdata/ref_short.fasta", 3, 1, 8, "testdata/goSimulateBed_expected_1.bed", "testdata/goSimulateBed_out_1.bed"},
+	{"testdata/ref_short.fasta", 100, 3, 3, "testdata/goSimulateBed_expected_2.bed", "testdata/goSimulateBed_out_2.bed"},
+}
+
+func TestGoSimulateBed(t * testing.T) {
+	var searchSpace []bed.Bed
+	var inputFile []fasta.Fasta
+	// var simRegions []bed.Bed
+	var err error
+	for idx, v := range GoSimulateBedTests {
 		rand.Seed(v.SetSeed)
 		inputFile = fasta.Read(v.InputFa)
 		searchSpace = bed.UngappedRegionsAllFromFa(inputFile)
-		log.Printf("test number %v, input posiion: %v, regionlength %v\n", idx, v.Pos, v.RegionLength)
-		GenerateBedRegion(searchSpace, v.Pos, v.RegionLength)
-		// region = GenerateBedRegion(searchSpace, v.Pos, v.RegionLength)
-		// if !bed.AllAreEqual(region, v.Expected) {
-		// 	t.Errorf("Error in generateBedRegion.")
-		// }
+		c := GoSimulateBed(searchSpace, v.RegionCount, v.RegionLength)
+		out := fileio.EasyCreate(v.OutFile)
+		for i := range c {
+			bed.WriteBed(out, i)
+		}
+		err = out.Close()
+		exception.PanicOnErr(err)
+		if !fileio.AreEqual(v.OutFile, v.Expected) {
+			t.Errorf("Error in GoSimulateBed testcase %v", idx)
+		}
 	}
 }
