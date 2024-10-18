8268cc6f9a16ae40c78f9e90128e9a4a0e2b551b
sweavs111 <seth.weaver@duke.edu>
2024-10-18 13:48:05 -0400

    fix bug

--- cmdx/regEleFam/regEleFam.go
+++ cmdx/regEleFam/regEleFam.go
@@ -41,17 +41,18 @@ func main() {
 
 	if len(flag.Args()) != 4 {
 		log.Fatalf("regEleFam selfChain.axt ocr.bed chrom.sizes out.txt\n")
-
 	}
+
 	var sb strings.Builder
 	var chromSizes map[string]chromInfo.ChromInfo = chromInfo.ReadToMap(flag.Arg(2))
 	var outfile *fileio.EasyWriter = fileio.EasyCreate(flag.Arg(3))
 	var chainOverlap, bedOverlap []interval.Interval
-	var lifted bed.Bed
+	var lifted bed.Bed = bed.Bed{FieldsInitialized: 3}
 	axTree := buildAxTree(flag.Arg(0))
 	bedTree, bedIntervals := buildBedTree(flag.Arg(1))
 	fmt.Println("done building trees")
 	regEleChan := bed.GoReadToChan(flag.Arg(1))
+	fmt.Println("bed chan read")
 	for i := range regEleChan {
 		sb.Reset()
 		sb.WriteString(bed.ToString(i, 5))
@@ -60,6 +61,7 @@ func main() {
 		for j := range chainOverlap {
 			lifted.Chrom, lifted.ChromStart, lifted.ChromEnd = lift.LiftCoordinatesWithAxt(chainOverlap[j].(axt.Axt), i, chromSizes[chainOverlap[j].(axt.Axt).QName].Size)
 			bedOverlap = interval.Query(bedTree, lifted, "any")
+			overlap
 			switch len(bedOverlap) {
 			case 0:
 				lifted.Name = i.Name + fmt.Sprintf("_lift.%d", j)
@@ -68,9 +70,9 @@ func main() {
 			case 1:
 				sb.WriteString(bedOverlap[0].(bed.Bed).Chrom + "_" + fileio.IntToString(bedOverlap[0].(bed.Bed).ChromStart) + "_" + fileio.IntToString(bedOverlap[0].(bed.Bed).ChromEnd) + "_" + bedOverlap[0].(bed.Bed).Name + ";")
 			case 2:
-				fmt.Println(bedOverlap[0], bedOverlap[1], lifted)
+				fmt.Println(bedOverlap, lifted)
 			default:
-				log.Fatalf("this shouldn't be greater than 1....")
+				fmt.Println(bedOverlap, lifted)
 			}
 
 		}
