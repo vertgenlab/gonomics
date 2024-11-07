8f7f40eac1d61bf9307549eb98b0109db08564c3
sweavs111 <seth.weaver@duke.edu>
2024-11-07 17:24:58 -0500

    bed comp

--- bed/compare.go
+++ bed/compare.go
@@ -67,6 +67,26 @@ func MergeBedsKeepNamesAndAnnotations(bedFile []Bed) []Bed {
 	return bedFile
 }
 
+func MergeBedsKeepNames(bedFile []Bed) []Bed {
+	SortByCoord(bedFile)
+	var i, j int
+	for i = 0; i < len(bedFile)-1; {
+		if !Overlap(bedFile[i], bedFile[i+1]) {
+			i++
+		} else {
+			bedFile[i].ChromStart = numbers.Min(bedFile[i].ChromStart, bedFile[i+1].ChromStart)
+			bedFile[i].ChromEnd = numbers.Max(bedFile[i].ChromEnd, bedFile[i+1].ChromEnd)
+			bedFile[i].Score = bedFile[i].Score + bedFile[i+1].Score
+			bedFile[i].Name = bedFile[i].Name + "," + bedFile[i+1].Name
+			for j = i + 1; j < len(bedFile)-1; j++ {
+				bedFile[j] = bedFile[j+1]
+			}
+			bedFile = bedFile[:len(bedFile)-1]
+		}
+	}
+	return bedFile
+}
+
 // MergeBedsIfNamesMatch will take a bed struct and return a merged struct such that any records which are of overlapping coordinates and names are identical will be merged.
 // Records with matching names must be adjacent
 func MergeBedsIfNamesMatch(bedFile []Bed) []Bed {
