3ab507921becec855c41dad2e9574bf7e29536db
RavenYL <yanting.raven.luo@gmail.com>
2024-11-07 14:37:21 -0500

    got updates from other branch faFormatToLower

--- bed/bedFasta.go
+++ bed/bedFasta.go
@@ -8,23 +8,27 @@ import (
 )
 
 // ToLower converts bases in a fasta sequence to lowercase in specified bed regions, where the bed chrom name matches the fasta record name
-func ToLower(records []fasta.Fasta, regions []Bed) {
+func ToLower(records []fasta.Fasta, regions []Bed, ignoreExtraRegions bool) {
 	recordsMap := fasta.ToMap(records)
 	var recordName string
+	var currRecordSeq []dna.Base
 	var found bool
 	for currRegion := range regions { // loop through each bed region
 		recordName = regions[currRegion].Chrom // bed chrom name matches the fasta record name
 		// only convert if that fasta record name exists in the fasta recordsMap
-		_, found = recordsMap[recordName]
+		currRecordSeq, found = recordsMap[recordName]
 		if found {
 			// assumes that the bed is formatted properly, ChromEnd > ChromStart
 			// if asking for a region that exceeds the length of the base sequence, log fatal error
-			if regions[currRegion].ChromEnd > len(recordsMap[recordName]) {
-				log.Fatalf("Error: ran out of sequence. Asking to manipulate a bed region where ChromEnd (%d) > length of sequence (%d).\n", regions[currRegion].ChromEnd, len(recordsMap[recordName]))
+			if regions[currRegion].ChromEnd > len(currRecordSeq) {
+				log.Fatalf("Error: ran out of sequence. Asking to manipulate a bed region where ChromEnd (%d) > length of sequence (%d).\n", regions[currRegion].ChromEnd, len(currRecordSeq))
 			}
-			dna.RangeToLower(recordsMap[recordName], regions[currRegion].ChromStart, regions[currRegion].ChromEnd)
+			dna.RangeToLower(currRecordSeq, regions[currRegion].ChromStart, regions[currRegion].ChromEnd)
+		} else {
+			if !ignoreExtraRegions {
+				log.Fatalf("Error: bed region chrom (%s) was not found as a fasta record name in the input fasta file\n", recordName)
+			}
 		}
-		//} else { } consider giving error if fasta record name doesn't exist in the fasta recordsMap
 	}
 }
 
