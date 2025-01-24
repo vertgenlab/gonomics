ea5768735a0b143cec76250b55a97250c926cb43
sweavs111 <seth.weaver@duke.edu>
2025-01-24 09:55:19 -0500

    improved help message

--- cmd/starrSeqAnalysis/starrSeqAnalysis.go
+++ cmd/starrSeqAnalysis/starrSeqAnalysis.go
@@ -8,12 +8,10 @@ import (
 
 func usage() {
 	fmt.Print("starrSeqAnalysis -- A suite of tools to analysis single cell STARR-seq data\n\n" +
-		"Subcommands:\n" +
-		"makeRef\n" +
-		"inputSeq\n" +
-		"outputSeq\n\n" +
-		"type a subcommand to print the help message for that command:\n" +
-		"starrSeqAnyalsis [subcommand]\n\n")
+		"\tstarrSeqAnalysis makeRef up.fa constructs.fa down.fa outfilePrefix\n" +
+		"\tstarrSeqAnalysis inputSeq [options] in.sam in.bed out.txt\n" +
+		"\tstarrSeqAnalysis outputSeq [options] in.sam out.txt\n" +
+		"enter a subcommand to view options and help message:\n")
 	flag.PrintDefaults()
 }
 
