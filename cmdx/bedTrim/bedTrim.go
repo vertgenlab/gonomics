5a56320f573ebfe30d37de9313fb29546dead426
sweavs111 <seth.weaver@duke.edu>
2025-02-02 16:08:02 -0500

    oops forgot to add usage

--- cmdx/bedTrim/bedTrim.go
+++ cmdx/bedTrim/bedTrim.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/vertgenlab/gonomics/bed"
 	"github.com/vertgenlab/gonomics/exception"
 	"github.com/vertgenlab/gonomics/fileio"
@@ -38,12 +39,30 @@ func bedTrim(trimPercent int, inBed string, outBed string) {
 	exception.PanicOnErr(out.Close())
 }
 
+func usage() {
+	fmt.Print("bedTrim -- Trim bed records in a file by N percent. An equal amount of bases " +
+		"will be removed from each side.\n" +
+		"Usage:\n" +
+		"bedTrim -trimPercent [int] in.bed out.bed\n ")
+	flag.PrintDefaults()
+}
+
 func main() {
 	var trimPercent *int = flag.Int("trimPercent", 0, "Input a percentage for each bed record to be shortened by. "+
 		"The bed region will be shorted by the same amount on either side ")
 
+	var expectedNumArgs int = 2
+	flag.Usage = usage
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flag.Parse()
+
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Expected %d arguments, got %d", expectedNumArgs, len(flag.Args()))
+	}
+
 	if parse.StringToInt(flag.Arg(0)) < 0 || parse.StringToInt(flag.Arg(0)) > 100 {
+		flag.Usage()
 		log.Fatalf("Error: trimPercent must be an integer between 0 and 100")
 	}
 
