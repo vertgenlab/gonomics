2bbec95fa8b737da8c517cac7d833cb3af9048e4
RavenYL <yanting.raven.luo@gmail.com>
2025-02-10 16:43:44 -0500

    addressed PR comments from Seth

--- cmdx/gcContent/gcContent_test.go
+++ cmdx/gcContent/gcContent_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/vertgenlab/gonomics/fileio"
 )
 
-var GcContentTests = []struct {
+var BedGcContentTests = []struct {
 	BedFile      string
 	FaFile       string
 	OutFile      string
@@ -30,12 +30,12 @@ var GcContentTests = []struct {
 		Species:      "Human_Chimp_Ancestor"},
 }
 
-func TestGcContent(t *testing.T) {
+func TestBedGcContent(t *testing.T) {
 	var err error
-	for _, v := range GcContentTests {
-		gcContent(v.BedFile, v.FaFile, v.OutFile, v.MultiFaMode, v.Species)
+	for _, v := range BedGcContentTests {
+		bedGcContent(v.BedFile, v.FaFile, v.OutFile, v.MultiFaMode, v.Species)
 		if !fileio.AreEqual(v.OutFile, v.ExpectedFile) {
-			t.Errorf("Error in gcContent. Output did not match expected.")
+			t.Errorf("Error in bedGcContent. Output did not match expected.")
 		} else {
 			err = os.Remove(v.OutFile)
 			exception.PanicOnErr(err)
