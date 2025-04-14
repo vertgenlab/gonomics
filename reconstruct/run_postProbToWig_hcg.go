7acc97fbc942735b01a0fdd55d451914b74df59a
Sarah Zhao <71564665+saqzhao@users.noreply.github.com>
2025-04-14 13:11:10 -0400

    havent made command yet will delete later

---
+++ reconstruct/run_postProbToWig_hcg.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/reconstruct"
+	"github.com/vertgenlab/gonomics/wig"
+)
+
+func main() {
+    fmt.Println("Hello")
+	postProbFile := "testdata/hcg_post_prob.csv"
+	inMaf := "/net/bmc-lab4/data/kellis/users/rimangan/primateT2T/alignment/work/yl726/PrimateT2T_15way/outputb/chr1.maf"
+	out = PostProbToWig(postProbFile, inMaf)
+	wig.Write(v.outDir, out)
+	
+	fmt.Println("")
+	fmt.Println("DONE")
+	fmt.Println("DONE")
+	fmt.Println("DONE")
+}
