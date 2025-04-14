adbcd05c26f4762731cf0e8370d5a79fdc58ca72
Sarah Zhao <71564665+saqzhao@users.noreply.github.com>
2025-04-14 13:20:32 -0400

    move directories

--- reconstruct/run_postProbToWig_hcg.go
+++ reconstruct/run_postProbToWig_hcg.go
@@ -1,6 +1,7 @@
-package reconstruct
+package main
 
 import (
+	"github.com/vertgenlab/gonomics/reconstruct"
 	"github.com/vertgenlab/gonomics/wig"
 )
 
@@ -8,7 +9,7 @@ func main() {
     fmt.Println("Hello")
 	postProbFile := "testdata/hcg_post_prob.csv"
 	inMaf := "/net/bmc-lab4/data/kellis/users/rimangan/primateT2T/alignment/work/yl726/PrimateT2T_15way/outputb/chr1.maf"
-	out = PostProbToWig(postProbFile, inMaf)
+	out = reconstruct.PostProbToWig(postProbFile, inMaf)
 	wig.Write("/net/bmc-lab4/data/kellis/users/sarahaz/data/pdna/hcg_trails_reconstruct/hcg_postProb.wig", out)
 	
 	fmt.Println("")
