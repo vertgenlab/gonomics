b0cca08cf3d9be8faca10e080e368fe0f0e53818
Sarah Zhao <71564665+saqzhao@users.noreply.github.com>
2025-04-16 14:12:22 -0400

    wrong file read in run file

--- cmd/ilsReconstructSeq_RunThis/run_postProbToWig_hcg.go
+++ cmd/ilsReconstructSeq_RunThis/run_postProbToWig_hcg.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
     fmt.Println("Hello")
-	postProbFile := "testdata/hcg_post_prob.csv"
+	postProbFile := "/net/bmc-lab4/data/kellis/users/sarahaz/data/pdna/hcg_trails_reconstruct/post_prob.csv"
 	inMafFile := "/net/bmc-lab4/data/kellis/users/rimangan/primateT2T/alignment/work/yl726/PrimateT2T_15way/outputb/chr1.maf"
 	inMaf := maf.Read(inMafFile)
 	out := reconstruct.PostProbToWig(postProbFile, inMaf)
