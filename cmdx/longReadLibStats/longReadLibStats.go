82cd95be658ca7134965e7cddaa158d13026873d
sweavs111 <seth.weaver@duke.edu>
2025-04-19 22:12:52 -0400

    draft

---
+++ cmdx/longReadLibStats/longReadLibStats.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fastq"
+	"github.com/vertgenlab/gonomics/fileio"
+	"sort"
+)
+
+func longReadLibStats(inFq string, readLengths string) {
+	var lens []int
+	var tot int
+	var out *fileio.EasyWriter
+
+	if readLengths != "" {
+		out = fileio.EasyCreate(readLengths)
+	}
+
+	reads := fastq.GoReadToChan(inFq)
+
+	for read := range reads {
+		tot += len(read.Seq)
+		lens = append(lens, len(read.Seq))
+		if readLengths != "" {
+			fileio.WriteToFileHandle(out, fmt.Sprintf("%d", len(read.Seq)))
+		}
+	}
+
+	sort.Slice(lens, func(i, j int) bool {
+		return lens[j] < lens[i]
+	})
+
+	calcN50(tot/2, lens)
+
+	if readLengths != "" {
+		exception.PanicOnErr(out.Close())
+	}
+}
+
+func calcN50(half int, lens []int) {
+	var tot int
+	for i := range lens {
+		tot += lens[i]
+		if tot >= half {
+			fmt.Println("N50: ", lens[i])
+			return
+		}
+	}
+}
+
+func main() {
+	var readLengths *string = flag.String("readLengths", "", "provide a filename for all the read lengths to be written out to")
+	flag.Parse()
+	longReadLibStats(flag.Arg(0), *readLengths)
+}
