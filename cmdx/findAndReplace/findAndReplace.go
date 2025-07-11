b64319a005c018f810e3d514a487e61d64eb9643
hcnapier <napier1@kenyon.edu>
2025-07-11 09:41:18 -0400

    adjust errors to accommodate default inFIleDelim = t

--- cmdx/findAndReplace/findAndReplace.go
+++ cmdx/findAndReplace/findAndReplace.go
@@ -113,9 +113,9 @@ func findAndReplace(inFile, inFileDelim, findReplaceFile, findReplaceDelim, outF
 	var done bool
 	var err error
 
-	if (regex && columnNumber != -1) || (regex && inFileDelim != "") {
+	if regex && columnNumber != -1 {
 		log.Fatalf("Error: Regex ignores columns. Regex is not compatible with columns in the input file.")
-	} else if (ignoreColumns && columnNumber != 1) || (ignoreColumns && inFileDelim != "") {
+	} else if ignoreColumns && columnNumber != 1 {
 		log.Fatalf("Error: IgnoreColumns is not compatible with columns in the input file.")
 	}
 
