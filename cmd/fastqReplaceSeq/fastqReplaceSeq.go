d70339e675581a9e9f10a51f3e8a8a96430d008c
hcnapier <napier1@kenyon.edu>
2025-07-25 15:10:16 -0400

    save progress

--- cmd/fastqReplaceSeq/fastqReplaceSeq.go
+++ cmd/fastqReplaceSeq/fastqReplaceSeq.go
@@ -12,26 +12,31 @@ import (
 	"github.com/vertgenlab/gonomics/fileio"
 )
 
-func readFindReplaceDNA(filename string, delim string) map[dna.Base]dna.Base {
-	// TODO: convert to DNA base from string
+// readFindReplaceDNA converts a delim separated findReplace file to a map of []dna.Base
+func readFindReplaceDNA(filename string, delim string) map[string][]dna.Base {
 	var in *fileio.EasyReader
-	var findReplaceMap = make(map[dna.Base]dna.Base)
-	var words []string
+	var findReplaceMap = make(map[string][]dna.Base)
+	var seqs []string
+	var replace []dna.Base
 	var found, done bool
 	var line string
 	var err error
 
 	in = fileio.EasyOpen(filename)
 	for line, done = fileio.EasyNextLine(in); !done; line, done = fileio.EasyNextLine(in) {
-		words = strings.Split(line, delim)
-		if len(words) != 2 {
+		seqs = strings.Split(line, delim)
+
+		if len(seqs) != 2 {
 			log.Fatalf("Error: the following line:\n\"%s\"\ndoes not give two substrings when split with \"%s\"", line, delim)
 		}
-		_, found = findReplaceMap[words[0]]
+
+		_, found = findReplaceMap[seqs[0]]
 		if found {
-			log.Fatalf("Error: this key:\"%s\" is found more than once in the findReplaceFile.\n", words[0])
+			log.Fatalf("Error: this key:\"%s\" is found more than once in the findReplaceFile.\n", seqs[0])
 		}
-		findReplaceMap[words[0]] = words[1]
+
+		replace = dna.StringToBases(seqs[1])
+		findReplaceMap[seqs[0]] = replace
 	}
 	err = in.Close()
 	exception.PanicOnErr(err)
@@ -79,44 +84,81 @@ func replacePrefix(seq []dna.Base, replace []dna.Base) []dna.Base {
 }
 
 type Settings struct {
-	inFile                   string
-	outFile                  string
-	findReplaceFile          string
-	findReplaceDelim         string
-	ignoreCase               bool
-	onlyPrintReplacedRecords bool
+	inFile              string
+	outFile             string
+	findReplaceFile     string
+	findReplaceDelim    string
+	ignoreCase          bool
+	replacedRecordsOnly bool
 }
 
 func usage() {
 	fmt.Print(
-		"bedMath - Performs comparative arithmetic operations on float values in bed files.\n" +
+		"fastqReplaceSeq\n" +
 			"Usage:\n" +
-			"bedMath a.bed operation b.bed out.bed\n" +
-			"operation may be one of the following options:" +
-			"plus\tminus\ttimes\tdivideBy\n" +
-			"Input bed files must be pre-sorted by coordinate.\n" +
+			"fastqReplaceSeq input.fastq findReplaceFile output.fastq\n" +
 			"options:\n")
 	flag.PrintDefaults()
 }
 
 func fastqReplaceSeq(s Settings) {
-	var findReplaceMap map[string]string
+	var findReplaceMap map[string][]dna.Base
+	var find string
+	var findBase []dna.Base
+	var replace []dna.Base
+	var replaceCounter int = 0
 	fq := fastq.GoReadToChan(s.inFile)
 	out := fileio.EasyCreate(s.outFile)
+	var err error
 
 	findReplaceMap = readFindReplaceDNA(s.findReplaceFile, s.findReplaceDelim)
 
 	for currRecord := range fq {
 		found := false
 		for find, replace = range findReplaceMap {
-			found = findSeq(currRecord.Seq, find, s.ignoreCase)
+			findBase = dna.StringToBases(find)
+			found = findSeq(currRecord.Seq, findBase, s.ignoreCase)
 			if found {
 				currRecord.Seq = replacePrefix(currRecord.Seq, replace)
+				replaceCounter++
 				fastq.WriteToFileHandle(out, currRecord)
+				break
 			}
 		}
-		if !found && !s.onlyPrintReplacedRecords {
+		if !found && !s.replacedRecordsOnly {
 			fastq.WriteToFileHandle(out, currRecord)
 		}
 	}
+	if replaceCounter == 0 {
+		log.Fatalf("Error: No pattern(s) found in input file.\n")
+	}
+	err = out.Close()
+	exception.PanicOnErr(err)
+}
+
+func main() {
+	var expectedNumArgs int = 3
+	var replaceDelim *string = flag.String("replaceDelim", "\t", "Lines in the findReplaceFile will be broken into columns based on this substring.  This should result in two substrings for each line where the first is the find and the second is the replace.")
+	var ignoreCase = flag.Bool("ignoreCase", true, "Ignore case when finding matches. Default is true.")
+	var replacedRecordsOnly = flag.Bool("replacedRecordsOnly", false, "Only output fastq records with a replacement in the sequence field. Default is false (outputs all records, regardless of whether a replacement was made).")
+	flag.Usage = usage
+
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Error: expecting %d arguments, but got %d\n", expectedNumArgs, len(flag.Args()))
+	}
+
+	inFile := flag.Arg(0)
+	findReplaceFile := flag.Arg(1)
+	outFile := flag.Arg(3)
+
+	s := Settings{
+		inFile,
+		outFile,
+		findReplaceFile,
+		*replaceDelim,
+		*ignoreCase,
+		*replacedRecordsOnly}
+
+	fastqReplaceSeq(s)
 }
