b74f63ff6cc958cf7309cd6b821b5030a36e7b44
Sarah Zhao <71564665+saqzhao@users.noreply.github.com>
2026-02-04 14:17:02 +0100

    scale base if prob of non-gap greater than prob of gap

--- reconstruct/ilsReconstruct.go
+++ reconstruct/ilsReconstruct.go
@@ -65,13 +65,15 @@ func IlsReconstructSeq(allPostProbs []map[string]wig.Wig, allRecons []pFasta.PFa
 	// TODO: if prob = 0 at that position, default set it to V0
 	// TODO: what if there is NOTHING at the position? 
 	// should there be a boolean option to say if you want default {0.25, 0.25, 0.25, 0.25}
-	weightedSum := pFasta.PFasta{Name: "ilsRecon", Seq:  make([]pDna.Float32Base, len(allWeightedRecons[0].Seq)),}
-	sum := pDna.Float32Base{A: 0, C: 0, G: 0, T: 0}
-
+	
 	n = len(allPostProbs[0][topoName].Values)
 	if len(allRecons[0].Seq) < n {
 		n = len(allRecons[0].Seq)
 	}
+
+	weightedSum := pFasta.PFasta{Name: "ilsRecon", Seq:  make([]pDna.Float32Base, len(allWeightedRecons[0].Seq)),}
+	sum := pDna.Float32Base{A: 0, C: 0, G: 0, T: 0}
+	zeroBase := pDna.Float32Base{A: 0, C: 0, G: 0, T: 0}
 
 	for pos := range n {
 		sum.A, sum.C, sum.G, sum.T = 0, 0, 0, 0
@@ -82,8 +84,16 @@ func IlsReconstructSeq(allPostProbs []map[string]wig.Wig, allRecons []pFasta.PFa
 
 		if pDna.SumsToOne(sum, precision) {
 			weightedSum.Seq[pos] = sum
-		} else { // TODO
-			log.Fatalf("Invalid pDNA created (does not sum to one)")
+		} else
+			for idx, recon := range allRecons {
+				if pDna.IsGap(recon.Seq[pos]) {
+					if allPostProbs[idx][allRecons[idx].Name].Values[pos] > 0.5 { // prob of gap greater than prob of non-gap
+						weightedSum.Seq[pos] = zeroBase
+					} else {
+						weightedSum.Seq[pos] = pDna.Scale(sum , 1/(1-allPostProbs[idx][allRecons[idx].Name].Values[pos]))
+					}
+				}
+			}
 		}
 	}
 	
