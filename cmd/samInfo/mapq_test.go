ffed25bfe2062b6f83337490b48c8e44cf294d43
seth weaver <70345529+sweavs111@users.noreply.github.com>
2026-02-24 10:17:42 -0500

    testing for bedregion

--- cmd/samInfo/mapq_test.go
+++ cmd/samInfo/mapq_test.go
@@ -13,7 +13,9 @@ var MaqqTests = []struct {
 	bedregions   string
 	expected     string
 }{
-	{infile: "testdata/mapq/test1.sam", outhistogram: "testdata/mapq/out.hist.txt", bedregions: "", expected: "testdata/mapq/out.hist.txt"},
+	{infile: "testdata/mapq/test1.sam", outhistogram: "testdata/mapq/out.hist.txt", bedregions: "", expected: "testdata/mapq/exp.hist.txt"},
+	{infile: "testdata/mapq/test1.bam", outhistogram: "testdata/mapq/out.hist.txt", bedregions: "testdata/mapq/bed1.bed", expected: "testdata/mapq/exp.hist.txt"},
+	{infile: "testdata/mapq/test1.bam", outhistogram: "testdata/mapq/out.hist.bed2.txt", bedregions: "testdata/mapq/bed2.bed", expected: "testdata/mapq/exp.hist.bed2.txt"},
 }
 
 func TestMapQ(t *testing.T) {
