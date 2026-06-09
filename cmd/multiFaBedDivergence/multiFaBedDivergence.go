25ce20d984d0e888330273991ae294abe931d349
Craig Lowe <craiglowe@users.noreply.github.com>
2026-06-09 10:55:48 -0400

    saving initial file

---
+++ cmd/multiFaBedDivergence/multiFaBedDivergence.go
@@ -0,0 +1,53 @@
+// Command Group: "FASTA and Multi-FASTA Tools"
+
+// Provides the mutational distance between two sequences human-readable multiple alignments for all entries in a bed file
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/vertgenlab/gonomics/bed"
+)
+
+func multFaBedDivergence(bedFile string, alnFile string, outFile string) {
+	b := bed.Read(bedFile)
+    var 
+
+	for i := 0; i < len(b); i++ {
+		fasta.(alnFile, outFile, b[i].ChromStart, b[i].ChromEnd, noMask, lineLength, false) //hard code endOfAlignment as false, as we are getting end positions from the beds.
+	}
+}
+
+func usage() {
+	fmt.Print(
+		"multFaVisualizer - Provides human-readable multiple alignments for all entries in a bed file.\n" +
+			"All bed entries must be on the same chromosome to interface with multiFa file.\n" +
+			"Usage:\n" +
+			"multiFaVisualizeBeds in.bed mult.fa\n" +
+			"options:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	var expectedNumArgs int = 2
+	var noMask *bool = flag.Bool("noMask", false, "Converts all bases to upper case.")
+	var outFormat *bool = flag.Bool("outFormatName", false, "Uses the name column as the outfile name (name.txt).")
+	var lineLength *int = flag.Int("lineLength", 100, "Sets to length of each alignment line.")
+	var outDir *string = flag.String("outDir", "", "Set a path for the output files. Should end with \"/\".")
+	flag.Usage = usage
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	flag.Parse()
+
+	if len(flag.Args()) != expectedNumArgs {
+		flag.Usage()
+		log.Fatalf("Error: expecting %d arguments, but got %d\n",
+			expectedNumArgs, len(flag.Args()))
+	}
+
+	bedFile := flag.Arg(0)
+	alnFile := flag.Arg(1)
+
+	multFaVisualizeBeds(bedFile, alnFile, *outFormat, *noMask, *lineLength, *outDir)
+}
